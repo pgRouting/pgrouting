1bfe2a9f1f41a9b9d4488a61efff0f67c6468cc5
anton <anton@d21bcd6f-0036-404f-87c1-d983b28855fd>
2008-01-29 03:53:10 +0000

    GAUL utils library location fixed

--- cmake/FindGAUL.cmake
+++ cmake/FindGAUL.cmake
@@ -5,10 +5,10 @@
 #  GAUL_FOUND, If false, do not try to use GAUL.
 #
 
-if(GAUL_INCLUDE_DIR AND GAUL_LIBRARIES)
+if(GAUL_INCLUDE_DIR AND GAUL_LIBRARIES AND GAUL_UTIL_LIBRARIES)
    set(GAUL_FOUND TRUE)
 
-else(GAUL_INCLUDE_DIR AND GAUL_LIBRARIES)
+else(GAUL_INCLUDE_DIR AND GAUL_LIBRARIES AND GAUL_UTIL_LIBRARIES)
 
   FIND_PATH(GAUL_INCLUDE_DIR gaul.h
       /usr/include
@@ -17,7 +17,19 @@ else(GAUL_INCLUDE_DIR AND GAUL_LIBRARIES)
       $ENV{SystemDrive}/gaul/*/include
       )
 
-  find_library(GAUL_LIBRARIES NAMES gaul gaul_util libgaul libgaul_util
+  find_library(GAUL_LIBRARIES NAMES gaul libgaul
+     PATHS
+     /usr/lib
+     /usr/local/lib
+     /usr/lib/gaul
+     /usr/lib64
+     /usr/local/lib64
+     /usr/lib64/gaul
+     $ENV{ProgramFiles}/GAUL/*/lib/
+     $ENV{SystemDrive}/GAUL/*/lib/
+     )
+
+  find_library(GAUL_UTIL_LIBRARIES NAMES gaul_util libgaul_util
      PATHS
      /usr/lib
      /usr/local/lib
@@ -29,14 +41,14 @@ else(GAUL_INCLUDE_DIR AND GAUL_LIBRARIES)
      $ENV{SystemDrive}/GAUL/*/lib/
      )
       
-  if(GAUL_INCLUDE_DIR AND GAUL_LIBRARIES)
+  if(GAUL_INCLUDE_DIR AND GAUL_LIBRARIES AND GAUL_UTIL_LIBRARIES)
     set(GAUL_FOUND TRUE)
-    message(STATUS "Found GAUL: ${GAUL_LIBRARIES}")
-  else(GAUL_INCLUDE_DIR AND GAUL_LIBRARIES)
+    message(STATUS "Found GAUL: ${GAUL_LIBRARIES}, ${GAUL_UTIL_LIBRARIES}")
+  else(GAUL_INCLUDE_DIR AND GAUL_LIBRARIES AND GAUL_UTIL_LIBRARIES)
     set(GAUL_FOUND FALSE)
     message(STATUS "GAUL not found.")
-  endif(GAUL_INCLUDE_DIR AND GAUL_LIBRARIES)
+  endif(GAUL_INCLUDE_DIR AND GAUL_LIBRARIES AND GAUL_UTIL_LIBRARIES)
 
-  mark_as_advanced(GAUL_INCLUDE_DIR GAUL_LIBRARIES)
+  mark_as_advanced(GAUL_INCLUDE_DIR GAUL_LIBRARIES GAUL_UTIL_LIBRARIES)
 
-endif(GAUL_INCLUDE_DIR AND GAUL_LIBRARIES)
+endif(GAUL_INCLUDE_DIR AND GAUL_LIBRARIES AND GAUL_UTIL_LIBRARIES)
