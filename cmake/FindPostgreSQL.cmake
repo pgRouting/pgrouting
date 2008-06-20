daba2a555a3f42b8eec723633bf2dc93825f5477
anton <anton@d21bcd6f-0036-404f-87c1-d983b28855fd>
2008-06-20 00:45:46 +0000

    PostgreSQL header files path added for cmake

--- cmake/FindPostgreSQL.cmake
+++ cmake/FindPostgreSQL.cmake
@@ -25,6 +25,8 @@ else(POSTGRESQL_INCLUDE_DIR AND POSTGRESQL_LIBRARIES)
       /usr/local/include/pgsql/server
       /usr/include/postgresql/server
       /usr/include/postgresql/*/server
+      /usr/local/include/postgresql/server
+      /usr/local/include/postgresql/*/server
       $ENV{ProgramFiles}/PostgreSQL/*/include/server
       $ENV{SystemDrive}/PostgreSQL/*/include/server
       )
