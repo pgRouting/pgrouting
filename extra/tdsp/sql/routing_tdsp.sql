85c4cc00dcfd21a419679135257a8a4b8261a954
Jay Mahadeokar <jai.mahadeokar@gmail.com>
2011-06-18 07:38:18 +0530

    Dynamic data generator plpgsql function added

---
+++ extra/tdsp/sql/routing_tdsp.sql
@@ -0,0 +1,13 @@
+-----------------------------------------------------------------------
+-- Core function for time_dependent_shortest_path computation
+-- See README for description
+-----------------------------------------------------------------------
+--TODO - Do we need to add another sql text for the query on time-dependent-weights table?
+--     - For now just checking with static data, so the query is similar to shortest_paths.
+
+CREATE OR REPLACE FUNCTION time_dependent_shortest_path(sql text, source_id integer, 
+        target_id integer, directed boolean, has_reverse_cost boolean)
+        RETURNS SETOF path_result
+        AS '$libdir/librouting'
+        LANGUAGE 'C' IMMUTABLE STRICT;
+
