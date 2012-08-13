c5bc130460e9031f2452fb3f1a9dfccd24b705b8
jinfu_leng <jinfu.leng@gmail.com>
2012-08-13 09:19:55 -0500

    updated the license for the source code

--- core/src/two_queue.hpp
+++ core/src/two_queue.hpp
@@ -1,4 +1,30 @@
-//  (C) Copyright Jinfu Leng 2012 
+/******************************************************************************
+ *
+ * Project: GSOC 2012 Project 
+ * Purpose: pgRouting mild_two_q algorithm
+ * Author:  Jinfu Leng
+ *
+
+******************************************************************************
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies of this Software or works derived from this Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+
+*****************************************************************************/
 
 #ifndef PGROUTING_TWO_QUEUE_HPP
 #define PGROUTING_TWO_QUEUE_HPP
@@ -99,26 +125,28 @@ public:
   bool empty() const { return a.empty()&&b.empty(); }
   size_type size() const { return a.size()+b.size(); }
   reference top() { 
-	_Sequence *sequence;	
-	if(a.empty()) sequence = &b;
-	else sequence = &a;
-	D d = get(*pDistance, sequence->front());
-	if(!compare(d,minDist)&&!compare(minDist,d)) UpdateMinDist();
-	return sequence->front();
+	if(a.empty()) return b.front();
+	else return a.front();
   }
   const_reference top() const {
-	_Sequence *sequence;	
-	if(a.empty()) sequence = &b;
-	else sequence = &a;
-	D d = get(*pDistance, sequence->front());
-	if(!compare(d,minDist)&&!compare(minDist,d)) UpdateMinDist();
-	return sequence->front();
+	if(a.empty()) return b.front();
+	else return a.front();
   }
-  void push_a(const value_type& __x) { a.push_back(__x); }
-  void push_b(const value_type& __x) { b.push_back(__x); }
-  void pop() { 
-	if(a.empty()) b.pop_front();
-	else a.pop_front(); 
+  void push_a(const value_type& __x) { a.push_back(__x); if(compare(__x,minDist)) minDist = __x;}
+  void push_b(const value_type& __x) { b.push_back(__x); if(compare(__x,minDist)) minDist = __x;}
+  void pop() {
+	D d;
+	if(a.empty())
+	{
+		d = get(*pDistance, b.front());	
+		b.pop_front();
+	}
+	else 
+	{
+		d = get(*pDistance, a.front());	
+		a.pop_front();
+	}
+	if(!compare(d,minDist)&&!compare(minDist,d)) UpdateMinDist();
   }
 
 };
