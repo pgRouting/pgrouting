f91afe76b790a86060a34f74ad1f1e0e2c31d556
Stephen Woodbridge <woodbri@swoodbridge.com>
2013-03-05 13:15:48 -0500

    Initial source tree restructuring.

---
+++ core/common/sql/pgr_types.sql
@@ -0,0 +1,23 @@
+--
+-- Copyright (c) 2005 Sylvain Pasche,
+--               2006-2007 Anton A. Patrushev, Orkney, Inc.
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+--
+
+
+CREATE TYPE path_result AS (vertex_id integer, edge_id integer, cost float8);
+CREATE TYPE vertex_result AS (x float8, y float8);
+
---
+++ core/shooting_star/sql/shooting_star.sql
@@ -0,0 +1,29 @@
+--
+-- Copyright (c) 2005 Sylvain Pasche,
+--               2006-2007 Anton A. Patrushev, Orkney, Inc.
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+--
+
+
+-----------------------------------------------------------------------
+-- Core function for shortest_path_astar computation
+-- Simillar to shortest_path in usage but uses the Shooting* algorithm
+-----------------------------------------------------------------------
+CREATE OR REPLACE FUNCTION shortest_path_shooting_star(sql text, source_id integer, 
+        target_id integer,directed boolean, has_reverse_cost boolean)
+         RETURNS SETOF path_result
+         AS '$libdir/librouting'
+         LANGUAGE C IMMUTABLE STRICT; 
