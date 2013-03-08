820064d6e32b1bb89542a04c6d8d3ee3c36a8338
Stephen Woodbridge <woodbri@swoodbridge.com>
2013-03-08 17:58:53 -0500

    Adding build directory for mingw scripts, and an inital script.

---
+++ build/makepgroutingw64.sh
@@ -0,0 +1,17 @@
+#!/bin/sh.exe
+export PROJECTS=/c/ming64/projects
+export PATHOLD=$PATH
+echo $PATHOLD >c:/path.txt
+
+export PATHOLD=".:/bin:/include:/mingw/bin:/mingw/include:/c/Windows/system32:/c/Windows:/usr/local/bin:/c/ming64/Silksvn/bin:"
+export PG_VER=92
+export PGWINVER=${PG_VER}edb
+export PATH="${PATHOLD}:${PROJECTS}/pgx64/pg${PG_VER}/bin:${PROJECTS}/pgx64/pg${PG_VER}/lib"
+export PATH="${PROJECTS}/gettext/rel-gettext-0.18.1/bin:${PROJECTS}/xsltproc:${PROJECTS}/gtk/bin:${PROJECTS}/rel-libiconv-1.13.1w64/include:${PATH}"
+
+echo $PATH
+echo "" >> c:/path.txt
+echo $PATH >> c:/path.txt
+
+
+cmake -G "MSYS Makefiles" -DWITH_TSP=ON -DWITH_DD=ON .
