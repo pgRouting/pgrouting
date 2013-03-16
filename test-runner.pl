6976dd7da0008e1d8d9c4d2b9897be8a82ed21f6
Stephen Woodbridge <woodbri@swoodbridge.com>
2013-03-16 02:15:35 -0400

    Added a simple test-runner.pl script and test files for trsp.

---
+++ test-runner.pl
@@ -0,0 +1,206 @@
+#! /usr/bin/perl -w
+    eval 'exec /usr/bin/perl -S $0 ${1+"$@"}'
+        if 0; #$running_under_some_shell
+
+use strict;
+use File::Find ();
+use File::Basename;
+use Data::Dumper;
+
+# for the convenience of &wanted calls, including -eval statements:
+use vars qw/*name *dir *prune/;
+*name   = *File::Find::name;
+*dir    = *File::Find::dir;
+*prune  = *File::Find::prune;
+
+my $VERBOSE = 0;
+my $DRYRUN = 0;
+
+my $DBNAME = "pgr_test__db__test";
+my $DBUSER = 'postgres';
+my $DBHOST = 'localhost';
+
+sub Usage {
+    die "Usage: test-runner.pl vpg vpgis [/path/to/psql]\n" .
+        "       vpg   - postgresql version\n" .
+        "       vpgis - postgis version\n";
+}
+
+my $vpg   = shift @ARGV || '';
+my $vpgis = shift @ARGV || '';
+
+%main::tests = ();
+my @cfgs = ();
+my %stats = (pass=>0, fail=>0);
+my $TMP = "/tmp/pgr-test-runner-$$";
+
+my $psql = findPsql() || die "ERROR: can not find psql, specify it on the command line.\n";
+
+# some unit tests
+#my $server_ver = getServerVersion();
+#print "PSQL=$psql\n";
+#print "SERVER_VERSION=$server_ver\n";
+#print "version_greater_eq('9.1')=" . version_greater_eq($server_ver, '9.1') . "\n";
+#print "version_greater_eq('8.3')=" . version_greater_eq($server_ver, '8.3') . "\n";
+#print "template_pgrouting=" . dbExists('template_pgrouting') . "\n";
+#exit;
+
+# Traverse desired filesystems
+File::Find::find({wanted => \&want_tests}, 'core');
+
+die "Error: no test files found. Run this command from the top level pgRouting directory!\n" unless @cfgs;
+
+createTestDB();
+
+for my $c (@cfgs) {
+    my $found = 0;
+
+    # load the config file for the tests
+    require $c;
+
+    print "test.conf = $c\n" if $VERBOSE;
+    print Data::Dumper->Dump([\%main::tests],['test']) if $VERBOSE;
+
+    if ($main::tests{any}) {
+        push @{$stats{$c}}, run_test($c, $main::tests{any});
+        $found++;
+    }
+    if ($main::tests{"$vpg-$vpgis"}) {
+        push @{$stats{$c}}, run_test($c, $main::tests{"$vpg-$vpgis"});
+        $found++;
+    }
+    if (! $found) {
+        $stats{$c} = "No tests were found for '$vpg-$vpgis'!";
+    }
+}
+
+dropTestDB();
+
+print Data::Dumper->Dump([\%stats], ['stats']);
+
+unlink $TMP;
+
+exit;
+
+sub run_test {
+    my $c = shift;
+    my $t = shift;
+    my %res = ();
+
+    my $dir = dirname($c);
+
+    $res{comment} = $t->{comment} if $t->{comment};
+    for my $x (@{$t->{data}}) {
+        mysystem("$psql -U $DBUSER -h $DBHOST -A -t -q -f '$dir/$x' $DBNAME");
+    }
+
+    for my $x (@{$t->{tests}}) {
+        mysystem("$psql -U $DBUSER -h $DBHOST -A -t -q -f '$dir/$x.test' $DBNAME > $TMP");
+        my $r = `diff '$dir/$x.rest' $TMP`;
+        $r =~ s/^\s*|\s*$//g;
+        if (length($r)) {
+            $res{'$dir/$x.test'} = "FAILED: $r";
+            $stats{fail}++;
+        }
+        else {
+            $res{"$dir/$x.test"} = "Passed";
+            $stats{pass}++;
+        }
+    }
+
+    return \%res;
+}
+
+sub createTestDB {
+    die "ERROR: test database '$DBNAME' exists, you must drop or rename it!\n"
+        if dbExists($DBNAME);
+    
+    my $dbver = getServerVersion();
+
+    if (version_greater_eq($dbver, '9.1')) {
+        mysystem("createdb -U $DBNAME -h $DBHOST $DBNAME");
+        die "ERROR: Failed to create database '$DBNAME'!\n"
+            unless dbExists($DBNAME);
+        mysystem("$psql -U $DBUSER -h $DBHOST -c 'create extension plpgsql' $DBNAME");
+        mysystem("$psql -U $DBUSER -h $DBHOST -c 'create extension postgis' $DBNAME");
+        mysystem("$psql -U $DBUSER -h $DBHOST -c 'create extension pgrouting' $DBNAME");
+    }
+    else {
+        my $template;
+        if (dbExists('template_pgrouting')) {
+            $template = 'template_pgrouting';
+        }
+        else {
+            die "ERROR: Could not find template_pgrouting database!\n";
+        }
+
+        mysystem("createdb -U $DBUSER -h $DBHOST -T $template $DBNAME");
+        die "ERROR: Failed to create database '$DBNAME'!\n"
+            if ! dbExists($DBNAME);
+    }
+
+}
+
+sub dropTestDB {
+    mysystem("dropdb -U $DBUSER -h $DBHOST $DBNAME");
+}
+
+sub version_greater_eq {
+    my ($a, $b) = @_;
+
+    return 0 if !$a || !$b;
+
+    my @a = split(/\./, $a);
+    my @b = split(/\./, $b);
+
+    $a = shift @a;
+    $b = shift @b;
+    return 0 if $a && $b && $a < $b;
+    
+    $a = shift @a;
+    $b = shift @b;
+    return 1 if !$a || !$b;
+    return 0 if $a < $b;
+
+    $a = shift @a;
+    $b = shift @b;
+    return 1 if !$a || !$b;
+    return 0 if $a < $b;
+    return 1;
+}
+
+
+sub getServerVersion {
+    my $v = `$psql -U $DBUSER -h $DBHOST -q -t -c "select version()" postgres`;
+    if ($v =~ m/PostgreSQL (\d+\.\d+(\.\d+)?)\b/) {
+        return $1;
+    }
+    return undef;
+}
+
+sub dbExists {
+    my $dbname = shift;
+
+    my $isdb = `$psql -U $DBUSER -h $DBHOST -l | grep $dbname`;
+    $isdb =~ s/^\s*|\s*$//g;
+    return length($isdb);
+}
+
+sub findPsql {
+    my $psql = `which psql`;
+    $psql =~ s/^\s*|\s*$//g;
+    return length($psql)?$psql:undef;
+}
+
+sub mysystem {
+    my $cmd = shift;
+    print "$cmd\n" if $VERBOSE || $DRYRUN;
+    system($cmd) unless $DRYRUN;
+}
+
+sub want_tests {
+    /^test\.conf\z/s &&
+    push @cfgs, $name;
+}
+
+
