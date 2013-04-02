426483061ac942bbf8794108a78726e2d203ab25
Daniel Kastl <daniel@georepublic.de>
2013-04-03 02:44:19 +0900

    added Alternative to pgrouting_analytics.sql

---
+++ src/common/sql/pgrouting_network_check.sql
@@ -0,0 +1,75 @@
+/*
+	Alternative to pgrouting_analytics.sql
+ */
+CREATE OR REPLACE FUNCTION data.PGR_analyze_graph(sql text, tolerance float)
+  RETURNS TABLE(
+	vertex bigint, 
+	source integer,
+	target integer,
+	isolate boolean,
+	geom geometry
+) AS
+$BODY$
+DECLARE 
+	rec	record;
+	pnt	record;
+	seg	record;
+BEGIN
+	-- Create temporary vertex table
+	CREATE TEMPORARY TABLE vertices_temp (
+		vertex bigint  PRIMARY KEY,
+		source integer DEFAULT 0,
+		target integer DEFAULT 0,
+		isolate boolean DEFAULT false,
+		geom geometry
+	) ON COMMIT DROP;
+
+	-- Count occurance of vertex as source/target
+	RAISE NOTICE 'Count occurance of vertex as source/target';
+	FOR rec IN EXECUTE sql
+	LOOP
+		-- Source
+		BEGIN
+			EXECUTE 'INSERT INTO vertices_temp (vertex,geom,source) VALUES ($1,$2,1)' 
+				USING rec.source, ST_Startpoint(rec.geom);
+				-- This assumes that source equals start point of geometry
+		EXCEPTION WHEN unique_violation THEN
+			EXECUTE 'UPDATE vertices_temp SET source = source + 1 WHERE vertex = $1' 
+				USING rec.source;
+		END;
+
+		-- Target
+		BEGIN
+			EXECUTE 'INSERT INTO vertices_temp (vertex,geom,target) VALUES ($1,$2,1)' 
+				USING rec.target, ST_Endpoint(rec.geom);
+				-- This assumes that target equals end point of geometry
+		EXCEPTION WHEN unique_violation THEN
+			EXECUTE 'UPDATE vertices_temp SET target = target + 1 WHERE vertex = $1' 
+				USING rec.target;
+		END;
+	END LOOP;
+
+	-- Analyze graph for gaps and zlev errors
+	RAISE NOTICE 'Analyze graph for gaps and zlev errors';
+	FOR pnt IN EXECUTE 'SELECT * FROM vertices_temp 
+				WHERE (source + target) = 1 ORDER BY vertex'
+	LOOP
+		FOR seg IN EXECUTE 'SELECT * FROM (' || sql || ') a WHERE ST_DWithin(a.geom,$1,$2)'
+				USING pnt.geom, tolerance
+		LOOP
+			IF pnt.vertex NOT IN (seg.source, seg.target) THEN
+				EXECUTE 'UPDATE vertices_temp SET isolate = TRUE WHERE vertex = $1'
+					USING pnt.vertex;
+			END IF;
+		END LOOP;
+	END LOOP;
+
+	RETURN QUERY SELECT * FROM vertices_temp;
+EXCEPTION
+	-- Not sure this is a good idea or not. It prevents to show "ugly" console output 
+	WHEN others THEN
+	RAISE EXCEPTION '%', SQLERRM;
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE STRICT
+  COST 100;
