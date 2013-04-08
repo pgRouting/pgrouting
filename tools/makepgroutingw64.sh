2f5de7f6ca408d068e92189baabdd67a265d7295
Stephen Woodbridge <woodbri@swoodbridge.com>
2013-04-08 09:22:31 -0400

    Clean up will looking for a bug.

--- tools/makepgroutingw64.sh
+++ tools/makepgroutingw64.sh
@@ -2,17 +2,17 @@
 
 # CHANGE THESE TO SUIT YOUR PROJECT
 
-export PROJECTS=/c/ming64/projects
+export PROJECTS=/c/ming${OS_BUILD}/projects
 export PG_VER=92
 
 
 export PATHOLD=$PATH
 echo $PATHOLD >c:/path.txt
 
-export PATHOLD=".:/bin:/include:/mingw/bin:/mingw/include:/c/Windows/system32:/c/Windows:/usr/local/bin:/c/ming64/Silksvn/bin:"
+export PATHOLD=".:/bin:/include:/mingw/bin:/mingw/include:/c/Windows/system32:/c/Windows:/usr/local/bin:/c/ming${OS_BUILD}/Silksvn/bin:"
 export PGWINVER=${PG_VER}edb
-export PATH="${PATHOLD}:${PROJECTS}/pgx64/pg${PG_VER}/bin:${PROJECTS}/pgx64/pg${PG_VER}/lib"
-export PATH="${PROJECTS}/gettext/rel-gettext-0.18.1/bin:${PROJECTS}/xsltproc:${PROJECTS}/gtk/bin:${PROJECTS}/rel-libiconv-1.13.1w64/include:${PATH}"
+export PATH="${PATHOLD}:${PROJECTS}/pgx${OS_BUILD}/pg${PG_VER}/bin:${PROJECTS}/pgx${OS_BUILD}/pg${PG_VER}/lib"
+export PATH="${PROJECTS}/gettext/rel-gettext-0.18.1/bin:${PROJECTS}/xsltproc:${PROJECTS}/gtk/bin:${PROJECTS}/rel-libiconv-1.13.1w${OS_BUILD}/include:${PATH}"
 
 echo $PATH
 echo "" >> c:/path.txt
@@ -21,7 +21,7 @@ echo $PATH >> c:/path.txt
 cd ${PROJECTS}
 #git clone git@github.com:pgRouting/pgrouting.git
 
-export PostgreSQL_ROOT=/c/ming64/projects/pgx64/pg${PG_VER}
+export PostgreSQL_ROOT=/c/ming${OS_BUILD}/projects/pgx${OS_BUILD}/pg${PG_VER}
 cd pgrouting
 
 #git checkout sew-devel-2_0
