412319bfe88a5295d661f775627ea9fdeb9af3fa
Stephen Woodbridge <woodbri@swoodbridge.com>
2013-04-08 09:19:59 -0400

    Fixing a typo so we get the 32 bit version.

--- tools/makepgroutingw32.sh
+++ tools/makepgroutingw32.sh
@@ -13,7 +13,7 @@ echo $PATHOLD >c:/path.txt
 export PATHOLD=".:/bin:/include:/mingw/bin:/mingw/include:/c/Windows/system32:/c/Windows:/usr/local/bin:/c/ming64/Silksvn/bin:"
 export PGWINVER=${PG_VER}edb
 export PATH="${PATHOLD}:${PROJECTS}/pgx${OS_BUILD}/pg${PG_VER}w${OS_BUILD}/bin:${PROJECTS}/pgx${OS_BUILD}/pg${PG_VER}w${OS_BUILD}/lib"
-export PATH="${PROJECTS}/gettext/rel-gettext-0.18.1/bin:${PROJECTS}/xsltproc:${PROJECTS}/gtk/bin:${PROJECTS}/rel-libiconv-1.13.1w64/include:${PATH}"
+export PATH="${PROJECTS}/gettext/rel-gettext-0.18.1/bin:${PROJECTS}/xsltproc:${PROJECTS}/gtk/bin:${PROJECTS}/rel-libiconv-1.13.1w${OS_BUILD}/include:${PATH}"
 
 echo $PATH
 echo "" >> c:/path.txt
