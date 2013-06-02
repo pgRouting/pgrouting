d1f544dd090202319848037da817a0e51f85004b
Daniel Kastl <daniel@georepublic.de>
2013-06-02 10:25:56 +0900

    Moved DB creation into tests

--- tools/travis/pgrouting_test.sh
+++ tools/travis/pgrouting_test.sh
@@ -6,8 +6,24 @@
 # Test pgRouting
 # ------------------------------------------------------------------------------
 
-DBUSER="gis"
-DATABASE="gis"
+DBUSER="postgres"
+DBNAME="pgrouting"
 
-psql -U $DBUSER -d $DATABASE -c "CREATE EXTENSION pgrouting;"
+# Create database 
+psql -U $DBUSER -c 'CREATE DATABASE $DBNAME;'
+
+# Add PostGIS extension to database
+if [[ "$POSTGIS_VERSION" == "1.5" ]]; then 
+	psql -U $DBUSER -d $DBNAME -f /usr/share/postgresql/9.1/contrib/postgis-1.5/postgis.sql
+	psql -U $DBUSER -d $DBNAME -f /usr/share/postgresql/9.1/contrib/postgis-1.5/spatial_ref_sys.sql
+fi
+
+if [[ "$POSTGIS_VERSION" != "1.5" ]]; then 
+	psql -U $DBUSER -d $DBNAME -c "CREATE EXTENSION postgis;" 
+fi
+
+# Add pgRouting extension to database
+psql -U $DBUSER -d $DBNAME -c "CREATE EXTENSION pgrouting;"
+
+# Test runner
 ../tools/test-runner.pl
