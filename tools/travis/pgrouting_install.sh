8cbf700ed62af3dfa04043a07e905caad1618610
Daniel Kastl <daniel@georepublic.de>
2013-06-03 19:42:53 +0900

    Remove all remaining files and folders

--- tools/travis/pgrouting_install.sh
+++ tools/travis/pgrouting_install.sh
@@ -33,7 +33,13 @@ sudo apt-get update -qq
 # Install PostgreSQL
 # ------------------------------------------------------------------------------
 sudo apt-get -qq --purge remove postgresql postgresql-9.1 
-sudo rm -Rf /etc/postgresql /etc/init.d/postgresql
+sudo rm -Rf /etc/postgresql 
+sudo rm -Rf /etc/init.d/postgresql
+sudo rm -Rf /etc/postgresql-common
+sudo rm -Rf /var/lib/postgresql
+sudo userdel -r postgres
+sudo groupdel postgres
+sudo ldconfig
 sudo apt-get install -y -qq postgresql-$POSTGRESQL_VERSION postgresql-contrib-$POSTGRESQL_VERSION
 
 # ------------------------------------------------------------------------------
