ff32a4011f942bf79a421e7a6b0d607bf447cb90
Daniel Kastl <daniel@georepublic.de>
2013-06-03 18:02:40 +0900

    Changed the way how PostgreSQL is installed

--- tools/travis/pgrouting_install.sh
+++ tools/travis/pgrouting_install.sh
@@ -9,6 +9,7 @@
 POSTGRESQL_VERSION="$1"
 POSTGIS_VERSION="$2"
 
+# ------------------------------------------------------------------------------
 # Add PPA's'
 # ------------------------------------------------------------------------------
 sudo apt-add-repository -y ppa:georepublic/pgrouting-travis
@@ -17,33 +18,30 @@ if [[ "$POSTGIS_VERSION" == "2.0" ]]; then
 	sudo apt-add-repository -y ppa:ubuntugis/ubuntugis-unstable
 fi
 
+# Add PostgreSQL Apt repository
+# ------------------------------------------------------------------------------
+echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > pgdg.list
+sudo mv pgdg.list /etc/apt/sources.list.d/
+wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
+
+# Reload package archive
+# ------------------------------------------------------------------------------
 sudo apt-get update -qq
 
+
+# ------------------------------------------------------------------------------
 # Install PostgreSQL
 # ------------------------------------------------------------------------------
+sudo apt-get install -y -qq postgresql-$POSTGRESQL_VERSION postgresql-contrib-$POSTGRESQL_VERSION
 
-if [[ "$POSTGRESQL_VERSION" != "9.1" ]]; then 
-	sudo service postgresql stop
-	sudo cp /etc/postgresql/9.1/main/pg_hba.conf ./
-	sudo apt-get remove postgresql postgresql-9.1 -qq --purge
-	echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > pgdg.list
-	sudo mv pgdg.list /etc/apt/sources.list.d/
-	wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
-	sudo apt-get update -qq
-	sudo apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confnew" install -qq postgresql-$POSTGRESQL_VERSION postgresql-contrib-$POSTGRESQL_VERSION
-	sudo service postgresql stop
-	sudo cp ./pg_hba.conf /etc/postgresql/$POSTGRESQL_VERSION/main				
-	sudo service postgresql start	
-fi
-
+# ------------------------------------------------------------------------------
 # Install dependecies
 # ------------------------------------------------------------------------------
 sudo apt-get install -y -qq cmake libcgal-dev libboost-graph-dev libboost-thread-dev postgresql-server-dev-$POSTGRESQL_VERSION
 
-
+# ------------------------------------------------------------------------------
 # Install PostGIS
 # ------------------------------------------------------------------------------
-
 if [[ "$POSTGRESQL_VERSION" == "9.1" ]]; 
 then 
 	sudo apt-get install -y -qq postgresql-9.1-postgis
---
+++ tools/travis/pgrouting_prepare.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# ------------------------------------------------------------------------------
+# Travis CI scripts 
+# Copyright(c) pgRouting Contributors
+#
+# Prepare pgRouting
+# ------------------------------------------------------------------------------
+
+POSTGRESQL_VERSION="$1"
+POSTGIS_VERSION="$2"
+
+# ------------------------------------------------------------------------------
+# Set PostgreSQL users and permissions
+# ------------------------------------------------------------------------------
+sudo cp $TRAVIS_BUILD_DIR/tools/travis/pg_hba.conf `find /etc/postgresql/*/main/pg_hba.conf`
+sudo service postgresql restart	
+
+# Disable password (better: create new user)
+psql -U postgres -d postgres -c "ALTER ROLE postgres WITH PASSWORD '';"
