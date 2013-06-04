ece354bcf35e862124fc6994d6d7c8e322711cb5
Daniel Kastl <daniel@georepublic.de>
2013-06-04 09:37:58 +0900

    Also load ubuntugis PPA for PostGIS 2.1, some syntax cleanup

--- tools/travis/pgrouting_install.sh
+++ tools/travis/pgrouting_install.sh
@@ -12,7 +12,7 @@ POSTGIS_VERSION="$2"
 # ------------------------------------------------------------------------------
 # Remove PostgreSQL and all its files
 # ------------------------------------------------------------------------------
-if [[ "$POSTGRESQL_VERSION" != "9.1" ]]; then
+if [ "$POSTGRESQL_VERSION" != "9.1" ]; then
 	sudo service postgresql stop
 	sudo apt-get remove postgresql postgresql-common postgresql-client-common postgresql-9.1 postgresql-client-9.1 libpq-dev libpq5  -qq --purge
 fi
@@ -21,13 +21,13 @@ fi
 # ------------------------------------------------------------------------------
 sudo apt-add-repository -y ppa:georepublic/pgrouting-travis
 
-if [[ "$POSTGIS_VERSION" == "2.0" ]]; then 
+if [ "$POSTGIS_VERSION" == "2.0" ] || [ "$POSTGIS_VERSION" == "2.1" ]; then 
 	sudo apt-add-repository -y ppa:ubuntugis/ubuntugis-unstable
 fi
 
 # Add PostgreSQL Apt repository
 # ------------------------------------------------------------------------------
-if [[ "$POSTGRESQL_VERSION" != "9.1" ]]; then
+if [ "$POSTGRESQL_VERSION" != "9.1" ]; then
 	echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > pgdg.list
 	sudo mv pgdg.list /etc/apt/sources.list.d/
 	wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
@@ -40,7 +40,7 @@ sudo apt-get update -qq
 # ------------------------------------------------------------------------------
 # Install PostgreSQL
 # ------------------------------------------------------------------------------
-if [[ "$POSTGRESQL_VERSION" != "9.1" ]]; then
+if [ "$POSTGRESQL_VERSION" != "9.1" ]; then
 	sudo apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confnew" install -y -qq postgresql-$POSTGRESQL_VERSION postgresql-contrib-$POSTGRESQL_VERSION
 fi
 
@@ -54,15 +54,15 @@ sudo apt-get install cmake libcgal-dev libboost-graph-dev libboost-thread-dev po
 # ------------------------------------------------------------------------------
 sudo apt-get install -y -qq build-essential libxml2-dev libproj-dev libjson0-dev xsltproc docbook-xsl docbook-mathml libgeos-dev libgdal1-dev 
 
-if [[ "$POSTGIS_VERSION" == "1.5" ]]; then 
+if [ "$POSTGIS_VERSION" == "1.5" ]; then 
 	wget --quiet http://download.osgeo.org/postgis/source/postgis-1.5.8.tar.gz
 fi
 
-if [[ "$POSTGIS_VERSION" == "2.0" ]]; then 
+if [ "$POSTGIS_VERSION" == "2.0" ]; then 
 	wget --quiet http://download.osgeo.org/postgis/source/postgis-2.0.3.tar.gz
 fi
 
-if [[ "$POSTGIS_VERSION" == "2.1" ]]; then 
+if [ "$POSTGIS_VERSION" == "2.1" ]; then 
 	sudo apt-get install -y -qq libpoppler-dev libarmadillo-dev libepsilon-dev liblzma-dev
 	wget --quiet http://postgis.net/stuff/postgis-2.1.0beta3dev.tar.gz
 fi
@@ -77,7 +77,7 @@ sudo make install
 sudo ldconfig
 
 # Build extension for PostGIS > 2.0
-if [[ "$POSTGIS_VERSION" != "1.5" ]]; then 
+if [ "$POSTGIS_VERSION" != "1.5" ]; then 
 	cd extensions && make && sudo make install
 fi
 
