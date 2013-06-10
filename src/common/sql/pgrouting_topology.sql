5d103f351fd4fff9a274116a0f5d27640eda94da
Stephen Woodbridge <woodbri@swoodbridge.com>
2013-06-09 20:43:23 -0400

    Another change trying to debug pre pg9.2 versions.

--- src/common/sql/pgrouting_topology.sql
+++ src/common/sql/pgrouting_topology.sql
@@ -119,9 +119,19 @@ BEGIN
         source_id := pgr_pointToId(points.source, tolerance);
         target_id := pgr_pointToId(points.target, tolerance);
                                 
-        EXECUTE 'UPDATE ' || pgr_quote_ident(geom_table) || 
+        sql := 'UPDATE ' || pgr_quote_ident(geom_table) || 
             ' SET source = ' || source_id || ', target = ' || target_id || 
             ' WHERE ' || quote_ident(gid_cname) || ' =  ' || points.id;
+
+        IF sql IS NULL THEN
+            RAISE NOTICE 'ERROR: UPDATE ' || pgr_quote_ident(geom_table) ||
+                ' SET source = ' || coalesce(source_id, '<NULL>') ||
+                ', target = ' || coalesce(target_id, '<NULL>') ||
+                ' WHERE ' || quote_ident(gid_cname) || ' =  ' ||
+                coalesce(points.id, '<NULL>');
+        ELSE
+            EXECUTE sql;
+        END IF;
     END LOOP;
 
     RETURN 'OK';
