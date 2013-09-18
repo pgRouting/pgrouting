0c62eb08943f972cc199464545fdc4364462651b
Stephen Woodbridge <woodbri@swoodbridge.com>
2013-09-18 17:42:57 -0400

    Generalize tools/publish-doc.sh

--- tools/publish-doc.sh
+++ tools/publish-doc.sh
@@ -5,6 +5,11 @@
 #
 # Publish Documentation
 # ------------------------------------------------------------------------------
+if test a$1 = a ; then
+    echo "Usage: publish-doc.sh dir"
+    echo "       where dir might be dev or v2.0"
+    exit 1
+fi
 
 VERSION=`git describe --tags --long`
 BRANCH=`git branch | grep '*' | awk '{print $2}'`
@@ -15,18 +20,19 @@ if test ! -d build ; then
 fi
 
 # Switch branch
-git checkout gh-pages && rm -Rf dev 
+git checkout gh-pages && rm -Rf "$1" 
 
 # Copy files and directories
-mkdir dev
-cp -R build/doc/html/en dev/en
+mkdir "$1"
+cp -R build/doc/html/en "$1"/en
 cp build/doc/html/index.html .
-cp build/doc/latex/en/pgRoutingDocumentation.pdf dev/en/
-gzip -c build/doc/man/en/pgrouting.7 > dev/en/pgrouting.7.gz
+cp build/doc/latex/en/pgRoutingDocumentation.pdf "$1"/en/
+gzip -c build/doc/man/en/pgrouting.7 > "$1"/en/pgrouting.7.gz
 
 # Commit changes
-git add dev 
-git commit -m "updated to commit $VERSION" . && git push origin gh-pages
+git add "$1" 
+git add index.html
+git commit -m "Updated to commit $VERSION" . && git push origin gh-pages
 
 # Switch branch
 git checkout $BRANCH
