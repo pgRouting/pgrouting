d45673b431c4dfac8c852102c01dadff107ba528
Stephen Woodbridge <woodbri@swoodbridge.com>
2013-09-18 09:55:10 -0400

    Fixed the library name.

--- src/partition/sql/pgr_pastar.sql
+++ src/partition/sql/pgr_pastar.sql
@@ -1,6 +1,6 @@
 -- put your sql link to the c code here.
 CREATE OR REPLACE FUNCTION pgr_pastar(tab text, snid integer, spid integer, tnid integer, tpid integer, has_rcost boolean DEFAULT false)
 RETURNS SETOF pgr_costResult
-AS '$libdir/librouting_pastar', 'pastar'
+AS '$libdir/librouting_partition', 'pastar'
 LANGUAGE c IMMUTABLE STRICT;
 
