9d1cfa70fee7d55a597f5170f5bf1223b0eb87f8
Stephen Woodbridge <woodbri@swoodbridge.com>
2013-10-02 10:28:41 -0400

    Change VRP function from IMMUTABLE to VOLATILE.

--- src/vrp_basic/sql/routing_vrp.sql
+++ src/vrp_basic/sql/routing_vrp.sql
@@ -18,6 +18,6 @@ create or replace function pgr_vrpOneDepot(
 	OUT tdepart integer)
 returns setof record as
 '$libdir/librouting_vrp', 'vrp'
-LANGUAGE c IMMUTABLE STRICT;
+LANGUAGE c VOLATILE STRICT;
 
 
