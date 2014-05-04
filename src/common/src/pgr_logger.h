9146aab41521d5d0cbdad623ffdba24aa3a09d69
Stephen Woodbridge <woodbri@swoodbridge.com>
2014-05-04 14:53:33 -0400

    Made a small enhancement to pgr_logger.h and prefaced all defines with PGR_.

--- src/common/src/pgr_logger.h
+++ src/common/src/pgr_logger.h
@@ -10,25 +10,27 @@
 
     USAGE:
 
-    #define LOGGERR_ON
-    #undef LOGGER_ON  // turn off logging for production
+    #define PGR_LOGGERR_ON
+    #undef PGR_LOGGER_ON  // turn off logging for production
 
     // override the default log file "/tmp/pgr_logger.log"
-    #define LOGGER_FILE "mylogger.log"
+    #define PGR_LOGGER_FILE "mylogger.log"
 
-    #undef LOGGER_LOC  // dont log filename and line number
-    #define LOGGER_LOC  // log filename and line number with log message
+    #undef PGR_LOGGER_LOC  // dont log filename and line number
+    #define PGR_LOGGER_LOC  // log filename and line number with log message
 
     // include the logger macros and configure based on defines above
     #include pgr_logger.h
     
     // log a message to the log file
-    LOG(format, args)
-    LOG("%s at %d\n", "this is a message", time())
+    PGR_LOGF(format, args);
+    PGR_LOGF("%s at %d\n", "this is a message", time());
+    PGR_LOG("just print a string to the log");
 
     This will log something like:
 
-    myfile:123: this is a message at 1399216875
+    myfile.cpp:123: this is a message at 1399216875
+    myfile.cpp:124: just print a string to the log
 
 */
 #ifndef PGR_LOGGER_H
@@ -36,26 +38,28 @@
 
 #include <stdio.h>
 
-#if defined(LOGGER_LOC)
-#  undef LOGGER_LOC_
-#  define LOGGER_LOC_ 1
+#if defined(PGR_LOGGER_LOC)
+#  undef PGR_LOGGER_LOC_
+#  define PGR_LOGGER_LOC_ 1
 #else
-#  undef LOGGER_LOC_
-#  define LOGGER_LOC_ 0
+#  undef PGR_LOGGER_LOC_
+#  define PGR_LOGGER_LOC_ 0
 #endif
 
-#ifndef LOGGER_FILE
-#  define LOGGER_FILE "/tmp/pgr_logger.log"
+#ifndef PGR_LOGGER_FILE
+#  define PGR_LOGGER_FILE "/tmp/pgr_logger.log"
 #endif
 
-#ifdef LOGGER_ON
-#  define LOG(format, ...) { \
-     FILE *fp = fopen(LOGGER_FILE, "a+"); \
-     if (LOGGER_LOC_) fprintf(fp, "%s:%d: ", __FILE__, __LINE__); \
+#ifdef PGR_LOGGER_ON
+#  define PGR_LOGF(format, ...) { \
+     FILE *fp = fopen(PGR_LOGGER_FILE, "a+"); \
+     if (PGR_LOGGER_LOC_) fprintf(fp, "%s:%d: ", __FILE__, __LINE__); \
      fprintf(fp, format, __VA_ARGS__); \
      fclose(fp); }
 #else  // ifndef LOGGER_ON
-#  define LOG(format, ...)
+#  define PGR_LOGF(format, ...)
 #endif  // ifndef LOGGER_ON
+
+#define PGR_LOG(str) LOGF("%s\n", str)
 
 #endif  // ifndef PGR_LOGGER_H
