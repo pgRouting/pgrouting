1167783538073bbc31848375f3da63328d3523cb
mapas de vicky <vicky_vergara@hotmail.com>
2014-07-28 19:52:29 -0500

    added tests, made adjustments, moved version dependant functions from utilities to version

--- src/common/sql/pgrouting_utilities.sql
+++ src/common/sql/pgrouting_utilities.sql
@@ -1,17 +1,40 @@
-/*
-  0: debuging   raise debug
-  1: report     raise notice
-  2: abort excecution  
-*/
+-- -------------------------------------------------------------------
+-- pgrouting_utilities.sql
+-- AuthorL Stephen Woodbridge <woodbri@imaptools.com>
+-- Copyright 2013 Stephen Woodbridge
+-- This file is release unde an MIT-X license.
+-- -------------------------------------------------------------------
 
-CREATE OR REPLACE FUNCTION pgr_onError(IN err boolean,IN action int, IN functionname text, IN msgerr text, IN hinto text default NULL,IN msgok text default 'OK' )
+
+/************************************************************************
+.. function:: pgr_onError(errCond,action,functionname,msgerr,hinto,msgok)
+  
+  If the error condition is is true, i.e., there is an error, it will raise a message based on the action:
+  0: debug      raise debug
+  1: report     raise notice
+  2: abort      throw a raise_exception  
+   Examples:  
+   
+	*	preforn pgr_onError( idname=gname, 2, 'pgr_createToplogy',  'Two columns share the same name');
+	*	preforn pgr_onError( idname=gname, 2, 'pgr_createToplogy',  'Two columns share the same name', 'Idname and gname must be different');
+        *	preforn pgr_onError( idname=gname, 2, 'pgr_createToplogy',  'Two columns share the same name', 'Idname and gname must be different','Column names are OK');
+
+   
+   Author: Vicky Vergara <vicky_vergara@hotmail.com>>
+
+  HISTORY
+     Created: 2014/JUl/28  handling the errors, and have a more visual output
+  
+************************************************************************/
+
+CREATE OR REPLACE FUNCTION pgr_onError(IN errCond boolean,IN action int, IN functionname text, IN msgerr text, IN hinto text default 'No hint',IN msgok text default 'OK' )
   RETURNS void AS
 $BODY$
 BEGIN
-  if err=true then 
+  if errCond=true then 
      if action=0 then
        raise debug '----> PGR ERROR in %: %',functionname,msgerr USING HINT = '  ---->'|| hinto;
-     else 
+     else
        if action = 2 then
          raise notice '----> PGR ERROR in %: %',functionname,msgerr USING HINT = '  ---->'|| hinto;
          raise raise_exception;
@@ -25,18 +48,8 @@ BEGIN
 END;
 $BODY$
 LANGUAGE plpgsql VOLATILE STRICT;
-COMMENT ON FUNCTION pgr_onError(boolean,int,text,text,text,text) IS 'args: err,action,functionname,msgerr,hinto=NULL, msgOK=OK,  -raises the error message and hint message at different levels';
+COMMENT ON FUNCTION pgr_onError(boolean,int,text,text,text,text) IS 'args: errCond,action,functionname,msgerr,hinto=NULL, msgOK=OK,  -raises the error message and hint message at different levels';
    
-
-
-
-
--- -------------------------------------------------------------------
--- pgrouting_utilities.sql
--- AuthorL Stephen Woodbridge <woodbri@imaptools.com>
--- Copyright 2013 Stephen Woodbridge
--- This file is release unde an MIT-X license.
--- -------------------------------------------------------------------
 
 
 /*
@@ -132,21 +145,25 @@ COMMENT ON FUNCTION pgr_getTableName(text,int) IS 'args: tab,reportErrs=1  -gets
 
 
 /*
-.. function:: pgr_getColumnName(tab,col)
-   
-   Examples:  
+.. function:: pgr_getColumnName(sname,tname,col,reportErrs default 1) returns text
+.. function:: pgr_getColumnName(tab,col,reportErrs default 1) returns text
+
+    Returns:
+          cname  registered column "col" in table "tab" or "sname.tname" considers Caps and when not found considers lowercases
+          NULL   when "tab"/"sname"/"tname" is not found or when "col" is not in table "tab"/"sname.tname"
+    unless otherwise indicated raises notices on errors
+ 
+ Examples:  
 	* 	 select  pgr_getColumnName('tab','col');
-        *        column text;
-		 excecute 'select pgr_getColumnName('||quote_literal('tab')||','||quote_literal('col')||')' INTO column;
-
-
-   Returns cname of column "col" in table "tab" considers Caps and when not found considers lowercases
-           NULL when "tab" is not found or when "col" is not in table "tab"
+	* 	 select  pgr_getColumnName('myschema','mytable','col');
+        	 excecute 'select pgr_getColumnName('||quote_literal('tab')||','||quote_literal('col')||')' INTO column;
+        	 excecute 'select pgr_getColumnName('||quote_literal(sname)||','||quote_literal(sname)||','||quote_literal('col')||')' INTO column;
 
    Author: Vicky Vergara <vicky_vergara@hotmail.com>>
 
   HISTORY
      Created: 2013/08/19  for handling schemas
+     Modified: 2014/JUL/28 added overloadig
 */
 
 
@@ -164,15 +181,17 @@ BEGIN
           WHERE table_name='||quote_literal(tname)||' and table_schema='||quote_literal(sname)||' and column_name='||quote_literal(col) into cname;
 
     IF cname is null  THEN
-    execute 'SELECT column_name FROM information_schema.columns WHERE table_name='||quote_literal(tname)||' and table_schema='||quote_literal(sname)||' and column_name='||quote_literal(lower(col))  into cname;
+    execute 'SELECT column_name FROM information_schema.columns 
+          WHERE table_name='||quote_literal(tname)||' and table_schema='||quote_literal(sname)||' and column_name='||quote_literal(lower(col))  into cname;
     END if;
 
     err=cname is null;
 
     perform pgr_onError(err, reportErrs, 'pgr_getColumnName',  'Column '|| col ||' not found', ' Check your column name','Column '|| col || ' found');
     RETURN cname;
+    EXCEPTION WHEN OTHERS THEN
+          RETURN NULL;
 END;
-
 $BODY$
 LANGUAGE plpgsql VOLATILE STRICT;
 COMMENT ON FUNCTION pgr_getColumnName(text,text,text,int) IS 'args: sname,tname,col -gets the rregistered column name of "col" in table "sname.tname"';
@@ -201,54 +220,28 @@ $BODY$
 LANGUAGE plpgsql VOLATILE STRICT;
 COMMENT ON FUNCTION pgr_getColumnName(text,text,int) IS 'args: tab,col -gets the registered column name of "col" in table "tab"';
 
-/*
-*/
-CREATE OR REPLACE FUNCTION pgr_checkVertTab(vertname text, columns  text[], reporterrs integer default 1,OUT sname text,OUT vname text)
-RETURNS record AS
-$BODY$
-DECLARE
-    cname text;
-    colname text;
-    naming record;
-    debuglevel text;
-    err boolean;
---checking vertices table
-
-BEGIN
-   execute 'show client_min_messages' into debuglevel;
-
-
-    BEGIN
-       raise DEBUG 'Checking table % exists ',vertname;
-       execute 'select * from pgr_getTableName('||quote_literal(vertname)||',2)' into naming;
-       sname=naming.sname;
-       vname=naming.tname;
-       EXCEPTION WHEN raise_exception THEN
-           raise notice '   --->Please create % using pgr_createTopology() or pgr_createVerticesTable()',vertname;
-    END;
-
-   FOREACH cname IN ARRAY columns loop
-      raise debug 'checking ---> %',cname;
-       execute 'select pgr_getcolumnName('||quote_literal(vertname)||','||quote_literal(cname)||',0)' into colname;
-       if colname is null     then
-         RAISE debug '  ------>Adding % column in %',cname,vertname;
-         set client_min_messages  to warning;
-                execute 'ALTER TABLE '||pgr_quote_ident(vertname)||' ADD COLUMN '||cname|| ' integer';
-         execute 'set client_min_messages  to '|| debuglevel;
-        end if;
-  end loop;
-  perform pgr_createIndex(vertname , 'id' , 'btree');
-
- END
-$BODY$
-LANGUAGE plpgsql VOLATILE STRICT;
-
-
-
-
 
 /*
+.. function:: pgr_getColumnType(sname,tname,col,reportErrs default 1) returns text
+.. function:: pgr_getColumnType(tab,col,reportErrs default 1) returns text
+
+    Returns:
+          type   the types of the registered column "col" in table "tab" or "sname.tname" 
+          NULL   when "tab"/"sname"/"tname" is not found or when "col" is not in table "tab"/"sname.tname"
+    unless otherwise indicated raises notices on errors
+ 
+ Examples:  
+	* 	 select  pgr_getColumnType('tab','col');
+	* 	 select  pgr_getColumnType('myschema','mytable','col');
+        	 excecute 'select pgr_getColumnType('||quote_literal('tab')||','||quote_literal('col')||')' INTO column;
+        	 excecute 'select pgr_getColumnType('||quote_literal(sname)||','||quote_literal(sname)||','||quote_literal('col')||')' INTO column;
+
+   Author: Vicky Vergara <vicky_vergara@hotmail.com>>
+
+  HISTORY
+     Created: 2014/JUL/28 
 */
+
 CREATE OR REPLACE FUNCTION pgr_getColumnType(sname text, tname text, cname text, IN reportErrs int default 1)
 RETURNS text AS
 $BODY$
@@ -410,13 +403,10 @@ BEGIN
     ELSE
         RETURN false;
     END IF;
---    EXCEPTION WHEN OTHERS THEN
---       RETURN false;
-
 END;
 $BODY$
   LANGUAGE plpgsql VOLATILE STRICT;
---COMMENT ON FUNCTION pgr_isColumnIndexed(text,text,text) IS 'args: tab,col  -returns true if column "col" in table "tab" is indexed';
+COMMENT ON FUNCTION pgr_isColumnIndexed(text,text,text) IS 'args: sname,tname,cname  -returns true if "cname" in table "sname.tname" is indexed';
 
 CREATE OR REPLACE FUNCTION public.pgr_isColumnIndexed(tab text, col text)
 RETURNS boolean AS
@@ -445,12 +435,27 @@ BEGIN
 END
 $BODY$
   LANGUAGE plpgsql VOLATILE STRICT;
---COMMENT ON FUNCTION pgr_isColumnIndexed(text,text) IS 'args: tab,col  -returns true if column "col" in table "tab" is indexed';
+COMMENT ON FUNCTION pgr_isColumnIndexed(text,text) IS 'args: tab,col  -returns true if column "col" in table "tab" is indexed';
 
 /*
+.. function:: pgr_createIndex(tab, col,indextype)
+              pgr_createIndex(sname,tname,colname,indextypes)
+              
+   if the column is not indexed it creates a 'gist' index otherwise a 'btree' index
+   Examples:  
+	* 	 select  pgr_createIndex('tab','col','btree');
+	* 	 select  pgr_createIndex('myschema','mytable','col','gist');
+	* 	 perform 'select pgr_createIndex('||quote_literal('tab')||','||quote_literal('col')||','||quote_literal('btree'))' ;
+	* 	 perform 'select pgr_createIndex('||quote_literal('myschema')||','||quote_literal('mytable')||','||quote_literal('col')||','||quote_literal('gist')')' ;
+
+  
+   Author: Vicky Vergara <vicky_vergara@hotmail.com>>
+
+  HISTORY
+     Created: 2014/JUL/28 
 */
 
-CREATE OR REPLACE FUNCTION pgr_createIndex(sname text,tname text, colname text, indextype text)
+CREATE OR REPLACE FUNCTION pgr_createIndex(sname text,tname text, colname text, indext text)
 RETURNS void AS
 $BODY$
 DECLARE
@@ -469,12 +474,12 @@ BEGIN
                          on '||tabname||' using btree('||quote_ident(colname)||')';
       RAISE DEBUG ' ------> Adding  index "%_%_idx". -->>>%',tabname,colname,q;
       set client_min_messages  to warning;
-      if indextype = 'btree' then
+      if indext = 'gist' then
           execute 'create  index '||pgr_quote_ident(tname||'_'||colname||'_idx')||' 
-                         on '||tabname||' using btree('||quote_ident(colname)||')';
+                         on '||tabname||' using gist('||quote_ident(colname)||')';
       else
           execute 'create  index '||pgr_quote_ident(tname||'_'||colname||'_gidx')||' 
-                         on '||tabname||' using gist ('||quote_ident(colname)||')';
+                         on '||tabname||' using btree ('||quote_ident(colname)||')';
       end if;
       execute 'set client_min_messages  to ' ||debuglevel;
     END IF;
@@ -482,10 +487,10 @@ END;
 
 $BODY$
   LANGUAGE plpgsql VOLATILE STRICT;
---COMMENT ON FUNCTION pgr_isColumnInTable(text,text) IS 'args: tab,col  -returns true when the column "col" is in table "tab"';
+COMMENT ON FUNCTION pgr_createIndex(text,text,text,text) IS 'args: sname,tname,colname,indext -creates indext on colname in table sname.tname';
 
 
-CREATE OR REPLACE FUNCTION pgr_createIndex(tabname text, colname text, indextype text)
+CREATE OR REPLACE FUNCTION pgr_createIndex(tabname text, colname text, indext text)
 RETURNS void AS
 $BODY$
 DECLARE
@@ -499,150 +504,68 @@ BEGIN
     execute 'select * from pgr_getTableName('||quote_literal(tabname)||',0)' into naming;
     sname=naming.sname;
     tname=naming.tname;
-    execute pgr_createIndex(sname,tname,colname,indextype);
+    execute pgr_createIndex(sname,tname,colname,indext);
 END;
 
 $BODY$
   LANGUAGE plpgsql VOLATILE STRICT;
---COMMENT ON FUNCTION pgr_isColumnInTable(text,text) IS 'args: tab,col  -returns true when the column "col" is in table "tab"';
+COMMENT ON FUNCTION pgr_createIndex(text,text,text) IS 'args: tabname,colname,indext -creates indext on colname in table tabname';
 
-
-
-
-create or replace function pgr_quote_ident(idname text)
-    returns text as
-$body$
-/*
-.. function:: pgr_quote_ident(text)
-
-   Author: Stephen Woodbridge <woodbri@imaptools.com>
-
-   Function to split a string on '.' characters and then quote the 
-   components as postgres identifiers and then join them back together
-   with '.' characters. multile '.' will get collapsed into a single
-   '.' so 'schema...table' till get returned as 'schema."table"' and 
-   'Schema.table' becomes '"Schema'.'table"'
-
-*/
-
-declare
-    t text[];
-    pgver text;
-
-begin
-    pgver := regexp_replace(version(), E'^PostgreSQL ([^ ]+)[ ,].*$', E'\\1');
 
 /*
-    RAISE NOTICE 'pgr_quote_ident(%), pgver: %, version: %, versionless %',
-        tab, pgver, version(), pgr_versionless(pgver, '9.2');
-*/
+.. function:: pgr_checkVertTab(vertname,columnsArr,reportErrs) returns record of sname,vname
 
-    if pgr_versionless(pgver, '9.2') then
-        select into t array_agg(quote_ident(term)) from
-            (select nullif(unnest, '') as term
-               from unnest(string_to_array(idname, '.'))) as foo;
-    else
-        select into t array_agg(quote_ident(term)) from
-            (select unnest(string_to_array(idname, '.', '')) as term) as foo;
-    end if;
-    return array_to_string(t, '.');
-end;
-$body$
-language plpgsql immutable;
-COMMENT ON function pgr_quote_ident(text) IS 'args: idname  -  quote_ident to all parts of the identifier "idname"';
-
-
-CREATE OR REPLACE FUNCTION pgr_versionless(v1 text, v2 text)
-  RETURNS boolean AS
-$BODY$
-/*
- * function for comparing version strings.
- * Ex: select pgr_version_less(postgis_lib_version(), '2.1');
-
-   Author: Stephen Woodbridge <woodbri@imaptools.com>
- *
- * needed because postgis 2.1 deprecates some function names and
- * we need to detect the version at runtime
-*/
-
-declare
-    v1a text[];
-    v2a text[];
-    nv1 integer;
-    nv2 integer;
-    ne1 integer;
-    ne2 integer;
+    Returns:
+          sname,vname  registered schemaname, vertices table name 
     
-begin
-    -- separate components into an array, like:
-    -- '2.1.0-beta3dev'  =>  {2,1,0,beta3dev}
-    v1a := regexp_matches(v1, E'^(\\d+)(?:[\\.](\\d+))?(?:[\\.](\\d+))?[-+\\.]?(.*)$');
-    v2a := regexp_matches(v2, E'^(\\d+)(?:[\\.](\\d+))?(?:[\\.](\\d+))?[-+\\.]?(.*)$');
+    if the table is not found will stop any further checking.
+    if a column is missing, then its added as integer ---  (id also as integer but is bigserial when the vertices table is created with the pgr functions)
+          
+ Examples:  
+	* 	execute 'select * from  pgr_checkVertTab('||quote_literal(vertname) ||', ''{"id","cnt","chk"}''::text[])' into naming;
+	* 	execute 'select * from  pgr_checkVertTab('||quote_literal(vertname) ||', ''{"id","ein","eout"}''::text[])' into naming;
 
-    -- convert modifiers to numbers for comparison
-    -- we do not delineate between alpha1, alpha2, alpha3, etc
-    ne1 := case when v1a[4] is null or v1a[4]='' then 5
-                when v1a[4] ilike 'rc%' then 4
-                when v1a[4] ilike 'beta%' then 3
-                when v1a[4] ilike 'alpha%' then 2
-                when v1a[4] ilike 'dev%' then 1
-                else 0 end;
+   Author: Vicky Vergara <vicky_vergara@hotmail.com>>
 
-    ne2 := case when v2a[4] is null or v2a[4]='' then 5
-                when v2a[4] ilike 'rc%' then 4
-                when v2a[4] ilike 'beta%' then 3
-                when v2a[4] ilike 'alpha%' then 2
-                when v2a[4] ilike 'dev%' then 1
-                else 0 end;
-
-    nv1 := v1a[1]::integer * 10000 +
-           coalesce(v1a[2], '0')::integer * 1000 +
-           coalesce(v1a[3], '0')::integer *  100 + ne1;
-    nv2 := v2a[1]::integer * 10000 + 
-           coalesce(v2a[2], '0')::integer * 1000 + 
-           coalesce(v2a[3], '0')::integer *  100 + ne2;
-
-    --raise notice 'nv1: %, nv2: %, ne1: %, ne2: %', nv1, nv2, ne1, ne2;
-
-    return nv1 < nv2;
-end;
+  HISTORY
+     Created: 2014/JUL/27 
+*/
+CREATE OR REPLACE FUNCTION pgr_checkVertTab(vertname text, columnsArr  text[], OUT sname text,OUT vname text)
+RETURNS record AS
 $BODY$
-  LANGUAGE plpgsql IMMUTABLE STRICT
-  COST 1;
-COMMENT ON function pgr_versionless(text,text) IS 'args: v1,v2  - returns true when v1 < v2';
+DECLARE
+    cname text;
+    colname text;
+    naming record;
+    debuglevel text;
+
+BEGIN
+   execute 'show client_min_messages' into debuglevel;
 
 
-create or replace function pgr_startPoint(g geometry)
-    returns geometry as
-$body$
-declare
+    BEGIN
+       raise DEBUG 'Checking table % exists ',vertname;
+       execute 'select * from pgr_getTableName('||quote_literal(vertname)||',2)' into naming;
+       sname=naming.sname;
+       vname=naming.tname;
+       EXCEPTION WHEN raise_exception THEN
+           raise notice '   --->Please create % using pgr_createTopology() or pgr_createVerticesTable()',vertname;
+    END;
 
-begin
-    if geometrytype(g) ~ '^MULTI' then
-        return st_startpoint(st_geometryn(g,1));
-    else
-        return st_startpoint(g);
-    end if;
-end;
-$body$
-language plpgsql IMMUTABLE;
-COMMENT ON function pgr_startPoint(geometry) IS 'args: g  - returns start point of the geometry "g" even if its multi';
+   FOREACH cname IN ARRAY columnsArr loop
+      raise debug 'checking ---> %',cname;
+       execute 'select pgr_getcolumnName('||quote_literal(vertname)||','||quote_literal(cname)||',0)' into colname;
+       if colname is null then
+         RAISE debug '  ------>Adding % column in %',cname,vertname;
+         set client_min_messages  to warning;
+                execute 'ALTER TABLE '||pgr_quote_ident(vertname)||' ADD COLUMN '||cname|| ' integer';
+         execute 'set client_min_messages  to '|| debuglevel;
+        end if;
+  end loop;
+  perform pgr_createIndex(vertname , 'id' , 'btree');
 
-
-
-create or replace function pgr_endPoint(g geometry)
-    returns geometry as
-$body$
-declare
-
-begin
-    if geometrytype(g) ~ '^MULTI' then
-        return st_endpoint(st_geometryn(g,1));
-    else
-        return st_endpoint(g);
-    end if;
-end;
-$body$
-language plpgsql IMMUTABLE;
-COMMENT ON function pgr_endPoint(geometry) IS 'args: g  - returns end point of the geometry "g" even if its multi';
+ END
+$BODY$
+LANGUAGE plpgsql VOLATILE STRICT;
+COMMENT ON FUNCTION pgr_checkVertTab(text,text[]) IS 'args: vertname,columnsArr -Checks structure and gets registered schema and table name of the vertices table';
 
--- src/common/sql/pgrouting_version.sql
+++ src/common/sql/pgrouting_version.sql
@@ -29,3 +29,138 @@ END;
 $BODY$
 LANGUAGE plpgsql IMMUTABLE;
 
+create or replace function pgr_quote_ident(idname text)
+    returns text as
+$body$
+/*
+.. function:: pgr_quote_ident(text)
+
+   Author: Stephen Woodbridge <woodbri@imaptools.com>
+
+   Function to split a string on '.' characters and then quote the 
+   components as postgres identifiers and then join them back together
+   with '.' characters. multile '.' will get collapsed into a single
+   '.' so 'schema...table' till get returned as 'schema."table"' and 
+   'Schema.table' becomes '"Schema'.'table"'
+
+*/
+
+declare
+    t text[];
+    pgver text;
+
+begin
+    pgver := regexp_replace(version(), E'^PostgreSQL ([^ ]+)[ ,].*$', E'\\1');
+
+    if pgr_versionless(pgver, '9.2') then
+        select into t array_agg(quote_ident(term)) from
+            (select nullif(unnest, '') as term
+               from unnest(string_to_array(idname, '.'))) as foo;
+    else
+        select into t array_agg(quote_ident(term)) from
+            (select unnest(string_to_array(idname, '.', '')) as term) as foo;
+    end if;
+    return array_to_string(t, '.');
+end;
+$body$
+language plpgsql immutable;
+COMMENT ON function pgr_quote_ident(text) IS 'args: idname  -  quote_ident to all parts of the identifier "idname"';
+
+
+/*
+ * function for comparing version strings.
+ * Ex: select pgr_version_less(postgis_lib_version(), '2.1');
+
+   Author: Stephen Woodbridge <woodbri@imaptools.com>
+ *
+ * needed because postgis 2.1 deprecates some function names and
+ * we need to detect the version at runtime
+*/
+CREATE OR REPLACE FUNCTION pgr_versionless(v1 text, v2 text)
+  RETURNS boolean AS
+$BODY$
+
+
+declare
+    v1a text[];
+    v2a text[];
+    nv1 integer;
+    nv2 integer;
+    ne1 integer;
+    ne2 integer;
+    
+begin
+    -- separate components into an array, like:
+    -- '2.1.0-beta3dev'  =>  {2,1,0,beta3dev}
+    v1a := regexp_matches(v1, E'^(\\d+)(?:[\\.](\\d+))?(?:[\\.](\\d+))?[-+\\.]?(.*)$');
+    v2a := regexp_matches(v2, E'^(\\d+)(?:[\\.](\\d+))?(?:[\\.](\\d+))?[-+\\.]?(.*)$');
+
+    -- convert modifiers to numbers for comparison
+    -- we do not delineate between alpha1, alpha2, alpha3, etc
+    ne1 := case when v1a[4] is null or v1a[4]='' then 5
+                when v1a[4] ilike 'rc%' then 4
+                when v1a[4] ilike 'beta%' then 3
+                when v1a[4] ilike 'alpha%' then 2
+                when v1a[4] ilike 'dev%' then 1
+                else 0 end;
+
+    ne2 := case when v2a[4] is null or v2a[4]='' then 5
+                when v2a[4] ilike 'rc%' then 4
+                when v2a[4] ilike 'beta%' then 3
+                when v2a[4] ilike 'alpha%' then 2
+                when v2a[4] ilike 'dev%' then 1
+                else 0 end;
+
+    nv1 := v1a[1]::integer * 10000 +
+           coalesce(v1a[2], '0')::integer * 1000 +
+           coalesce(v1a[3], '0')::integer *  100 + ne1;
+    nv2 := v2a[1]::integer * 10000 + 
+           coalesce(v2a[2], '0')::integer * 1000 + 
+           coalesce(v2a[3], '0')::integer *  100 + ne2;
+
+    --raise notice 'nv1: %, nv2: %, ne1: %, ne2: %', nv1, nv2, ne1, ne2;
+
+    return nv1 < nv2;
+end;
+$BODY$
+  LANGUAGE plpgsql IMMUTABLE STRICT
+  COST 1;
+COMMENT ON function pgr_versionless(text,text) IS 'args: v1,v2  - returns true when v1 < v2';
+
+
+
+
+create or replace function pgr_startPoint(g geometry)
+    returns geometry as
+$body$
+declare
+
+begin
+    if geometrytype(g) ~ '^MULTI' then
+        return st_startpoint(st_geometryn(g,1));
+    else
+        return st_startpoint(g);
+    end if;
+end;
+$body$
+language plpgsql IMMUTABLE;
+COMMENT ON function pgr_startPoint(geometry) IS 'args: g  - returns start point of the geometry "g" even if its multi';
+
+
+
+create or replace function pgr_endPoint(g geometry)
+    returns geometry as
+$body$
+declare
+
+begin
+    if geometrytype(g) ~ '^MULTI' then
+        return st_endpoint(st_geometryn(g,1));
+    else
+        return st_endpoint(g);
+    end if;
+end;
+$body$
+language plpgsql IMMUTABLE;
+COMMENT ON function pgr_endPoint(geometry) IS 'args: g  - returns end point of the geometry "g" even if its multi';
+
