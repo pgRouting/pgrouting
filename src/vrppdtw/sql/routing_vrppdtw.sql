7f197132a03e71c4747d6183a55a5b74a11aa287
Stephen Woodbridge <woodbri@swoodbridge.com>
2014-08-05 11:13:23 -0400

    Some fixes for Mani to get integration started.

--- src/vrppdtw/sql/routing_vrppdtw.sql
+++ src/vrppdtw/sql/routing_vrppdtw.sql
@@ -7,7 +7,12 @@
 create or replace function pgr_gsoc_vrppdtw(
                 sql text,
                 vehicle_num integer,
-                capacity integer
+                capacity integer,
+                OUT seq integer,
+                OUT rid integer,
+                OUT nseq integer,
+                OUT nid integer,
+                OUT cost float
                 )
 returns setof record as
 '$libdir/librouting_vrppdtw', 'vrppdtw'
