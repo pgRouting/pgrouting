8276ee81b92b03735dde56099f9e8ac9cde4a856
mapas de vicky <vicky_vergara@hotmail.com>
2015-02-26 14:10:18 -0600

    YenTopKShortestPathsAlg pretty code

--- src/ksp/src/YenTopKShortestPathsAlg.cpp
+++ src/ksp/src/YenTopKShortestPathsAlg.cpp
@@ -1,7 +1,7 @@
 ///////////////////////////////////////////////////////////////////////////////
 ///  YenTopKShortestPathsAlg.cpp
-///  The implementation of Yen's algorithm to get the top k shortest paths 
-///  connecting a pair of vertices in a graph. 
+///  The implementation of Yen's algorithm to get the top k shortest paths
+///  connecting a pair of vertices in a graph.
 ///
 ///  @remarks <TODO: insert remarks here>
 ///
@@ -11,204 +11,106 @@
 ///
 ///////////////////////////////////////////////////////////////////////////////
 
-//#include <set>
-//#include <map>
-//#include <algorithm>
 #include <deque>
-#include <cassert>
 #include "GraphElements.h"
-#include "Graph.h"
 #include "DijkstraShortestPathAlg.h"
 #include "YenTopKShortestPathsAlg.h"
 
-using namespace std;
 
-void YenTopKShortestPathsAlg::clear()
-{
+void YenTopKShortestPathsAlg::clear() {
         DijkstraShortestPathAlg::clear();
-	m_ResultList.clear();
+        m_ResultList.clear();
         m_Heap.clear();
 }
 
-void YenTopKShortestPathsAlg::_init()
-{
-	clear();
-	BasePath shortestPath = Dijkstra(m_Source_id, m_Target_id);
+void YenTopKShortestPathsAlg::_init() {
+        clear();
+        BasePath shortestPath = Dijkstra(m_Source_id, m_Target_id);
         DijkstraShortestPathAlg::clear();
-#if 0
-shortestPath.PrintOut(std::cout);
-#endif
- 	if ( !shortestPath.isEmpty()) {   //to be empty has no edges
-	    m_ResultList.push_back(shortestPath);
+         if (!shortestPath.isEmpty()) { 
+            m_ResultList.push_back(shortestPath);
         }
 }
 
 
-std::deque<BasePath> YenTopKShortestPathsAlg::Yen( int  source, int  sink, int K ) {
-	clear();
-        if (  source !=sink && K > 0 && exist_vertex(source) && exist_vertex(sink) ) {
-
-	     m_Source_id = source;
-	     m_Target_id = sink;
+std::deque<BasePath> YenTopKShortestPathsAlg::Yen(int  source, int  sink, int K) {
+        clear();
+        if (source !=sink && K > 0 && exist_vertex(source) && exist_vertex(sink)) {
+             m_Source_id = source;
+             m_Target_id = sink;
              sourceID = find_vertex(source);
              targetID = find_vertex(sink);
-             get_shortest_paths( find_vertex(source), find_vertex(sink), K );
-#if 0
-std::cout<<"paths found "<<m_ResultList.size()<<"\n";
-#endif
+             get_shortest_paths(find_vertex(source), find_vertex(sink), K);
         }
         return m_ResultList;
 }
 
 
-BasePath YenTopKShortestPathsAlg::get_shortest_path( POS  sourcePos, POS  targetPos )
-{
-	int source_id = m_vtVertices[ sourcePos ].getID();
-	int target_id = m_vtVertices[ targetPos ].getID();
-	return Dijkstra( source_id,  target_id );
+BasePath YenTopKShortestPathsAlg::get_shortest_path(POS  sourcePos, POS  targetPos) {
+        int source_id = m_vtVertices[ sourcePos ].getID();
+        int target_id = m_vtVertices[ targetPos ].getID();
+        return Dijkstra(source_id,  target_id);
 }
 
-/*
-bool YenTopKShortestPathsAlg::has_next()
-{
-	return !m_quPathCandidates.empty();
-}
-*/
-void YenTopKShortestPathsAlg::next()
-{
-#if 0
-std::cout<<"Entering YenTopKShortestPathsAlg::next\n";
-#endif
-        
-        POS currPathId =  m_ResultList.size()-1 ;
-	BasePath curr_result_path = m_ResultList[ currPathId ];
-#if 0
-std::cout<<"current path\n";
-curr_result_path.PrintOut(std::cout);
-std::cout<<"size is "<<curr_result_path.size()<<"\n";
-#endif
-//        POS currPathSize= curr_result_path.size();
+void YenTopKShortestPathsAlg::next() {
+        POS currPathId =  m_ResultList.size()-1;
+        BasePath curr_result_path = m_ResultList[ currPathId ];
 
 
         BaseEdge* spurEdge;
         POS spurNode;
         BasePath rootPath;
-	for (POS i = 0; i < curr_result_path.size(); ++i)
-	{
-#if 0
-std::cout<<"Entering cycle\n";
-#endif
+        for (POS i = 0; i < curr_result_path.size(); ++i) {
             spurEdge = curr_result_path[i];
-#if 0
-std::cout<<"\nspur Edge\n";
-spurEdge->PrintOut(std::cout);
-#endif
             spurNode = spurEdge->getStart();
-#if 0
-std::cout<<"\nspur node\n";
-m_vtVertices[spurNode].PrintOut(std::cout, m_vtEdges);
-#endif
-            curr_result_path.subPath(rootPath, i);  
-#if 0
-//std::cout<<"SubPath results:\ncurrent path\n";
-//curr_result_path.PrintOut(std::cout);
-std::cout<<"root path\n";
-rootPath.PrintOut(std::cout);
-#endif
+            curr_result_path.subPath(rootPath, i);
 
-		
-               //for each path p in A:
+
+               // for each path p in A:
                //   if rootPath == p.nodes(0, i):
                //     Remove the links that are part of the previous shortest paths which share the same root path.
                //     remove p.edge(i, i + 1) from Graph;
 
-#if 0
-std::cout<<"\nGraph status before removed edges\n";
-PrintOut(std::cout);
-#endif
-            for ( POS j=0; j < m_ResultList.size(); j++) {
-               BasePath workingPath = m_ResultList[j]; // p (luego lo arreglo para que no se copie
-#if 0
-std::cout<<"\nworkingPath #"<<j<<"\n";
-workingPath.PrintOut(std::cout);
-#endif
- 	       if (rootPath.isEqual(workingPath) ) {  //this isequal compares up to the size of rootPath
-	            int edgeToBeRemoved = workingPath[i]->getNID(); 
-		    remove_edge( edgeToBeRemoved );
-#if 0
-std::cout<<"\nremoved Edge for processing\n";
-m_vtEdges[ edgeToBeRemoved ].PrintOut(std::cout);
-#endif
-	       }
-             }
+            for (POS j=0; j < m_ResultList.size(); j++) {
+               BasePath workingPath = m_ResultList[j];  // TODO(vicky): can be placed inside the condition
+                if (rootPath.isEqual(workingPath)) {
+                    int edgeToBeRemoved = workingPath[i]->getNID();
+                    remove_edge(edgeToBeRemoved);
+               }
+            }
 
-        
+            removeNodes(rootPath);
 
-               removeNodes( rootPath ); 
+            BasePath spurPath;
+            spurPath = DijkstraShortestPathAlg::Dijkstra(spurNode , targetID, true);
+            DijkstraShortestPathAlg::clear();
 
-               BasePath spurPath;
-#if 0
-std::cout<<"\n !!!!!!!!!going to Dijstra" <<  m_vtVertices[ spurNode ].getID() <<" to "<< m_Target_id <<"\n";
-std::cout<<"aka" <<  spurNode  <<" to "<< targetID <<"\n";
-if (spurNode ==1 ) PrintOut(std::cout);
-#endif
-               //spurPath = DijkstraShortestPathAlg::Dijkstra( m_vtVertices[ spurNode ].getID(), m_Target_id);
-               spurPath = DijkstraShortestPathAlg::Dijkstra(  spurNode , targetID, true);
-               DijkstraShortestPathAlg::clear();
-#if 0
-std::cout<<"\nGraph status after Dij clear\n";
-PrintOut(std::cout);
-std::cout<<"\n*****************\n";
-#endif
 
-  
-#if 0
-std::cout<<"root path\n";
-rootPath.PrintOut(std::cout);
-std::cout<<"spur path\n";
-spurPath.PrintOut(std::cout);
-#endif
-               // Entire path is made up of the root path and spur path.
-               rootPath.append(spurPath);  //this should also update the cost of the totalPath
-#if 0
-std::cout<<"total path\n";
-rootPath.PrintOut(std::cout);
-#endif
+            // Entire path is made up of the root path and spur path.
+            rootPath.append(spurPath);  // this should also update the cost of the totalPath
 
-               // Add the potential k-shortest path to the heap.
-               if (rootPath.FromTo(sourceID, targetID)) {
-                   m_Heap.push_back(rootPath); 
-#if 0
-std::cout<<"\npath added to HEAP\n";
-rootPath.PrintOut(std::cout);
-//assert(true==false);
-#endif
-             }
-	}
+            // Add the potential k-shortest path to the heap.
+            if (rootPath.FromTo(sourceID, targetID)) {
+                   m_Heap.push_back(rootPath);
+            }
+        }
 }
 
-/*
-bool OrderByCost (const BasePath &p1,const BasePath &p2) { 
+/* TODO(Vicky): do the sorting
+bool OrderByCost (const BasePath &p1,const BasePath &p2) {
        return p1.Weight() < p2.Weight()
               || p1.size() < p2.size()
               || p1.EdgesLessComapre(p2);
 }
 */
-void YenTopKShortestPathsAlg::get_shortest_paths( POS source_id, POS target_id, int K)
-{
-
-	  _init(); // get the best using Dijkstra
-	  while ( m_ResultList.size() < (unsigned int) K )
-	  {
-		next();
+void YenTopKShortestPathsAlg::get_shortest_paths(POS source_id, POS target_id, int K) {
+          _init();  // get the best using Dijkstra
+          while ( m_ResultList.size() < (unsigned int) K ) {
+                next();
                 if ( m_Heap.empty() ) break;
+// TODO(Vicky): do the sorting
 //                std::sort( m_Heap.begin() , m_Heap.end() );  // , OrderByCost );
-                m_ResultList.push_back( m_Heap[0] );
-#if 0
-std::cout<<"\npath added to results\n";
-m_ResultList[ m_ResultList.size()-1 ].PrintOut(std::cout);
-//assert(true==false);
-#endif
-                m_Heap.erase( m_Heap.begin() );
-	  }
+                m_ResultList.push_back(m_Heap[0]);
+                m_Heap.erase(m_Heap.begin());
+          }
 }
--- src/ksp/src/YenTopKShortestPathsAlg.h
+++ src/ksp/src/YenTopKShortestPathsAlg.h
@@ -1,63 +1,43 @@
 ///////////////////////////////////////////////////////////////////////////////
 ///  YenTopKShortestPathsAlg.h
-///  The implementation of Yen's algorithm to get the top k shortest paths 
-///  connecting a pair of vertices in a graph. 
+///  The implementation of Yen's algorithm to get the top k shortest paths
+///  connecting a pair of vertices in a graph.
 ///
 ///  @remarks <TODO: insert remarks here>
 ///
 ///  @author Yan Qi @date 7/10/2010
-/// 
+///
 ///  $Id: YenTopKShortestPathsAlg.h 65 2010-09-08 06:48:36Z yan.qi.asu $
 ///
 ///////////////////////////////////////////////////////////////////////////////
-#ifndef _YENTOPKSHORTESTPATHSALG_H
-#define _YENTOPKSHORTESTPATHSALG_H
+#ifndef SRC_KSP_SRC_YENTOPKSHORTESTPATHSALG_H_
+#define SRC_KSP_SRC_YENTOPKSHORTESTPATHSALG_H_
 
 #include <deque>
 #include "DijkstraShortestPathAlg.h"
 
-class YenTopKShortestPathsAlg: public DijkstraShortestPathAlg
-{
-	std::deque<BasePath> m_ResultList;
+class YenTopKShortestPathsAlg: public DijkstraShortestPathAlg {
+        std::deque<BasePath> m_ResultList;
         std::deque<BasePath> m_Heap;
 
         int m_Source_id;
         int m_Target_id;
         POS sourceID;
         POS targetID;
-        int m_K;
 
-	//map<BasePath*, BaseVertex*> m_mpDerivationVertexIndex;
-	//multiset<BasePath*, WeightLess<BasePath> > m_quPathCandidates;
 
-	//BaseVertex* m_pSourceVertex;
-	//BaseVertex* m_pTargetVertex;
-
-	//int m_nGeneratedPathNum;
-
-private:
-	BasePath get_shortest_path(POS sourcePos, POS targetPos);
-	void get_shortest_paths(POS pSource, POS pTarget, int top_k);
-	void _init();
-
-public:
-/*
-	YenTopKShortestPathsAlg(const Graph& graph)
-	{
-		YenTopKShortestPathsAlg(graph, NULL, NULL);
-	}
-*/
-	YenTopKShortestPathsAlg(const Graph& graph)
+ public:
+        explicit YenTopKShortestPathsAlg(const Graph& graph)
                 : DijkstraShortestPathAlg(graph),
-		 m_Source_id(0), m_Target_id(0),
-                 m_K(1)
-	{ }
+                 m_Source_id(0), m_Target_id(0) {}
 
-	~YenTopKShortestPathsAlg(void){ clear(); }
-        std::deque<BasePath> Yen( int source, int target, int numb_paths );
-	void clear();
-	//bool has_next();	
-	void next();
-
+        ~YenTopKShortestPathsAlg(void) {clear();}
+        std::deque<BasePath> Yen(int source, int target, int numb_paths);
+        void clear();
+ private:
+        void get_shortest_paths(POS pSource, POS pTarget, int top_k);
+        BasePath get_shortest_path(POS pSource, POS pTarget);
+        void _init();
+        void next();
 };
-#endif
+#endif  // SRC_KSP_SRC_YENTOPKSHORTESTPATHSALG_H_
