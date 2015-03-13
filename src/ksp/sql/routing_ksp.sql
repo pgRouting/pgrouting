9b4cfbe23aff20aaabf9ef5c93b564ca3642d975
mapas de vicky <vicky_vergara@hotmail.com>
2015-03-13 10:43:09 -0600

    idea working fixed the sql

--- src/ksp/sql/routing_ksp.sql
+++ src/ksp/sql/routing_ksp.sql
@@ -14,17 +14,6 @@ CREATE OR REPLACE FUNCTION _pgr_ksp(sql text, source_id integer, target_id integ
     AS '$libdir/librouting_ksp', 'kshortest_path'
     LANGUAGE c IMMUTABLE STRICT;
 
-/*
-CREATE OR REPLACE FUNCTION pgr-ksp(sql text, source_id integer, target_id integer, no_paths integer, has_reverse_cost boolean)
-  RETURNS SETOF pgr_costresult3 AS
-  $BODY$ begin 
-    return query SELECT * FROM pgr_ksp($1, $2, $3, $4, $5) where id1 < $4; 
-  end 
-  $BODY$
-  LANGUAGE plpgsql VOLATILE
-  COST 100
-  ROWS 1000;
-*/
 CREATE OR REPLACE FUNCTION pgr_ksp(sql text, source_id integer, target_id integer, no_paths integer, has_reverse_cost boolean, heap_paths boolean default false)
   RETURNS SETOF pgr_costresult3 AS
   $BODY$ begin 
