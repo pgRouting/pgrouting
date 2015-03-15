adc577617058e9aa7cda0d8eddf175a2420243c6
mapas de vicky <vicky_vergara@hotmail.com>
2015-03-15 14:59:51 -0600

    sql function checks the types before calling the c code

--- src/ksp/sql/routing_ksp.sql
+++ src/ksp/sql/routing_ksp.sql
@@ -9,20 +9,39 @@ CREATE OR REPLACE FUNCTION _pgr_ksp(sql text, source_id integer, target_id integ
     LANGUAGE c IMMUTABLE STRICT;
 */
 
-CREATE OR REPLACE FUNCTION _pgr_ksp(sql text, source_id integer, target_id integer, no_paths integer, has_reverse_cost boolean)
+CREATE OR REPLACE FUNCTION _pgr_ksp(sql text, source_id bigint, target_id bigint, no_paths integer, has_reverse_cost boolean)
     RETURNS SETOF pgr_costResult3
     AS '$libdir/librouting_ksp', 'kshortest_path'
     LANGUAGE c IMMUTABLE STRICT;
 
-CREATE OR REPLACE FUNCTION pgr_ksp(sql text, source_id integer, target_id integer, no_paths integer, has_reverse_cost boolean, heap_paths boolean default false)
+CREATE OR REPLACE FUNCTION pgr_ksp(sql text, source_id bigint, target_id bigint, no_paths integer, has_reverse_cost boolean, heap_paths boolean default false)
   RETURNS SETOF pgr_costresult3 AS
-  $BODY$ begin 
-    if $6 = false then
-       return query SELECT * FROM _pgr_ksp($1, $2, $3, $4, $5) where id1 < $4; 
-    else
-       return query SELECT * FROM _pgr_ksp($1, $2, $3, $4, $5); 
+  $BODY$  
+
+  DECLARE
+  rec record;
+
+  BEGIN 
+    execute 'select pg_typeof(id)::text as id_type, pg_typeof(source)::text as source_type, pg_typeof(target)::text as target_type '
+            || ' from ('||sql||' limit 1) AS a ' into rec;
+    -- raise notice 'id = %, source = %, target = %, typeof =%', rec.id_type,rec.st,rec.tt, pg_typeof(rec.id_type);
+    if not (rec.id_type in ('integer'::text, 'smallint'::text)) then
+        RAISE EXCEPTION 'id is NOT of type integer or smallint';
     end if;
-  end 
+    if not (rec.source_type in ('integer'::text, 'smallint'::text)) then
+        RAISE EXCEPTION 'id is NOT of type integer or smallint';
+    end if;
+    if not (rec.target_type in ('integer'::text, 'smallint'::text)) then
+        RAISE EXCEPTION 'id is NOT of type integer or smallint';
+    end if;
+        
+       
+    if $6 = false then
+       return query SELECT * FROM _pgr_ksp($1, $2, $3, $4, $5) where id1 < $4;
+    else
+       return query SELECT * FROM _pgr_ksp($1, $2, $3, $4, $5);
+    end if;
+  end
   $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100
