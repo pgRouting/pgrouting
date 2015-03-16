b1f7b07449e7356f12fc69e8a975bcbf6a8cd9d4
mapas de vicky <vicky_vergara@hotmail.com>
2015-03-16 14:10:45 -0600

    more fixes to sql part

--- src/ksp/sql/routing_ksp.sql
+++ src/ksp/sql/routing_ksp.sql
@@ -24,15 +24,14 @@ CREATE OR REPLACE FUNCTION _pgr_ksp(sql text, source_id bigint, target_id bigint
     LANGUAGE c IMMUTABLE STRICT;
 
 /* invert the comments when pgRouting decides for bigints */
-CREATE OR REPLACE FUNCTION pgr_ksp(sql text, source_id bigint, target_id bigint, no_paths integer, has_reverse_cost boolean default false, heap_paths boolean default false)
-  --RETURNS SETOF pgr_costresult3Big AS
-  RETURNS SETOF pgr_costresult3 AS
+CREATE OR REPLACE FUNCTION _pgr_parameter_check(sql text)
+  RETURNS bool AS
   $BODY$  
 
   DECLARE
   rec record;
   rec1 record;
-  reverse boolean;
+  has_reverse boolean;
   BEGIN 
     -- checking query is executable
     BEGIN
@@ -55,7 +54,7 @@ CREATE OR REPLACE FUNCTION pgr_ksp(sql text, source_id bigint, target_id bigint,
     BEGIN
     execute 'select pg_typeof(id)::text as id_type, pg_typeof(source)::text as source_type, pg_typeof(target)::text as target_type, pg_typeof(cost)::text as cost_type'
             || ' from ('||sql||' limit 1) AS a ' into rec;
-    /*
+    /* 
     if not (rec.id_type in ('bigint'::text, 'integer'::text, 'smallint'::text))
        OR   not (rec.source_type in ('bigint'::text, 'integer'::text, 'smallint'::text))
        OR   not (rec.target_type in ('bigint'::text, 'integer'::text, 'smallint'::text))
@@ -74,38 +73,80 @@ CREATE OR REPLACE FUNCTION pgr_ksp(sql text, source_id bigint, target_id bigint,
 
     -- checking the data types of the optional reverse_cost
     --  execute 'select pg_typeof(reverse_cost) as reverse_type::text, reverse_cost  from ('||sql||' limit 1) AS a ' into rec1;
-    if (has_reverse_Cost) then
+--    if (has_reverse_Cost) then
+         has_reverse = false;
          BEGIN
             execute 'select reverse_cost  from ('||sql||' limit 1) AS a ' into rec1;
+            has_reverse := true;
             EXCEPTION
               WHEN OTHERS THEN
-                 raise EXCEPTION 'has_reverse_cost set to true but reverse_cost not found';
+                 has_reverse = false;
+                 -- raise EXCEPTION 'has_reverse_cost set to true but reverse_cost not found';
          END;
-         execute 'select pg_typeof(reverse_cost)::text as reverse_type from ('||sql||' limit 1) AS a ' into rec1;
-         if (rec1.reverse_type != 'double precision') then
-               raise EXCEPTION 'Reverse_cost is not double precision';
+         if (has_reverse) then
+            execute 'select pg_typeof(reverse_cost)::text as reverse_type from ('||sql||' limit 1) AS a ' into rec1;
+            if (rec1.reverse_type != 'double precision') then
+                 raise EXCEPTION 'Reverse_cost is not double precision';
+            end if;
          end if;
-    else
+/*    else
          BEGIN
             execute 'select reverse_cost  from ('||sql||' limit 1) AS a ' into rec1;
             raise NOTICE 'has_reverse_cost set to false. reverse_cost found, Ignoring reverse_cost';
             EXCEPTION
-              WHEN OTHERS THEN  CONTINUE;
+              WHEN OTHERS THEN  sql=sql;
          END;
     end if;
+*/
+    return has_reverse;
+  END
+  $BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 1;
 
 
+-- invert the comments when pgRouting decides for bigints 
+CREATE OR REPLACE FUNCTION pgr_ksp(sql text, source_id bigint, target_id bigint, no_paths integer, has_reverse_cost boolean default false, heap_paths boolean default false)
+  --RETURNS SETOF pgr_costresult3Big AS
+  RETURNS SETOF pgr_costresult3 AS
+  $BODY$  
+  DECLARE
+  has_reverse boolean;
+  BEGIN
+      has_reverse =_pgr_parameter_check(sql);
+      -- for backwards comptability uncomment latter:
+      /*
+      if (has_reverse != has_reverse_cost) 
+         if (has_reverse) raise EXCEPTION 'has_reverse_cost set to found but reverse_cost column found';
+         else raise EXCEPTION 'has_reverse_cost set to true but reverse_cost not found';
+         end if;
+      end if;
+      */
 
-    if $6 = false then
-       return query SELECT seq,id1,id2::integer, id3::integer,cost FROM _pgr_ksp($1, $2, $3, $4, $5) where id1 < $4;
-       -- return query SELECT * FROM _pgr_ksp($1, $2, $3, $4, $5) where id1 < $4;
-    else
-       return query SELECT seq,id1,id2::integer, id3::integer,cost FROM _pgr_ksp($1, $2, $3, $4, $5);
-       -- return query SELECT * FROM _pgr_ksp($1, $2, $3, $4, $5);
-    end if;
-  end
+      if $6 = false then
+         return query SELECT seq,id1,id2::integer, id3::integer,cost FROM _pgr_ksp($1, $2, $3, $4, has_reverse) where id1 < $4;
+         -- return query SELECT * FROM _pgr_ksp($1, $2, $3, $4, $5) where id1 < $4;
+      else
+         return query SELECT seq,id1,id2::integer, id3::integer,cost FROM _pgr_ksp($1, $2, $3, $4, has_reverse);
+         -- return query SELECT * FROM _pgr_ksp($1, $2, $3, $4, $5);
+      end if;
+  END
   $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100
   ROWS 1000;
 
+CREATE OR REPLACE FUNCTION pgr_dijkstra(sql text, source_id bigint, target_id bigint)
+  --RETURNS SETOF pgr_costresult3Big AS
+  RETURNS SETOF pgr_costresult AS
+  $BODY$  
+  BEGIN
+         return query SELECT seq, id2 as id1 , id3 as id2 , cost FROM pgr_ksp($1, $2, $3, 1);
+         -- return query SELECT * FROM _pgr_ksp($1, $2, $3, $4, $5);
+  END
+  $BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100
+  ROWS 1000;
+
+       
