7691dc0fbb8ff05d07c1c7c8a66027e27c630e6d
mapas de vicky <vicky_vergara@hotmail.com>
2015-05-12 13:44:47 -0500

    cleaning code

--- src/dijkstra/src/dijkstra_driver.cpp
+++ src/dijkstra/src/dijkstra_driver.cpp
@@ -26,7 +26,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 extern "C" {
 #include "postgres.h"
-#include "./dijkstra.h"
+// #include "./dijkstra.h"
 }
 
 #include <deque>
