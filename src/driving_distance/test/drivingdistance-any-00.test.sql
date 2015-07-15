9b3f076e6b4e9e135b0261f7c0212ce37e766122
mapas de vicky <vicky_vergara@hotmail.com>
2015-07-15 16:44:37 -0500

    fixed driving distance test

--- src/driving_distance/test/drivingdistance-any-00.test.sql
+++ src/driving_distance/test/drivingdistance-any-00.test.sql
@@ -8,15 +8,15 @@ select cost, count(*) from (
     select * from pgr_drivingdistance('select id, source, target, 1.0::float8 as cost from ddnoded2', 1274, 10, false, false)
 ) as foo group by cost order by cost asc;
 
-select tot_cost, count(*) from (
+select agg_cost, count(*) from (
     select * from pgr_drivingdistance('select id, source, target, 1 as cost from ddnoded2', 1274, 10, false)
-) as foo group by tot_cost order by tot_cost asc;
+) as foo group by agg_cost order by tot_cost asc;
 
 --- this ones are equivalent
-select tot_cost, count(*) from (
+select agg_cost, count(*) from (
     select * from pgr_drivingdistance('select id, source, target, 1 as cost from ddnoded2', 1274, 10, true)
-) as foo group by tot_cost order by tot_cost asc;
+) as foo group by agg_cost order by tot_cost asc;
 
-select tot_cost, count(*) from (
+select agg_cost, count(*) from (
     select * from pgr_drivingdistance('select id, source, target, 1 as cost from ddnoded2', 1274, 10)
-) as foo group by tot_cost order by tot_cost asc;
+) as foo group by agg_cost order by tot_cost asc;
