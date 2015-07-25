04d6cca68458644b26d9af1f43d75b9fd4b00899
mapas de vicky <vicky_vergara@hotmail.com>
2015-07-25 10:47:17 -0500

    documentation of v2.1.0 alpha

--- v2.1.0-alpha/src/tsp/src/CMakeFiles/tsp.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/tsp.dir/tsp2.c.o"
-  "CMakeFiles/tsp.dir/tsplib.c.o"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang C)
-  INCLUDE(CMakeFiles/tsp.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
--- v2.1.0-alpha/src/vrppdtw/src/CMakeFiles/vrppdtw.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/vrppdtw.dir/pdp.c.o"
-  "CMakeFiles/vrppdtw.dir/pdp_solver.cpp.o"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang C CXX)
-  INCLUDE(CMakeFiles/vrppdtw.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
