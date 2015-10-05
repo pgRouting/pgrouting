806e2884ad1b15370c3bd9a4952d08617fe7a2f4
vicky <vicky@georepublic.de>
2015-10-05 22:50:55 +0900

    back to square one

--- tools/travis/pgrouting_install.sh
+++ tools/travis/pgrouting_install.sh
@@ -16,17 +16,17 @@ ERROR=0
 # ------------------------------------------------------------------------------
 # Remove PostgreSQL and all its files
 # ------------------------------------------------------------------------------
-#sudo service postgresql stop
-#sudo apt-get remove postgresql libpq-dev libpq5 postgresql-client-common postgresql-common -qq --purge -y 
-#sudo rm -rf /var/lib/postgresql
+sudo service postgresql stop
+sudo apt-get remove postgresql libpq-dev libpq5 postgresql-client-common postgresql-common -qq --purge -y 
+sudo rm -rf /var/lib/postgresql
 
 # Add PPA's'
 # ------------------------------------------------------------------------------
-#sudo apt-add-repository -y ppa:georepublic/pgrouting-travis
+sudo apt-add-repository -y ppa:georepublic/pgrouting-travis
 
-#if [ "$POSTGIS_VERSION" == "2.0" ] || [ "$POSTGIS_VERSION" == "2.1" ]; then 
-#	sudo apt-add-repository -y ppa:ubuntugis/ppa
-#fi
+if [ "$POSTGIS_VERSION" == "2.0" ] || [ "$POSTGIS_VERSION" == "2.1" ]; then 
+	sudo apt-add-repository -y ppa:ubuntugis/ppa
+fi
 
 # Add PostgreSQL Apt repository
 # UPDATE: seems to be already available in Travis
@@ -39,22 +39,17 @@ sudo apt-get update -qq
 # ------------------------------------------------------------------------------
 # Install PostgreSQL (always install from PostgreSQL Apt repository)
 # ------------------------------------------------------------------------------
-#sudo apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confnew" install -y -qq postgresql-$POSTGRESQL_VERSION postgresql-contrib-$POSTGRESQL_VERSION postgresql-server-dev-$POSTGRESQL_VERSION
+sudo apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confnew" install -y -qq postgresql-$POSTGRESQL_VERSION postgresql-contrib-$POSTGRESQL_VERSION postgresql-server-dev-$POSTGRESQL_VERSION
 
 # Install packages
 # ------------------------------------------------------------------------------
 echo "Installing packages ... this may take some time."
-sudo apt-get install -y -qq cmake
-# checkinstall libcgal-dev libboost-graph-dev libboost-thread-dev libxml2-dev libproj-dev libjson0-dev xsltproc docbook-xsl docbook-mathml libgeos-dev libgdal1-dev 
-#sudo apt-get install -y -qq packaging-dev cmake checkinstall libcgal-dev libboost-graph-dev libboost-thread-dev libxml2-dev libproj-dev libjson0-dev xsltproc docbook-xsl docbook-mathml libgeos-dev libgdal1-dev 
+sudo apt-get install -y -qq packaging-dev cmake checkinstall libcgal-dev libboost-graph-dev libboost-thread-dev libxml2-dev libproj-dev libjson0-dev xsltproc docbook-xsl docbook-mathml libgeos-dev libgdal1-dev 
 
-#----------------------------------------
-#  POSTGIS
-#----------------------------------------
-#if [ "$POSTGIS_VERSION" == "1.5" ]; then 
-#	RELEASE="1.5.8"
-#	wget --quiet -O - http://download.osgeo.org/postgis/source/postgis-${RELEASE}.tar.gz | tar xzf -
-#fi
+if [ "$POSTGIS_VERSION" == "1.5" ]; then 
+	RELEASE="1.5.8"
+	wget --quiet -O - http://download.osgeo.org/postgis/source/postgis-${RELEASE}.tar.gz | tar xzf -
+fi
 
 if [ "$POSTGIS_VERSION" == "2.0" ]; then 
 	RELEASE="2.0.7"
@@ -74,14 +69,11 @@ cd postgis-*
 make
 sudo make install
 sudo ldconfig
-#----------------------------------------POSTGIS
-
-
 
 # Build extension for PostGIS > 2.0
-#if [ "$POSTGIS_VERSION" != "1.5" ]; then 
+if [ "$POSTGIS_VERSION" != "1.5" ]; then 
 	cd extensions && make && sudo make install
-#fi
+fi
 
 # ------------------------------------------------------------------------------
 # Restart once
