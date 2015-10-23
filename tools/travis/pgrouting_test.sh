b6b83fd62dfc5e1e56cedc9bf17aaab7031fb425
mapas de vicky <vicky_vergara@hotmail.com>
2015-10-23 13:06:13 -0500

    dealing with  tests on travis with postgres 9.2 and 9.1 matrix 3

--- tools/travis/pgrouting_test.sh
+++ tools/travis/pgrouting_test.sh
@@ -20,7 +20,7 @@ ERROR=0
 
 # Define alias function for psql command
 run_psql () {
-    PGOPTIONS='--client-min-messages=warning' psql -X -q -v ON_ERROR_STOP=1 --pset pager=off "$@"
+    PGOPTIONS='--client-min-messages=warning' psql -U $PGUSER  -X -q -v ON_ERROR_STOP=1 --pset pager=off "$@"
     if [ "$?" -ne 0 ]
     then 
         echo "Test query failed: $@"
@@ -31,23 +31,23 @@ run_psql () {
 # ------------------------------------------------------------------------------
 # CREATE DATABASE
 # ------------------------------------------------------------------------------
-export PGUSER
+#export PGUSER
 run_psql -l
 run_psql -c "CREATE DATABASE $PGDATABASE;"
-export PGDATABASE
+#export PGDATABASE
 
 # ------------------------------------------------------------------------------
 # CREATE EXTENSION
 # ------------------------------------------------------------------------------
-run_psql -c "CREATE EXTENSION postgis;"
-run_psql -c "CREATE EXTENSION pgrouting;"
+run_psql -d $PGDATABASE -c "CREATE EXTENSION postgis;"
+run_psql -d $PGDATABASE -c "CREATE EXTENSION pgrouting;"
 
 # ------------------------------------------------------------------------------
 # Get version information
 # ------------------------------------------------------------------------------
-run_psql -c "SELECT version();"    
-run_psql -c "SELECT postgis_full_version();"    
-run_psql -c "SELECT pgr_version();"
+run_psql -d $PGDATABASE -c "SELECT version();"    
+run_psql -d $PGDATABASE -c "SELECT postgis_full_version();"    
+run_psql -d $PGDATABASE -c "SELECT pgr_version();"
 
 PGROUTING_VERSION=`run_psql -A -t -c "SELECT version FROM pgr_version();"`
 
