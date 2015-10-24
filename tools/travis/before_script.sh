9dfbf16b8244ed19840331b6a510c9d9076c469d
mapas de vicky <vicky_vergara@hotmail.com>
2015-10-23 21:20:48 -0500

    dealing with  tests on travis with postgres 9.2 and 9.1 matrix 17

--- tools/travis/before_script.sh
+++ tools/travis/before_script.sh
@@ -9,19 +9,6 @@
 # exit script on error
 set -e 
 
-# build pgRouting
-
 psql --version
 psql -U postgres -c "SELECT version();"
 
-mkdir build
-cd build
-cmake -DCMAKE_CXX_COMPILER=g++-4.8 \
-      -DCMAKE_C_COMPILER=gcc-4.8 \
-      -DPOSTGRESQL_VERSION=${POSTGRESQL_VERSION} \
-      -DPOSTGIS_VERSION=${POSTGIS_VERSION} \
-      ..
-make
-sudo make install
-cd ..
-./tools/test-runner.pl -pgver $POSTGRESQL_VERSION -ignorenotice
