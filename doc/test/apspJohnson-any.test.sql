7a81b537a7adbb598ba930f7847020fcf28f119e
mapas de vicky <vicky_vergara@hotmail.com>
2015-11-10 18:38:57 -0600

    fixed a test of old apsp

--- doc/test/apspJohnson-any.test.sql
+++ doc/test/apspJohnson-any.test.sql
@@ -6,6 +6,6 @@ BEGIN;
 ------------------------------------------------------------------------------------------------------
 SELECT seq, id1 AS FROM, id2 AS to, cost
     FROM pgr_apspJohnson(
-        'SELECT source, target, cost FROM edge_table'
+        'SELECT source, target, cost FROM edge_table order by id'
     ) limit 10; 
 ROLLBACK;
