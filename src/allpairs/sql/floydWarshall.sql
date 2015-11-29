aa9d5df5523752a9df07da7d43f6fbccb0cb66cc
vicky vergara <vicky_vergara@hotmail.com>
2015-11-28 20:55:50 -0600

    updated johnson licenses

--- src/allpairs/sql/floydWarshall.sql
+++ src/allpairs/sql/floydWarshall.sql
@@ -1,11 +1,16 @@
-/*PGR
+/*PGR-GNU*****************************************************************
+
+File: floydWarshall.sql
 
 Template:
 Copyright (c) 2015 pgRouting developers
+Mail: project@pgrouting.org
 
 Function developer:
 Copyright (c) 2013 Vicky Vergara
 vicky_vergara@hotmail.com
+
+------
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -21,7 +26,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-*/
+********************************************************************PGR-GNU*/
 
 CREATE OR REPLACE FUNCTION pgr_floydWarshall(edges_sql TEXT, directed BOOLEAN DEFAULT TRUE, 
   OUT start_vid BIGINT, OUT end_vid BIGINT, OUT agg_cost float)
--- src/allpairs/sql/johnson.sql
+++ src/allpairs/sql/johnson.sql
@@ -1,12 +1,16 @@
-/*PGR
+/*PGR-GNU*****************************************************************
+
 File: johnson.sql
 
-Generated with Template by:
+Template:
 Copyright (c) 2015 pgRouting developers
+Mail: project@pgrouting.org
 
-Function's developer: 
-Copyright (c) 2015 Celia Virginia Vergara Castillo
+Function developer:
+Copyright (c) 2013 Vicky Vergara
+vicky_vergara@hotmail.com
 
+------
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -22,7 +26,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-*/
+********************************************************************PGR-GNU*/
 
 CREATE OR REPLACE FUNCTION pgr_johnson(edges_sql TEXT, directed BOOLEAN DEFAULT TRUE,
   OUT start_vid BIGINT, OUT end_vid BIGINT, OUT agg_cost float)
