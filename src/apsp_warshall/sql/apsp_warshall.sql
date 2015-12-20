8ae0726cc1fee3bdf8c7cd1c2a87fd8460842429
vicky vergara <vicky_vergara@hotmail.com>
2015-12-20 10:49:46 -0600

    apspwarshall: deleted unused code and data

--- src/apsp_warshall/sql/apsp_warshall.sql
+++
@@ -1,6 +0,0 @@
-
-
-CREATE OR REPLACE FUNCTION pgr_apspWarshall(sql text, directed boolean, has_reverse_cost boolean)
-    RETURNS SETOF pgr_costResult
-    AS '$libdir/${PGROUTING_LIBRARY_NAME}', 'apsp_warshall'
-    LANGUAGE 'c' IMMUTABLE STRICT;
--- src/apsp_warshall/test/apsp_warshall-any-00.test.sql
+++ src/apsp_warshall/test/apsp_warshall-any-00.test.sql
@@ -41,19 +41,19 @@ BEGIN;
 
     PREPARE q1 AS
     SELECT id1, id2, round(cost::numeric, 2) as cost
-    FROM pgr_apspWarshall('select id, source, target, cost from apspw', false, false);
+    FROM pgr_apspWarshall('select id, source, target, cost from edge_table', false, false);
 
     PREPARE q2 AS
     SELECT  id1, id2, round(cost::numeric, 2) as cost
-    FROM pgr_apspWarshall('select id, source, target, cost from apspw', TRUE, false);
+    FROM pgr_apspWarshall('select id, source, target, cost from edge_table', TRUE, false);
 
     PREPARE q11 AS
     SELECT start_vid::INTEGER AS id1, end_vid::INTEGER AS id2, round(agg_cost::numeric, 2) as cost
-    FROM pgr_floydWarshall('select id, source, target, cost from apspw', false);
+    FROM pgr_floydWarshall('select id, source, target, cost from edge_table', false);
 
     PREPARE q12 AS
     SELECT start_vid::INTEGER AS id1, end_vid::INTEGER AS id2, round(agg_cost::numeric, 2) as cost
-    FROM pgr_floydWarshall('select id, source, target, cost from apspw', TRUE);
+    FROM pgr_floydWarshall('select id, source, target, cost from edge_table', TRUE);
 
     SELECT set_eq('q1', 'q11','1: With undirected: Compare with pgr_floydWarshall -> must give the same results');
     SELECT set_eq('q2', 'q12','2: With directed: Compare with pgr_floydWarshall -> must give the same results');
