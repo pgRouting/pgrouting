87dbae81ac85d921f6bbe40119f9249edca24298
vicky vergara <vicky_vergara@hotmail.com>
2016-01-06 14:49:36 -0600

    installing pgtap manually 7

--- tools/travis/install_pgtap.sh
+++ tools/travis/install_pgtap.sh
@@ -30,6 +30,7 @@ sudo cp pgtap.control /usr/share/postgresql/$1/extension/pgtap.control
 sudo cp sql/pgtap--0.95.0.sql /usr/share/postgresql/$1/extension/pgtap--0.95.0.sql
 sudo cp sql/pgtap-core--0.95.0.sql  /usr/share/postgresql/$1/extension/pgtap-core--0.95.0.sql
 sudo cp sql/pgtap-schema--0.95.0.sql  /usr/share/postgresql/$1/extension/pgtap-schema--0.95.0.sql
+sudo updatedb
 locate pgtap
   
 
