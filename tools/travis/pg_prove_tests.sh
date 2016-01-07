b0bbebc8e131526651c3a7be6cbc498a72f04408
vicky vergara <vicky_vergara@hotmail.com>
2016-01-07 14:28:33 -0600

    travis: pg_prove 6

---
+++ tools/travis/pg_prove_tests.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+# ------------------------------------------------------------------------------
+# Travis CI scripts 
+# Copyright(c) pgRouting Contributors
+#
+# Test pgRouting
+# ------------------------------------------------------------------------------
+
+set -e 
+
+PGUSER="postgres"
+PGDATABASE="___pgr___test___"
+
+# Define alias function for psql command
+run_psql () {
+    PGOPTIONS='--client-min-messages=warning' psql -U $PGUSER  -d $PGDATABASE -X -q -v ON_ERROR_STOP=1 --pset pager=off "$@"
+    if [ "$?" -ne 0 ]
+    then 
+        echo "Test query failed: $@"
+        ERROR=1
+    fi 
+}
+
+
+echo "cd ./tools/testers/"
+cd ./tools/testers/
+echo "psql -f setup_db.sql"
+psql -f setup_db.sql
+echo "pg_prove ../../src/trsp/test/pgtap/*"
+pg_prove ../../src/trsp/test/pgtap/* -d $PGDATABASE
+
+if [ "$?" -ne 0 ]
+then
+    ERROR=1
+fi
+
+# Return success or failure
+# ------------------------------------------------------------------------------
+exit $ERROR
