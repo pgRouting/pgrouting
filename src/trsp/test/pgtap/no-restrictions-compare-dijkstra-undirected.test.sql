ba3cb83498d1ed318585537ef8737a9c0cdfb731
vicky vergara <vicky_vergara@hotmail.com>
2016-01-07 11:38:42 -0600

    added 4 tests to be handled with pg_prove

--- src/trsp/test/pgtap/no-restrictions-compare-dijkstra-undirected.test.sql
+++ src/trsp/test/pgtap/no-restrictions-compare-dijkstra-undirected.test.sql
@@ -1,50 +1,6 @@
-/*PGR-GNU*****************************************************************
 
-Copyright (c) 2015 pgRouting developers
-Mail: project@pgrouting.org
+\i setup.sql
 
-------
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
-********************************************************************PGR-GNU*/
-/*
-This test is for the equivalence pgr_trsp when:
-- no restrictions are given 
-
-With pgr_dijkstra one To One signature
-for both: directed versions
-
-*/
-
-\set ECHO none
-\set QUIET 1
--- Turn off echo and keep things quiet.
-
--- Format the output for nice TAP.
-\pset format unaligned
-\pset tuples_only true
-\pset pager
-\set VERBOSITY terse
-
--- Revert all changes on failure.
-\set ON_ERROR_ROLLBACK true
-\set ON_ERROR_STOP true
-\set QUIET 1
-
-BEGIN;
     SELECT plan(1224);
 
     UPDATE edge_table SET cost = cost + 0.001 * id * id, reverse_cost = reverse_cost + 0.001 * id * id;
@@ -85,7 +41,6 @@ BEGIN;
                     -- This should happen allways even when its being corrected
                     RETURN query SELECT set_eq(trsp_sql, dijkstra_sql, msg);
                     EXCEPTION WHEN OTHERS THEN
-                        msg := k || '-1 ' || directed || ', with reverse_cost, marked as being used: from '  || i || ' to ' || j;
                         RETURN query SELECT is_empty(dijkstra_sql, msg);
                 END;
 
@@ -133,8 +88,8 @@ BEGIN;
 $BODY$
 language plpgsql;
 
-SELECT * from foo(18, true);
---SELECT * from foo(18, false);
+--SELECT * from foo(18, true);
+SELECT * from foo(18, false);
 
 
 -- Finish the tests and clean up.
