a8c458de7ce8765f11cc3050ed278a2c291d8f9e
vicky vergara <vicky_vergara@hotmail.com>
2016-01-12 14:21:57 -0600

    trying to instal postgis on 9.4 ...1

--- src/driving_distance/test/drivingdistance-any-00.test.sql
+++ src/driving_distance/test/drivingdistance-any-00.test.sql
@@ -1,46 +1,68 @@
-/*PGR-GNU*****************************************************************
-
-Copyright (c) 2015 pgRouting developers
-Mail: project@pgrouting.org
-
-------
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
-********************************************************************PGR-GNU*/
 -- each ring will increas by 4 because it is on a square grid
 -- to find the start node number
 -- select *, st_distance(st_makepoint(25,25), the_geom) from vertices_tmp where st_dwithin(st_makepoint(25,25), the_geom, 1.0) order by st_distance(st_makepoint(25,25), the_geom) limit 1;
-set client_min_messages to NOTICE;
+BEGIN;
+
+    -- create 50x50 grid of overlapping lines horizontal and vertical
 
 
+    SET client_min_messages TO WARNING;
 
---this are equivalent
-select cost, count(*) from (
-    select * from pgr_drivingdistance('select id, source, target, 1.0::float8 as cost from ddnoded2', 1274, 10, false, false)
-) as foo group by cost order by cost asc;
+    create table ddunnoded (
+        id serial not null primary key
+    );
 
-select agg_cost, count(*) from (
-    select * from pgr_drivingdistance('select id, source, target, 1 as cost from ddnoded2', 1274, 10, false)
-) as foo group by agg_cost order by agg_cost asc;
+    SELECT  addgeometrycolumn('ddunnoded', 'the_geom', ST_SRID('POINT(0 0)'::geometry), 'LINESTRING', 2);
 
---- this ones are equivalent
-select agg_cost, count(*) from (
-    select * from pgr_drivingdistance('select id, source, target, 1 as cost from ddnoded2', 1274, 10, true)
-) as foo group by agg_cost order by agg_cost asc;
+    INSERT into ddunnoded (the_geom)
+    select st_astext(st_makeline(st_makepoint(0,s1), st_makepoint(51,s1))) from (select generate_series(1,50) as s1) as foo
+    union all
+    select st_astext(st_makeline(st_makepoint(s1,0), st_makepoint(s1,51))) from (select generate_series(1,50) as s1) as foo;
 
-select agg_cost, count(*) from (
-    select * from pgr_drivingdistance('select id, source, target, 1 as cost from ddnoded2', 1274, 10)
-) as foo group by agg_cost order by agg_cost asc;
+    -- node the grid so we can use it
+    select pgr_nodenetwork('ddunnoded',0.000001);
+
+    -- copy the noded table into a table we can use for a graph
+    -- and add the required columns
+
+
+    create table ddnoded2 (
+        gid serial not null primary key,
+        id integer,
+        source integer,
+        target integer,
+        cost float8
+    );
+
+    select addgeometrycolumn('ddnoded2', 'the_geom', ST_SRID('POINT(0 0)'::geometry), 'LINESTRING', 2);
+
+    insert into ddnoded2 (id, cost, the_geom)
+    select a.id, 1.0::float8 as cost, a.the_geom
+    from ddunnoded_noded a order by id, sub_id;
+
+    -- now create a topology
+    select pgr_createtopology('ddnoded2', 0.000001, id:='gid');
+
+    -- Total query runtime: 8080 ms.
+
+
+    SET client_min_messages TO NOTICE;
+    --this are equivalent
+    select cost, count(*) from (
+        select * from pgr_drivingdistance('select id, source, target, 1.0::float8 as cost from ddnoded2', 1274, 10, false, false)
+    ) as foo group by cost order by cost asc;
+
+    select agg_cost, count(*) from (
+        select * from pgr_drivingdistance('select id, source, target, 1 as cost from ddnoded2', 1274, 10, false)
+    ) as foo group by agg_cost order by agg_cost asc;
+
+    --- this ones are equivalent
+    select agg_cost, count(*) from (
+        select * from pgr_drivingdistance('select id, source, target, 1 as cost from ddnoded2', 1274, 10, true)
+    ) as foo group by agg_cost order by agg_cost asc;
+
+    select agg_cost, count(*) from (
+        select * from pgr_drivingdistance('select id, source, target, 1 as cost from ddnoded2', 1274, 10)
+    ) as foo group by agg_cost order by agg_cost asc;
+
+    ROLLBACK;
