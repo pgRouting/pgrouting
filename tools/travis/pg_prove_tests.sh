3218ee2ffa07bafc8a1a507eac3c597cb2357ac6
vicky vergara <vicky_vergara@hotmail.com>
2016-01-20 14:21:53 -0600

    added pgTap tests to check that correct types are used in allPairs family (new & old)

--- tools/travis/pg_prove_tests.sh
+++ tools/travis/pg_prove_tests.sh
@@ -31,8 +31,8 @@ run_psql -f setup_db.sql
 
 #pg_prove ../../src/trsp/test/pgtap/* -d $PGDATABASE
 pg_prove ../../src/allpairs/test/pgtap/* -d $PGDATABASE
-#pg_prove ../../src/apsp_johnson/test/pgtap/* -d $PGDATABASE
-#pg_prove ../../src/apsp_warshall/test/pgtap/* -d $PGDATABASE
+pg_prove ../../src/apsp_johnson/test/pgtap/* -d $PGDATABASE
+pg_prove ../../src/apsp_warshall/test/pgtap/* -d $PGDATABASE
 #pg_prove ../../src/dijkstra/test/pgtap/* -d $PGDATABASE
 #pg_prove ../../src/kdijkstra/test/pgtap/* -d $PGDATABASE
 #pg_prove ../../src/ksp/test/pgtap/* -d $PGDATABASE
