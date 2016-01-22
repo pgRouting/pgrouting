3fc081c205100c97d4b22ef8ad5ad4debaaecd67
vicky vergara <vicky_vergara@hotmail.com>
2016-01-22 15:30:02 -0600

    finished making a pgTap test for bdDijkstra and documentation query

--- tools/travis/pg_prove_tests.sh
+++ tools/travis/pg_prove_tests.sh
@@ -41,7 +41,8 @@ run_psql -f setup_db.sql
 #pg_prove ../../src/trsp/test/pgtap/* -d $PGDATABASE
 
 #pg_prove ../../src/astar/test/pgtap/* -d $PGDATABASE
-pg_prove ../../src/bd_astar/test/pgtap/* -d $PGDATABASE
+#pg_prove ../../src/bd_astar/test/pgtap/* -d $PGDATABASE
+pg_prove ../../src/bd_dijkstra/test/pgtap/* -d $PGDATABASE
 
 if [ "$?" -ne 0 ]
 then
