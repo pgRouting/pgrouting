46d06a0a1d25a291faf1c306c58f21751937b562
rohith <rohithrockzzz@gmail.com>
2016-01-25 00:04:27 +0530

    Fixed errors part-2

--- src/contraction/sql/contraction.sql
+++ src/contraction/sql/contraction.sql
@@ -1,5 +1,16 @@
 
-CREATE OR REPLACE FUNCTION contract_graph(edges_sql text, level bigint)
-  RETURNS SETOF RECORD AS
- '$libdir/libmyrouting_library', 'contract_graph'
-    LANGUAGE c IMMUTABLE STRICT;
+DROP TYPE IF EXISTS pgr_contracted_blob;
+ CREATE TYPE pgr_contracted_blob AS
+ (
+ 	contracted_graph_name text,
+ 	contracted_graph_blob text,
+ 	removedVertices text,
+ 	removedEdges text,
+ 	psuedoEdges text
+ );
+ 
+CREATE OR REPLACE FUNCTION pgr_contractgraph(query text,level bigint,
+	has_rcost boolean)
+RETURNS SETOF pgr_contracted_blob AS 
+'$libdir/${PGROUTING_LIBRARY_NAME}', 'pgr_contractgraph' 
+LANGUAGE c IMMUTABLE STRICT;
--- src/contraction/sql/mydijkstra.sql
+++ src/contraction/sql/mydijkstra.sql
@@ -1,5 +1,7 @@
 
-CREATE OR REPLACE FUNCTION my_dijkstra(edges_sql text, start_vid bigint, end_vid bigint)
-  RETURNS SETOF RECORD AS
- '$libdir/libmyrouting_library', 'shortest_path'
+
+CREATE OR REPLACE FUNCTION pgr_contracted_dijkstra(query text,start_vid bigint,
+	end_vid bigint,has_rcost boolean)
+RETURNS SETOF pathElement AS
+'$libdir/${PGROUTING_LIBRARY_NAME}', 'shortest_path_c'
     LANGUAGE c IMMUTABLE STRICT;
