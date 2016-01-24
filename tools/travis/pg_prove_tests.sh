6996e9134544584b63506cfe02000d01b65bf8e0
vicky vergara <vicky_vergara@hotmail.com>
2016-01-24 08:35:05 -0600

    saving changes

--- tools/travis/pg_prove_tests.sh
+++ tools/travis/pg_prove_tests.sh
@@ -35,7 +35,7 @@ run_psql -f setup_db.sql
 #pg_prove ../../src/ksp/test/pgtap/* -d $PGDATABASE
 #pg_prove ../../src/topology/test/pgtap/* -d $PGDATABASE
 #pg_prove ../../src/common/test/pgtap/* -d $PGDATABASE
-#pg_prove ../../src/dijkstra/test/pgtap/* -d $PGDATABASE
+pg_prove ../../src/dijkstra/test/pgtap/* -d $PGDATABASE
 #pg_prove ../../src/kdijkstra/test/pgtap/* -d $PGDATABASE
 #pg_prove ../../src/withPoints/test/pgtap/* -d $PGDATABASE
 #pg_prove ../../src/trsp/test/pgtap/* -d $PGDATABASE
@@ -43,7 +43,7 @@ run_psql -f setup_db.sql
 #pg_prove ../../src/astar/test/pgtap/* -d $PGDATABASE
 #pg_prove ../../src/bd_astar/test/pgtap/* -d $PGDATABASE
 #pg_prove ../../src/bd_dijkstra/test/pgtap/* -d $PGDATABASE
-pg_prove ../../src/convinience/test/pgtap/* -d $PGDATABASE
+#pg_prove ../../src/convinience/test/pgtap/* -d $PGDATABASE
 
 if [ "$?" -ne 0 ]
 then
