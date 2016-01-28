4b9f75d69ca3a800dd825c0a2f09670daf7e6287
vicky vergara <vicky_vergara@hotmail.com>
2016-01-28 11:48:30 -0600

    tyring ksp again 10

--- src/ksp/src/pgr_ksp.cpp
+++ src/ksp/src/pgr_ksp.cpp
@@ -21,13 +21,6 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 ********************************************************************PGR-GNU*/
-#ifdef __MINGW32__
-#include <winsock2.h>
-#include <windows.h>
-#ifdef unlink
-#undef unlink(x)
-#endif
-#endif
 
 #include <deque>
 #include <set>
--- src/ksp/src/withPoints_ksp_driver.cpp
+++ src/ksp/src/withPoints_ksp_driver.cpp
@@ -31,9 +31,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #ifdef __MINGW32__
 #include <winsock2.h>
 #include <windows.h>
-#ifdef unlink
-#undef unlink(x)
-#endif
 #endif
 
 #if 0
