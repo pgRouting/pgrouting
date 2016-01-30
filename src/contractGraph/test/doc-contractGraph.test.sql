6943dcfaa61bd7aac4e8a76b455b560039e1c399
rohith <rohithrockzzz@gmail.com>
2016-01-31 00:08:02 +0530

    Created a directory using template

---
+++ src/contractGraph/test/doc-contractGraph.test.sql
@@ -0,0 +1,13 @@
+
+
+\echo --q1
+SELECT * FROM pgr_contractGraph(
+    'SELECT id, source, target, cost, reverse_cost FROM edge_table', 
+    2, ARRAY[5, 3]);
+\echo --q2
+
+SELECT * FROM pgr_contractGraph(
+    'SELECT id, source, target, cost, reverse_cost FROM edge_table', 
+    2, ARRAY[5, 3]);
+\echo --q3
+
