361972e83e793d4e79fc74691a481643e1db930d
cvvergara <vicky_vergara@hotmail.com>
2016-02-04 09:43:38 -0600

    trying to install g++-4.8 travis precise

--- src/withPoints/test/doc-pgr_withPointsDD.test.sql
+++ src/withPoints/test/doc-pgr_withPointsDD.test.sql
@@ -5,26 +5,26 @@ BEGIN;
     SELECT * FROM pgr_withPointsDD(
         'SELECT id, source, target, cost, reverse_cost FROM edge_table ORDER BY id',
         'SELECT pid, edge_id, fraction, side from pointsOfInterest',
-        1, 4.0);
+        -1, 3.8);
     \echo --q2
     SELECT * FROM pgr_withPointsDD(
         'SELECT id, source, target, cost, reverse_cost FROM edge_table ORDER BY id',
         'SELECT pid, edge_id, fraction, side from pointsOfInterest',
-        1, 4.0,
+        -1, 3.8,
         driving_side := 'r',
         details := true);
     \echo --q3
     SELECT * FROM pgr_withPointsDD(
         'SELECT id, source, target, cost, reverse_cost FROM edge_table ORDER BY id',
         'SELECT pid, edge_id, fraction, side from pointsOfInterest',
-        1, 4.0,
+        -1, 3.8,
         driving_side := 'l',
         details := true);
     \echo --q4
     SELECT * FROM pgr_withPointsDD(
         'SELECT id, source, target, cost, reverse_cost FROM edge_table ORDER BY id',
         'SELECT pid, edge_id, fraction, side from pointsOfInterest',
-        1, 4.0,
+        -1, 3.8,
         driving_side := 'b',
         details := true);
     \echo --q5
