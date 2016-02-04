8e47be83d540bb36e7b08d23908b0b7a3e715492
rohith <rohithrockzzz@gmail.com>
2016-02-04 23:26:05 +0530

    Cleanup includes part 1

--- src/contraction/src/contractGraph_driver.cpp
+++ src/contraction/src/contractGraph_driver.cpp
@@ -37,8 +37,8 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #include <sstream>
 #include <deque>
 #include <vector>
-#include "./pgr_dijkstra.hpp"
 #include "./contractGraph_driver.h"
+#include "./contract_function.h"
 
 // #define DEBUG
 
