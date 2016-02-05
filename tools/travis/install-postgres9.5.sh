2bf48b4beb513502b10a1f2efe5d9b22abe5a778
cvvergara <vicky_vergara@hotmail.com>
2016-02-05 14:28:33 -0600

    travis: deleting double bracket

--- tools/travis/install-postgres9.5.sh
+++ tools/travis/install-postgres9.5.sh
@@ -8,7 +8,7 @@
 
 POSTGRESQL_VERSION="$1"
 
-if [[ "$POSTGRESQL_VERSION" == "9.5" ]] ; then
+if [ "$POSTGRESQL_VERSION" == "9.5" ] ; then
 
     sudo apt-get install -y postgresql-9.5
     sudo apt-get install -y postgresql-9.5-postgis
