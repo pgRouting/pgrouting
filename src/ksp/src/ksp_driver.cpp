223864fb48510520293f8587a7c6a467913de697
cvvergara <vicky_vergara@hotmail.com>
2016-02-04 19:01:15 -0600

    Jenkins: undefining unlink in ksp_driver

--- src/ksp/src/ksp_driver.cpp
+++ src/ksp/src/ksp_driver.cpp
@@ -25,13 +25,12 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #ifdef __MINGW32__
 #include <winsock2.h>
 #include <windows.h>
-//#ifdef unlink
-//#undef unlink
-//#endif
+#ifdef unlink
+#undef unlink
+#endif
 #endif
 
 
-#include <unistd.h>
 #include <deque>
 #include <sstream>
 
