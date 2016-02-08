08129e0cd03a93b3f28dfdb46c2119f8656f2057
cvvergara <vicky_vergara@hotmail.com>
2016-02-08 14:54:03 -0600

    Jenkins: adding wrapper of winsock for vrpp classes

--- src/vrppdtw/src/Route.h
+++ src/vrppdtw/src/Route.h
@@ -23,6 +23,11 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 
 #pragma once
+#ifdef __MINGW32__
+#include <winsock2.h>
+#include <windows.h>
+#endif
+
 
 #include <vector>
 #include <algorithm>
