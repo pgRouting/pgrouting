500fbf7156272762e3234e27c68f11d9037936d5
cvvergara <vicky_vergara@hotmail.com>
2016-02-10 09:43:05 -0600

    travis: trying to install 9.5

--- tools/travis/install-postgres9.5.sh
+++ tools/travis/install-postgres9.5.sh
@@ -16,27 +16,28 @@ echo "User $PGUSER"
 
 if test "$POSTGRESQL_VERSION" = "9.5" ; then
 
-    echo "Installing postgresql 9.5 & postgis for 9.5 "
+    echo "Installing postgresql 9.5 & postgis for 9.5 pgtap & pg_prove"
     sudo apt-get install -y postgresql-9.5 postgresql-9.5-postgis pgtap libtap-parser-sourcehandler-pgtap-perl
+    #sudo apt-get install -y pgtap libtap-parser-sourcehandler-pgtap-perl
     sudo cp /usr/lib/postgresql/$POSTGRESQL_VERSION/bin/pg_config /usr/bin/pg_config
 
-else
+#else
 
-echo "starting server"
-sudo /etc/init.d/postgresql start $POSTGRESQL_VERSION
+#echo "starting server"
+#sudo /etc/init.d/postgresql start $POSTGRESQL_VERSION
 
 
-sudo cp /usr/lib/postgresql/$POSTGRESQL_VERSION/bin/pg_config /usr/bin/pg_config
+#sudo cp /usr/lib/postgresql/$POSTGRESQL_VERSION/bin/pg_config /usr/bin/pg_config
 
-echo "Installing pgtap ... this may take some time."
-wget https://github.com/theory/pgtap/archive/master.zip
-unzip master.zip
-cd pgtap-master
-make
-make installcheck
-sudo make install
-cd ..
+#echo "Installing pgtap ... this may take some time."
+#wget https://github.com/theory/pgtap/archive/master.zip
+#unzip master.zip
+#cd pgtap-master
+#make
+#make installcheck
+#sudo make install
+#cd ..
 
-sudo apt-get install -y libtap-parser-sourcehandler-pgtap-perl
+#sudo apt-get install -y libtap-parser-sourcehandler-pgtap-perl
 
 fi
