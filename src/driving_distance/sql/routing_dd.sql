fad3debbc2ac6485a392694b55f7a905b333f3a8
Omar <omar.pessoa@gmail.com>
2016-02-22 11:58:20 -0300

    Update routing_dd.sql
    Missing alpha argument on last update.

--- src/driving_distance/sql/routing_dd.sql
+++ src/driving_distance/sql/routing_dd.sql
@@ -149,7 +149,7 @@ CREATE OR REPLACE FUNCTION pgr_pointsAsPolygon(query varchar, alpha float8 DEFAU
 		geoms := array[]::geometry[];
 		i := 1;
 
-		FOR vertex_result IN EXECUTE 'SELECT x, y FROM pgr_alphashape($1) ' USING query
+		FOR vertex_result IN EXECUTE 'SELECT x, y FROM pgr_alphashape($1,$2) ' USING query, alpha
 		LOOP
 			x[i] = vertex_result.x;
 			y[i] = vertex_result.y;
