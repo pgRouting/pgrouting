e2e7a31fc6e86d987a42da83ee07d7ccedcd9f65
rohith <rohithreddy2219@gmail.com>
2016-03-03 01:00:08 +0530

    Added new classes for contraction part-2

--- src/contraction/src/contraction_structs.hpp
+++ src/contraction/src/contraction_structs.hpp
@@ -13,15 +13,18 @@ enum class edgeType {
 
 class Vertex_c {
 	
+public:
 	int64_t id;
 	contractionType type;
 	std::deque<Vertex_c> removed_vertices;
 	void add_contracted_vertex (Vertex_c v);
 	void set_contraction_type (contractionType ctype);
-	friend std::ostringstream& operator <<(std::ostringstream &os,const Vertex_c& v);	
+	friend std::ostringstream& operator <<(std::ostringstream& os,const Vertex_c& v);	
 };
 
 class Edge_c {
+
+public:
 	int64_t id;
 	edgeType type;
 	int64_t source;
@@ -30,7 +33,7 @@ class Edge_c {
 	std::deque<Vertex_c> removed_vertices;
 	void add_contracted_vertex (Vertex_c v);
 	void set_contraction_type (edgeType etype);
-	friend std::ostringstream& operator << (std::ostringstream &os,const Edge_c& e);
+	friend std::ostringstream& operator <<(std::ostringstream& os,const Edge_c& e);
 };
 
 void Vertex_c::add_contracted_vertex (Vertex_c v) {
@@ -43,11 +46,11 @@ void Vertex_c::set_contraction_type (contractionType ctype) {
 	type = ctype;
 }
 
-std::ostringstream& operator <<(std::ostringstream &os,const Vertex_c& v)
+std::ostringstream& operator <<(std::ostringstream& os,const Vertex_c& v)
 {
 	os << "{ id: " << v.id << ", type: " << v.type ;
 	os << " removed_vertices: {";
-	for (auto const& v.removed_vertices : removed_vertex )
+	for (auto removed_vertex : v.removed_vertices )
 	{
 		os << removed_vertex << ", ";
 	}
@@ -67,11 +70,11 @@ void Edge_c::set_contraction_type (edgeType etype) {
 	type = etype;
 }
 
-std::ostringstream& operator <<(std::ostringstream &os,const Edge_c& e)
+std::ostringstream& operator <<(std::ostringstream& os,const Edge_c& e)
 {
 	os << "{ id: " << e.id << ", type: " << e.type ;
 	os << " removed_vertices: {";
-	for (auto const& e.removed_vertices : removed_vertex )
+	for (auto removed_vertex : e.removed_vertices )
 	{
 		os << removed_vertex << ", ";
 	}
--- src/contraction/src/pgr_contract.hpp
+++ src/contraction/src/pgr_contract.hpp
@@ -32,7 +32,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #include <iostream>
 #include <sstream>
 #include <deque>
-
+#include <string> 
 extern "C" {
 #include "postgres.h"
 }
