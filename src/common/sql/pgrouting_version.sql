2cf93ddec03a548f5ce0bdd625cc7976bfbb3036
cvvergara <vicky_vergara@hotmail.com>
2016-03-04 09:45:48 -0600

    fixed 2.1 signature for the updates

--- src/common/sql/pgrouting_version.sql
+++ src/common/sql/pgrouting_version.sql
@@ -1,39 +1,15 @@
-/*PGR-MIT*****************************************************************
-
--- pgrouting_version.sql
+-- -------------------------------------------------------------------
+-- pgrouting_utilities.sql
 -- AuthorL Stephen Woodbridge <woodbri@imaptools.com>
 -- Copyright 2013 Stephen Woodbridge
-
-------
-MIT/X license
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-
-********************************************************************PGR-MIT*/
+-- This file is release unde an MIT-X license.
+-- -------------------------------------------------------------------
 
 CREATE OR REPLACE FUNCTION pgr_version()
 RETURNS TABLE(
 		"version" varchar, 
---		tag varchar,
---		build varchar,
+		tag varchar,
+		--build varchar,
 		hash varchar,
 		branch varchar,
 		boost varchar
@@ -50,9 +26,9 @@ $BODY$
 DECLARE
 
 BEGIN
-    RETURN QUERY SELECT '${PGROUTING_GIT_TAG}'::varchar AS version, 
---			  '${PGROUTING_GIT_TAG}'::varchar AS tag, 
---                        '${PGROUTING_GIT_BUILD}'::varchar AS build, 
+    RETURN QUERY SELECT '${PGROUTING_VERSION_STRING}'::varchar AS version, 
+    					'${PGROUTING_GIT_TAG}'::varchar AS tag, 
+                        --'${PGROUTING_GIT_BUILD}'::varchar AS build, 
                         '${PGROUTING_GIT_HASH}'::varchar AS hash, 
                         '${PGROUTING_GIT_BRANCH}'::varchar AS branch, 
                         '${Boost_MAJOR_VERSION}.${Boost_MINOR_VERSION}.${Boost_SUBMINOR_VERSION}'::varchar AS boost;
