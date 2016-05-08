e92e41f94104ad1a9b5f96e50b0e848ba4a17559
cvvergara <vicky_vergara@hotmail.com>
2016-05-07 21:22:55 -0500

    moving new astar files to correct directory

--- src/astar/test/doc-astar.test.sql
+++ src/astar/test/doc-astar.test.sql
@@ -5,12 +5,12 @@ BEGIN;
 ------------------------------------------------------------------------------------------------------
 ------------------------------------------------------------------------------------------------------
 \echo --q1
-SELECT * FROM pgr_AStar(
+SELECT * FROM pgr_astar(
     'SELECT id::INTEGER, source::INTEGER, target::INTEGER, cost, x1, y1, x2, y2 FROM edge_table',
     4, 1, false, false);
 \echo --q2
-SELECT * FROM pgr_AStar(
-    'SELECT id::INTEGER, source::INTEGER, target::INTEGER, cost, x1, y1, x2, y2, reverse_cost FROM edge_table ',
+SELECT * FROM pgr_astar(
+    'SELECT id::INTEGER, source::INTEGER, target::INTEGER, cost, reverse_cost, x1, y1, x2, y2 FROM edge_table',
     4, 1, true, true);
 \echo --q3
 ROLLBACK;
