2e021cf8b65b455a2e097fc5ae028d9908602f15
Rohith Reddy <rohithreddy2219@gmail.com>
2016-06-04 22:58:56 +0530

    Added 2 unit tests and another test for sample data

--- src/contraction/test/development.test.sql
+++ src/contraction/test/development.test.sql
@@ -2,8 +2,11 @@
 SELECT * FROM pgr_contractGraph(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table WHERE id = 1',
     ARRAY[2]::BIGINT[], ARRAY[0]::integer[], 1, true);
-
 \echo --q2 Checking for two edges
 SELECT * FROM pgr_contractGraph(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table WHERE id < 3',
+    ARRAY[2]::BIGINT[], ARRAY[0]::integer[], 1, true);
+\echo --q3 Checking for sample data
+SELECT * FROM pgr_contractGraph(
+    'SELECT id, source, target, cost, reverse_cost FROM edge_table',
     ARRAY[2]::BIGINT[], ARRAY[0]::integer[], 1, true);
