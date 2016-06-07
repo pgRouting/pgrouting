5dec45fb1ee7118c4fcef38d20f7e7e7b823fdae
Rohith Reddy <rohithreddy2219@gmail.com>
2016-06-07 20:18:05 +0530

    Removed debug definition

--- src/contraction/src/contractGraph.c
+++ src/contraction/src/contractGraph.c
@@ -44,7 +44,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
 
-#define DEBUG
+// #define DEBUG
 #include "fmgr.h"
 #include "./../../common/src/debug_macro.h"
 #include "./../../common/src/pgr_types.h"
