5b544e42a76c1069ea35b402b9946400205435e3
Rohith Reddy <rohithreddy2219@gmail.com>
2016-06-07 20:17:35 +0530

    Added a test for contraction on a graph with no dead end vertices

--- src/contraction/test/development.test.sql
+++ src/contraction/test/development.test.sql
@@ -5,12 +5,17 @@ SELECT * FROM pgr_contractGraph(
 \echo --q2 Checking for two edges
 SELECT * FROM pgr_contractGraph(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table WHERE id < 3',
-    ARRAY[2]::BIGINT[], ARRAY[0]::integer[], 1, true);
+    ARRAY[]::BIGINT[], ARRAY[0]::integer[], 1, true);
 \echo --q3 Checking for sample data
 SELECT * FROM pgr_contractGraph(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table',
-    ARRAY[2]::BIGINT[], ARRAY[0]::integer[], 1, true);
+    ARRAY[]::BIGINT[], ARRAY[0]::integer[], 1, true);
 \echo --q4 Checking that forbidden vertices can only be one dimensional or empty
 SELECT * FROM pgr_contractGraph(
 	'SELECT id, source, target, cost, reverse_cost FROM edge_table WHERE id = 1',
 	ARRAY[ [2,3,4,5], [4,5,6,7] ]::BIGINT[][], ARRAY[0]::integer[], 1, true);
+\echo --q5 Checking contraction for a graph with no dead end vertex
+SELECT * FROM pgr_contractGraph(
+	'SELECT id, source, target, cost, reverse_cost FROM edge_table 
+	WHERE id = 2 OR id = 4 OR id = 5 OR id = 8',
+	ARRAY[]::BIGINT[], ARRAY[0]::integer[], 1, true);
