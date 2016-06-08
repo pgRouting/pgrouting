7997fbf63a549e68b7e8ec230c01dbb0735951c5
Rohith Reddy <rohithreddy2219@gmail.com>
2016-06-08 12:59:32 +0530

    Added a function that checks for a linear vertex

--- src/contraction/src/pgr_linearContraction.hpp
+++ src/contraction/src/pgr_linearContraction.hpp
@@ -50,6 +50,7 @@ namespace pgRouting {
 				typedef typename G::E_i E_i;
 				typedef typename G::EO_i EO_i;
 				typedef typename G::EI_i EI_i;
+				typedef typename G::degree_size_type degree_size_type;
 				void setForbiddenVertices(G &graph,
 						Identifiers<int64_t> forbidden_vertices,
 						std::ostringstream& debug);
@@ -93,3 +94,75 @@ namespace pgRouting {
 			}
 
 		}
+template < class G >
+		bool Pgr_linearContraction<G>::is_linear(G &graph, V v,
+				std::ostringstream& debug) {
+
+			debug << "Is linear: " << graph.graph[v].id << "?\n";
+			//debug << "in_degree: " << graph.in_degree(vertex_id) << '\n';
+			//debug << "out_degree: " << graph.out_degree(vertex_id) << '\n';
+			degree_size_type in_degree, out_degree;
+			in_degree = graph.in_degree(v);
+			out_degree = graph.out_degree(v);
+			//case 1
+			if(in_degree == 1 && out_degree == 1) {
+
+				debug << "Case 1\n"; 
+				return true;
+			}
+			
+			if(out_degree == 2 || in_degree == 2) {
+				int64_t incoming_eids[2] = {-1, -1};
+				int64_t outgoing_eids[2] = {-1, -1};
+				int incoming_count, outgoing_count;
+				incoming_count = outgoing_count = 0;
+				EO_i out, out_end;
+				EI_i in, in_end;
+				// Storing the incoming and outgoing edge ids in arrays
+				for (boost::tie(out, out_end) = out_edges(v, graph.graph);
+						out != out_end; ++out) {
+					outgoing_eids[outgoing_count++] = graph.graph[*out].id;
+				}
+				for (boost::tie(in, in_end) = in_edges(v, graph.graph);
+						in != in_end; ++in) {
+					incoming_eids[incoming_count++] = graph.graph[*in].id;
+				}
+				pgassert(in_degree == incoming_count);
+				pgassert(out_degree == outgoing_count);
+				// case 2
+				if (out_degree == 2 && in_degree == 2) {
+					debug << "Case 2\n";
+					if(incoming_eids[0] == outgoing_eids[0] &&
+						incoming_eids[0] == outgoing_eids[1])
+						return true;
+					if(incoming_eids[0] == outgoing_eids[1] &&
+						incoming_eids[1] == outgoing_eids[0])
+						return true;
+
+				}
+
+				// case 3
+				if (out_degree == 1 && in_degree == 2) {
+					debug << "Case 3\n";
+					if(outgoing_eids[0] == incoming_eids[0] ||
+						outgoing_eids[0] == incoming_eids[1])
+						return true;
+				}
+
+				// case 4
+				if (out_degree == 2 && in_degree == 1) {
+					debug << "Case 4\n";
+					if(incoming_eids[0] == outgoing_eids[0] ||
+						incoming_eids[0] == outgoing_eids[1])
+						return true;	
+				}
+				
+				debug << "No\n"; 
+				return false;
+			}
+
+
+			debug << "No\n"; 
+			return false;
+		}
+
