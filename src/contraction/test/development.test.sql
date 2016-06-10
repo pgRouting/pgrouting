1b5d560a724240e5c81a10e8195a67cbfadf17d5
Rohith Reddy <rohithreddy2219@gmail.com>
2016-06-10 10:07:37 +0530

    Added a test for linear contraction followed by dead contraction on sample data

--- src/contraction/test/development.test.sql
+++ src/contraction/test/development.test.sql
@@ -78,3 +78,9 @@ SELECT * FROM pgr_contractGraph(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table' ,
     ARRAY[]::BIGINT[], ARRAY[0, 1]::integer[], 1, true);
 -- \echo --q10 -------------------------------------------
+
+\echo --q12 Checking linear contraction, dead end for sample data
+SELECT * FROM pgr_contractGraph(
+    'SELECT id, source, target, cost, reverse_cost FROM edge_table' ,
+    ARRAY[]::BIGINT[], ARRAY[1, 0]::integer[], 1, true);
+-- \echo --q10 -------------------------------------------
