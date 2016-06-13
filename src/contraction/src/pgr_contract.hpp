a64a5ebb23d6a7de41bc889248bb1b3b2d70b71e
Rohith Reddy <rohithreddy2219@gmail.com>
2016-06-14 00:06:49 +0530

    Updated code that performs linear contraction

--- src/contraction/src/pgr_contract.hpp
+++ src/contraction/src/pgr_contract.hpp
@@ -73,14 +73,13 @@ void perform_deadEnd(G &graph,
 
 template < class G >
 void perform_linear(G &graph,
-    Identifiers<int64_t> forbidden_vertices,
+    Identifiers<int64_t>& forbidden_vertices,
     std::ostringstream& debug)
 {
     pgRouting::Pgr_linearContraction<G> linearContractor;
     debug << "Setting forbidden_vertices";
     linearContractor.setForbiddenVertices(graph, forbidden_vertices
         , debug);
-
     linearContractor.calculateVertices(graph, debug);
     try
     {
