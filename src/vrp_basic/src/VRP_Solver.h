ce811a030cffc937e4dd608f7c362ee3f94aa9ec
J Smith <dark.panda@gmail.com>
2016-06-14 16:56:12 -0400

    Include stdlib.h for srand() and rand() to fix build failure

--- src/vrp_basic/src/VRP_Solver.h
+++ src/vrp_basic/src/VRP_Solver.h
@@ -29,6 +29,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #include <utility>
 #include <string>
 #include <math.h>
+#include <stdlib.h>
 
 #define MAXIMUM_TRY 15
 #define TOTAL_NUMBER_OF_SEARCH 15
