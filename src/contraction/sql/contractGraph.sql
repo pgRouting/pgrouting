06509c28405a9a96c1bdba8bac4c90b1e6342c8b
Rohith Reddy <rohithreddy2219@gmail.com>
2016-06-22 13:21:41 +0530

    Updated argument types

--- src/contraction/sql/contractGraph.sql
+++ src/contraction/sql/contractGraph.sql
@@ -29,9 +29,9 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 CREATE OR REPLACE FUNCTION pgr_contractGraph(
     edges_sql TEXT,
-    forbidden_vertices BIGINT[],
+    forbidden_vertices ANYARRAY,
     contraction_order ANYARRAY,
-    max_cycles BIGINT,
+    max_cycles integer,
     directed BOOLEAN DEFAULT true,
     OUT seq integer,
     OUT id BIGINT,
