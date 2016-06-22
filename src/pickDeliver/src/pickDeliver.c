332925a6c5e0d5eaafa867e7e05254d807e703dd
cvvergara <vicky_vergara@hotmail.com>
2016-06-21 22:46:52 -0500

    recovering deleted line

--- src/pickDeliver/src/pickDeliver.c
+++ src/pickDeliver/src/pickDeliver.c
@@ -50,6 +50,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #include "./pickDeliver_driver.h"
 
 PG_FUNCTION_INFO_V1(pickDeliver);
+PGDLLEXPORT Datum
 pickDeliver(PG_FUNCTION_ARGS);
 
 
