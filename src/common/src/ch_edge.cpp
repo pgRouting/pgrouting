9a43e9911496510c21486f6bda97f0c26570cbfb
Rohith Reddy <rohithreddy2219@gmail.com>
2016-06-24 19:07:06 +0530

    Updated the contraction edge class

--- src/common/src/ch_edge.cpp
+++ src/common/src/ch_edge.cpp
@@ -4,9 +4,9 @@ namespace pgRouting
 namespace contraction {
 
 void Edge::cp_members(const Edge &other, std::ostringstream& log) {
-        log << "cp_members of edge\n";
+        log << "Copying members of edge\n";
         log << other;
-        
+        #if 0
         this->cost = other.cost;
         log << "after copying cost of edge\n";
         log << *this;
@@ -14,10 +14,6 @@ void Edge::cp_members(const Edge &other, std::ostringstream& log) {
         log << "after copying first of edge\n";
         log << *this;
         
-        this->id = other.id;
-        log << "after copying id of edge\n";
-        log << *this;
-        #if 1
         this->source = other.source;
         log << "after copying source of edge\n";
         log << *this;
@@ -28,23 +24,21 @@ void Edge::cp_members(const Edge &other, std::ostringstream& log) {
         log << "after adding contracted vertices\n";
         log << *this;
         #endif
-        #if 0
+        this->cost = other.cost;
+        this->id = other.id;
+        this->first = other.first;
         this->source = other.source;
         this->target = other.target;
-        this->cost = other.cost;
-        this->first = other.first;
         this->m_contracted_vertices += other.contracted_vertices();
+
+        #if 0
+        log << "copying eid: " << other.id << "\n";
+        this->id = other.id;
+        //log << "after copying id of edge\n";
+        log << *this;
+        
         #endif
     }
-#if 0
-void Edge::cp_members(const Basic_edge &other) {
-        this->id = other.id;
-        this->source = other.source;
-        this->target = other.target;
-        this->cost = other.cost;
-        this->first = other.first;
-    }
-#endif
 bool Edge::has_contracted_vertices() const {
     if (m_contracted_vertices.size() == 0)
         return false;
@@ -76,27 +70,6 @@ void Edge::add_contracted_edge_vertices(Edge &e) {
     e.clear_contracted_vertices();
 }
 
-void Edge::add_contracted_vertices(const Edge &e) {
-
-    //adding the ids of the contracted vertices of the given edge e
-    m_contracted_vertices += e.contracted_vertices();
-
-}
-
-#if 0
-const std::string Edge_c::type_str() const {
-    switch (Edge_c::type()) {
-        case Edge_c::Edge_type::ordinary:
-            return "Ordinary Edge";
-        case Edge_c::Edge_type::shortcut:
-            return "Shortcut Edge";
-        default:
-            return "Undefined Edge";
-    }
-}
-#endif
-
-
 std::ostream& operator <<(std::ostream& os, const Edge& e) {
     os << "{\n    id: " << e.id << ",\n";
     os << "    source: " << e.source << ",\n";
@@ -105,12 +78,6 @@ std::ostream& operator <<(std::ostream& os, const Edge& e) {
     os << "    first: " << e.first << ",\n";
     os << "    contracted vertices: ";
     os << e.contracted_vertices();
-    // <<  ", type: " << v.type_str();
-    //os << ", isDeleted: " << v.isDeleted();
-    /*os << " removed_vertices: {";
-    for (auto removed_vertex : v.removed_vertices().ids()) {
-        os << removed_vertex << ", ";
-    }*/
     os << "\n}";
     os << "\n";
     return os;
