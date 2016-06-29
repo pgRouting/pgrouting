c04d108c0d191f4c2c2ec02e77791568af09ec4c
cvvergara <vicky_vergara@hotmail.com>
2016-06-28 23:49:25 -0500

    appveyor: /common strdup problems

--- src/common/src/get_check_data.c
+++ src/common/src/get_check_data.c
@@ -22,7 +22,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 ********************************************************************PGR-GNU*/
 
-#ifdef _MSC_VER
+#if !defined(strdup) && defined(_MSC_VER)
 #define strcat _strcat
 #endif
 
--- src/common/src/restrictions_input.c
+++ src/common/src/restrictions_input.c
@@ -22,7 +22,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 ********************************************************************PGR-GNU*/
 
-#ifdef _MSC_VER
+#if !defined(strdup) && defined(_MSC_VER)
 #define strdup _strdup
 #define strtok _strtok
 #endif
