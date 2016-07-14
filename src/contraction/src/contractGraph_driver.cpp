4ee3d215aabc6082c8deea22a96ee421490f8436
cvvergara <vicky_vergara@hotmail.com>
2016-07-13 22:42:18 -0500

    fix for warning C4002: too many actual parameters for macro unlink

--- src/contraction/src/contractGraph_driver.cpp
+++ src/contraction/src/contractGraph_driver.cpp
@@ -31,6 +31,9 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #ifdef __MINGW32__
 #include <winsock2.h>
 #include <windows.h>
+#ifdef unlink
+#undef unlink
+#endif
 #endif
 
 
