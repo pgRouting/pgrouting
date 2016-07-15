f34157231f1ad9b1904bfc4f0d2f2f54bc374d3c
cvvergara <vicky_vergara@hotmail.com>
2016-07-15 10:59:06 -0500

    trying to keep boost on cache 14

--- ci/appveyor/install.bat
+++ ci/appveyor/install.bat
@@ -22,14 +22,18 @@ if /I "%platform%"=="x86" ( set arch=32) else ( set arch=64)
 mkdir build\downloads 2>NUL
 
 :: CMake 3.5.2 (upgrade) workaround
+cmake --version
 curl -L -O -S -s --output build\downloads\cmake-3.5.2-win32-x86.msi https://cmake.org/files/v3.5/cmake-3.5.2-win32-x86.msi
 start /wait msiexec /i build\downloads\cmake-3.5.2-win32-x86.msi /qn
 cmake --version
 
 :: PostGIS 2.2.2
+echo Downoading postGIS
 curl -L -O -S -s --output build\downloads\postgis-pg94-binaries-2.2.2w64gcc48.zip http://winnie.postgis.net/download/windows/pg94/buildbot/postgis-pg94-binaries-2.2.2w64gcc48.zip
-7z x build\downloads\postgis-pg94-binaries-2.2.2w64gcc48.zip
-xcopy /e /y /q build\downloads\postgis-pg94-binaries-2.2.2w64gcc48 C:\Progra~1\PostgreSQL\9.4
+echo Extracting postGIS
+7z x -oc:\build\ build\downloads\postgis-pg94-binaries-2.2.2w64gcc48.zip
+echo Installing postGIS
+xcopy /e /y /q postgis-pg94-binaries-2.2.2w64gcc48 C:\Progra~1\PostgreSQL\9.4
 
 
 :: =========================================================
@@ -44,7 +48,7 @@ set BOOST_VER_USC=%BOOST_VERSION:.=_%
     
 
 
-if not exist "c:\build\boost_%BOOST_VER_USC%" (
+if not exist "build\boost_%BOOST_VER_USC%" (
 
     if not exist build\downloads\boost_%BOOST_VER_USC%-zip (
         echo Downloading Boost %BOOST_VERSION% ...
@@ -60,8 +64,8 @@ if not exist "c:\build\boost_%BOOST_VER_USC%" (
     echo Extracting Boost_%BOOST_VERSION%.zip ...
     7z x -oc:\build\ Boost_%BOOST_VER_USC%.zip
     echo Done extractig.
-    if not exist "c:\build\boost_%BOOST_VER_USC%" (
-        echo something went wrong!!!!!!!!!
+    if not exist "build\boost_%BOOST_VER_USC%" (
+        echo something went wrong on boos extraction!!!!!!!!!
     )
 ) else (
     echo Boost_%BOOST_VER_USC% Already Extracted
@@ -71,13 +75,14 @@ if not exist "C:\local\boost_%BOOST_VER_USC%\lib%arch%-msvc-14.0" (
     :: download installer??
     if not exist build\downloads\boost_%BOOST_VER_USC%-msvc-14.0-%arch%.exe (
         echo Downloading Boost %BOOST_VERSION% %arch% bits...
-        curl --silent --fail --location --max-time 1600 --connect-timeout 30 --output build\downloads\boost_%BOOST_VER_USC%-msvc-14.0-%arch%.exe http://sourceforge.net/projects/boost/files/boost-binaries/%BOOST_VERSION%/boost_%BOOST_VER_USC%-msvc-14.0-%arch%.exe/download
+        curl --silent --fail --location --max-time 1600 --connect-timeout 30 --output build\downloads\boost_%BOOST_VER_USC%-msvc-14.0-%arch%.exe http://sourceforge.net/projects/boost/files/boost-binaries/%BOOST_VERSION%/boost_%BOOST_VER_USC%-msvc-13.0-%arch%.exe/download
         echo Done downloading.
     ) else (
         echo Boost %BOOST_VERSION% %arch% bits already downloaded
     )
+
     echo Installing Boost %BOOST_VERSION% %arch% bits...
-    build\downloads\boost_%BOOST_VER_USC%-msvc-14.0-%arch%.exe /silent /verysilent /sp- /suppressmsgboxes
+    build\downloads\boost_%BOOST_VER_USC%-msvc-13.0-%arch%.exe /silent /verysilent /sp- /suppressmsgboxes
     echo Done installing.
 ) else (
     echo Boost %BOOST_VERSION% %arch% bits already installed
