102105366570a2b405d08828c44390e604741e40
cvvergara <vicky_vergara@hotmail.com>
2016-07-15 00:58:37 -0500

    trying to keep boost on cache 7

--- ci/appveyor/install.bat
+++ ci/appveyor/install.bat
@@ -43,7 +43,7 @@ if not exist "C:\build\boost_%BOOST_VER_USC%" (
     )
 
     echo Extracting Boost_%BOOST_VERSION%.zip ...
-    7z x boost_1_58_0.zip -oc:\build\boost_%BOOST_VER_USC%
+    7z x boost_1_58_0.zip -oc:\build
     echo Done extractig.
     if not exist "C:\build\boost_%BOOST_VER_USC%" (
         echo something went wrong!!!!!!!!!
