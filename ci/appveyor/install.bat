5c618cffb997add50895a8fcf8650661a5daa9fd
cvvergara <vicky_vergara@hotmail.com>
2016-07-15 09:24:55 -0500

    trying to keep boost on cache 8

--- ci/appveyor/install.bat
+++ ci/appveyor/install.bat
@@ -1,4 +1,8 @@
 @echo off
+
+:: each sub-script starts and end on this directory:
+cd c:\build
+
 Setlocal EnableDelayedExpansion EnableExtensions
 
 echo APPVEYOR_BUILD_FOLDER %APPVEYOR_BUILD_FOLDER%
@@ -22,6 +26,7 @@ mkdir build\downloads 2>NUL
 
 
 
+
 :: =========================================================
 :: Download and install Boost
 ::
@@ -32,22 +37,27 @@ if not defined BOOST_VERSION set BOOST_VERSION=1.58.0
 set BOOST_VER_USC=%BOOST_VERSION:.=_%
 
 
-if not exist "C:\build\boost_%BOOST_VER_USC%" (
+if not exist "c:\build\boost_%BOOST_VER_USC%" (
+    cd downloads
 
-    if not exist build\downloads\boost_%BOOST_VER_USC%-zip (
+    if not exist "c:\build\downloads\boost_%BOOST_VER_USC%-zip" (
         echo Downloading Boost %BOOST_VERSION% ...
         curl -L -O -S -s http://downloads.sourceforge.net/project/boost/boost/%BOOST_VERSION%/boost_%BOOST_VER_USC%.zip
         echo Done downloading Boost.
+        if not exist "c:\build\downloads\boost_%BOOST_VER_USC%-zip" (
+            echo something went wrong!!!!!!!!!
+        )
     ) else (
         echo Boost_%BOOST_VER_USC%  already downloaded
     )
 
     echo Extracting Boost_%BOOST_VERSION%.zip ...
-    7z x boost_1_58_0.zip -oc:\build
+    7z x -oc:\build\ Boost_%BOOST_VER_USC%.zip
     echo Done extractig.
     if not exist "C:\build\boost_%BOOST_VER_USC%" (
         echo something went wrong!!!!!!!!!
     )
+    cd ..
 ) else (
     echo Boost_%BOOST_VER_USC% Already Extracted
 )
@@ -55,10 +65,13 @@ if not exist "C:\build\boost_%BOOST_VER_USC%" (
 ::
 :: =========================================================
 
+cd c:\build
+
 echo.
 echo ======================================================
 echo Installation of Boost done.
 echo Platform - %platform%
+echo Boost - C:\build\boost_%BOOST_VER_USC%
 echo ======================================================
 echo.
 
