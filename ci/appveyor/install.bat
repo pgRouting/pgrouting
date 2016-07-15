f3a3eaf3f44dcfd3032d5e4009fcb651b3e80bc0
cvvergara <vicky_vergara@hotmail.com>
2016-07-15 10:13:28 -0500

    trying to keep boost on cache 9

--- ci/appveyor/install.bat
+++ ci/appveyor/install.bat
@@ -36,16 +36,18 @@ if not defined BOOST_VERSION set BOOST_VERSION=1.58.0
 :: replace dots with underscores
 set BOOST_VER_USC=%BOOST_VERSION:.=_%
 
+    
+
 
 if not exist "c:\build\boost_%BOOST_VER_USC%" (
-    cd downloads
+    cd c:\build\downloads
 
     if not exist "c:\build\downloads\boost_%BOOST_VER_USC%-zip" (
         echo Downloading Boost %BOOST_VERSION% ...
         curl -L -O -S -s http://downloads.sourceforge.net/project/boost/boost/%BOOST_VERSION%/boost_%BOOST_VER_USC%.zip
         echo Done downloading Boost.
         if not exist "c:\build\downloads\boost_%BOOST_VER_USC%-zip" (
-            echo something went wrong!!!!!!!!!
+            echo something went wrong on boost download !!!!!!!!!
         )
     ) else (
         echo Boost_%BOOST_VER_USC%  already downloaded
@@ -54,7 +56,7 @@ if not exist "c:\build\boost_%BOOST_VER_USC%" (
     echo Extracting Boost_%BOOST_VERSION%.zip ...
     7z x -oc:\build\ Boost_%BOOST_VER_USC%.zip
     echo Done extractig.
-    if not exist "C:\build\boost_%BOOST_VER_USC%" (
+    if not exist "c:\build\boost_%BOOST_VER_USC%" (
         echo something went wrong!!!!!!!!!
     )
     cd ..
@@ -62,6 +64,21 @@ if not exist "c:\build\boost_%BOOST_VER_USC%" (
     echo Boost_%BOOST_VER_USC% Already Extracted
 )
 
+if not exist "C:\local\boost_%BOOST_VER_USC%\lib%arch%-msvc-14.0" (
+    :: download installer??
+    if not exist build\downloads\boost_%BOOST_VER_USC%-msvc-14.0-%arch%.exe (
+        echo Downloading Boost %BOOST_VERSION% %arch% bits...
+        curl --silent --fail --location --max-time 1600 --connect-timeout 30 --output build\downloads\boost_%BOOST_VER_USC%-msvc-14.0-%arch%.exe http://sourceforge.net/projects/boost/files/boost-binaries/%BOOST_VERSION%/boost_%BOOST_VER_USC%-msvc-14.0-%arch%.exe/download
+        echo Done downloading.
+    ) else (
+        echo Boost %BOOST_VERSION% %arch% bits already downloaded
+    )
+    echo Installing Boost %BOOST_VERSION% %arch% bits...
+    build\downloads\boost_%BOOST_VER_USC%-msvc-14.0-%arch%.exe /silent /verysilent /sp- /suppressmsgboxes
+    echo Done installing.
+) else (
+    echo Boost %BOOST_VERSION% %arch% bits already installed
+)
 ::
 :: =========================================================
 
