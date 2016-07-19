6f5fd90526438eb1624049778f7b8fcf4bdbb8e7
cvvergara <vicky_vergara@hotmail.com>
2016-07-19 17:50:36 -0500

    appveyor making a file for pgrouting installation 21

--- ci/appveyor/msbuild_pgrouting.bat
+++ ci/appveyor/msbuild_pgrouting.bat
@@ -2,9 +2,6 @@
 
 set PG_VER="%1"
 
-rem doskey cmake="%PROGRAMFILES% (x86)\CMake\bin\cmake.exe"
-echo APPVEYOR=%APPVEYOR%
-
 echo MSVC_VER %MSVC_VER%
 echo BUILD_ROOT_DIR %BUILD_ROOT_DIR%
 echo CGAL_LIBRARIES %CGAL_LIBRARIES%
@@ -17,62 +14,26 @@ echo BOOST_INCLUDE_DIR %BOOST_INCLUDE_DIR%
 echo PLATFORM %PLATFORM%
 echo GMP_INCLUDE_DIR %GMP_INCLUDE_DIR%
 echo CGAL_INCLUDE_DIR %CGAL_INCLUDE_DIR%
+echo PG_VER %PG_VER%
+echo PROGRAMFILES %PROGRAMFILES%
+
+set POSTGRESQL_DIR=%PROGRAMFILES%\PostgreSQL\%PG_VER%
 
 
 
 set PGROUTING_SRC_DIR=%~dp0..\..\
-
-set RUNTIME=msvc%MSVC_VER:.=%
-
-echo PG_VER="%PG_VER%"
-echo MSVC_VER="%MSVC_VER%"
-echo RUNTIME="%RUNTIME%"
-echo CMAKE_GENERATOR="%CMAKE_GENERATOR%"
-echo REBUILD="%REBUILD%"
-
-set VS100COMNTOOLS=%PROGRAMFILES% (x86)\Microsoft Visual Studio 10.0\Common7\Tools\
-rem TODO:
-set VCVARSALL_BAT_PATH="%PROGRAMFILES% (x86)\Microsoft Visual Studio %MSVC_VER%\VC\vcvarsall.bat"
-if "%RUNTIME%"=="msvc100" if "%PLATFORM%"=="x64" (
-    set OPTION=x86
-) else if "%PLATFORM%"=="x86" (
-    set OPTION=x86
-) else if "%PLATFORM%"=="x64" (
-    set OPTION=amd64
-)
-
-call %VCVARSALL_BAT_PATH% %OPTION%
-
-if "%RUNTIME%"=="msvc100" if "%PLATFORM%"=="x64" (
-    rem quotes to avoid escape error
-    set INCLUDE="%INCLUDE%;%PROGRAMFILES%\Microsoft SDKs\Windows\v7.1\Include"
-    if "%PLATFORM%"=="x86" (
-        set LIB="%LIB%;%PROGRAMFILES%\Microsoft SDKs\Windows\v7.1\Lib"
-    ) else if "%PLATFORM%"=="x64" (
-        set LIB="%LIB%;%PROGRAMFILES%\Microsoft SDKs\Windows\v7.1\Lib\x64"
-    )
-)
-
-rem remove quotes
-set INCLUDE=%INCLUDE:"=%
-set LIB=%LIB:"=%
-
-rem echo INCLUDE="%INCLUDE%"
-rem echo LIB="%LIB%"
+echo PGROUTING_SRC_DIR %PGROUTING_SRC_DIR%
 
 path %PATH%;%PROGRAMFILES% (x86)\CMake\bin
+echo %PATH%
 
 set PGROUTING_BUILD_DIR=%PGROUTING_SRC_DIR%build\pg%PG_VER:.=%\%PLATFORM%
 set PGROUTING_INSTALL_DIR=%PGROUTING_SRC_DIR%lib\pg%PG_VER:.=%\%PLATFORM%
-if "%PLATFORM%"=="x86" (
-    set POSTGRESQL_DIR=%PROGRAMFILES% ^(x86^)\PostgreSQL\%PG_VER%
-) else (
-    set POSTGRESQL_DIR=%PROGRAMFILES%\PostgreSQL\%PG_VER%
-)
 
-echo PGROUTING_BUILD_DIR="%PGROUTING_BUILD_DIR%"
-echo PGROUTING_INSTALL_DIR="%PGROUTING_INSTALL_DIR%"
-echo POSTGRESQL_DIR="%POSTGRESQL_DIR%"
+
+echo PGROUTING_BUILD_DIR %PGROUTING_BUILD_DIR%
+echo PGROUTING_INSTALL_DIR %PGROUTING_INSTALL_DIR%
+echo POSTGRESQL_DIR %POSTGRESQL_DIR%
 
 
 rem ### pgRouting ###
