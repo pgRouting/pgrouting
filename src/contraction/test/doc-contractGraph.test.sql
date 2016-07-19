068cb1e07d1dd8c716439195c326bfe193ea092b
Rohith Reddy <rohithreddy2219@gmail.com>
2016-07-20 02:13:49 +0530

    Removed some tests belonging to other document

--- src/contraction/test/doc-contractGraph.test.sql
+++ src/contraction/test/doc-contractGraph.test.sql
@@ -1,6 +1,3 @@
-\echo -- q0
-SELECT id, source, target, cost, reverse_cost FROM edge_table;
-
 \echo -- q1
 SELECT * FROM pgr_contractGraph(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table',
@@ -52,14 +49,3 @@ SELECT * FROM pgr_contractGraph(
     ARRAY[]::bigint[], ARRAY[0, 1]::integer[], 1, false) WHERE type = 'v';
 
 \echo -- q11
-ALTER TABLE edge_table ADD is_contracted BOOLEAN DEFAULT false;
-
-\echo -- q12
-ALTER TABLE edge_table ADD contracted_vertices integer[];
-
-\echo -- q13
-SELECT id, source, target, cost, reverse_cost, is_contracted,
-contracted_vertices FROM edge_table;
-
-\echo -- q14
-
