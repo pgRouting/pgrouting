e453f0e212acc6cd57e3c17d2e96a69c43451aa0
cvvergara <vicky_vergara@hotmail.com>
2016-07-19 21:50:19 -0500

    removed unnecesary build directory

--- src/build/src/common/src/CMakeFiles/common.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/common.dir/connection.c.o"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang C)
-  INCLUDE(CMakeFiles/common.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
