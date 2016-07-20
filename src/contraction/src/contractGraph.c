1468eb15fd8a0b17c8a391cce88ea1db1df7701b
cvvergara <vicky_vergara@hotmail.com>
2016-07-20 02:35:39 -0500

    only testing one query 6

--- src/contraction/src/contractGraph.c
+++ src/contraction/src/contractGraph.c
@@ -72,7 +72,7 @@ process(char* edges_sql,
     pgr_SPI_connect();
 
     PGR_DBG("num_cycles %ld ", num_cycles);
-    PGR_DBG("directed %d ", directed;
+    PGR_DBG("directed %d ", directed);
     PGR_DBG("edges_sql %s",edges_sql);
     PGR_DBG("Load data");
     pgr_edge_t *edges = NULL;
