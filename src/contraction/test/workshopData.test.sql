096ce57b01812fb9de7812a047f7cb151399fecc
cvvergara <vicky_vergara@hotmail.com>
2016-07-20 00:52:08 -0500

    contraction: adding workshopData 1 query test

--- src/contraction/test/workshopData.test.sql
+++ src/contraction/test/workshopData.test.sql
@@ -2,22 +2,20 @@
 SELECT * FROM pgr_contractGraph(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table',
     ARRAY[]::BIGINT[], ARRAY[0]::integer[], 1, true);
--- \echo --q3 -------------------------------------------
 
+/*
 \echo --q1 Checking linear contraction for workshop data
 SELECT * FROM pgr_contractGraph(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table',
     ARRAY[]::BIGINT[], ARRAY[1]::integer[], 1, true);
--- \echo --q1 -------------------------------------------
 
 \echo --q2 Checking dead end, linear contraction for workshop data
 SELECT * FROM pgr_contractGraph(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table',
     ARRAY[]::BIGINT[], ARRAY[0, 1]::integer[], 1, true);
--- \echo --q2 -------------------------------------------
 
 \echo --q3 Checking linear contraction, dead end for workshop data
 SELECT * FROM pgr_contractGraph(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table',
     ARRAY[]::BIGINT[], ARRAY[1, 0]::integer[], 1, true);
--- \echo --q3 -------------------------------------------
+*/
