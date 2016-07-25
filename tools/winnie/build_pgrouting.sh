ac00d8c81a35aa119182b5b945067ad5e8401ab5
cvvergara <vicky_vergara@hotmail.com>
2016-07-25 00:31:58 -0500

    Jenkins: added some debug messages

--- tools/winnie/build_pgrouting.sh
+++ tools/winnie/build_pgrouting.sh
@@ -1,30 +1,66 @@
 #!/bin/sh.exe
 
-# CHANGE THESE TO SUIT YOUR PROJECT
-#all these get passed in by jenkins
-#export OS_BUILD=64 
-#export PG_VER=9.2
-#export PGHOST=localhost
-#export PGPORT=8442
-export PGUSER=postgres
-#export PGROUTING_VER=2.1
-#POSTGIS_VER=2.1.0SVN
+#-------------------------
+# File used in Jenkis setup
+#-------------------------
+
+JENKINS_DEBUG=1
+
+#-----------------
+# variables setup
+#  Variables recived by jenkins setup
+#-----------------
+#export OS_BUILD=
+#export PG_VER=
+#export PGHOST=
+#export PGPORT=
+#export PGROUTING_VER=
+#export POSTIGS_VER=
 #GCC_TYPE=
-#export POSTIGS_VER=2.1.0beta3
-#export GIT_COMMIT=whatever
+#export GIT_COMMIT=
+
+
+if [ -z $JENKINS_DEBUG ]
+    echo "OS_BUILD ${OS_BUILD}"
+    echo "PG_VER ${PG_VER}"
+    echo "PGHOST ${PGHOST}"
+    echo "PGPORT ${PGPORT}"
+    echo "PGROUTING_VER ${PGROUTING_VER}"
+    echo "POSTGIS_VER ${POSTGIS_VER}"
+    echo "GCC_TYPE ${GCC_TYPE}"
+    echo "GIT_COMMIT ${GIT_COMMIT}"
+fi
+
+#---------------
+# Deduced variables
+#---------------
+
+export PGUSER=postgres
 export PROJECTS=/projects
 export PGPATHEDB=${PROJECTS}/postgresql/rel/pg${PG_VER}w${OS_BUILD}${GCC_TYPE}edb  #this is so winnie know's where to copy the dlls for vc++ edb compiled postgresql testing
-export PATHOLD=$PATH
 export PGPATH=${PROJECTS}/postgresql/rel/pg${PG_VER}w${OS_BUILD}${GCC_TYPE}
-
-#export PROJECTS=/c/ming${OS_BUILD}/projects
-
-export PostgreSQL_ROOT=${PGPATH}
-
+export PATHOLD=$PATH
+echo "PATHOLD ${PATHOLD}"
 export PATHOLD="/mingw/bin:/mingw/include:/c/Windows/system32:/c/Windows"
+echo "PATHOLD ${PATHOLD}"
 export PGWINVER=${PG_VER}w${OS_BUILD}${GCC_TYPE}edb
+echo "PGWINVER ${PGWINVER}"
 export PATH="${PATHOLD}:${PGPATH}/bin:${PGPATH}/lib:${PGPATH}/include"
+echo "PATH ${PATH}"
 export PATH="${PROJECTS}/rel-libiconv-1.13.1w${OS_BUILD}${GCC_TYPE}/include:${PATH}"
+echo "PATH ${PATH}"
+
+
+if [ -z $JENKINS_DEBUG ]
+    echo "PGUSER ${PGUSER}"
+    echo "PROJECTS ${PROJECTS}"
+    echo "PGPATHEDB ${PGPATHEDB}"
+    echo "PGPATH ${PGPATH}"
+    echo "PATHOLD ${PATHOLD}"
+    echo "PGWINVER ${PGWINVER}"
+    echo "PATH ${PATH}"
+fi
+
 GMP_VER=5.1.2
 MPFR_VER=3.1.2
 CGAL_VER=4.2
@@ -32,27 +68,42 @@ BOOST_VER=1.59.0
 BOOST_VER_WU=1_59_0
 BOOST_VER_WUM=1_59
 ZLIB_VER=1.2.8
-#export PATH="/mingw/bin:/mingw/include:/c/Windows/system32"
+
+if [ -z $JENKINS_DEBUG ]
+    echo "GMP_VER ${GMP_VER}"
+    echo "MPFR_VER ${MPFR_VER}"
+    echo "GAL_VER ${CGAL_VER}"
+    echo "BOOST_VER_WU ${BOOST_VER_WU}"
+    echo "BOOST_VER_WUM ${BOOST_VER_WUM}"
+    echo "ZLIB_VER ${ZLIB_VER}"
+fi
+
 #zlib
 PATH="${PATH}:${PROJECTS}/zlib/rel-${ZLIB_VER}w${OS_BUILD}${GCC_TYPE}/include:${PROJECTS}/zlib/rel-${ZLIB_VER}w${OS_BUILD}${GCC_TYPE}/lib:${PROJECTS}/zlib/rel-${ZLIB_VER}w${OS_BUILD}${GCC_TYPE}/bin"
+echo "PATH ${PATH}"
 
 #gmp
 PATH="${PATH}:${PROJECTS}/CGAL/rel-gmp-${GMP_VER}w${OS_BUILD}${GCC_TYPE}/include:${PROJECTS}/CGAL/rel-gmp-${GMP_VER}w${OS_BUILD}${GCC_TYPE}/lib"
+echo "PATH ${PATH}"
 
 #mpfr
 PATH="${PATH}:${PROJECTS}/CGAL/rel-mpfr-${MPFR_VER}w${OS_BUILD}${GCC_TYPE}/include:${PROJECTS}/CGAL/rel-mpfr-${MPFR_VER}w${OS_BUILD}${GCC_TYPE}/lib:${PROJECTS}/CGAL/rel-mpfr-${MPFR_VER}w${OS_BUILD}${GCC_TYPE}/bin"
+echo "PATH ${PATH}"
 
 #boost
 PATH="${PATH}:${PROJECTS}/boost/rel-${BOOST_VER_WU}w${OS_BUILD}${GCC_TYPE}/lib"
-#BOOSTROOT_PATH
 BOOSTROOT_PATH="${PROJECTS}/boost/rel-${BOOST_VER_WU}w${OS_BUILD}${GCC_TYPE}"
+echo "PATH ${PATH}"
 
 #cgal
 PATH="${PATH}:${PROJECTS}/CGAL/rel-cgal-${CGAL_VER}w${OS_BUILD}${GCC_TYPE}/include:${PROJECTS}/CGAL/rel-cgal-${CGAL_VER}w${OS_BUILD}${GCC_TYPE}/lib"
+echo "PATH ${PATH}"
 
 #cmake
 export PATH="${PATH}:/cmake/bin"
+echo "PATH ${PATH}"
 export PATH="${PATH}:.:/bin:/include"
+echo "PATH ${PATH}"
 
 cd ${PROJECTS}/pgrouting
 rm -rf build${PGROUTING_VER}w${OS_BUILD}${GCC_TYPE}
@@ -62,9 +113,9 @@ cmake -G "MSYS Makefiles"  -DCMAKE_VERBOSE_MAKEFILE=ON -DBOOST_ROOT:PATH=${BOOST
 
 #first delete old pgrouting files from installed folder before we reinstall
 
-
-echo "The git commit is ${GIT_COMMIT}"
+ls ${PGPATH}/lib/
 rm ${PGPATH}/lib/librouting*
+ls ${PGPATH}/share/extension/
 rm ${PGPATH}/share/extension/pgrouting*
 make && make install
 
@@ -78,9 +129,6 @@ cp lib/*.control ${PGPATHEDB}/share/extension/
 cd ${PROJECTS}/pgrouting/branches/${PGROUTING_VER}
 
 #perl tools/test-runner.pl   -pgver ${PG_VER} -pgport "${PGPORT}"  -clean
-echo "PGVER ${PG_VER}"
-echo "POSTGIS_VER ${POSTGIS_VER}"
-echo "PGPORT ${PGPORT}"
 perl tools/testers/algorithm-tester.pl  -pgver ${PG_VER} -pgisver "${POSTGIS_VER}" -pgport "${PGPORT}"  -alg common
 perl tools/testers/algorithm-tester.pl  -pgver ${PG_VER} -pgisver "${POSTGIS_VER}" -pgport "${PGPORT}"  -alg dijkstra
 perl tools/testers/algorithm-tester.pl  -pgver ${PG_VER} -pgisver "${POSTGIS_VER}" -pgport "${PGPORT}"  -alg contraction
