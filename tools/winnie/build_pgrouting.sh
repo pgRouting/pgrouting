de3a62a8fb484e0d0d96fb07bd61e383924c80f0
cvvergara <vicky_vergara@hotmail.com>
2016-07-25 01:30:54 -0500

    Jenkins: looks like if not linking

--- tools/winnie/build_pgrouting.sh
+++ tools/winnie/build_pgrouting.sh
@@ -41,15 +41,10 @@ export PROJECTS=/projects
 export PGPATHEDB=${PROJECTS}/postgresql/rel/pg${PG_VER}w${OS_BUILD}${GCC_TYPE}edb  #this is so winnie know's where to copy the dlls for vc++ edb compiled postgresql testing
 export PGPATH=${PROJECTS}/postgresql/rel/pg${PG_VER}w${OS_BUILD}${GCC_TYPE}
 export PATHOLD=$PATH
-echo "PATHOLD ${PATHOLD}"
 export PATHOLD="/mingw/bin:/mingw/include:/c/Windows/system32:/c/Windows"
-echo "PATHOLD ${PATHOLD}"
 export PGWINVER=${PG_VER}w${OS_BUILD}${GCC_TYPE}edb
-echo "PGWINVER ${PGWINVER}"
 export PATH="${PATHOLD}:${PGPATH}/bin:${PGPATH}/lib:${PGPATH}/include"
-echo "PATH ${PATH}"
 export PATH="${PROJECTS}/rel-libiconv-1.13.1w${OS_BUILD}${GCC_TYPE}/include:${PATH}"
-echo "PATH ${PATH}"
 
 
 if [ $JENKINS_DEBUG -eq 1 ]
@@ -84,32 +79,27 @@ fi
 #zlib
 ZLIB_PATH="${PROJECTS}/zlib/rel-${ZLIB_VER}w${OS_BUILD}${GCC_TYPE}"
 PATH="${PATH}:${ZLIB_PATH}/include:${ZLIB_PATH}/lib:${ZLIB_PATH}/bin"
-echo "PATH ${PATH}"
 
 #gmp
 GMP_PATH="${PROJECTS}/CGAL/rel-gmp-${GMP_VER}w${OS_BUILD}${GCC_TYPE}"
 PATH="${PATH}:${GMP_PATH}/include:${GMP_PATH}/lib"
-echo "PATH ${PATH}"
 
 #mpfr
 MPFR_PATH="${PROJECTS}/CGAL/rel-mpfr-${MPFR_VER}w${OS_BUILD}${GCC_TYPE}"
 PATH="${PATH}:${MPFR_PATH}/include:${MPFR_PATH}/lib:${MPFR_PATH}/bin"
-echo "PATH ${PATH}"
 
 #boost
 BOOSTROOT_PATH="${PROJECTS}/boost/rel-${BOOST_VER_WU}w${OS_BUILD}${GCC_TYPE}"
 PATH="${PATH}:${BOOSTROOT_PATH}/lib"
-echo "PATH ${PATH}"
 
 #cgal
 CGAL_PATH="${PROJECTS}/CGAL/rel-cgal-${CGAL_VER}w${OS_BUILD}${GCC_TYPE}"
 PATH="${PATH}:${CGAL_PATH}/include:${CGAL_PATH}/lib"
-echo "PATH ${PATH}"
 
 #cmake
 export PATH="${PATH}:/cmake/bin"
-echo "PATH ${PATH}"
 export PATH="${PATH}:.:/bin:/include"
+
 echo "PATH ${PATH}"
 
 cd ${PROJECTS}/pgrouting
@@ -117,14 +107,7 @@ rm -rf build${PGROUTING_VER}w${OS_BUILD}${GCC_TYPE}
 mkdir build${PGROUTING_VER}w${OS_BUILD}${GCC_TYPE}
 cd build${PGROUTING_VER}w${OS_BUILD}${GCC_TYPE}
 
-cmake -G "MSYS Makefiles" \
-    -DCMAKE_VERBOSE_MAKEFILE=ON \
-    -DBOOST_ROOT:PATH=${BOOSTROOT_PATH} \
-    -DCGAL_ROOT:PATH=${CGAL_PATH} \
-    -DGMP_ROOT:PATH=${PROJECTS}/CGAL/rel-gmp-${GMP_VER}w${OS_BUILD}${GCC_TYPE} \
-    -DBoost_USE_STATIC_LIBS=ON \
-    -DBoost_USE_MULTITHREADED=ON \
-    -DCMAKE_CXX_FLAGS="-I${PROJECTS}/CGAL/rel-gmp-${GMP_VER}w${OS_BUILD}${GCC_TYPE}/include -I${PROJECTS}/CGAL/rel-mpfr-${MPFR_VER}w${OS_BUILD}${GCC_TYPE}/include"  ../branches/${PGROUTING_VER}
+cmake -G "MSYS Makefiles" -DCMAKE_VERBOSE_MAKEFILE=ON -DBOOST_ROOT:PATH=${BOOSTROOT_PATH} -DCGAL_ROOT:PATH=${CGAL_PATH} -DGMP_ROOT:PATH=${PROJECTS}/CGAL/rel-gmp-${GMP_VER}w${OS_BUILD}${GCC_TYPE} -DBoost_USE_STATIC_LIBS=ON -DBoost_USE_MULTITHREADED=ON -DCMAKE_CXX_FLAGS="-I${PROJECTS}/CGAL/rel-gmp-${GMP_VER}w${OS_BUILD}${GCC_TYPE}/include -I${PROJECTS}/CGAL/rel-mpfr-${MPFR_VER}w${OS_BUILD}${GCC_TYPE}/include"  ../branches/${PGROUTING_VER}
 
 #first delete old pgrouting files from installed folder before we reinstall
 
@@ -143,7 +126,8 @@ ls ${PGPATH}/share/extension/pgrouting*
 ls ${PGPATHEDB}/lib/libpgrouting*
 ls ${PGPATHEDB}/share/extension/pgrouting*
 
-make && make install
+make 
+make install
 
 #we need uninstall and reinstall copy to VC++ EDB instance if we want to test on standard Windows installed versions
 cp lib/*.dll ${PGPATHEDB}/lib/
