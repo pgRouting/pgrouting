365d835e16f2ff1aec6210d2bf7ad9f2754c4c91
Rohith Reddy <rohithreddy2219@gmail.com>
2016-07-31 00:43:55 +0530

    Removed lint errors of files in common directory

--- src/common/src/ch_edge.h
+++ src/common/src/ch_edge.h
@@ -1,21 +1,48 @@
-#ifndef CH_EDGE_H
-#define CH_EDGE_H
+#ifndef SRC_COMMON_SRC_CH_EDGE_H_
+#define SRC_COMMON_SRC_CH_EDGE_H_
 
+/*PGR-GNU*****************************************************************
+File: ch_edge.h
+
+Generated with Template by:
+Copyright (c) 2015 pgRouting developers
+Mail: project@pgrouting.org
+
+Function's developer: 
+Copyright (c) 2016 Rohith Reddy
+Mail: 
+
+------
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ ********************************************************************PGR-GNU*/
 #include <iostream>
 #include <sstream>
-#include "ch_vertex.h"
-namespace pgrouting
-{
+#include "./ch_vertex.h"
+namespace pgrouting {
 namespace contraction {
 class Edge {
  public:
     Edge() = default;
     Edge(int64_t eid, int64_t source, int64_t target, double cost) :
     id(eid), source(source),
-    target(target), cost(cost), first(true){}
+    target(target), cost(cost), first(true) {}
     Edge(int64_t eid, int64_t source, int64_t target, double cost, bool first) :
     id(eid), source(source),
-    target(target), cost(cost), first(first){}
+    target(target), cost(cost), first(first) {}
     void cp_members(const Edge &other, std::ostringstream& log);
     int64_t id;
     int64_t source;
@@ -28,10 +55,11 @@ class Edge {
     void clear_contracted_vertices() { m_contracted_vertices.clear(); }
     const Identifiers<int64_t>& contracted_vertices() const;
     friend std::ostream& operator <<(std::ostream& os, const Edge& e);
+
  private:
     Identifiers<int64_t> m_contracted_vertices;
 };
 
 }  // namespace contraction
-}  // namespace pgRouting
-#endif
+}  // namespace pgrouting
+#endif  // SRC_COMMON_SRC_CH_EDGE_H_
