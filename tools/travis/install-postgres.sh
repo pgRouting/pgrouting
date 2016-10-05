39d41219ccbbb6220e62013dbfc2fa74e3505bc0
cvvergara <cvvergara@georepublic.de>
2016-10-05 11:06:12 -0500

    travis adding generalizing install postgres

---
+++ tools/travis/install-postgres.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# ------------------------------------------------------------------------------
+# Travis CI scripts 
+# Copyright(c) pgRouting Contributors
+#
+# Install pgRouting prerequesits
+# ------------------------------------------------------------------------------
+
+set -e
+
+POSTGRESQL_VERSION="$1"
+PGUSER="$2"
+
+if [[ -z "$POSTGRESQL_VERSION" ]] ; then
+    exit 1
+fi
+
+
+if [[ "$POSTGRESQL_VERSION" = "9.5" ||  "$POSTGRESQL_VERSION" = "9.6"  ]] ; then
+    POSTGIS_VERSION="2.3"
+fi
+
+echo "Postgres $POSTGRESQL_VERSION"
+echo "User $PGUSER"
+
+sudo /etc/init.d/postgresql stop
+
+## removing unused postgresql
+sudo apt-get -y remove --purge postgresql
+
+echo "Installing postgresql $POSTGRESQL_VERSION  & postgis "
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+sudo apt-get install wget ca-certificates
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo apt-get update
+
+sudo apt-get install -y postgresql-$POSTGRESQL_VERSION postgresql-server-dev-$POSTGRESQL_VERSION  postgresql-9.5-postgis-$POSTGIS_VERSION
+
+#sudo apt-get install -y pgtap libtap-parser-sourcehandler-pgtap-perl
+sudo cp /usr/lib/postgresql/$POSTGRESQL_VERSION/bin/pg_config /usr/bin/pg_config
+sudo /etc/init.d/postgresql stop
+sudo /etc/init.d/postgresql stop
+ps -fea | grep postgres
+#echo "making grep before change"
+#grep port /etc/postgresql/$POSTGRESQL_VERSION/main/postgresql.conf
+#echo "finished grep"
+#sudo sed -i -e 's/port = 5433/port = 5432/g' /etc/postgresql/$POSTGRESQL_VERSION/main/postgresql.conf
+#echo "making grep after change"
+#grep port /etc/postgresql/$POSTGRESQL_VERSION/main/postgresql.conf
+#echo "finished grep"
+#sudo cp $TRAVIS_BUILD_DIR/tools/travis/pg_hba.conf /etc/postgresql/$POSTGRESQL_VERSION/main/pg_hba.conf
+sudo /etc/init.d/postgresql start $POSTGRESQL_VERSION
+ps -fea | grep postgres
+
