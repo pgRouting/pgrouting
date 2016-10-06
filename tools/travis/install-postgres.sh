1bb0242eb89ea6e88b315822b1cb454a48ba77fe
cvvergara <cvvergara@georepublic.de>
2016-10-05 22:30:12 -0500

    .travis.yml: more simplification 2

--- tools/travis/install-postgres.sh
+++ tools/travis/install-postgres.sh
@@ -38,36 +38,26 @@ if [[ "$POSTGRESQL_VERSION" = "9.5" ||  "$POSTGRESQL_VERSION" = "9.6"  ]] ; then
     sudo apt-get -y remove --purge postgresql-9.6
 
     echo "Installing postgresql $POSTGRESQL_VERSION  & postgis "
-    # sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-    # sudo apt-get install wget ca-certificates
-    # wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-    # sudo apt-get update
 
     sudo apt-get install -y \
         postgresql-$POSTGRESQL_VERSION \
         postgresql-$POSTGRESQL_VERSION-postgis-$POSTGIS_VERSION
 
-    #sudo apt-get install -y pgtap libtap-parser-sourcehandler-pgtap-perl
     sudo cp /usr/lib/postgresql/$POSTGRESQL_VERSION/bin/pg_config /usr/bin/pg_config
-    sudo /etc/init.d/postgresql stop
-    sudo /etc/init.d/postgresql stop
-    ps -fea | grep postgres
 
-    #sudo sh -c 'echo "local all postgres trust" > /etc/postgresql/$POSTGRESQL_VERSION/main/pg_hba.conf'
-    #sudo sh -c 'echo -n "host all all 127.0.0.1/32 trust" >> /etc/postgresql/$POSTGRESQL_VERSION/main/pg_hba.conf'
+    sudo /etc/init.d/postgresql stop
+
 
     sudo cat /etc/postgresql/$POSTGRESQL_VERSION/main/pg_hba.conf
 
-    #echo "making grep before change"
-    #grep port /etc/postgresql/$POSTGRESQL_VERSION/main/postgresql.conf
-    #echo "finished grep"
     sudo sed -i -e 's/port = 5433/port = 5432/g' /etc/postgresql/$POSTGRESQL_VERSION/main/postgresql.conf
-    #echo "making grep after change"
-    #grep port /etc/postgresql/$POSTGRESQL_VERSION/main/postgresql.conf
-    #echo "finished grep"
     sudo cp $TRAVIS_BUILD_DIR/tools/travis/pg_hba.conf /etc/postgresql/$POSTGRESQL_VERSION/main/pg_hba.conf
+
     ps -fea | grep postgres
 fi
 
 sudo /etc/init.d/postgresql start $POSTGRESQL_VERSION
+
+# intall pgtap after postgres instance has started
 sudo apt-get install -y  postgresql-$POSTGRESQL_VERSION-pgtap
+sudo apt-get install -y libtap-parser-sourcehandler-pgtap-perl
