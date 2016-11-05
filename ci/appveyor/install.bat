e4848614e798e60eb3688c65ea4c7f5e5ee2e359
cvvergara <cvvergara@georepublic.de>
2016-11-05 11:40:32 -0600

    testing appveyor for latests postgis

--- ci/appveyor/install.bat
+++ ci/appveyor/install.bat
@@ -20,7 +20,7 @@ if not defined COMMON_INSTALL_DIR set COMMON_INSTALL_DIR=%BUILD_ROOT_DIR%\local\
 
 :: for cmake its the min version
 if not defined CMAKE_VERSION set CMAKE_VERSION=3.5.2
-if not defined PG_VERSION set PG_VERSION=2.2.2
+if not defined PG_VERSION set PG_VERSION=2.3.1
 if not defined BOOST_VERSION set BOOST_VERSION=1.58.0
 if not defined CGAL_VERSION set CGAL_VERSION=4.8.1
 
