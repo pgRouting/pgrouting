232a03e5e5eb58f0b756eb939b33381bc24e65f7
cvvergara <cvvergara@georepublic.de>
2016-11-07 12:32:25 -0600

    continue with wrapping postgres *.h files on  tsp & max_flow

--- src/max_flow/src/edge_disjoint_paths_many_to_many.c
+++ src/max_flow/src/edge_disjoint_paths_many_to_many.c
@@ -27,22 +27,26 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 ********************************************************************PGR-GNU*/
 
-#include "postgres.h"
+#include "./../../common/src/postgres_connection.h"
+
+#ifdef __GNUC__
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#endif
+
 #include "funcapi.h"
+
+#ifdef __GNUC__
+#pragma GCC diagnostic pop
+#endif
+
 #include "utils/array.h"
 #include "catalog/pg_type.h"
 #if PGSQL_VERSION > 92
 #include "access/htup_details.h"
 #endif
 
-/*
- * Uncomment when needed
- */
-
-// #define DEBUG
 
 #include "fmgr.h"
-#include "./../../common/src/postgres_connection.h"
 #include "./../../common/src/debug_macro.h"
 #include "./../../common/src/time_msg.h"
 #include "./../../common/src/pgr_types.h"
