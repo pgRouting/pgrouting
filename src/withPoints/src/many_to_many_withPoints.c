b7b94b6eef602ad90d5e4e8f2e5e69d4711e3201
cvvergara <cvvergara@georepublic.de>
2016-11-07 12:06:31 -0600

    continue with wrapping postgres *.h files on withPoints

--- src/withPoints/src/many_to_many_withPoints.c
+++ src/withPoints/src/many_to_many_withPoints.c
@@ -27,8 +27,18 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 ********************************************************************PGR-GNU*/
 
-#include "postgres.h"
+#include "./../../common/src/postgres_connection.h"
+
+#ifdef __GNUC__
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#endif
+
 #include "funcapi.h"
+
+#ifdef __GNUC__
+#pragma GCC diagnostic pop
+#endif
+
 #include "utils/array.h"
 #include "catalog/pg_type.h"
 #if PGSQL_VERSION > 92
@@ -37,7 +47,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 
 #include "fmgr.h"
-#include "./../../common/src/postgres_connection.h"
 #include "./../../common/src/debug_macro.h"
 #include "./../../common/src/time_msg.h"
 #include "./../../common/src/pgr_types.h"
