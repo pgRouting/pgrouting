2b50a792cef64cbc2cf7960bdaa27c32c574bbe0
cvvergara <cvvergara@georepublic.de>
2016-11-27 13:26:12 -0600

    max_flow: started linting include files 1

--- src/max_flow/src/pgr_maxflow.hpp
+++ src/max_flow/src/pgr_maxflow.hpp
@@ -26,7 +26,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 #pragma once
 
-#if 0
+#if 1
 #if defined(__MINGW32__) || defined(_MSC_VER)
 #include <winsock2.h>
 #include <windows.h>
