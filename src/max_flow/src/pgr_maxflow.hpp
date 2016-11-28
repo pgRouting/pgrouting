9f1eb32d0f20ea627a4a23b3ddab669fd048bfd5
cvvergara <cvvergara@georepublic.de>
2016-11-28 13:43:17 -0600

    linting headers on max_flow 1

--- src/max_flow/src/pgr_maxflow.hpp
+++ src/max_flow/src/pgr_maxflow.hpp
@@ -26,14 +26,14 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 #pragma once
 
-#if 1
+#if 0
 #if defined(__MINGW32__) || defined(_MSC_VER)
 #include <winsock2.h>
 #include <windows.h>
+#endif
+#endif
 #ifdef unlink
 #undef unlink
-#endif
-#endif
 #endif
 
 #include <boost/config.hpp>
@@ -49,6 +49,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #include <utility>
 #include <vector>
 #include <set>
+#include <limits>
 
 
 // user's functions
