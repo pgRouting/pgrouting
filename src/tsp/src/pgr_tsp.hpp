156e3caf6c58e0fde6577b33858f2a9107b40b24
cvvergara <cvvergara@georepublic.de>
2016-11-29 13:17:11 -0600

    linting headers 13

--- src/tsp/src/pgr_tsp.hpp
+++ src/tsp/src/pgr_tsp.hpp
@@ -26,6 +26,7 @@
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
  *  ********************************************************************PGR-GNU*/
+
 #pragma once
 
 #include <sstream>
