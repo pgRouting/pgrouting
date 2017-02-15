5f9a6bad00781eba8991fe83adcbabef66dc885c
cvvergara <cvvergara@georepublic.de>
2017-02-15 14:39:45 -0600

    added astar innerquery tests

---
+++ src/astar/test/pgtap/astar-innerQuery-v2.0.sql
@@ -0,0 +1,75 @@
+\i setup.sql
+
+SELECT plan(87);
+SET client_min_messages TO ERROR;
+
+
+SELECT has_function('pgr_astar',
+    ARRAY['text', 'integer', 'integer', 'boolean', 'boolean']);
+
+SELECT function_returns('pgr_astar',
+    ARRAY['text', 'integer', 'integer', 'boolean', 'boolean'],
+    'setof pgr_costresult');
+
+
+-- ONE TO ONE
+--with reverse cost
+SELECT test_integer('pgr_astar', ',2,3, true, true)',
+    ARRAY['id', 'source', 'target', 'cost', 'reverse_cost', 'x1', 'y1', 'x2', 'y2'],
+    'id');
+SELECT test_integer('pgr_astar', ',2,3, true, true)',
+    ARRAY['id', 'source', 'target', 'cost', 'reverse_cost', 'x1', 'y1', 'x2', 'y2'],
+    'source');
+SELECT test_integer('pgr_astar', ',2,3, true, true)',
+    ARRAY['id', 'source', 'target', 'cost', 'reverse_cost', 'x1', 'y1', 'x2', 'y2'],
+    'target');
+SELECT test_float('pgr_astar', ',2,3, true, true)',
+    ARRAY['id', 'source', 'target', 'cost', 'reverse_cost', 'x1', 'y1', 'x2', 'y2'],
+    'cost');
+SELECT test_float('pgr_astar', ',2,3, true, true)',
+    ARRAY['id', 'source', 'target', 'cost', 'reverse_cost', 'x1', 'y1', 'x2', 'y2'],
+    'reverse_cost');
+SELECT test_float('pgr_astar', ',2,3, true, true)',
+    ARRAY['id', 'source', 'target', 'cost', 'reverse_cost', 'x1', 'y1', 'x2', 'y2'],
+    'x1');
+SELECT test_float('pgr_astar', ',2,3, true, true)',
+    ARRAY['id', 'source', 'target', 'cost', 'reverse_cost', 'x1', 'y1', 'x2', 'y2'],
+    'y1');
+SELECT test_float('pgr_astar', ',2,3, true, true)',
+    ARRAY['id', 'source', 'target', 'cost', 'reverse_cost', 'x1', 'y1', 'x2', 'y2'],
+    'x2');
+SELECT test_float('pgr_astar', ',2,3, true, true)',
+    ARRAY['id', 'source', 'target', 'cost', 'reverse_cost', 'x1', 'y1', 'x2', 'y2'],
+    'y2');
+
+
+--without reverse cost
+SELECT test_integer('pgr_astar', ',2,3, true, false)',
+    ARRAY['id', 'source', 'target', 'cost', 'x1', 'y1', 'x2', 'y2'],
+    'id');
+SELECT test_integer('pgr_astar', ',2,3, true, false)',
+    ARRAY['id', 'source', 'target', 'cost', 'x1', 'y1', 'x2', 'y2'],
+    'source');
+SELECT test_integer('pgr_astar', ',2,3, true, false)',
+    ARRAY['id', 'source', 'target', 'cost', 'x1', 'y1', 'x2', 'y2'],
+    'target');
+SELECT test_float('pgr_astar', ',2,3, true, false)',
+    ARRAY['id', 'source', 'target', 'cost', 'x1', 'y1', 'x2', 'y2'],
+    'cost');
+SELECT test_float('pgr_astar', ',2,3, true, false)',
+    ARRAY['id', 'source', 'target', 'cost', 'x1', 'y1', 'x2', 'y2'],
+    'x1');
+SELECT test_float('pgr_astar', ',2,3, true, false)',
+    ARRAY['id', 'source', 'target', 'cost', 'x1', 'y1', 'x2', 'y2'],
+    'y1');
+SELECT test_float('pgr_astar', ',2,3, true, false)',
+    ARRAY['id', 'source', 'target', 'cost', 'x1', 'y1', 'x2', 'y2'],
+    'x2');
+SELECT test_float('pgr_astar', ',2,3, true, false)',
+    ARRAY['id', 'source', 'target', 'cost', 'x1', 'y1', 'x2', 'y2'],
+    'y2');
+
+
+
+SELECT finish();
+ROLLBACK;
