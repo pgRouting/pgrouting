cb7179cd71d64ada036c9aaadce494612156aeff
cvvergara <cvvergara@georepublic.de>
2017-02-15 20:37:24 -0600

    added innerQuery tests to kdijkstra directory

---
+++ src/kdijkstra/test/pgtap/kdijkstraPath-innerquery.sql
@@ -0,0 +1,51 @@
+\i setup.sql
+
+SELECT plan(47);
+
+
+SELECT has_function('pgr_kdijkstrapath',
+    ARRAY['text', 'integer', 'integer[]', 'boolean', 'boolean']);
+
+SELECT function_returns('pgr_kdijkstrapath',
+    ARRAY['text', 'integer', 'integer[]', 'boolean', 'boolean'],
+    'setof pgr_costresult3');
+
+
+-- ONE TO ONE
+--with reverse cost
+SELECT test_integer('pgr_kdijkstrapath', ',2,ARRAY[3], true, true)',
+    ARRAY['id', 'source', 'target', 'cost', 'reverse_cost'],
+    'id');
+SELECT test_integer('pgr_kdijkstrapath', ',2,ARRAY[3], true, true)',
+    ARRAY['id', 'source', 'target', 'cost', 'reverse_cost'],
+    'source');
+SELECT test_integer('pgr_kdijkstrapath', ',2,ARRAY[3], true, true)',
+    ARRAY['id', 'source', 'target', 'cost', 'reverse_cost'],
+    'target');
+SELECT test_float('pgr_kdijkstrapath', ',2,ARRAY[3], true, true)',
+    ARRAY['id', 'source', 'target', 'cost', 'reverse_cost'],
+    'cost');
+SELECT test_float('pgr_kdijkstrapath', ',2,ARRAY[3], true, true)',
+    ARRAY['id', 'source', 'target', 'cost', 'reverse_cost'],
+    'reverse_cost');
+
+
+--without reverse cost
+SELECT test_integer('pgr_kdijkstrapath', ',2,ARRAY[3], true, false)',
+    ARRAY['id', 'source', 'target', 'cost'],
+    'id');
+SELECT test_integer('pgr_kdijkstrapath', ',2,ARRAY[3], true, false)',
+    ARRAY['id', 'source', 'target', 'cost'],
+    'source');
+SELECT test_integer('pgr_kdijkstrapath', ',2,ARRAY[3], true, false)',
+    ARRAY['id', 'source', 'target', 'cost'],
+    'target');
+SELECT test_float('pgr_kdijkstrapath', ',2,ARRAY[3], true, false)',
+    ARRAY['id', 'source', 'target', 'cost'],
+    'cost');
+
+
+
+
+SELECT finish();
+ROLLBACK;
