a25a46edbb7373ab71d6c297eecb0b34bed7df1b
cvvergara <cvvergara@georepublic.de>
2017-03-02 10:58:16 -0600

    adding a new pgtap test for pgr_bdAstarCostMatrix inner queries

---
+++ src/bdAstar/test/pgtap/bdAstarCostMatrix-innerQuery.sql
@@ -0,0 +1,15 @@
+\i setup.sql
+
+SELECT plan(172);
+
+
+SELECT has_function('pgr_bdastarcostmatrix', ARRAY['text', 'anyarray', 'boolean', 'integer', 'numeric', 'numeric']);
+
+SELECT function_returns('pgr_bdastarcostmatrix', ARRAY['text', 'anyarray', 'boolean', 'integer', 'numeric', 'numeric'],
+    'setof record');
+
+SELECT style_astar('pgr_bdastarcostmatrix', ', ARRAY[2, 3], true)');
+SELECT style_astar('pgr_bdastarcostmatrix', ', ARRAY[2, 3], false)');
+
+SELECT finish();
+ROLLBACK;
