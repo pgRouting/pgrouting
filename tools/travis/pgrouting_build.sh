c150b53050f736cedf127b52c1433067c4e2fe3c
cvvergara <cvvergara@georepublic.de>
2017-03-20 07:37:32 -0600

    fixing travis build

--- tools/travis/pgrouting_build.sh
+++ tools/travis/pgrouting_build.sh
@@ -10,6 +10,8 @@
 set -e 
 
 # build pgRouting
+mkdir build
+cd build
 cmake  -DPOSTGRESQL_VERSION=$POSTGRESQL_VERSION -DWITH_DOC=ON -DBUILD_DOXY=ON -DCMAKE_BUILD_TYPE=Debug
 make
 sudo make install
