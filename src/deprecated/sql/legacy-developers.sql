470e2728e80b41ae5c5af76af6a91459800a1a4b
cvvergara <cvvergara@georepublic.de>
2017-03-24 12:54:57 -0600

    fixed some syntax

--- src/deprecated/sql/legacy-developers.sql
+++ src/deprecated/sql/legacy-developers.sql
@@ -2,33 +2,11 @@
 -- Legacy functions that were ment to be used by 
 -- pgRouting developers
 -- These functions wer used on the plpgsql functions
+
+
+-- FILE intended to be used on 3.0
 -------------------------------------------------------------------------------
 
-
-/*PGR-GNU*****************************************************************
-
-FILE: noUnderUtilities.sql
-
-Copyright (c) 2015 Celia Vriginia Vergara Castillo
-Mail: vicky_vergara@hotmail.com
-
-------
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
-********************************************************************PGR-GNU*/
 
 -- deprecated on 2.1.0
 CREATE OR REPLACE FUNCTION pgr_getColumnName(tab text, col text)
@@ -37,9 +15,9 @@ $BODY$
 DECLARE
     table_oid regclass;
 BEGIN
-    table_oid := tab::regclass;
-    tab := replace(tab, '"', '');
-    return _pgr_getColumnName(tab, col, 0, 'pgr_getColumnName');
+    table_oid := $1::regclass;
+    $1 := replace($1, '"', '');
+    return _pgr_getColumnName($1, col, 0, 'pgr_getColumnName');
 END;
 $BODY$
 LANGUAGE plpgsql VOLATILE STRICT;
@@ -53,9 +31,9 @@ RETURNS RECORD AS
 $BODY$ 
 DECLARE table_oid regclass;
 BEGIN
-    table_oid := tab::regclass;
-    tab := replace(tab, '"', '');
-    SELECT * from _pgr_getTableName(tab, 0, 'pgr_getTableName') into sname,tname;
+    table_oid := $1::regclass;
+    $1 := replace($1, '"', '');
+    SELECT * FROM _pgr_getTableName($1, 0, 'pgr_getTableName') into sname,tname;
 END;
 $BODY$
 LANGUAGE plpgsql VOLATILE STRICT;
@@ -70,9 +48,9 @@ RETURNS boolean AS
 $BODY$
 DECLARE table_oid regclass;
 BEGIN
-    table_oid := tab::regclass;
-    tab := replace(tab, '"', '');
-    return  _pgr_isColumnIndexed(tab, col);
+    table_oid := $1::regclass;
+    $1 := replace(tab, '"', '');
+    return  _pgr_isColumnIndexed($1, $2);
 END;
 $BODY$
 LANGUAGE plpgsql VOLATILE STRICT;
@@ -88,9 +66,9 @@ DECLARE
     cname text;
     table_oid regclass;
 BEGIN
-    table_oid := tab::regclass;
-    tab := replace(tab, '"', '');
-    select * from _pgr_getColumnName(tab, col, 0, 'pgr_isColumnInTable') into cname;
+    table_oid := $1::regclass;
+    $1 := replace($1, '"', '');
+    SELECT * FROM _pgr_getColumnName($1, $2, 0, 'pgr_isColumnInTable') into cname;
     return  cname IS not NULL;
 END;
 $BODY$
@@ -100,12 +78,12 @@ $BODY$
 
 
 -- deprecated on 2.1.0
-create or replace function pgr_quote_ident(idname text)
+CREATE OR REPLACE function pgr_quote_ident(idname text)
 returns text as
 $BODY$
-    SELECT  _pgr_quote_ident(idname);
+    SELECT  quote_ident($1);
 $BODY$
-LANGUAGE sql VOLATILE STRICT;
+LANGUAGE sql IMMUTABLE;
 
 
 
@@ -115,33 +93,39 @@ LANGUAGE sql VOLATILE STRICT;
 CREATE OR REPLACE FUNCTION pgr_versionless(v1 text, v2 text)
 RETURNS boolean AS
 $BODY$
-BEGIN
-    return  _pgr_versionless(v1,v2);
-END;
+    SELECT _pgr_versionless($1, $2);
 $BODY$
-LANGUAGE plpgsql VOLATILE STRICT;
+LANGUAGE sql VOLATILE STRICT;
 
 
 
 
 -- deprecated on 2.1.0
-create or replace function pgr_startPoint(g geometry)
+CREATE OR REPLACE function pgr_startPoint(g geometry)
     returns geometry as
-$body$
-    SELECT  ST_startPoint(g);
-$body$
-language sql IMMUTABLE;
+$BODY$
+    SELECT  ST_startPoint($1);
+$BODY$
+LANGUAGE sql IMMUTABLE;
 
 
 
 
 -- deprecated on 2.1.0
-create or replace function pgr_endPoint(g geometry)
+CREATE OR REPLACE function pgr_endPoint(g geometry)
     returns geometry as
-$body$
-    SELECT  ST_endPoint(g);
-$body$
-language sql IMMUTABLE;
+$BODY$
+    SELECT  ST_endPoint($1);
+$BODY$
+LANGUAGE sql IMMUTABLE;
 
 
+
+-- deprecated on 2.1.0
+CREATE OR REPLACE FUNCTION pgr_pointoid(point geometry, tolerance double precision, vertname text, srid integer)
+RETURNS bigint AS
+$BODY$
+    SELECT _pgr_pointoid(i$1, $2, $3, $4);
+$BODY$
+LANGUAGE sql VOLATILE STRICT;
 
