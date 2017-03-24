8ba7ff391f954216815543e12ba960183023946e
cvvergara <cvvergara@georepublic.de>
2017-03-24 13:24:11 -0600

    added dijstra deprecated signature on 2.1.0

--- src/deprecated/sql/legacy.sql
+++ src/deprecated/sql/legacy.sql
@@ -7,3 +7,20 @@
 -- FILE intended to be used on 3.0
 -------------------------------------------------------------------------------
 
+
+-- Deprecated signature on 2.1.0
+CREATE OR REPLACE FUNCTION pgr_dijkstra(
+    edges_sql TEXT,
+    start_vid INTEGER,
+    end_vid INTEGER,
+    directed BOOLEAN,
+    has_rcost BOOLEAN)
+RETURNS SETOF pgr_costresult AS
+$BODY$
+    SELECT seq-1 , node::integer, edge::integer, cost
+    FROM pgr_dijkstra($1, ARRAY[$2]::BIGINT[], ARRAY[$3]::BIGINT[], directed);
+$BODY$
+LANGUAGE sql VOLATILE
+COST 100
+ROWS 1000;
+
