6c0a15aeffacd73235ef3706b87b90ee56efedb2
cvvergara <cvvergara@georepublic.de>
2017-03-24 13:29:37 -0600

    added driving distance  deprecated signature on 2.1.0

--- src/deprecated/sql/legacy.sql
+++ src/deprecated/sql/legacy.sql
@@ -7,6 +7,7 @@
 -- FILE intended to be used on 3.0
 -------------------------------------------------------------------------------
 
+-- I do not know yet if they will create a conflict when putting the legacy
 
 -- Deprecated signature on 2.1.0
 CREATE OR REPLACE FUNCTION pgr_dijkstra(
@@ -21,6 +22,20 @@ $BODY$
     FROM pgr_dijkstra($1, ARRAY[$2]::BIGINT[], ARRAY[$3]::BIGINT[], directed);
 $BODY$
 LANGUAGE sql VOLATILE
+COST 100
+ROWS 1000;
+
+
+
+
+-- Deprecated signature on 2.1.0
+CREATE OR REPLACE FUNCTION pgr_drivingDistance(edges_sql text, source INTEGER, distance FLOAT8, directed BOOLEAN, has_rcost BOOLEAN)
+RETURNS SETOF pgr_costresult AS
+$BODY$
+    SELECT seq - 1, node::integer, edge::integer, agg_cost
+    FROM pgr_drivingDistance($1, ARRAY[$2]::BIGINT[], $3, $4);
+$BODY$
+LANGUAGE plpgsql VOLATILE
 COST 100
 ROWS 1000;
 
