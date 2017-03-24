a91427c057d388f42977908b887d9cc9de5e40b3
cvvergara <cvvergara@georepublic.de>
2017-03-24 13:05:27 -0600

    added file for legacy for 3.0

---
+++ src/deprecated/sql/legacy.sql
@@ -0,0 +1,9 @@
+-------------------------------------------------------------------------------
+-- Legacy FUNCTIONs that were ment to be used by 
+-- Temporarly while the rewrite of 2.0
+-- These FUNCTIONs wer used on the plpgsql functions
+
+
+-- FILE intended to be used on 3.0
+-------------------------------------------------------------------------------
+
