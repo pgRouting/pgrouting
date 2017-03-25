c9daea4e03723590e097b97e4f317ffd1cc56301
cvvergara <cvvergara@georepublic.de>
2017-03-25 17:19:07 -0600

    changed language of pgr_version to sql instead of plpgsql

--- src/common/sql/pgrouting_version.sql
+++ src/common/sql/pgrouting_version.sql
@@ -22,16 +22,11 @@ RETURNS TABLE(
 		boost varchar
 	) AS
 $BODY$
-
-DECLARE
-
-BEGIN
-    RETURN QUERY SELECT '${PGROUTING_VERSION}'::varchar AS version, 
-    					'${PGROUTING_FULL_VERSION}'::varchar AS tag, 
-                        '${PGROUTING_GIT_HASH}'::varchar AS hash, 
-                        '${PGROUTING_GIT_BRANCH}'::varchar AS branch, 
-                        '${Boost_MAJOR_VERSION}.${Boost_MINOR_VERSION}.${Boost_SUBMINOR_VERSION}'::varchar AS boost;
-END;
+    SELECT '${PGROUTING_VERSION}'::varchar AS version, 
+        '${PGROUTING_FULL_VERSION}'::varchar AS tag, 
+        '${PGROUTING_GIT_HASH}'::varchar AS hash, 
+        '${PGROUTING_GIT_BRANCH}'::varchar AS branch, 
+        '${Boost_MAJOR_VERSION}.${Boost_MINOR_VERSION}.${Boost_SUBMINOR_VERSION}'::varchar AS boost;
 $BODY$
-LANGUAGE plpgsql IMMUTABLE;
+LANGUAGE sql IMMUTABLE;
 
