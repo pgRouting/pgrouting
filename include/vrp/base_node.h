059b874146422cc4b8491409099c349e8fcd41ba
cvvergara <cvvergara@georepublic.de>
2017-04-25 16:36:00 -0500

    trying to fix appveyor errors up to this point 3

--- include/vrp/base_node.h
+++ include/vrp/base_node.h
@@ -31,7 +31,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 #include <string>
 #include <ostream>
-#include <stdlib.h>
+#include <stdint.h>
 
 namespace pgrouting {
 namespace vrp {
