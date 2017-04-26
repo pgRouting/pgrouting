f72ea92b8a2e5bead10c6d2ceaf9b1e489e7bc34
cvvergara <cvvergara@georepublic.de>
2017-04-25 20:10:57 -0500

    code compiles after moving pgr_allpairs.hpp

--- src/allpairs/src/floydWarshall_driver.cpp
+++ src/allpairs/src/floydWarshall_driver.cpp
@@ -33,7 +33,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #include <deque>
 #include <vector>
 
-#include "./pgr_allpairs.hpp"
+#include "algorithm/pgr_allpairs.hpp"
 
 #include "cpp_common/pgr_assert.h"
 
