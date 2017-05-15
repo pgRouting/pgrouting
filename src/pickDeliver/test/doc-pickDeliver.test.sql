adde41063fdc1d34297236c9f4c24cc26b8e87c4
cvvergara <cvvergara@georepublic.de>
2017-05-15 11:08:19 -0500

    initial test pass

--- src/pickDeliver/test/doc-pickDeliver.test.sql
+++ src/pickDeliver/test/doc-pickDeliver.test.sql
@@ -1,7 +1,9 @@
 \echo --q1
+-- using the default values
 SELECT * FROM pgr_pickDeliver(
     'SELECT * FROM orders ORDER BY id',
     'SELECT * from vehicles',
+    -- matrix query
     'WITH
     A AS (
         SELECT p_node_id AS id, p_x AS x, p_y AS y FROM orders
@@ -11,7 +13,7 @@ SELECT * FROM pgr_pickDeliver(
         SELECT start_node_id, start_x, start_y FROM vehicles
     )
     SELECT A.id AS start_vid, B.id AS end_vid, sqrt( (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y)) AS agg_cost
-    FROM A, A AS B WHERE A.id != B.id',
-    30);
+    FROM A, A AS B WHERE A.id != B.id'
+    );
 
 \echo --q2
