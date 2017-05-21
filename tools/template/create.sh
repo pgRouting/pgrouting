fa4abe32528c92b11677b6341c18bcb84b5f4734
cvvergara <cvvergara@georepublic.de>
2017-05-21 17:37:46 -0500

    removing testing directory

--- tools/template/create.sh
+++ tools/template/create.sh
@@ -29,7 +29,7 @@ fi
 
 
 #dont put pgr_ on MY_FUNCTION_NAME that is done automatically
-MY_FUNCTION_NAME="vickyDijkstra"
+MY_FUNCTION_NAME="funnyDijkstra"
 DEVELOPER_NAME="Celia Virginia Vergara Castillo"
 DEVELOPER_EMAIL="vicky_vergara\@hotmail.com" 
 YEAR="2017"
