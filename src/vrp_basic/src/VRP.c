7731ed1ade9f7d19cbe2441bf0fad394da828998
cvvergara <cvvergara@georepublic.de>
2017-05-22 11:04:22 -0500

    appveyor fixing arrays_input 4

--- src/vrp_basic/src/VRP.c
+++ src/vrp_basic/src/VRP.c
@@ -25,6 +25,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #include "catalog/pg_type.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <search.h>
 #include <string.h>
 
