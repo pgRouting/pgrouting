cc4bfedcb1fed84f0bcce8f824e5775fc65bd1ef
cvvergara <cvvergara@georepublic.de>
2017-05-23 10:28:27 -0500

    fixing preprocessor conditional 2

--- src/common/src/vehicles_input.c
+++ src/common/src/vehicles_input.c
@@ -26,7 +26,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 #include <limits.h>
 
-#if defind PGSQL_VERSION && PGSQL_VERSION == 100
+#if PGSQL_VERSION == 100
 #include <float.h>
 #endif
 
