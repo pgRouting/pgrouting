0aaaf3f0363e07776aa3c8dcb6b06e9e20af43af
vidhan13j07 <vidhanj1307@gmail.com>
2017-06-02 22:33:22 +0530

    Fixed file location

--- src/dijkstraTRSP/test/pgtap/empty_set.test.sql
+++ src/dijkstraTRSP/test/pgtap/empty_set.test.sql
@@ -1,14 +1,19 @@
+\i setup.sql
 
-\echo -- testing from an existing starting vertex to an non-existing destination in directed graph
-\echo -- expecting results: empty set
+SELECT plan(8);
+
+-- testing from an existing starting vertex to an non-existing destination in directed graph
+-- expecting results: empty set
+PREPARE q1 AS
 SELECT * FROM pgr_dijkstraTRSP(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table WHERE id = 4 OR id = 7',
     'SELECT * FROM restrictions',
     2, 3
 );
 
-\echo -- testing from an existing starting vertex to an non-existing destination in undirected graph
-\echo -- expecting results: empty set
+-- testing from an existing starting vertex to an non-existing destination in undirected graph
+-- expecting results: empty set
+PREPARE q2 AS
 SELECT * FROM pgr_dijkstraTRSP(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table WHERE id = 4 OR id = 7',
     'SELECT * FROM restrictions',
@@ -16,16 +21,18 @@ SELECT * FROM pgr_dijkstraTRSP(
     FALSE
 );
 
-\echo -- testing from an existing starting vertex to an non-existing destination in directed graph
-\echo -- expecting results: empty set
+-- testing from an existing starting vertex to an non-existing destination in directed graph
+-- expecting results: empty set
+PREPARE q3 AS
 SELECT * FROM pgr_dijkstraTRSP(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table WHERE id = 4 OR id = 7',
     'SELECT * FROM restrictions where id > 10',
     2, 3
 );
 
-\echo -- testing from an existing starting vertex to an non-existing destination in undirected graph
-\echo -- expecting results: empty set
+-- testing from an existing starting vertex to an non-existing destination in undirected graph
+-- expecting results: empty set
+PREPARE q4 AS
 SELECT * FROM pgr_dijkstraTRSP(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table WHERE id = 4 OR id = 7',
     'SELECT * FROM restrictions where id > 10',
@@ -33,17 +40,18 @@ SELECT * FROM pgr_dijkstraTRSP(
     FALSE
 );
 
-
-\echo -- testing from an non-existing starting vertex to an existing destination in directed graph
-\echo -- expecting results: empty set
+-- testing from an non-existing starting vertex to an existing destination in directed graph
+-- expecting results: empty set
+PREPARE q5 AS
 SELECT * FROM pgr_dijkstraTRSP(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table WHERE id = 4 OR id = 7',
     'SELECT * FROM restrictions',
     6, 8
 );
 
-\echo -- testing from an non-existing starting vertex to an existing destination in undirected graph
-\echo -- expecting results: empty set
+-- testing from an non-existing starting vertex to an existing destination in undirected graph
+-- expecting results: empty set
+PREPARE q6 AS
 SELECT * FROM pgr_dijkstraTRSP(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table WHERE id = 4 OR id = 7',
     'SELECT * FROM restrictions',
@@ -51,20 +59,32 @@ SELECT * FROM pgr_dijkstraTRSP(
     FALSE
 );
 
-\echo -- testing from an non-existing starting vertex to an existing destination in directed graph
-\echo -- expecting results: empty set
+-- testing from an non-existing starting vertex to an existing destination in directed graph
+-- expecting results: empty set
+PREPARE q7 AS
 SELECT * FROM pgr_dijkstraTRSP(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table WHERE id = 4 OR id = 7',
     'SELECT * FROM restrictions where id > 10',
     6, 8
 );
 
-\echo -- testing from an non-existing starting vertex to an existing destination in undirected graph
-\echo -- expecting results: empty set
+-- testing from an non-existing starting vertex to an existing destination in undirected graph
+-- expecting results: empty set
+PREPARE q8 AS
 SELECT * FROM pgr_dijkstraTRSP(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table WHERE id = 4 OR id = 7',
     'SELECT * FROM restrictions where id > 10',
     6, 8,
     FALSE
 );
-\echo -- q3
+SELECT is_empty('q1');
+SELECT is_empty('q2');
+SELECT is_empty('q3');
+SELECT is_empty('q4');
+SELECT is_empty('q5');
+SELECT is_empty('q6');
+SELECT is_empty('q7');
+SELECT is_empty('q8');
+-- q3
+SELECT * FROM finish();
+ROLLBACK;
