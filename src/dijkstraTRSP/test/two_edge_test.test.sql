5e7655bd8c307e3a590488ce7f9992e2c47e0fd9
vidhan13j07 <vidhanj1307@gmail.com>
2017-06-02 20:55:51 +0530

    Added tests

--- src/dijkstraTRSP/test/two_edge_test.test.sql
+++ src/dijkstraTRSP/test/two_edge_test.test.sql
@@ -1,12 +1,14 @@
 
-\echo -- q1
+\echo -- testing from an existing starting vertex to an non-existing destination in directed graph
+\echo -- expecting results: empty set
 SELECT * FROM pgr_dijkstraTRSP(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table WHERE id = 4 OR id = 7',
     'SELECT * FROM restrictions_table',
     2, 3
 );
 
-\echo -- q2
+\echo -- testing from an existing starting vertex to an non-existing destination in undirected graph
+\echo -- expecting results: empty set
 SELECT * FROM pgr_dijkstraTRSP(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table WHERE id = 4 OR id = 7',
     'SELECT * FROM restrictions_table',
