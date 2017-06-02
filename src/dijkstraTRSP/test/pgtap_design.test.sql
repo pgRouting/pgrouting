4b2272395a8f8f5c4f327756068d1467396fb5a4
vidhan13j07 <vidhanj1307@gmail.com>
2017-06-02 23:35:19 +0530

    remove comments

--- src/dijkstraTRSP/test/pgtap_design.test.sql
+++ src/dijkstraTRSP/test/pgtap_design.test.sql
@@ -1,14 +1,9 @@
-
-\echo -- testing from an existing starting vertex to an non-existing destination in directed graph
-\echo -- expecting results: empty set
 SELECT * FROM pgr_dijkstraTRSP(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table WHERE id = 4 OR id = 7',
     'SELECT * FROM restrictions',
     2, 3
 );
 
-\echo -- testing from an existing starting vertex to an non-existing destination in undirected graph
-\echo -- expecting results: empty set
 SELECT * FROM pgr_dijkstraTRSP(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table WHERE id = 4 OR id = 7',
     'SELECT * FROM restrictions',
@@ -16,16 +11,12 @@ SELECT * FROM pgr_dijkstraTRSP(
     FALSE
 );
 
-\echo -- testing from an existing starting vertex to an non-existing destination in directed graph
-\echo -- expecting results: empty set
 SELECT * FROM pgr_dijkstraTRSP(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table WHERE id = 4 OR id = 7',
     'SELECT * FROM restrictions where id > 10',
     2, 3
 );
 
-\echo -- testing from an existing starting vertex to an non-existing destination in undirected graph
-\echo -- expecting results: empty set
 SELECT * FROM pgr_dijkstraTRSP(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table WHERE id = 4 OR id = 7',
     'SELECT * FROM restrictions where id > 10',
@@ -33,17 +24,12 @@ SELECT * FROM pgr_dijkstraTRSP(
     FALSE
 );
 
-
-\echo -- testing from an non-existing starting vertex to an existing destination in directed graph
-\echo -- expecting results: empty set
 SELECT * FROM pgr_dijkstraTRSP(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table WHERE id = 4 OR id = 7',
     'SELECT * FROM restrictions',
     6, 8
 );
 
-\echo -- testing from an non-existing starting vertex to an existing destination in undirected graph
-\echo -- expecting results: empty set
 SELECT * FROM pgr_dijkstraTRSP(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table WHERE id = 4 OR id = 7',
     'SELECT * FROM restrictions',
@@ -51,20 +37,15 @@ SELECT * FROM pgr_dijkstraTRSP(
     FALSE
 );
 
-\echo -- testing from an non-existing starting vertex to an existing destination in directed graph
-\echo -- expecting results: empty set
 SELECT * FROM pgr_dijkstraTRSP(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table WHERE id = 4 OR id = 7',
     'SELECT * FROM restrictions where id > 10',
     6, 8
 );
 
-\echo -- testing from an non-existing starting vertex to an existing destination in undirected graph
-\echo -- expecting results: empty set
 SELECT * FROM pgr_dijkstraTRSP(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table WHERE id = 4 OR id = 7',
     'SELECT * FROM restrictions where id > 10',
     6, 8,
     FALSE
 );
-\echo -- q3
