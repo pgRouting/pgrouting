25461e75fd9d19d040cb3568b230947fe4aaa6e8
vidhan13j07 <vidhanj1307@gmail.com>
2017-06-02 20:31:33 +0530

    Added edges 4 and 7 only in two edge test

--- src/dijkstraTRSP/test/two_edge_test.test.sql
+++ src/dijkstraTRSP/test/two_edge_test.test.sql
@@ -1,14 +1,14 @@
 
 \echo -- q1
 SELECT * FROM pgr_dijkstraTRSP(
-    'SELECT id, source, target, cost, reverse_cost FROM edge_table',
+    'SELECT id, source, target, cost, reverse_cost FROM edge_table WHERE id = 4 OR id = 7',
     'SELECT * FROM restrictions_table',
     2, 3
 );
 
 \echo -- q2
 SELECT * FROM pgr_dijkstraTRSP(
-    'SELECT id, source, target, cost, reverse_cost FROM edge_table',
+    'SELECT id, source, target, cost, reverse_cost FROM edge_table WHERE id = 4 OR id = 7',
     'SELECT * FROM restrictions_table',
     2, 3,
     FALSE
