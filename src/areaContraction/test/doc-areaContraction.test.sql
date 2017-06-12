c86755edef203582fba21e2809be108d17b64aad
Ankur Shukla <work.ankurshukla@gmail.com>
2017-06-12 18:46:42 +0530

    Added correct border vertices to test

--- src/areaContraction/test/doc-areaContraction.test.sql
+++ src/areaContraction/test/doc-areaContraction.test.sql
@@ -2,13 +2,13 @@
 \echo -- q1
 SELECT * FROM pgr_areaContraction(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table',
-    ARRAY[2, 3]
+    ARRAY[1,4,7,13]
 );
 
 \echo -- q2
 SELECT * FROM pgr_areaContraction(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table',
-    ARRAY[2, 3],
+    ARRAY[1,4,7,13],
     FALSE
 );
 \echo -- q3
