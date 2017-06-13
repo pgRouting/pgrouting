6c6ad42080f424777668dd135d6091183678f4c2
Maoguang Wang <xjtumg1007@gmail.com>
2017-06-13 21:37:41 +0800

    added q2 & q3

--- src/components/test/doc-pgr_connectedComponentsV.test.sql
+++ src/components/test/doc-pgr_connectedComponentsV.test.sql
@@ -3,3 +3,13 @@
 SELECT * FROM pgr_connectedComponentsV(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table'
 );
+
+\echo -- q2
+SELECT * FROM pgr_connectedComponentsV(
+    'SELECT id, source, target, cost, reverse_cost FROM edge_table'
+);
+
+\echo -- q3
+SELECT * FROM pgr_connectedComponentsV(
+    'SELECT id, source, target, cost, reverse_cost FROM edge_table'
+);
