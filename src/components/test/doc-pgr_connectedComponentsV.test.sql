be71e5c8a7bb1bb2218be62dafab8e3f0fc5668c
Maoguang Wang <xjtumg1007@gmail.com>
2017-06-14 01:07:06 +0800

    fix documentation test

--- src/components/test/doc-pgr_connectedComponentsV.test.sql
+++ src/components/test/doc-pgr_connectedComponentsV.test.sql
@@ -8,8 +8,3 @@ SELECT * FROM pgr_connectedComponentsV(
 SELECT * FROM pgr_connectedComponentsV(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table'
 );
-
-\echo -- q3
-SELECT * FROM pgr_connectedComponentsV(
-    'SELECT id, source, target, cost, reverse_cost FROM edge_table'
-);
