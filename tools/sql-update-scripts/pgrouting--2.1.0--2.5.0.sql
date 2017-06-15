ed56279c9b2f63b8cc019b8985edfa33013ae4a4
cvvergara <cvvergara@georepublic.de>
2017-06-15 10:51:53 -0500

    adding the queries files

--- tools/sql-update-scripts/pgrouting--2.1.0--2.5.0.sql
+++ tools/sql-update-scripts/pgrouting--2.1.0--2.5.0.sql
@@ -3327,7 +3327,6 @@ CREATE OR REPLACE FUNCTION _pgr_pickDeliver(
     OUT vehicle_id BIGINT,
     OUT stop_seq INTEGER,
     OUT stop_type INTEGER,
-    OUT stop_id BIGINT,
     OUT order_id BIGINT,
     OUT cargo FLOAT,
     OUT travel_time FLOAT,
@@ -3369,6 +3368,70 @@ CREATE OR REPLACE FUNCTION _pgr_pickDeliverEuclidean (
  'MODULE_PATHNAME', 'pickDeliverEuclidean'
     LANGUAGE c VOLATILE;
 
+
+
+-- for the sake of Reginas book I am keeping this signature
+
+CREATE OR REPLACE FUNCTION _pgr_pickDeliver(
+    customers_sql TEXT,
+    max_vehicles INTEGER,
+    capacity FLOAT,
+    speed FLOAT DEFAULT 1,
+    max_cycles INTEGER DEFAULT 10,
+
+    OUT seq INTEGER,
+    OUT vehicle_id INTEGER,
+    OUT vehicle_seq INTEGER,
+    OUT stop_id BIGINT,
+    OUT travel_time FLOAT,
+    OUT arrival_time FLOAT,
+    OUT wait_time FLOAT,
+    OUT service_time FLOAT,
+    OUT departure_time FLOAT
+)
+RETURNS SETOF RECORD AS
+$BODY$
+DECLARE
+    orders_sql TEXT;
+    vehicles_sql TEXT;
+    final_sql TEXT;
+BEGIN
+    orders_sql = $$WITH
+        customer_data AS ($$ || customers_sql || $$ ),
+        pickups AS (
+            SELECT id, demand, x as p_x, y as p_y, opentime as p_open, closetime as p_close, servicetime as p_service
+            FROM  customer_data WHERE pindex = 0 AND id != 0
+        ),
+        deliveries AS (
+            SELECT pindex AS id, x as d_x, y as d_y, opentime as d_open, closetime as d_close, servicetime as d_service
+            FROM  customer_data WHERE dindex = 0 AND id != 0
+        )
+        SELECT * FROM pickups JOIN deliveries USING(id) ORDER BY pickups.id
+    $$;
+
+    vehicles_sql = $$WITH
+        customer_data AS ($$ || customers_sql || $$ )
+        SELECT id, x AS start_x, y AS start_y,
+            opentime AS start_open, closetime AS start_close, $$ ||
+            capacity || $$ AS capacity, $$ || max_vehicles || $$ AS number, $$ || speed || $$ AS speed
+            FROM customer_data WHERE id = 0 LIMIT 1
+        $$;
+--  seq | vehicle_id | vehicle_seq | stop_id | travel_time | arrival_time | wait_time | service_time | departure_time
+    final_sql = $$ WITH
+        customer_data AS ($$ || customers_sql || $$ ),
+        p_deliver AS (SELECT * FROM _pgr_pickDeliverEuclidean('$$ || orders_sql || $$',  '$$ || vehicles_sql || $$',  1, $$ || max_cycles || $$ )),
+        picks AS (SELECT p_deliver.*, pindex, dindex, id AS the_id FROM p_deliver JOIN customer_data ON (id = order_id AND stop_type = 2)),
+        delivers AS (SELECT p_deliver.*, pindex, dindex, dindex AS the_id FROM p_deliver JOIN customer_data ON (id = order_id AND stop_type = 3)),
+        depots AS (SELECT p_deliver.*, 0 as pindex, 0 as dindex, 0 AS the_id FROM p_deliver WHERE (stop_type IN (-1,1,6))),
+        the_union AS (SELECT * FROM picks UNION SELECT * FROM delivers UNION SELECT * from depots)
+
+        SELECT (row_number() over(ORDER BY a.seq))::INTEGER, vehicle_seq, a.stop_seq, the_id::BIGINT, a.travel_time, a.arrival_time, a.wait_time, a.service_time, a.departure_time
+        FROM (SELECT * FROM the_union) AS a ORDER BY a.seq
+        $$;
+    RETURN QUERY EXECUTE final_sql;
+END;
+$BODY$
+LANGUAGE plpgsql VOLATILE STRICT;
 
 -----------------------------------------------------------------------
 -- Core function for vrp with sigle depot computation
