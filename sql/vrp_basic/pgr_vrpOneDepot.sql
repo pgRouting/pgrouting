1d2a605e631acc7ebbf39dc8c6a960f5a1a4f46a
cvvergara <cvvergara@georepublic.de>
2017-06-18 23:54:28 -0500

    adding the wrapper for pgr_vrpOneDepot

---
+++ sql/vrp_basic/pgr_vrpOneDepot.sql
@@ -0,0 +1,56 @@
+/*PGR-GNU*****************************************************************
+
+Copyright (c) 2015 pgRouting developers
+Mail: project@pgrouting.org
+
+------
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+********************************************************************PGR-GNU*/
+-----------------------------------------------------------------------
+-- Core function for vrp with sigle depot computation
+-- See README for description
+-----------------------------------------------------------------------
+--
+--
+
+create or replace function pgr_vrpOneDepot(
+	order_sql text,
+	vehicle_sql text,
+	cost_sql text,
+	depot_id integer,
+
+	OUT oid integer,
+	OUT opos integer,
+	OUT vid integer,
+	OUT tarrival integer,
+	OUT tdepart integer)
+RETURNS SETOF RECORD AS
+$BODY$
+BEGIN
+    RETURN query SELECT order_id::INTEGER, stop_seq::INTEGER, vehicle_id::INTEGER, arrival_time::INTEGER, departure_time::INTEGER
+    FROM _pgr_vrpOneDepot($1, $2,
+       '
+            SELECT src_id AS start_vid, dest_id AS end_vid, traveltime AS agg_cost FROM ('||$3||') AS a
+       ',
+        $4
+    ) a;
+END
+$BODY$
+LANGUAGE plpgsql VOLATILE
+COST 100
+ROWS 1000;
+
