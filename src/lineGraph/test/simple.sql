ee361c9f890462f544f7ccaa24af17dc73a6a470
vidhan13j07 <vidhanj1307@gmail.com>
2017-07-20 03:31:57 +0530

    changed table name

--- src/lineGraph/test/simple.sql
+++ src/lineGraph/test/simple.sql
@@ -1,4 +1,5 @@
-Create Table edge_table(
+DROP TABLE IF EXISTS edge_table11;
+Create Table edge_table11(
     id BIGINT,
     source BIGINT,
     target BIGINT,
@@ -6,7 +7,7 @@ Create Table edge_table(
     reverse_cost FLOAT
 );
 
-INSERT INTO edge_table(id, source, target, cost, reverse_cost) VALUES
+INSERT INTO edge_table11(id, source, target, cost, reverse_cost) VALUES
 (1, 1, 2, 7, 7),
 (2, 1, 3, 6, 9),
 (3, 1, 6, 14, 14),
