75d9ade43ebee4b16d77de76236f980773fd4dee
cvvergara <cvvergara@georepublic.de>
2017-08-11 12:06:00 -0500

    moving the changes on develop of tools/winnie/build_pgrouting.sh to master

--- tools/winnie/build_pgrouting.sh
+++ tools/winnie/build_pgrouting.sh
@@ -22,6 +22,12 @@ JENKINS_DEBUG=1
 
 if [ $JENKINS_DEBUG -eq 1 ]
 then
+    #---------------
+    echo
+    echo "***************************"
+    echo Recived variables
+    echo "**************************"
+    #---------------
     echo "OS_BUILD ${OS_BUILD}"
     echo "PG_VER ${PG_VER}"
     echo "PGHOST ${PGHOST}"
@@ -33,7 +39,10 @@ then
 fi
 
 #---------------
-# Deduced variables
+echo
+echo "***************************"
+echo Deduced variables
+echo "***************************"
 #---------------
 
 export PGUSER=postgres
@@ -41,11 +50,10 @@ export PROJECTS=/projects
 export PGPATHEDB=${PROJECTS}/postgresql/rel/pg${PG_VER}w${OS_BUILD}${GCC_TYPE}edb  #this is so winnie know's where to copy the dlls for vc++ edb compiled postgresql testing
 export PGPATH=${PROJECTS}/postgresql/rel/pg${PG_VER}w${OS_BUILD}${GCC_TYPE}
 export PATHOLD=$PATH
-export PATHOLD="/mingw/bin:/mingw/include:/c/Windows/system32:/c/Windows"
+#export PATHOLD="/mingw/bin:/mingw/include:/c/Windows/system32:/c/Windows"
 export PGWINVER=${PG_VER}w${OS_BUILD}${GCC_TYPE}edb
-export PATH="${PATHOLD}:${PGPATH}/bin:${PGPATH}/lib:${PGPATH}/include"
+export PATH="${PATHOLD}:/usr/bin:${PGPATH}/bin:${PGPATH}/lib:${PGPATH}/include"
 export PATH="${PROJECTS}/rel-libiconv-1.13.1w${OS_BUILD}${GCC_TYPE}/include:${PATH}"
-
 
 if [ $JENKINS_DEBUG -eq 1 ]
 then
@@ -100,12 +108,57 @@ PATH="${PATH}:${CGAL_PATH}/include:${CGAL_PATH}/lib"
 export PATH="${PATH}:/cmake/bin"
 export PATH="${PATH}:.:/bin:/include"
 
+cmake --version
+
 echo "PATH ${PATH}"
 
 cd ${PROJECTS}/pgrouting
 rm -rf build${PGROUTING_VER}w${OS_BUILD}${GCC_TYPE}
 mkdir build${PGROUTING_VER}w${OS_BUILD}${GCC_TYPE}
 cd build${PGROUTING_VER}w${OS_BUILD}${GCC_TYPE}
+
+
+#---------------
+echo
+echo "***************************"
+echo "Current contents of PGPATH ${PGPATH}"
+echo "***************************"
+#---------------
+ls ${PGPATH}/lib/libpgrouting*
+ls ${PGPATH}/share/extension/pgrouting*
+
+#---------------
+echo
+echo "***************************"
+echo "Current contents of PGPATHEDB ${PGPATHEDB}"
+echo "***************************"
+#---------------
+ls ${PGPATHEDB}/lib/libpgrouting*
+ls ${PGPATHEDB}/share/extension/pgrouting*
+
+rm ${PGPATH}/lib/libpgrouting*
+rm ${PGPATH}/share/extension/pgrouting*
+rm ${PGPATHEDB}/lib/libpgrouting*
+rm ${PGPATHEDB}/share/extension/pgrouting*
+
+#---------------
+echo
+echo "***************************"
+echo "After removing in PGPATH ${PGPATH}"
+echo "***************************"
+#---------------
+ls ${PGPATH}/lib/libpgrouting*
+ls ${PGPATH}/share/extension/pgrouting*
+
+#---------------
+echo
+echo "***************************"
+echo "After removing in PGPATHEDB ${PGPATHEDB}"
+echo "***************************"
+#---------------
+ls ${PGPATHEDB}/lib/libpgrouting*
+ls ${PGPATHEDB}/share/extension/pgrouting*
+cmake --version
 
 cmake -G "MSYS Makefiles" -DCMAKE_VERBOSE_MAKEFILE=ON \
  -DBOOST_ROOT:PATH=${BOOSTROOT_PATH} \
@@ -118,40 +171,54 @@ cmake -G "MSYS Makefiles" -DCMAKE_VERBOSE_MAKEFILE=ON \
  -DCMAKE_BUILD_TYPE=Release \
  ../branches/${PGROUTING_VER}
 
-#first delete old pgrouting files from installed folder before we reinstall
-
-echo "Current contents of PGPATH ${PGPATH}"
-ls ${PGPATH}/lib/libpgrouting*
-ls ${PGPATH}/share/extension/pgrouting*
-echo "Current contents of PGPATHEDB ${PGPATHEDB}"
-ls ${PGPATHEDB}/lib/libpgrouting*
-ls ${PGPATHEDB}/share/extension/pgrouting*
-
-rm ${PGPATH}/lib/libpgrouting*
-rm ${PGPATH}/share/extension/pgrouting*
-rm ${PGPATHEDB}/lib/libpgrouting*
-rm ${PGPATHEDB}/share/extension/pgrouting*
-
-echo "After removing in PGPATH ${PGPATH}"
-ls ${PGPATH}/lib/libpgrouting*
-ls ${PGPATH}/share/extension/pgrouting*
-echo "After removing in PGPATHEDB ${PGPATHEDB}"
-ls ${PGPATHEDB}/lib/libpgrouting*
-ls ${PGPATHEDB}/share/extension/pgrouting*
-
+#---------------
+echo
+echo "***************************"
+echo make
+echo "***************************"
+#---------------
 make 
+
+#---------------
+echo
+echo "***************************"
+echo make install
+echo "***************************"
+#---------------
 make install
+
+#---------------
+echo
+echo "***************************"
+echo "Current contents of PGPATH ${PGPATH}"
+echo "***************************"
+#---------------
+ls ${PGPATH}/lib/libpgrouting*
+ls ${PGPATH}/share/extension/pgrouting*
+
+#---------------
+echo
+echo "***************************"
+echo "Current contents of PGPATHEDB ${PGPATHEDB}"
+echo Should be empty
+echo "***************************"
+#---------------
+ls ${PGPATHEDB}/lib/libpgrouting*
+ls ${PGPATHEDB}/share/extension/pgrouting*
+
 
 #we need uninstall and reinstall copy to VC++ EDB instance if we want to test on standard Windows installed versions
 #cp *.dll ${PGPATHEDB}/lib/  #TODO remove this once we fix so the .dlls are created in lib folder
 cp lib/*.dll ${PGPATHEDB}/lib/
-cp lib/*.sql ${PGPATHEDB}/share/extension/
-cp lib/*.control ${PGPATHEDB}/share/extension/
+cp sql/pgrouting*.sql ${PGPATHEDB}/share/extension/
+cp sql/common/*.control ${PGPATHEDB}/share/extension/
 
-echo "After copy in PGPATH ${PGPATH}"
-ls ${PGPATH}/lib/libpgrouting*
-ls ${PGPATH}/share/extension/pgrouting*
-echo "After copyin PGPATHEDB ${PGPATHEDB}"
+#---------------
+echo
+echo "***************************"
+echo "After copying to PGPATHEDB ${PGPATHEDB}"
+echo "***************************"
+#---------------
 ls ${PGPATHEDB}/lib/libpgrouting*
 ls ${PGPATHEDB}/share/extension/pgrouting*
 
@@ -161,8 +228,21 @@ cd ${PROJECTS}/pgrouting/branches/${PGROUTING_VER}
 #perl tools/testers/algorithm-tester.pl  -pgver ${PG_VER} -pgisver "${POSTGIS_VER}" -pgport "${PGPORT}"  -alg common -clean -v
 #perl tools/testers/algorithm-tester.pl  -pgver ${PG_VER} -pgisver "${POSTGIS_VER}" -pgport "${PGPORT}"  -alg dijkstra -clean -v
 #perl tools/testers/algorithm-tester.pl  -pgver ${PG_VER} -pgisver "${POSTGIS_VER}" -pgport "${PGPORT}"  -alg contraction
-perl tools/testers/algorithm-tester.pl  -pgver ${PG_VER} -pgisver "${POSTGIS_VER}" -pgport "${PGPORT}"
 
+#perl tools/testers/algorithm-tester.pl  -pgver ${PG_VER} -pgisver "${POSTGIS_VER}" -pgport "${PGPORT}"
+
+if [ $OS_BUILD -eq 32 ] 
+then
+
+    perl tools/testers/algorithm-tester.pl  -pgver ${PG_VER} -pgisver "${POSTGIS_VER}" -pgport "${PGPORT}"
+
+else
+
+    psql -c "CREATE DATABASE ___pgr___test___"
+    sh tools/testers/pg_prove_tests.sh ${PGUSER}
+    psql -c "DROP DATABASE ___pgr___test___"
+
+fi
 
 cd ${PROJECTS}/pgrouting/build${PGROUTING_VER}w${OS_BUILD}${GCC_TYPE}/lib
 strip *.dll
