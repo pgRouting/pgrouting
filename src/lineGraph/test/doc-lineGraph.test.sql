863198e8b0e97d0041e5137df2e8b2da4d00e98f
vidhan13j07 <vidhanj1307@gmail.com>
2017-08-12 20:59:26 +0530

    remove unnecessary sql queries

--- src/lineGraph/test/doc-lineGraph.test.sql
+++ src/lineGraph/test/doc-lineGraph.test.sql
@@ -13,9 +13,3 @@ SELECT * FROM pgr_lineGraph(
 SELECT * FROM pgr_lineGraph(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table255'
 );
-
-\echo -- q4
-SELECT * FROM pgr_lineGraph(
-    'SELECT id, source, target, cost, reverse_cost FROM edge_table255',
-    FALSE
-);
