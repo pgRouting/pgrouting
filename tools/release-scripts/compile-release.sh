6f870d09accd4cecd0829a4d0b7bbe09eb51722f
cvvergara <cvvergara@georepublic.de>
2017-08-27 10:50:49 -0500

    bumping up CMakeLists to 2.5.0-beta

--- tools/release-scripts/compile-release.sh
+++ tools/release-scripts/compile-release.sh
@@ -2,6 +2,27 @@
 
 set -e
 
+if [[ -z  $1 ]]; then
+    echo "Cpp version missing";
+    echo "Usage:"
+    echo "tools/release-scripts/compile-release.sh Cpp Minor Micro";
+    exit 1;
+fi
+
+if [[ -z  $1 ]]; then
+    echo "Minor missing";
+    echo "Usage:"
+    echo "tools/release-scripts/compile-release.sh Cpp Minor Micro";
+    exit 1;
+fi
+
+if [[ -z  $1 ]]; then
+    echo "Micro missing";
+    echo "Usage:"
+    echo "tools/release-scripts/compile-release.sh Cpp Minor Micro";
+    exit 1;
+fi
+
 CPPVERSION=$1
 MINOR=$2
 MICRO=$3
@@ -9,10 +30,11 @@ FULL_VER="$MINOR.$MICRO"
 
 function test_compile {                                                                                                                                                                                                          
 
-echo ------------------------------------
-echo ------------------------------------
+echo
+echo
 echo Compiling with $1
 echo ------------------------------------
+echo
 
 sudo update-alternatives --set gcc /usr/bin/gcc-$1
 
@@ -36,6 +58,7 @@ cd ..
 
 echo "  - [x] Compilation"
 
+tools/testers/algorithm-tester.pl --documentation >> build/tmp_make.txt
 tools/testers/algorithm-tester.pl >> build/tmp_make.txt
 echo "  - [x] Documentation tests"
 
