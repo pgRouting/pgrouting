fe60528c4c844009148237785ec5dc49b143c04f
cvvergara <cvvergara@georepublic.de>
2017-09-05 14:27:43 -0500

    appveyor errors on trsp 2

--- src/trsp/src/trsp_driver.cpp
+++ src/trsp/src/trsp_driver.cpp
@@ -24,11 +24,9 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 ********************************************************************PGR-GNU*/
 
-#ifdef __MINGW32__
-#include <winsock2.h>
-#include <windows.h>
-#endif
 
+#include <utility>
+#include <vector>
 #include "drivers/trsp/trsp_driver.h"
 #include "GraphDefinition.h"
 
