81475bb163582849ed6857bd60c6ba757fb78db2
Conned Ascension <atasca10@gmail.com>
2017-09-15 16:25:01 -0400

    1. Added pgtap test for comparing one to one dijkstraCost with the turnPenaltyTable 2. Fixed all other tests

--- src/turnPenaltyGraph/test/doc-turnPenaltyGraph.test.sql
+++ src/turnPenaltyGraph/test/doc-turnPenaltyGraph.test.sql
@@ -1,12 +1,6 @@
 
 \echo -- q1
 SELECT * FROM pgr_turnPenaltyGraph(
-    'SELECT id, source, target, cost, reverse_cost FROM edge_table'
+    'SELECT id, source, target, cost FROM edge_table'
 );
-
 \echo -- q2
-SELECT * FROM pgr_turnPenaltyGraph(
-    'SELECT id, source, target, cost, reverse_cost FROM edge_table',
-    FALSE
-);
-\echo -- q3
