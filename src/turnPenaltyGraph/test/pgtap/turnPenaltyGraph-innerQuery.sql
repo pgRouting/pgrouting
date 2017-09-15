16f93d50b79741b4df68b617c4aa77106595b32f
Anthony Tasca <atasca10@gmail.com>
2017-09-15 16:25:01 -0400

    1. Added pgtap test for comparing one to one dijkstraCost with the turnPenaltyTable 2. Fixed all other tests

--- src/turnPenaltyGraph/test/pgtap/turnPenaltyGraph-innerQuery.sql
+++ src/turnPenaltyGraph/test/pgtap/turnPenaltyGraph-innerQuery.sql
@@ -1,20 +1,11 @@
-\i setup.sql
+SELECT plan(2);
 
-SELECT plan(137);
 SET client_min_messages TO ERROR;
 
-
 SELECT has_function('pgr_turnpenaltygraph',
-    ARRAY['text', 'boolean']);
+    'text');
 
-SELECT function_returns('pgr_turnpenaltygraph',
-    ARRAY['text', 'boolean'],
-    'setof record');
-
-SELECT style_lineGraph('pgr_turnpenaltygraph', ')');
-SELECT style_lineGraph('pgr_turnpenaltygraph', ', true)');
-SELECT style_lineGraph('pgr_turnpenaltygraph', ', false)');
-
+SELECT function_returns('pgr_turnpenaltygraph', 'setof record');
 
 SELECT finish();
 ROLLBACK;
--- src/turnPenaltyGraph/test/pgtap/turnPenaltyGraph-typesCheck.sql
+++ src/turnPenaltyGraph/test/pgtap/turnPenaltyGraph-typesCheck.sql
@@ -3,12 +3,10 @@ SELECT plan(4);
 
 SELECT has_function('pgr_turnpenaltygraph');
 
-SELECT has_function('pgr_turnpenaltygraph', ARRAY[ 'text', 'boolean' ]);
-
-SELECT function_returns('pgr_turnpenaltygraph', ARRAY[ 'text', 'boolean' ], 'setof record');
+SELECT function_returns('pgr_turnpenaltygraph','setof record');
 
 -- testing column names
 SELECT bag_has(
     $$SELECT  proargnames from pg_proc where proname = 'pgr_turnpenaltygraph'$$,
-    $$SELECT  '{"","directed","seq","source","target","cost","reverse_cost"}'::TEXT[] $$
+    $$SELECT  '{"","seq","source","target","cost","original_source_vertex","original_source_edge","original_target_vertex","original_target_edge"}'::TEXT[] $$
 );
