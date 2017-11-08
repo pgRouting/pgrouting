f07e030c90391a5a80de04becd962339e758851c
Anthony Tasca <atasca10@gmail.com>
2017-11-08 14:39:37 -0500

    Adding more pgtap test designs

--- test/turnPenaltyGraph/pgtap_design.test.sql
+++ test/turnPenaltyGraph/pgtap_design.test.sql
@@ -26,4 +26,7 @@ SELECT * FROM pgr_turnPenaltyGraph(
 SELECT * FROM pgr_turnPenaltyGraph(
     'SELECT id*100 AS id, source*1000 as source, target*1000 as target , cost FROM edge_table WHERE id = 1'
 );
+SELECT * FROM pgr_turnPenaltyGraph( 
+    'SELECT id*100 AS id, source*1000 as source, target*1000 as target , cost FROM edge_table WHERE id in (6,7)'
+);
 
