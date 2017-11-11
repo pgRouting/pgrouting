761a101b893852fb4a22d3662e7f45de7e5e8445
Anthony Tasca <atasca10@gmail.com>
2017-11-10 22:13:05 -0500

    Fixing pgtap test

--- pgtap/turnPenaltyGraph/turnPenaltyGraph-reverse_cost_equivalence.sql
+++ pgtap/turnPenaltyGraph/turnPenaltyGraph-reverse_cost_equivalence.sql
@@ -5,5 +5,5 @@ SELECT bag_has(
   $$SELECT * FROM pgr_turnPenaltyGraph(
     'select id, source, target, cost from edge_table where id = 1
      UNION ALL
-     select id, target AS source, source AS target, reverse_cost AS cost from edge_table where id = 1')$$
+     select -1*id, target AS source, source AS target, reverse_cost AS cost from edge_table where id = 1')$$
 );
