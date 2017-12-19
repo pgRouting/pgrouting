a997faf3c265e68d907a5a740953aaac3f12b602
Anthony Tasca <atasca10@gmail.com>
2017-12-19 15:00:51 -0500

    Removed old proof of concepts and added new ones

--- test/lineGraph/vicky.test.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE turntesttable (seq integer, source bigint, target bigint, cost float);
-insert into turntesttable values (1,7000,8000,1);
-insert into turntesttable values (2,2,5000,1);
-insert into turntesttable values (3,8000,2,0);
