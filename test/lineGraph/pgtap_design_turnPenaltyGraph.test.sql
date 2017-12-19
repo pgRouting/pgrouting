1652cc7eb2fb0c22aac8bf5397a04a2c09ad882a
Anthony Tasca <atasca10@gmail.com>
2017-12-19 15:23:30 -0500

    Fixed pgtap_design test for turnPenaltyGraph

--- test/lineGraph/pgtap_design_turnPenaltyGraph.test.sql
+++ test/lineGraph/pgtap_design_turnPenaltyGraph.test.sql
@@ -1,5 +1,3 @@
-\echo -- 0 edge graph
-
 UPDATE edge_table SET id = id * 100, source = 1000 * source, target = 1000 * target;
 UPDATE edge_table_vertices_pgr SET id = id * 1000;
 
@@ -10,27 +8,23 @@ SELECT  *  FROM pgr_turnPenaltyGraph(
     $$SELECT id, source, target, cost FROM edge_table WHERE id = 1$$
 );
 
-\echo -- one edge graph
 SELECT  *  FROM pgr_turnPenaltyGraph(
     $$SELECT id, source, target, cost, reverse_cost
     FROM edge_table
     WHERE id = 200$$
 );
-\echo a many one edge component graph
 SELECT  *  FROM pgr_turnPenaltyGraph(
     $$SELECT id, source, target, cost, reverse_cost
     FROM edge_table
     WHERE id = 1100$$
 );
 
-\echo -- two edge graph
 SELECT  *  FROM pgr_turnPenaltyGraph(
     $$SELECT id, source, target, cost, reverse_cost
     FROM edge_table
     WHERE id = 100$$
 );
 
-\echo -- strange results here (because 2 edges have the same id)
 SELECT  *  FROM pgr_turnPenaltyGraph($$
     SELECT id AS id, target, source, reverse_cost AS cost FROM edge_table WHERE id = 100
     UNION
@@ -38,7 +32,6 @@ SELECT  *  FROM pgr_turnPenaltyGraph($$
     $$
 );
 
-\echo -- good results here (notice the negative on the id on the second SELECT)
 SELECT  *  FROM pgr_turnPenaltyGraph($$
     SELECT id, target, source, reverse_cost AS cost FROM edge_table WHERE id = 100
     UNION
@@ -46,15 +39,12 @@ SELECT  *  FROM pgr_turnPenaltyGraph($$
     $$
 );
 
-\echo -- two edge graph
 SELECT  *  FROM pgr_turnPenaltyGraph(
     $$SELECT id, source, target, cost, reverse_cost
     FROM edge_table
     WHERE id IN (200, 300)$$
 );
 
-
-\echo -- 4 edge Graph
 SELECT  *  FROM pgr_turnPenaltyGraph( 
     $$SELECT id * 100 AS id, source * 1000 AS source, target * 1000 AS target,
         cost, reverse_cost
@@ -62,7 +52,6 @@ SELECT  *  FROM pgr_turnPenaltyGraph(
     WHERE id in (600, 700)$$
 );
 
-\echo -- other 4 edge Graph
 SELECT  *  FROM pgr_turnPenaltyGraph(
     $$SELECT id * 100 AS id, source * 1000 AS source, target * 1000 AS target,
         cost, reverse_cost
