4be99d940c6c74b47922f674095e2a4fce6cfbbe
Anthony Tasca <atasca10@gmail.com>
2017-12-19 15:17:41 -0500

    Removed old pgtap tests that rely on old table format

--- pgtap/lineGraph/turnPenaltyGraph-typesCheck.sql
+++ pgtap/lineGraph/turnPenaltyGraph-typesCheck.sql
@@ -8,5 +8,5 @@ SELECT function_returns('pgr_turnpenaltygraph','setof record');
 -- testing column names
 SELECT bag_has(
     $$SELECT  proargnames from pg_proc where proname = 'pgr_turnpenaltygraph'$$,
-    $$SELECT  '{"","seq","source","target","cost","original_source_vertex","original_source_edge","original_target_vertex","original_target_edge"}'::TEXT[] $$
+    $$SELECT  '{"","seq","source","target","cost","edge"}'::TEXT[] $$
 );
