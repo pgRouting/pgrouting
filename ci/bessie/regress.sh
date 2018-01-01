bfbbdd3180d63fe1418dd4666206bffecf0d47d2
Regina Obe <lr@pcorp.us>
2018-01-01 02:22:35 -0500

    add pg prove

--- ci/bessie/regress.sh
+++ ci/bessie/regress.sh
@@ -2,7 +2,7 @@
 #--
 # Jenkins variables passed in
 #--
-# 
+#
 # export BRANCH=develop
 export PGROUTING_VER=$BRANCH
 
@@ -19,3 +19,7 @@ make
 sudo make install
 cd ../${PGROUTING_VER}
 perl tools/testers/algorithm-tester.pl
+
+psql -c "CREATE DATABASE ___pgr___test___"
+sh tools/testers/pg_prove_tests.sh ${PGUSER}
+psql -c "DROP DATABASE ___pgr___test___"
