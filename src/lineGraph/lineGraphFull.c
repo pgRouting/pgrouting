992e784cadfdeb2b692a236724552b4cf7b9368f
Anthony Tasca <atasca10@gmail.com>
2018-01-05 17:15:01 -0500

    Fixing compile errors (WIP)

--- src/lineGraph/lineGraphFull.c
+++ src/lineGraph/lineGraphFull.c
@@ -149,7 +149,7 @@ PGDLLEXPORT Datum lineGraphFull(PG_FUNCTION_ARGS) {
         /*                                                                    */
         /**********************************************************************/
 
-#if PGSQL_VERSION > 94
+#if PGSQL_VERSION > 95
         funcctx->max_calls = result_count;
 #else
         funcctx->max_calls = (uint32_t)result_count;
