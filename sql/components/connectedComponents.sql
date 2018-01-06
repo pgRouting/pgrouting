1d7e1eff2fd0ba05d14d1dfa27dc8268798fa284
cvvergara <vicky@georepublic.de>
2018-01-06 14:04:08 -0600

    remove trailing spaces on sql directory

--- sql/components/connectedComponents.sql
+++ sql/components/connectedComponents.sql
@@ -5,7 +5,7 @@ Generated with Template by:
 Copyright (c) 2016 pgRouting developers
 Mail: project@pgrouting.org
 
-Function's developer: 
+Function's developer:
 Copyright (c) 2017 Celia Virginia Vergara Castillo
 Mail: vicky_vergara@hotmail.com
 
