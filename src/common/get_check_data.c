4f7d51aeea9663095228cb32c623c0a16ca8b2fb
apsNazii <adityapratap.singh28@gmail.com>
2018-01-18 23:25:56 +0530

    added doxygen code

--- src/common/get_check_data.c
+++ src/common/get_check_data.c
@@ -29,11 +29,48 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 #include "c_common/debug_macro.h"
 
+/*!
+@brief  Function will check whether the colNumber represents any specified column or NULL (SPI_ERROR_NOATTRIBUTE).
+
+@param colNumber Column number (count starts at 1).
+
+@return Function that returns a boolean value.
+ 
+ */
 
 bool
 column_found(int colNumber) {
     return !(colNumber == SPI_ERROR_NOATTRIBUTE);
 }
+
+/*!
+@brief Function will check whether column exists or not.
+
+@param info contain column information.
+ 
+Determine the column number for the specified 
+column name and stored in info.
+
+~~~~~~~~~~~~~~~{.c}
+info->colNumber =  SPI_fnumber(SPI_tuptable->tupdesc, info->name);
+~~~~~~~~~~~~~~~
+
+Returns the OID of the data type of the specified column 
+and stored in info.
+
+~~~~~~~~~~~~~~~{.c}
+info->type = SPI_gettypeid(SPI_tuptable->tupdesc, (info->colNumber));
+~~~~~~~~~~~~~~~
+
+
+@return Function that returns a boolean value.
+
+####References####
+[SPI_fnumber](https://www.postgresql.org/docs/8.2/static/spi-spi-fnumber.html)
+[OID](https://www.postgresql.org/docs/9.4/static/datatype-oid.html)
+[SPI_gettypeid](https://www.postgresql.org/docs/9.1/static/spi-spi-gettypeid.html)
+
+ */
 
 static
 bool
@@ -61,6 +98,23 @@ fetch_column_info(
     return false;
 }
 
+/*!
+@brief  Function tells expected type of each column and then check the correspondence type of column.
+
+@param info[] contain one or more column information.
+@param info_size number of columns.
+
+Column expected type should be:
+ - ANY_INTEGER
+ - ANY_NUMERICAL
+ - TEXT
+ - CHAR1
+ - ANY_INTEGER_ARRAY 
+
+  
+@return NULL is always returned.
+ 
+ */
 
 void pgr_fetch_column_info(
         Column_info_t info[],
@@ -91,7 +145,14 @@ void pgr_fetch_column_info(
     }
 }
 
+/*!
+@brief The function check whether column type is CHAR or not.
 
+For CHAR the value of column type must be 1042(BPCHAROID).
+
+@return NULL is always returned.
+ 
+ */
 
 void
 pgr_check_char_type(Column_info_t info) {
@@ -100,12 +161,29 @@ pgr_check_char_type(Column_info_t info) {
     }
 }
 
+/*!
+@brief The function check whether column type is TEXT or not.
+
+For TEXT column the value of column type must be 25(TEXTOID).
+
+@return NULL is always returned.
+ 
+ */
 void
 pgr_check_text_type(Column_info_t info) {
     if (!(info.type == TEXTOID)) {
         elog(ERROR, "Unexpected Column '%s' type. Expected TEXT", info.name);
     }
 }
+
+/*!
+@brief The function check whether column type is ANY-INTEGER or not.
+
+For ANY-INTEGER the value of column type must be 21(INT2OID) or 23(INT4OID) or 20(INT8OID).
+
+@return NULL is always returned.
+ 
+ */
 
 void
 pgr_check_any_integer_type(Column_info_t info) {
@@ -117,6 +195,14 @@ pgr_check_any_integer_type(Column_info_t info) {
                 info.name);
     }
 }
+/*!
+@brief The function check whether column type is ANY-INTEGER-ARRAY or not.
+
+For ANY-INTEGER-ARRAY the value of column type must be 1007(INT4ARRAYOID) or 1016.
+
+@return RNULL is always returned.
+  
+ */
 
 void
 pgr_check_any_integerarray_type(Column_info_t info) {
@@ -128,6 +214,15 @@ pgr_check_any_integerarray_type(Column_info_t info) {
                 info.name);
     }
 }
+
+/*!
+@brief The function check whether column type is ANY-NUMERICAL.
+
+For ANY-NUMERICAL the value of column type must be 23(INT4OID) or 20(INT8OID) or 700(FLOAT4OID) or 701(FLOAT8OID).
+
+@return NULL is always returned.
+ 
+ */
 
 void pgr_check_any_numerical_type(Column_info_t info) {
     if (!(info.type == INT2OID
@@ -144,6 +239,28 @@ void pgr_check_any_numerical_type(Column_info_t info) {
 
 /*
  * http://doxygen.postgresql.org/include_2catalog_2pg__type_8h.html;
+ */
+/*!
+@brief Function returns the value of the specified column in char type.
+
+@param tuple input row to be examined.
+@param tupdesc input row description.
+@param info contain column information.
+@param strict boolean value of strict.
+@param default_value returned when column contain NULL value.
+
+Return the binary value of the specified column
+and stored in binval.
+
+~~~~~~~~~~~~~~~{.c}
+binval = SPI_getbinval(*tuple, *tupdesc, info.colNumber, &isnull);
+~~~~~~~~~~~~~~~
+  
+@return Char type of column value is returned.
+
+####References####
+[SPI_getbinval](https://www.postgresql.org/docs/8.1/static/spi-spi-getbinval.html)
+
  */
 char
 pgr_SPI_getChar(
@@ -168,7 +285,29 @@ pgr_SPI_getChar(
     return value;
 }
 
+/*!
+@brief Function returns the values of the specified columns in array.
 
+@param tuple input row to be examined.
+@param tupdesc input row description.
+@param info contain column information.
+@param the_size number of element in array.
+
+Return the binary value of the specified column
+and stored in binval.
+
+~~~~~~~~~~~~~~~{.c}
+binval = SPI_getbinval(*tuple, *tupdesc, info.colNumber, &isnull);
+~~~~~~~~~~~~~~~
+  
+@return Pointer of columns value is returned.
+
+####References####
+[SPI_getbinval](https://www.postgresql.org/docs/8.1/static/spi-spi-getbinval.html)
+[DatumGetArrayTypeP](https://doxygen.postgresql.org/array_8h.html#aa1b8e77c103863862e06a7b7c07ec532)
+[pgr_get_bigIntArray](http://docs.pgrouting.org/doxy/2.2/arrays__input_8c_source.html)
+
+ */
 
 
 int64_t*
@@ -185,7 +324,29 @@ pgr_SPI_getBigIntArr(
     return (int64_t*) pgr_get_bigIntArray(the_size, pg_array);
 }
 
+/*!
+@brief Function returns the value of the specified column in integer type.
 
+@param tuple input row to be examined.
+@param tupdesc input row description.
+@param info contain column information.
+
+Return the binary value of the specified column
+and stored in binval.
+
+~~~~~~~~~~~~~~~{.c}
+binval = SPI_getbinval(*tuple, *tupdesc, info.colNumber, &isnull);
+~~~~~~~~~~~~~~~
+  
+@return Integer type in 64 bits of column value is returned.
+
+####References####
+[SPI_getbinval](https://www.postgresql.org/docs/8.1/static/spi-spi-getbinval.html)
+[DatumGetInt16](https://doxygen.postgresql.org/postgres_8h.html#aec991e04209850f29a8a63df0c78ba2d)
+[DatumGetInt32](https://doxygen.postgresql.org/postgres_8h.html#aacbc8a3ac6d52d85feaf0b7ac1b1160c)
+[DatumGetInt62](https://doxygen.postgresql.org/postgres_8h.html#aedf6286d5147eaf3c6f7e998f2662eab) 
+
+ */
 
 int64_t
 pgr_SPI_getBigInt(HeapTuple *tuple, TupleDesc *tupdesc, Column_info_t info) {
@@ -215,6 +376,32 @@ pgr_SPI_getBigInt(HeapTuple *tuple, TupleDesc *tupdesc, Column_info_t info) {
 #endif
     return value;
 }
+
+/*!
+@brief Function returns the value of the specified column in double type.
+
+@param tuple input row to be examined.
+@param tupdesc input row description.
+@param info contain column information.
+
+Return the binary value of the specified column
+and stored in binval.
+
+~~~~~~~~~~~~~~~{.c}
+binval = SPI_getbinval(*tuple, *tupdesc, info.colNumber, &isnull);
+~~~~~~~~~~~~~~~
+  
+@return Double type of column value is returned.
+
+####References####
+[SPI_getbinval](https://www.postgresql.org/docs/8.1/static/spi-spi-getbinval.html)
+[DatumGetInt16](https://doxygen.postgresql.org/postgres_8h.html#aec991e04209850f29a8a63df0c78ba2d)
+[DatumGetInt32](https://doxygen.postgresql.org/postgres_8h.html#aacbc8a3ac6d52d85feaf0b7ac1b1160c)
+[DatumGetInt62](https://doxygen.postgresql.org/postgres_8h.html#aedf6286d5147eaf3c6f7e998f2662eab) 
+[DatumGetFloat4](https://doxygen.postgresql.org/postgres_8h.html#af1b5fdd0a7d9dfe714f728f9bf6dbf4b)
+[DatumGetFloat8](https://doxygen.postgresql.org/postgres_8h.html#aa8579111ead7079875ae647287cc00c1)  
+
+ */
 
 double
 pgr_SPI_getFloat8(HeapTuple *tuple, TupleDesc *tupdesc, Column_info_t info) {
@@ -252,8 +439,25 @@ pgr_SPI_getFloat8(HeapTuple *tuple, TupleDesc *tupdesc, Column_info_t info) {
     return value;
 }
 
-/**
- * under development
+/*!
+###under development###
+@brief Function returns the string representation of the value of the specified column.
+ 
+@param tuple input row to be examined.
+@param tupdesc input row description.
+@param info contain column information.
+
+Return the string value of the specified column.
+
+~~~~~~~~~~~~~~~{.c}
+SPI_getvalue(*tuple, *tupdesc, info.colNumber);
+~~~~~~~~~~~~~~~
+  
+@return Pointer of string is returned.
+
+####References####
+[SPI_getvalue](https://www.postgresql.org/docs/8.2/static/spi-spi-getvalue.html)  
+
  */
 char*
 pgr_SPI_getText(HeapTuple *tuple, TupleDesc *tupdesc,  Column_info_t info) {
