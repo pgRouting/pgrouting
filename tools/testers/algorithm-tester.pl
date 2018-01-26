48ae8a4ee18bf62adf9b1b297df59a4282e90c59
cvvergara <vicky@georepublic.de>
2018-01-26 09:01:43 -0600

    Adding a missing licence

--- tools/testers/algorithm-tester.pl
+++ tools/testers/algorithm-tester.pl
@@ -1,4 +1,36 @@
 #! /usr/bin/perl -w
+
+=pod
+File: aplgorithm testes
+
+Copyright (c) 2013 pgRouting developers
+
+Function contributors:
+  	Celia Virginia Vergara Castillo
+  	Stephen Woodbridge
+  	Vadim Zhukov
+	Nagase Ko
+
+Mail:
+
+------
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+=cut
+
+
 eval 'exec /usr/bin/perl -S $0 ${1+"$@"}'
 if 0; #$running_under_some_shell
 
