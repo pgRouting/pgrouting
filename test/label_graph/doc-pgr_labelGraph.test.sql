8b9ce8880276de983c49bb7d50acebcf93e60e99
cvvergara <vicky@georepublic.de>
2018-04-17 21:37:14 -0500

    removing pgr_labelgraph, use instead components family of functions

--- test/label_graph/doc-pgr_labelGraph.test.sql
+++
@@ -1,5 +0,0 @@
-\echo --q1
-SET client_min_messages TO WARNING;
-SELECT pgr_labelGraph('edge_table', 'id', 'source', 'target', 'subgraph');
-SELECT DISTINCT subgraph FROM edge_table ORDER BY subgraph;
-\echo --q2
