123f42028145a8eb7933954c6daedbda68a275c0
apsNazii <adityapratap.singh28@gmail.com>
2018-05-20 01:37:24 +0530

    Fix parameters

---
+++ include/drivers/prim/prim_driver.h
@@ -0,0 +1,59 @@
+/*PGR-GNU*****************************************************************
+File: prim_driver.h
+
+Generated with Template by:
+Copyright (c) 2015 pgRouting developers
+Mail: project@pgrouting.org
+
+Function's developer:
+Copyright (c) 2018 Celia Virginia Vergara Castillo
+Mail: adityapratap.singh28@gmail.com
+
+------
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+********************************************************************PGR-GNU*/
+
+#ifndef INCLUDE_DRIVERS_PRIM_PRIM_DRIVER_H_
+#define INCLUDE_DRIVERS_PRIM_PRIM_DRIVER_H_
+#pragma once
+
+#include "c_types/pgr_edge_t.h"
+#include "c_types/pgr_prim_t.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*********************************************************
+   TEXT,
+********************************************************/
+
+    void
+        do_pgr_prim(
+                pgr_edge_t  *data_edges,
+                size_t total_edges,
+                pgr_prim_t **return_tuples,
+                size_t *return_count,
+                char ** log_msg,
+                char ** notice_msg,
+                char ** err_msg);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // INCLUDE_DRIVERS_PRIM_PRIM_DRIVER_H_
---
+++ sql/prim/prim.sql
@@ -0,0 +1,42 @@
+/*PGR-GNU*****************************************************************
+File: prim.sql
+
+Generated with Template by:
+Copyright (c) 2016 pgRouting developers
+Mail: project@pgrouting.org
+
+Function's developer:
+Copyright (c) 2018 Celia Virginia Vergara Castillo
+Mail: adityapratap.singh28@gmail.com
+
+------
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+********************************************************************PGR-GNU*/
+
+CREATE OR REPLACE FUNCTION pgr_prim(
+    TEXT,                       -- Edge sql
+        OUT seq INTEGER,        -- Seq
+    OUT start_node BIGINT,	-- Start node 
+    OUT end_node BIGINT,	-- End node 
+    OUT edge BIGINT,		-- Edge linked to that node
+    OUT cost FLOAT,		-- Cost of edge
+    OUT agg_cost FLOAT)		-- Total cost 
+
+RETURNS SETOF RECORD AS
+'$libdir/${PGROUTING_LIBRARY_NAME}', 'prim'
+LANGUAGE c IMMUTABLE STRICT;
+
