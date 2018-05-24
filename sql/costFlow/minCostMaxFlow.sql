0321cd8559e6959c4046de7d4190d5fe0a39a5fb
Maoguang Wang <xjtumg1007@gmail.com>
2018-05-24 18:05:40 +0800

    other versions of costFlow

--- sql/costFlow/minCostMaxFlow.sql
+++ sql/costFlow/minCostMaxFlow.sql
@@ -49,3 +49,49 @@ RETURNS SETOF RECORD AS
 '$libdir/${PGROUTING_LIBRARY_NAME}', 'minCostMaxFlow_many_to_many'
 LANGUAGE c IMMUTABLE STRICT;
 
+------------------------
+--    ONE TO MANY
+------------------------
+
+CREATE OR REPLACE FUNCTION pgr_minCostMaxFlow(
+    edges_sql TEXT,                 -- edges_sql
+    sources BIGINT,                 -- source
+    targets ANYARRAY)               -- targets
+  RETURNS FLOAT AS
+  $BODY$
+        SELECT * 
+        FROM pgr_minCostMaxFlow(_pgr_get_statement($1), ARRAY[$2]::BIGINT[], $3::BIGINT[], only_cost := false);
+  $BODY$
+  LANGUAGE SQL VOLATILE;
+
+------------------------
+--    MANY TO ONE 
+------------------------
+
+CREATE OR REPLACE FUNCTION pgr_minCostMaxFlow(
+    edges_sql TEXT,                 -- edges_sql
+    sources ANYARRAY,               -- sources
+    targets BIGINT)                 -- target
+  RETURNS FLOAT AS
+  $BODY$
+        SELECT *
+        FROM pgr_minCostMaxFlow(_pgr_get_statement($1), $2::BIGINT[], ARRAY[$3]::BIGINT[], only_cost := false);
+  $BODY$
+  LANGUAGE SQL VOLATILE;
+
+------------------------
+--    ONE TO ONE 
+------------------------
+
+CREATE OR REPLACE FUNCTION pgr_minCostMaxFlow(
+    edges_sql TEXT,                 -- edges_sql
+    sources BIGINT,                 -- source
+    targets BIGINT)                 -- target
+  RETURNS FLOAT AS
+  $BODY$
+        SELECT * 
+        FROM pgr_minCostMaxFlow(_pgr_get_statement($1), ARRAY[$2]::BIGINT[], ARRAY[$3]::BIGINT[], only_cost := false);
+  $BODY$
+  LANGUAGE SQL VOLATILE;
+
+
