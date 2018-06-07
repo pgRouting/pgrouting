5b9f02d8dfaf12fbbb3dcdcc6ae8bd8f75692e11
codeSG <sourabh.8.june.1996@gmail.com>
2018-06-08 00:18:30 +0530

    [bellmanFord] Modified pgTap files

--- pgtap/bellman_ford/bellman_ford-innerQuery.sql
+++ pgtap/bellman_ford/bellman_ford-innerQuery.sql
@@ -1,19 +1,22 @@
 \i setup.sql
 
-SELECT plan(2);
+SELECT plan(137);
 SET client_min_messages TO ERROR;
 
 
-SELECT has_function('pgr_bellman_ford',
-    ARRAY['text', 'bigint', 'bigint', 'boolean','boolean']);
+SELECT has_function('pgr_bellman_ford', ARRAY['text','bigint','bigint','boolean']);
+SELECT has_function('pgr_bellman_ford', ARRAY['text','bigint','anyarray','boolean']);
+SELECT has_function('pgr_bellman_ford', ARRAY['text','anyarray','bigint','boolean']);
+SELECT has_function('pgr_bellman_ford', ARRAY['text','anyarray','anyarray','boolean']);
 
-SELECT function_returns('pgr_bellman_ford',
-    ARRAY['text', 'bigint', 'bigint', 'boolean','boolean'],
-    'setof record');
+SELECT function_returns('pgr_bellman_ford', ARRAY['text','bigint','bigint','boolean'],'setof record');
+SELECT function_returns('pgr_bellman_ford', ARRAY['text','bigint','anyarray','boolean'],'setof record');
+SELECT function_returns('pgr_bellman_ford', ARRAY['text','anyarray','bigint','boolean'],'setof record');
+SELECT function_returns('pgr_bellman_ford', ARRAY['text','anyarray','anyarray','boolean'],'setof record');
 
---SELECT style_dijkstra('pgr_bellman_ford', ', 2, 3)');
---SELECT style_dijkstra('pgr_bellman_ford', ', 2, 3, true)');
---SELECT style_dijkstra('pgr_bellman_ford', ', 2, 3, false)');
+SELECT style_dijkstra('pgr_bellman_ford', ', 2, 3)');
+SELECT style_dijkstra('pgr_bellman_ford', ', 2, 3, true)');
+SELECT style_dijkstra('pgr_bellman_ford', ', 2, 3, false)');
 
 
 
