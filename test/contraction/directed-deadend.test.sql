65fc0caeb2d0f59afecbc9c9543487c25e8e2316
Rohith Reddy <rohithreddy2219@gmail.com>
2018-06-13 01:19:40 +0530

    Tests for identified bugs in contraction

--- test/contraction/directed-deadend.test.sql
+++ test/contraction/directed-deadend.test.sql
@@ -1,30 +1,36 @@
---q0 Checking dead end contraction with no dead end node
+\echo --q0
+--Checking dead end contraction with invalid forbidden vertices
+SELECT * FROM pgr_contractGraph(
+    'SELECT id, source, target, cost, reverse_cost FROM edge_table WHERE id = 1',
+    ARRAY[1]::integer[], 1, ARRAY[20]::BIGINT[], true);
+
+\echo --q1
+-- Checking dead end contraction with no dead end node(old test)
 SELECT * FROM pgr_contractGraph(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table WHERE id = 2 OR id = 4 OR id = 5 OR id = 8',
     ARRAY[1]::integer[], 1, ARRAY[]::BIGINT[], true);
---q0 -------------------------------------------
 
---q1 Checking dead end contraction for single dead end node
+\echo --q2
+--Checking dead end contraction for single dead end node(old test)
 SELECT * FROM pgr_contractGraph(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table WHERE id = 1',
     ARRAY[1]::integer[], 1, ARRAY[]::BIGINT[], true);
---q1 -------------------------------------------
 
---q2 Checking dead end contraction for two dead end nodes
+\echo --q3
+-- Checking dead end contraction for two dead end nodes
 SELECT * FROM pgr_contractGraph(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table WHERE id = 2 or id = 3',
     ARRAY[1]::integer[], 1, ARRAY[]::BIGINT[], true);
---q2 -------------------------------------------
 
---q3 Checking dead end contraction for multiple dead end nodes
+\echo --q4
+--Checking dead end contraction for multiple dead end nodes
 SELECT * FROM pgr_contractGraph(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table WHERE id = 1 OR id = 2 or id = 3',
     ARRAY[1]::integer[], 1, ARRAY[]::BIGINT[], true);
---q3 -------------------------------------------
 
---q4 Checking dead end contraction with forbidden vertices not belonging to input graph
+\echo --q5
+-- Checking dead end contraction for sample data(old test)
 SELECT * FROM pgr_contractGraph(
-    'SELECT id, source, target, cost, reverse_cost FROM edge_table WHERE id = 2 or id = 3',
-    ARRAY[1]::integer[], 1, ARRAY[7,8]::BIGINT[], true);
---q4 -------------------------------------------
+    'SELECT id, source, target, cost, reverse_cost FROM edge_table',
+    ARRAY[1]::integer[], 1, ARRAY[]::BIGINT[], true);
 
--- test/contraction/directed-linear.test.sql
+++ test/contraction/directed-linear.test.sql
@@ -1,25 +1,32 @@
---q0 Checking linear contraction with no linear vertex
+\echo --q0
+--Checking linear contraction with invalid forbidden vertices
+SELECT * FROM pgr_contractGraph(
+    'SELECT id, source, target, cost, reverse_cost FROM edge_table WHERE id = 2 or id = 3',
+    ARRAY[2]::integer[], 1, ARRAY[20]::BIGINT[], true);
+
+\echo --q1
+--Checking linear contraction with no linear vertex
 SELECT * FROM pgr_contractGraph(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table
     WHERE id = 1 OR id = 2 OR id = 4',
     ARRAY[2]::integer[], 1, ARRAY[]::BIGINT[], true);
---q0 -------------------------------------------
 
---q1 Checking linear contraction with one linear vertex
+\echo --q2
+--Checking linear contraction with one linear vertex(old test)
 SELECT * FROM pgr_contractGraph(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table
     WHERE id = 3 OR id = 5',
     ARRAY[2]::integer[], 1, ARRAY[]::BIGINT[], true);
---q1 -------------------------------------------
 
---q2 Checking linear contraction with two linear vertices
+\echo --q3 
+--Checking linear contraction with two linear vertices
 SELECT * FROM pgr_contractGraph(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table
     WHERE id = 5 OR id = 11 OR id = 13',
     ARRAY[2]::integer[], 1, ARRAY[]::BIGINT[], true);
---q2 -------------------------------------------
 
---q3 Checking linear contraction with multiple linear vertices
+\echo --q4
+--Checking linear contraction with multiple linear vertices
 SELECT * FROM pgr_contractGraph(
     'SELECT id, source, target, cost FROM edge_table
     WHERE id = ANY(ARRAY[8, 9, 11, 13]) AND cost > 0 
@@ -27,5 +34,5 @@ SELECT * FROM pgr_contractGraph(
     SELECT id, target, source, reverse_cost FROM edge_table
     WHERE id = ANY(ARRAY[15, 16]) AND reverse_cost > 0',
     ARRAY[2]::integer[], 1, ARRAY[]::BIGINT[], true);
---q3 -------------------------------------------
+
 
