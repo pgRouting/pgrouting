51962cc4c9a9e41445192ee111ac84edadd9848f
codeSG <sourabh.8.june.1996@gmail.com>
2018-06-19 01:16:47 +0530

    [bellmanFord] Change function's name

--- pgtap/bellman_ford/bellman_ford-typesCheck.sql
+++ pgtap/bellman_ford/bellman_ford-typesCheck.sql
@@ -1,21 +1,21 @@
 
 SELECT plan(10);
 
-SELECT has_function('pgrBellmanFord');
+SELECT has_function('pgr_bellmanFord');
 
-SELECT has_function('pgrBellmanFord', ARRAY['text','bigint','bigint','boolean']);
-SELECT has_function('pgrBellmanFord', ARRAY['text','bigint','anyarray','boolean']);
-SELECT has_function('pgrBellmanFord', ARRAY['text','anyarray','bigint','boolean']);
-SELECT has_function('pgrBellmanFord', ARRAY['text','anyarray','anyarray','boolean']);
+SELECT has_function('pgr_bellmanFord', ARRAY['text','bigint','bigint','boolean']);
+SELECT has_function('pgr_bellmanFord', ARRAY['text','bigint','anyarray','boolean']);
+SELECT has_function('pgr_bellmanFord', ARRAY['text','anyarray','bigint','boolean']);
+SELECT has_function('pgr_bellmanFord', ARRAY['text','anyarray','anyarray','boolean']);
 
-SELECT function_returns('pgrBellmanFord', ARRAY['text','bigint','bigint','boolean'],'setof record');
-SELECT function_returns('pgrBellmanFord', ARRAY['text','bigint','anyarray','boolean'],'setof record');
-SELECT function_returns('pgrBellmanFord', ARRAY['text','anyarray','bigint','boolean'],'setof record');
-SELECT function_returns('pgrBellmanFord', ARRAY['text','anyarray','anyarray','boolean'],'setof record');
+SELECT function_returns('pgr_bellmanFord', ARRAY['text','bigint','bigint','boolean'],'setof record');
+SELECT function_returns('pgr_bellmanFord', ARRAY['text','bigint','anyarray','boolean'],'setof record');
+SELECT function_returns('pgr_bellmanFord', ARRAY['text','anyarray','bigint','boolean'],'setof record');
+SELECT function_returns('pgr_bellmanFord', ARRAY['text','anyarray','anyarray','boolean'],'setof record');
 
 -- testing column names
 SELECT bag_has(
-    $$SELECT  proargnames from pg_proc where proname = 'pgrBellmanFord'$$,
+    $$SELECT  proargnames from pg_proc where proname = 'pgr_bellmanFord'$$,
     $$SELECT  '{"","","","directed","seq","path_seq","node","edge","cost","agg_cost"}'::TEXT[] $$
 );
 
