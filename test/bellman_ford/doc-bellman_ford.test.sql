97d2be1551c1ca59709d1f9c82805a2e736fa061
codeSG <sourabh.8.june.1996@gmail.com>
2018-06-18 19:00:34 +0530

    Function's name changed from pgr_bellman_ford to pgrBellmanFord

--- test/bellman_ford/doc-bellman_ford.test.sql
+++ test/bellman_ford/doc-bellman_ford.test.sql
@@ -1,12 +1,12 @@
 
 \echo -- q1
-SELECT * FROM pgr_bellman_ford(
+SELECT * FROM pgrBellmanFord(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table',
     2, 3
 );
 
 \echo -- q2
-SELECT * FROM pgr_bellman_ford(
+SELECT * FROM pgrBellmanFord(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table',
     2, 3,
     FALSE
