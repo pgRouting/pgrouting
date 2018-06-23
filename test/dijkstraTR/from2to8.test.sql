84b399ba91bf3e29a044f80530c88166914362a0
cvvergara <vicky@georepublic.de>
2018-06-23 17:38:37 -0500

    [turnRestrictedPath] testing all up to date tests

--- test/dijkstraTR/from2to8.test.sql
+++ test/dijkstraTR/from2to8.test.sql
@@ -2,5 +2,5 @@
 SELECT *
 FROM pgr_turnRestrictedPath(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table',
-    'SELECT * FROM new_restrictions', 2, 8);
+    'SELECT * FROM new_restrictions', 2, 8, 3);
 
