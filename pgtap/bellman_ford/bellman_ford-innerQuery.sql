d61784e6613ee043348fc7f75f8410df7be93cbd
cvvergara <vicky@georepublic.de>
2018-07-01 18:17:28 -0500

    [bellmanFord] fixed failing pgtap tests

--- pgtap/bellman_ford/bellman_ford-innerQuery.sql
+++ pgtap/bellman_ford/bellman_ford-innerQuery.sql
@@ -1,18 +1,8 @@
 \i setup.sql
 
-SELECT plan(143);
+SELECT plan(315);
 SET client_min_messages TO ERROR;
 
-
-SELECT has_function('pgr_bellmanFord', ARRAY['text','bigint','bigint','boolean']);
-SELECT has_function('pgr_bellmanFord', ARRAY['text','bigint','anyarray','boolean']);
-SELECT has_function('pgr_bellmanFord', ARRAY['text','anyarray','bigint','boolean']);
-SELECT has_function('pgr_bellmanFord', ARRAY['text','anyarray','anyarray','boolean']);
-
-SELECT function_returns('pgr_bellmanFord', ARRAY['text','bigint','bigint','boolean'],'setof record');
-SELECT function_returns('pgr_bellmanFord', ARRAY['text','bigint','anyarray','boolean'],'setof record');
-SELECT function_returns('pgr_bellmanFord', ARRAY['text','anyarray','bigint','boolean'],'setof record');
-SELECT function_returns('pgr_bellmanFord', ARRAY['text','anyarray','anyarray','boolean'],'setof record');
 
 SELECT style_dijkstra('pgr_bellmanFord', ', 2, 3)');
 SELECT style_dijkstra('pgr_bellmanFord', ', 2, 3, true)');
