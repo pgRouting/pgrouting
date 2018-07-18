c0fe8dcd81d7960dc31ddfe27170929734c7607f
Maoguang Wang <xjtumg1007@gmail.com>
2018-07-18 19:13:18 +0800

    documentation tests for directedChPP

--- test/ChPP/doc-pgr_directedChPP.test.sql
+++ test/ChPP/doc-pgr_directedChPP.test.sql
@@ -1,38 +1,16 @@
 
 \echo -- q1
-SELECT * FROM pgr_minCostMaxFlow(
+SELECT * FROM pgr_directedChPP(
     'SELECT id,
      source, target,
-     capacity, reverse_capacity,
-     cost, reverse_cost FROM edge_table',
-    2, 3
+     cost, reverse_cost FROM edge_table where id < 17'
 );
 
 \echo -- q2
-SELECT * FROM pgr_minCostMaxFlow(
+SELECT * FROM pgr_directedChPP(
     'SELECT id,
      source, target,
-     capacity, reverse_capacity,
-     cost, reverse_cost FROM edge_table',
-    ARRAY[1, 7, 14], 12
+     cost, reverse_cost FROM edge_table where id = 9 or id = 11 or id = 13 or id = 15'
 );
 
 \echo -- q3
-SELECT * FROM pgr_minCostMaxFlow(
-    'SELECT id,
-     source, target,
-     capacity, reverse_capacity,
-     cost, reverse_cost FROM edge_table',
-    13, ARRAY[7, 1, 4]
-);
-
-\echo -- q4
-SELECT * FROM pgr_minCostMaxFlow(
-    'SELECT id,
-     source, target,
-     capacity, reverse_capacity,
-     cost, reverse_cost FROM edge_table',
-    ARRAY[7, 13], ARRAY[3, 9]
-);
-
-\echo -- q5
