1e3f443e4c9255ceb712afbd68bf49b597d0a37a
cvvergara <vicky@georepublic.de>
2018-09-10 18:32:28 -0500

    [costMatrix] testing and fixing #1085

---
+++ pgtap/costMatrix/no_crash_test-dijkstraCostMatrix.sql
@@ -0,0 +1,43 @@
+\i setup.sql
+
+SELECT plan(14);
+
+PREPARE edges AS
+SELECT id, source, target, cost, reverse_cost  FROM edge_table;
+
+PREPARE null_ret AS
+SELECT array_agg(id) FROM edge_table_vertices_pgr  WHERE id IN (-1);
+
+SELECT isnt_empty('edges', 'Should be not empty to tests be meaningful');
+SELECT set_eq('null_ret', 'SELECT NULL::BIGINT[]', 'Should be empty to tests be meaningful');
+
+
+CREATE OR REPLACE FUNCTION test_function()
+RETURNS SETOF TEXT AS
+$BODY$
+DECLARE
+params TEXT[];
+subs TEXT[];
+BEGIN
+    params = ARRAY['$$edges$$','ARRAY[1,2]::BIGINT[]']::TEXT[];
+    subs = ARRAY[
+    'NULL',
+    '(SELECT array_agg(id) FROM edge_table_vertices_pgr  WHERE id IN (-1))'
+    ]::TEXT[];
+
+    RETURN query SELECT * FROM no_crash_test('pgr_dijkstraCostMatrix', params, subs);
+
+    subs = ARRAY[
+    'NULL',
+    'NULL::BIGINT[]'
+    ]::TEXT[];
+    RETURN query SELECT * FROM no_crash_test('pgr_dijkstraCostMatrix', params, subs);
+
+END
+$BODY$
+LANGUAGE plpgsql VOLATILE;
+
+
+SELECT * FROM test_function();
+
+ROLLBACK;
