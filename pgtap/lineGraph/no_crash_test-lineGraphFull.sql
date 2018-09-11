f292c50f78d68d6399d6b3b0f72e49624e99064a
cvvergara <vicky@georepublic.de>
2018-09-11 10:22:03 -0500

    [lineGraph] testing #1085

---
+++ pgtap/lineGraph/no_crash_test-lineGraphFull.sql
@@ -0,0 +1,37 @@
+\i setup.sql
+
+SELECT plan(9);
+
+PREPARE edges AS
+SELECT id, source, target, cost, reverse_cost  FROM edge_table;
+
+SELECT isnt_empty('edges', 'Should be not empty to tests be meaningful');
+
+
+CREATE OR REPLACE FUNCTION test_function()
+RETURNS SETOF TEXT AS
+$BODY$
+DECLARE
+params TEXT[];
+subs TEXT[];
+BEGIN
+    params = ARRAY['$$SELECT id, source, target, cost, reverse_cost  FROM edge_table$$']::TEXT[];
+    subs = ARRAY[
+    'NULL'
+    ]::TEXT[];
+
+    RETURN query SELECT * FROM no_crash_test('pgr_lineGraphFull', params, subs);
+
+    subs = ARRAY[
+    'NULL'
+    ]::TEXT[];
+    RETURN query SELECT * FROM no_crash_test('pgr_lineGraphFull', params, subs);
+
+END
+$BODY$
+LANGUAGE plpgsql VOLATILE;
+
+
+SELECT * FROM test_function();
+
+ROLLBACK;
