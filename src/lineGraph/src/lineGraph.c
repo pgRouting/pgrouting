7fd8ec2ec1227f6645293e21b2d07fa9ef535774
cvvergara <vicky@georepublic.de>
2018-09-18 12:15:42 -0500

    fixing a compilation warning found using gcc4.8

--- src/lineGraph/src/lineGraph.c
+++ src/lineGraph/src/lineGraph.c
@@ -163,7 +163,7 @@ PGDLLEXPORT Datum lineGraph(PG_FUNCTION_ARGS) {
         /*                                                                    */
         /**********************************************************************/
 
-#if PGSQL_VERSION > 94
+#if PGSQL_VERSION > 95
         funcctx->max_calls = result_count;
 #else
         funcctx->max_calls = (uint32_t)result_count;
