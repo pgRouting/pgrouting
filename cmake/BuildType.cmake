6b55710dd2db69dfeb9dd56e02cfbdb0cebffea9
cvvergara <vicky@georepublic.de>
2018-10-06 13:38:00 -0500

    [cmake] detecting build type

---
+++ cmake/BuildType.cmake
@@ -0,0 +1,19 @@
+
+# Set a default build type if none was specified
+set(PGROUTING_DEFULT_BUILD_TYPE "Release")
+if(EXISTS "${CMAKE_SOURCE_DIR}/.git")
+    set(PGROUTING_DEFULT_BUILD_TYPE "Debug")
+endif()
+
+if (CMAKE_BUILD_TYPE AND
+        NOT CMAKE_BUILD_TYPE MATCHES "^(Debug|Release|RelWithDebInfo|MinSizeRel)$")
+    message(FATAL_ERROR "Invalid value for CMAKE_BUILD_TYPE: ${CMAKE_BUILD_TYPE}
+    valid values are Debug|Release|RelWithDebInfo|MinSizeRel")
+endif()
+
+if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
+    message(STATUS "Setting build type to '${PGROUTING_DEFULT_BUILD_TYPE}' as none was specified.")
+    set(CMAKE_BUILD_TYPE "${PGROUTING_DEFULT_BUILD_TYPE}" CACHE
+        STRING "Choose the type of build." FORCE)
+endif()
+message(STATUS "CMAKE_BUILD_TYPE ${CMAKE_BUILD_TYPE}")
