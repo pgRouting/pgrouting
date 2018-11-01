ebe1cf361ece2037e871be4dfb6921c367c523ff
cvvergara <vicky@georepublic.de>
2018-11-01 09:40:16 -0600

    [pgr_withPointsDD] apply rfc-3

--- sql/driving_distance/withPoints_dd.sql
+++ sql/driving_distance/withPoints_dd.sql
@@ -22,7 +22,11 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 ********************************************************************PGR-GNU*/
 
-CREATE OR REPLACE FUNCTION pgr_withPointsDD(
+-------------------
+-- pgr_withPointsDD
+-------------------
+
+CREATE OR REPLACE FUNCTION _pgr_withPointsDD(
     edges_sql TEXT,
     points_sql TEXT,
     start_pid ANYARRAY,
@@ -39,16 +43,17 @@ CREATE OR REPLACE FUNCTION pgr_withPointsDD(
     OUT edge BIGINT,
     OUT cost FLOAT,
     OUT agg_cost FLOAT)
-  RETURNS SETOF RECORD AS
-     'MODULE_PATHNAME', 'many_withPointsDD'
- LANGUAGE c VOLATILE STRICT;
+RETURNS SETOF RECORD AS
+'MODULE_PATHNAME', 'many_withPointsDD'
+LANGUAGE C VOLATILE STRICT;
 
 
+-- SINGLE
 CREATE OR REPLACE FUNCTION pgr_withPointsDD(
-    edges_sql TEXT,
-    points_sql TEXT,
-    start_pid BIGINT,
-    distance FLOAT,
+    TEXT,   --edges_sql (required)
+    TEXT,   -- points_sql (required)
+    BIGINT, -- from_vid (required)
+    FLOAT,  -- distance (required)
 
     directed BOOLEAN DEFAULT TRUE,
     driving_side CHAR DEFAULT 'b',
@@ -59,13 +64,45 @@ CREATE OR REPLACE FUNCTION pgr_withPointsDD(
     OUT edge BIGINT,
     OUT cost FLOAT,
     OUT agg_cost FLOAT)
-  RETURNS SETOF RECORD AS
+RETURNS SETOF RECORD AS
 $BODY$
-    SELECT a.seq, a.node, a.edge, a.cost, a.agg_cost
-    FROM pgr_withPointsDD($1, $2, ARRAY[$3]::BIGINT[], $4, $5, $6, $7, false) a;
+    SELECT seq, node, edge, cost, agg_cost
+    FROM _pgr_withPointsDD(_pgr_get_statement($1), _pgr_get_statement($2), ARRAY[$3]::BIGINT[], $4, $5, $6, $7, false);
 $BODY$
 LANGUAGE SQL VOLATILE STRICT
 COST 100
 ROWS 1000;
 
+-- MULTIPLE
+CREATE OR REPLACE FUNCTION pgr_withPointsDD(
+    TEXT,     --edges_sql (required)
+    TEXT,     -- points_sql (required)
+    ANYARRAY, -- from_vid (required)
+    FLOAT,    -- distance (required)
 
+    directed BOOLEAN DEFAULT TRUE,
+    driving_side CHAR DEFAULT 'b',
+    details BOOLEAN DEFAULT FALSE,
+    equicost BOOLEAN DEFAULT FALSE,
+
+    OUT seq INTEGER,
+    OUT start_vid BIGINT,
+    OUT node BIGINT,
+    OUT edge BIGINT,
+    OUT cost FLOAT,
+    OUT agg_cost FLOAT)
+RETURNS SETOF RECORD AS
+$BODY$
+    SELECT *
+    FROM _pgr_withPointsDD(_pgr_get_statement($1), _pgr_get_statement($2), $3, $4, $5, $6, $7, $8);
+$BODY$
+LANGUAGE SQL VOLATILE STRICT
+COST 100
+ROWS 1000;
+
+-- COMMENTS
+
+COMMENT ON FUNCTION pgr_withPointsDD(TEXT, TEXT, BIGINT, FLOAT, BOOLEAN, CHAR, BOOLEAN)
+IS 'pgr_withPointsDD(Single_vertex) -- edges_sql(id,source,target,cost[,reverse_cost]), points_sql([pid],edge_id,fraction[,side]), from_vid, distance [,directed, driving_side, details]';
+COMMENT ON FUNCTION pgr_withPointsDD(TEXT, ANYARRAY, FLOAT, BOOLEAN, CHAR, BOOLEAN, BOOLEAN)
+IS 'pgr_withPointsDD(Multiple vertices) -- edges_sql(id,source,target,cost[,reverse_cost]), points_sql([pid],edge_id,fraction[,side]), from_vids, distance [,directed, driving_side, details, equicost]';
