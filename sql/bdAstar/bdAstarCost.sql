ec8340f53ef33947e8b09d566ac94ee310b554d8
Martha Vergara <mvergaratrejo@gmail.com>
2018-11-13 13:24:04 -0600

    [bdAstar] Names of functions

--- sql/bdAstar/bdAstarCost.sql
+++ sql/bdAstar/bdAstarCost.sql
@@ -24,6 +24,9 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 ********************************************************************PGR-GNU*/
 
+--------------------
+-- pgr_bdAstarCost
+--------------------
 
 -- one to one
 CREATE OR REPLACE FUNCTION pgr_bdAstarCost(
