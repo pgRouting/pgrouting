04683d8fe73ce2c459cd31f1ab60593d136039b6
Martha Vergara <mvergaratrejo@gmail.com>
2018-11-13 13:19:37 -0600

    [pgr_bdAstarCost(Many to Many)] Adding comments

--- sql/bdAstar/bdAstarCost.sql
+++ sql/bdAstar/bdAstarCost.sql
@@ -166,8 +166,19 @@ COST 100
 ROWS 1000;
 
 COMMENT ON FUNCTION pgr_bdAstarCost(TEXT, ANYARRAY, ANYARRAY, BOOLEAN, INTEGER, NUMERIC, NUMERIC)
-IS 'pgr_bdAstarCost--Many to Many--(edges_sql(id,source,target,cost[,reverse_cost],x1,y1,x2,y2), from_vids, to_vids [,directed, heuristic, factor, epsilon])';
-
+IS 'pgr_bdAstarCost(Many to Many)
+ - Parameters:
+   - edges SQL with columns: id, source, target, cost [,reverse_cost], x1, y1, x2, y2
+   - From ARRAY[vertices identifiers]
+   - To ARRAY[vertices identifiers]
+ - Optional Parameters: 
+   - directed := true
+   - heuristic := 5
+   - factor := 1
+   - epsilon := 1
+ - Documentation:
+   - ${PGROUTING_DOC_LINK}/pgr_bdAstarCost.html
+';
 
 -- COMMENTS
 
