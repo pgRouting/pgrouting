574850add6faaea6a57d0917c2f65388041f27d5
Martha Vergara <mvergaratrejo@gmail.com>
2018-11-13 13:33:24 -0600

    [astar] }names of functions

--- sql/astar/astarCost.sql
+++ sql/astar/astarCost.sql
@@ -27,6 +27,12 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 ********************************************************************PGR-GNU*/
 
+
+-----------------
+-- pgr_aStarCost
+-----------------
+
+
 CREATE OR REPLACE FUNCTION pgr_aStarCost(
     TEXT,     -- edges sql (required)
     BIGINT,   -- from_vid (required)
