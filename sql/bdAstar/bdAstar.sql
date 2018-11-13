1ac4837236dc4c69ca9ca7a756200bee015d13ca
Martha Vergara <mvergaratrejo@gmail.com>
2018-11-13 13:24:04 -0600

    [bdAstar] Names of functions

--- sql/bdAstar/bdAstar.sql
+++ sql/bdAstar/bdAstar.sql
@@ -23,6 +23,10 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 ********************************************************************PGR-GNU*/
+--------------------
+-- pgr_bdAstar
+--------------------
+
 
 -- one to one
 CREATE OR REPLACE FUNCTION pgr_bdAstar(
