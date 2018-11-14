a5f80b0162a7657e73fa967ca59d9c64d25d49ef
Martha Vergara <mvergaratrejo@gmail.com>
2018-11-14 11:57:11 -0600

    [Ccontraction and components] standarizing indentation

--- sql/components/biconnectedComponents.sql
+++ sql/components/biconnectedComponents.sql
@@ -68,7 +68,7 @@ COMMENT ON FUNCTION pgr_biconnectedComponents(TEXT)
 IS'pgr_biconnectedComponents
 - Undirected graph
 - Parameters:
-   - Edges SQL with columns: id, source, target, cost [,reverse_cost]
+    - Edges SQL with columns: id, source, target, cost [,reverse_cost]
 - Documentation:
-   - ${PGROUTING_DOC_LINK}/pgr_biconnectedComponents.html
+    - ${PGROUTING_DOC_LINK}/pgr_biconnectedComponents.html
 ';
