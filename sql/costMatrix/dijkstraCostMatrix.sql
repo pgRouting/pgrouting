90136ca70cb700285294920dd0bc37dda6f8c042
Martha Vergara <mvergaratrejo@gmail.com>
2018-11-14 13:45:29 -0600

    [pgr_dijkstraCostMatrix] adding comment

--- sql/costMatrix/dijkstraCostMatrix.sql
+++ sql/costMatrix/dijkstraCostMatrix.sql
@@ -24,8 +24,10 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 ********************************************************************PGR-GNU*/
 
+-----------------------------
+-- dijkstraCostMatrix
+-----------------------------
 
---  DIJKSTRA DMatrix
 
 CREATE OR REPLACE FUNCTION pgr_dijkstraCostMatrix(
     TEXT,     -- edges_sql (required)
@@ -46,5 +48,14 @@ COST 100
 ROWS 1000;
 
 
-COMMENT ON FUNCTION pgr_dijkstraCostMatrix(TEXT, ANYARRAY, BOOLEAN) IS
-'pgr_dijkstraCostMatrix(edges_sql(id,source,target,cost[,reverse_cost]), vids, [,directed])';
+COMMENT ON FUNCTION pgr_dijkstraCostMatrix(TEXT, ANYARRAY, BOOLEAN) 
+IS 'pgr_dijkstraCostMatrix
+- EXPERIMENTAL
+- Parameters:
+    - Edges SQL with columns: id, source, target, cost [,reverse_cost]
+    - - ARRAY [vertices identifiers]
+- Optional Parameters
+    - directed := true
+- Documentation:
+    - ${PGROUTING_DOC_LINK}/pgr_withPointsCostMatrix.html
+';
