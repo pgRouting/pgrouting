700cd65c0cd63fe3d675f60601be0e6178f27ae6
Martha Vergara <mvergaratrejo@gmail.com>
2018-11-13 20:57:10 -0600

    [_pgr_directedChPP] Name of the Function

--- sql/ChPP/_pgr_directedChPP.sql
+++ sql/ChPP/_pgr_directedChPP.sql
@@ -26,11 +26,11 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 ********************************************************************PGR-GNU*/
-
+----------
 ----------
 -- ChPP
 ----------
-
+----------
 
 CREATE OR REPLACE FUNCTION _pgr_directedChPP(
     edges_sql TEXT,
