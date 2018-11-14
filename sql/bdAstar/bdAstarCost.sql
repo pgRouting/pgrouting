11faf3a4eda1da69e97f2c53f70e316049ee6855
Martha Vergara <mvergaratrejo@gmail.com>
2018-11-14 12:08:48 -0600

    [bdAstar] standarizing indentation

--- sql/bdAstar/bdAstarCost.sql
+++ sql/bdAstar/bdAstarCost.sql
@@ -129,63 +129,63 @@ ROWS 1000;
 
 COMMENT ON FUNCTION pgr_bdAstarCost(TEXT, BIGINT, BIGINT, BOOLEAN, INTEGER, NUMERIC, NUMERIC)
 IS 'pgr_bdAstarCost(One to One)
- - Parameters:
-   - edges SQL with columns: id, source, target, cost [,reverse_cost], x1, y1, x2, y2
-   - From vertex identifier
-   - To vertex identifier
- - Optional Parameters: 
-   - directed := true
-   - heuristic := 5
-   - factor := 1
-   - epsilon := 1
- - Documentation:
-   - ${PGROUTING_DOC_LINK}/pgr_bdAstarCost.html
+- Parameters:
+  - edges SQL with columns: id, source, target, cost [,reverse_cost], x1, y1, x2, y2
+  - From vertex identifier
+  - To vertex identifier
+- Optional Parameters: 
+  - directed := true
+  - heuristic := 5
+  - factor := 1
+  - epsilon := 1
+- Documentation:
+  - ${PGROUTING_DOC_LINK}/pgr_bdAstarCost.html
 ';
 
 
 COMMENT ON FUNCTION pgr_bdAstarCost(TEXT, BIGINT, ANYARRAY, BOOLEAN, INTEGER, NUMERIC, NUMERIC)
 IS 'pgr_bdAstarCost(One to Many)
- - Parameters:
-   - edges SQL with columns: id, source, target, cost [,reverse_cost], x1, y1, x2, y2
-   - From vertex identifier
-   - To ARRAY[vertices identifiers]
- - Optional Parameters: 
-   - directed := true
-   - heuristic := 5
-   - factor := 1
-   - epsilon := 1
- - Documentation:
-   - ${PGROUTING_DOC_LINK}/pgr_bdAstarCost.html
+- Parameters:
+  - edges SQL with columns: id, source, target, cost [,reverse_cost], x1, y1, x2, y2
+  - From vertex identifier
+  - To ARRAY[vertices identifiers]
+- Optional Parameters: 
+  - directed := true
+  - heuristic := 5
+  - factor := 1
+  - epsilon := 1
+- Documentation:
+  - ${PGROUTING_DOC_LINK}/pgr_bdAstarCost.html
 ';
 
 
 COMMENT ON FUNCTION pgr_bdAstarCost(TEXT, ANYARRAY, BIGINT, BOOLEAN, INTEGER, NUMERIC, NUMERIC)
 IS 'pgr_bdAstarCost(Many to One)
- - Parameters:
-   - edges SQL with columns: id, source, target, cost [,reverse_cost], x1, y1, x2, y2
-   - From ARRAY[vertices identifiers]
-   - To vertex identifier
- - Optional Parameters: 
-   - directed := true
-   - heuristic := 5
-   - factor := 1
-   - epsilon := 1
- - Documentation:
-   - ${PGROUTING_DOC_LINK}/pgr_bdAstarCost.html
+- Parameters:
+  - edges SQL with columns: id, source, target, cost [,reverse_cost], x1, y1, x2, y2
+  - From ARRAY[vertices identifiers]
+  - To vertex identifier
+- Optional Parameters: 
+  - directed := true
+  - heuristic := 5
+  - factor := 1
+  - epsilon := 1
+- Documentation:
+  - ${PGROUTING_DOC_LINK}/pgr_bdAstarCost.html
 ';
 
 
 COMMENT ON FUNCTION pgr_bdAstarCost(TEXT, ANYARRAY, ANYARRAY, BOOLEAN, INTEGER, NUMERIC, NUMERIC)
 IS 'pgr_bdAstarCost(Many to Many)
- - Parameters:
-   - edges SQL with columns: id, source, target, cost [,reverse_cost], x1, y1, x2, y2
-   - From ARRAY[vertices identifiers]
-   - To ARRAY[vertices identifiers]
- - Optional Parameters: 
-   - directed := true
-   - heuristic := 5
-   - factor := 1
-   - epsilon := 1
- - Documentation:
-   - ${PGROUTING_DOC_LINK}/pgr_bdAstarCost.html
+- Parameters:
+  - edges SQL with columns: id, source, target, cost [,reverse_cost], x1, y1, x2, y2
+  - From ARRAY[vertices identifiers]
+  - To ARRAY[vertices identifiers]
+- Optional Parameters: 
+  - directed := true
+  - heuristic := 5
+  - factor := 1
+  - epsilon := 1
+- Documentation:
+  - ${PGROUTING_DOC_LINK}/pgr_bdAstarCost.html
 ';
