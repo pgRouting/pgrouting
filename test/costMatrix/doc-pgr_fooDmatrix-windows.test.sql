149efad3c15eb8141c9c32868f50fd27e0642d3b
cvvergara <vicky@georepublic.de>
2018-11-20 09:38:34 -0600

    Fix for issue #1098
    * Notes & news
    	modified:   NEWS
    	modified:   doc/src/release_notes.rst
    * Fix:
    	modified:   src/costMatrix/test/doc-pgr_fooDmatrix-windows.result

--- test/costMatrix/doc-pgr_fooDmatrix-windows.test.sql
+++ test/costMatrix/doc-pgr_fooDmatrix-windows.test.sql
@@ -115,21 +115,4 @@ SELECT * FROM pgr_bdAstarCostMatrix(
     'SELECT id, source, target, cost, reverse_cost, x1, y1, x2, y2 FROM edge_table',
     (SELECT array_agg(id) FROM edge_table_vertices_pgr WHERE id < 5)
 );
-\echo -- bdAstar q2
-SELECT * FROM pgr_bdAstarCostMatrix(
-    'SELECT id, source, target, cost, reverse_cost, x1, y1, x2, y2 FROM edge_table',
-    (SELECT array_agg(id) FROM edge_table_vertices_pgr WHERE id < 5),
-    false
-);
-\echo -- bdAstar q3
-SELECT * FROM pgr_TSP(
-    $$
-    SELECT * FROM pgr_bdAstarCostMatrix(
-        'SELECT id, source, target, cost, reverse_cost, x1, y1, x2, y2 FROM edge_table',
-        (SELECT array_agg(id) FROM edge_table_vertices_pgr WHERE id < 5),
-        false
-    )
-    $$,
-    randomize := false
-);
 \echo -- bdAstar q4
