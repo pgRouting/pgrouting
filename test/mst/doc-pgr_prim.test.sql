60e773495ef7cb61c3a91fbc6f86b1606f8a4658
cvvergara <vicky@georepublic.de>
2018-11-25 10:12:01 -0600

    fixing documentation test

--- test/mst/doc-pgr_prim.test.sql
+++ test/mst/doc-pgr_prim.test.sql
@@ -1,7 +1,7 @@
 
 \echo -- q1
-SELECT * FROM pgr_prim(
+SELECT edge, cost FROM pgr_prim(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table WHERE id < 14'
-);
+) ORDER BY edge;
 
 \echo -- q2
