fe40c6c3746a39841d79b6eb839bc40d22bdc134
Martha Vergara <mvergaratrejo@gmail.com>
2018-11-27 12:45:38 -0600

    [pgr_withPoints Many to many] Adding and fixing comments

--- sql/withPoints/withPoints.sql
+++ sql/withPoints/withPoints.sql
@@ -94,7 +94,7 @@ CREATE OR REPLACE FUNCTION pgr_withPoints(
     points_sql TEXT,
     start_pids ANYARRAY,
     end_pid BIGINT,
-    
+
     directed BOOLEAN DEFAULT true,
     driving_side CHAR DEFAULT 'b', -- 'r'/'l'/'b'/NULL
     details BOOLEAN DEFAULT false,
@@ -167,7 +167,7 @@ IS 'pgr_withPoints (One to Many)
 - Parameters:
     - Edges SQL with columns: id, source, target, cost [,reverse_cost]
     - From vertex identifier
-    - To vertex identifier
+    - To ARRAY[vertices identifiers]
 - Optional Parameters
     - directed := ''true''
     - driving_side := ''b''
@@ -181,8 +181,23 @@ COMMENT ON FUNCTION pgr_withPoints(TEXT, TEXT, ANYARRAY, BIGINT, BOOLEAN, CHAR, 
 IS 'pgr_withPoints (Many to One)
 - Parameters:
     - Edges SQL with columns: id, source, target, cost [,reverse_cost]
-    - From vertex identifier
+    - From  ARRAY[vertices identifiers]
     - To vertex identifier
+- Optional Parameters
+    - directed := ''true''
+    - driving_side := ''b''
+    - details := ''false''
+- Documentation:
+  - ${PGROUTING_DOC_LINK}/pgr_withPoints.html
+';
+
+
+COMMENT ON FUNCTION pgr_withPoints(TEXT, TEXT, ANYARRAY, ANYARRAY, BOOLEAN, CHAR, BOOLEAN)
+IS 'pgr_withPoints (Many to Many)
+- Parameters:
+    - Edges SQL with columns: id, source, target, cost [,reverse_cost]
+    - From ARRAY[vertices identifiers]
+    - To ARRAY[vertices identifiers]
 - Optional Parameters
     - directed := ''true''
     - driving_side := ''b''
