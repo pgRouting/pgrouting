79c092be1f1fc1fe5181feef47d84d026ebbd269
cvvergara <vicky@georepublic.de>
2018-12-10 20:44:13 -0600

    [pgr_extractVertices] adding documentation queries

---
+++ test/topology/doc-pgr_extractVertices.test.sql
@@ -0,0 +1,13 @@
+\echo --q1
+SELECT  * FROM pgr_extractVertices('SELECT source, target, the_geom FROM edge_table');
+\echo --q1.1
+
+\echo --q2
+SELECT  * FROM pgr_extractVertices('SELECT source, target FROM edge_table WHERE id < 5');
+\echo --q2.1
+
+\echo --q3
+DROP IF EXISTS edge_table_vertices_pgr;
+SELECT  * INTO edge_table_vertices_pgr FROM pgr_extractVertices('SELECT source, target, the_geom FROM edge_table');
+\echo --q3.1
+
