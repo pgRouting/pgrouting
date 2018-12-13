ae8d6b0e03eacfd5f3f2bf9abbc44d36beafe31c
cvvergara <vicky@georepublic.de>
2018-12-12 23:17:16 -0600

    (pgr_extractVertices) fixing a test

--- test/topology/doc-pgr_extractVertices.test.sql
+++ test/topology/doc-pgr_extractVertices.test.sql
@@ -7,7 +7,9 @@ SELECT  * FROM pgr_extractVertices('SELECT source, target FROM edge_table WHERE 
 \echo --q2.1
 
 \echo --q3
-DROP IF EXISTS edge_table_vertices_pgr;
-SELECT  * INTO edge_table_vertices_pgr FROM pgr_extractVertices('SELECT source, target, the_geom FROM edge_table');
+DROP TABLE IF EXISTS edge_table_vertices_pgr;
+SELECT  * INTO edge_table_vertices_pgr
+FROM pgr_extractVertices('SELECT source, target, the_geom FROM edge_table');
+SELECT * FROM edge_table_vertices_pgr;
 \echo --q3.1
 
