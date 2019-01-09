450de7a8180359a1467e87253f4247217677be0f
Manuel Fuentes <manuel@uct.geocuba.cu>
2019-01-09 14:13:00 +0000

    [multimodal] creating tests with pgtap

--- pgtap/topology/createTopologyMultiModal.sql
+++ pgtap/topology/createTopologyMultiModal.sql
@@ -1,46 +1,61 @@
+\i setup.sql
+
+-- Cant be Warning because postgis is printing into warning channel.
+set client_min_messages TO error;
+select plan(1);
+
 drop TABLE IF EXISTS test_table_l1;
 create table test_table_l1(
-                            geom geometry('linestring',4326),
-                            id SERIAL
+   geom geometry('linestring',4326),
+   id integer primary key
 );
-insert into test_table_l1 VALUES ('SRID=4326;linestring(5 0,10 10, 15 10)');
-insert into test_table_l1 VALUES ('SRID=4326;linestring(0 0, 10 10)');
-insert into test_table_l1 VALUES ('SRID=4326;linestring(10 10,10 0)');
-insert into test_table_l1 VALUES ('SRID=4326;linestring(8 0, 10 10)');
+
+insert into test_table_l1 VALUES ('SRID=4326;linestring(5 0,10 10, 15 10)',1);
+insert into test_table_l1 VALUES ('SRID=4326;linestring(0 0, 10 10)',2);
+insert into test_table_l1 VALUES ('SRID=4326;linestring(10 10,10 0)',3);
+insert into test_table_l1 VALUES ('SRID=4326;linestring(8 0, 10 10)',4);
 
 drop table if EXISTS test_table_p1;
 create TABLE test_table_p1(
-                            geom geometry('point',4326),
-                            id serial
-);
-insert into test_table_p1 values('SRID=4326;point(10 10)');
-insert into test_table_p1 values('SRID=4326;point(10 0)');
+   geom geometry('point',4326),
+   id serial
+  );
 
-SELECT pgr_createtopology_layers('{
+insert into test_table_p1 values('SRID=4326;point(10 10)',1);
+insert into test_table_p1 values('SRID=4326;point(10 0)',2);
+prepare createTopology_1 as
+
+SELECT count(*) from pgr_createtopology_layers('{
   "1": [
-    "layerA"
+    "manoL"
   ]
-}','{"groupA":["layerA"]}'
+}','{"manoP":["manoL"]}'
          , '{
-  "layerA": {
-    "sql": "select id as id, geom as the_geom,0 as z_start, 0 as z_end from \"".\"test_table_l1\"",
+  "manoL": {
+    "sql": "select id as id, geom as the_geom,0 as z_start, 0 as z_end from \"test_table_l1\"",
     "pconn": 0,
-    "zconn": 1
+    "zconn": 2
   },
-  "groupA":{
-    "sql":"select id as id, geom as the_geom,0 as z_start, 0 as z_end from \"".\"test_table_p1\"",
+  "manoP":{
+    "sql":"select id as id, geom as the_geom,0 as z_start, 0 as z_end from \"test_table_p1\"",
     "pconn":1,
     "zconn":2
    }
-}', 'graph_lines_222', ', 0.000001);
+}', 'graph_lines', 'public', 0.000001);
+
+select results_eq('createTopology_1', array[0]::bigint[]);
 
 
 
-------------------z index-------------------------------
+
+select * from pgr_create_top_graph_ptos;
+
+
+                ------------------z index-------------------------------
 drop TABLE IF EXISTS test_table_l1;
 create table test_table_l1(
-geom geometry('linestringz',4326),
-id SERIAL
+                                             geom geometry('linestringz',4326),
+                                             id SERIAL
 );
 insert into test_table_l1 VALUES ('SRID=4326;linestring(5 0 0,10 10 10, 15 10 0)');
 insert into test_table_l1 VALUES ('SRID=4326;linestring(0 0 0, 10 10 0)');
@@ -49,39 +64,39 @@ insert into test_table_l1 VALUES ('SRID=4326;linestring(8 0 0, 10 10 10)');
 
 drop table if EXISTS test_table_p1;
 create TABLE test_table_p1(
-  geom geometry('pointz',4326),
-  id serial
+                                             geom geometry('pointz',4326),
+                                             id serial
 );
 insert into test_table_p1 values('SRID=4326;point(10 10 10)');
 insert into test_table_p1 values('SRID=4326;point(10 10 0)');
 insert into test_table_p1 values('SRID=4326;point(10 0 0)');
 
 SELECT pgr_createtopology_layers('{
-"1": [
-"layerA"
-]
-}','{"groupA":["layerA"]}'
-, '{
-"layerA": {
-"sql": "select id as id, geom as the_geom,0 as z_start, 0 as z_end from \"".\"test_table_l1\"",
-"pconn": 0,
-"zconn": 0
-},
-"groupA":{
-"sql":"select id as id, geom as the_geom,0 as z_start, 0 as z_end from \"".\"test_table_p1\"",
-"pconn":1,
-"zconn":0
-}
-}', 'graph_lines_222', ', 0.000001);
+  "1": [
+    "manoL"
+  ]
+}','{"manoP":["manoL"]}'
+         , '{
+  "manoL": {
+    "sql": "select id as id, geom as the_geom,0 as z_start, 0 as z_end from \"test_table_l1\"",
+    "pconn": 0,
+    "zconn": 0
+  },
+  "manoP":{
+    "sql":"select id as id, geom as the_geom,0 as z_start, 0 as z_end from \"test_table_p1\"",
+    "pconn":1,
+    "zconn":0
+   }
+}', 'graph_lines_222', 'mod_graphmanager', 0.000001);
 
 
 ------------------------Using z from column--------------------------------------------
 drop TABLE IF EXISTS test_table_l1;
 create table test_table_l1(
-                            geom geometry('linestring',4326),
-                            z_start FLOAT,
-                            z_end FLOAT,
-                            id SERIAL
+                                             geom geometry('linestring',4326),
+                                             z_start FLOAT,
+                                             z_end FLOAT,
+                                             id SERIAL
 );
 insert into test_table_l1 VALUES ('SRID=4326;linestring(5 0,10 10, 15 10)',0,0);
 insert into test_table_l1 VALUES ('SRID=4326;linestring(0 0,10 10, 20 10)', 0, 10);
@@ -90,179 +105,179 @@ insert into test_table_l1 VALUES ('SRID=4326;linestring(8 0, 10 10)',0, 10);
 
 drop table if EXISTS test_table_p1;
 create TABLE test_table_p1(
-                            geom geometry('point',4326),
-                            z FLOAT,
-                            id serial
+                                             geom geometry('point',4326),
+                                             z FLOAT,
+                                             id serial
 );
 insert into test_table_p1 values('SRID=4326;point(10 10)',10);
 insert into test_table_p1 values('SRID=4326;point(10 0)',0);
 
 SELECT pgr_createtopology_layers('{
   "1": [
-    "layerA"
+    "manoL"
   ]
-}','{"groupA":["layerA"]}'
+}','{"manoP":["manoL"]}'
          , '{
-  "layerA": {
-    "sql": "select id , geom as the_geom,z_start, z_end from \"".\"test_table_l1\"",
+  "manoL": {
+    "sql": "select id , geom as the_geom,z_start, z_end from \"test_table_l1\"",
     "pconn": 1,
     "zconn": 1
   },
-  "groupA":{
-    "sql":"select id , geom as the_geom,z  from \"".\"test_table_p1\"",
+  "manoP":{
+    "sql":"select id , geom as the_geom,z  from \"test_table_p1\"",
     "pconn":0,
     "zconn":1
    }
-}', 'graph_lines_222', ', 0.000001);
+}', 'graph_lines_222', 'mod_graphmanager', 0.000001);
 
 
 ------------------------Many Groups---------------------------------------------------
 drop TABLE IF EXISTS test_table_l1;
 create table test_table_l1(
-  geom geometry('linestring',4326),
-  id SERIAL
+                                             geom geometry('linestring',4326),
+                                             id SERIAL
 );
 insert into test_table_l1 VALUES ('SRID=4326;linestring(5 0,10 0, 15 0)'),
                                                   ('SRID=4326;linestring(5 1,10 1, 15 1)');
 
 drop table if EXISTS test_table_p1;
 create TABLE test_table_p1(
-  geom geometry('point',4326),
-  id serial
+                                             geom geometry('point',4326),
+                                             id serial
 );
 insert into test_table_p1 values('SRID=4326;point(10 0)');
 
 drop table if EXISTS test_table_l2;
 create TABLE test_table_l2(
-  geom geometry('linestring',4326),
-  id serial
+                                             geom geometry('linestring',4326),
+                                             id serial
 );
 
 INSERT into test_table_l2 values ('SRID=4326;linestring(10 -1, 10 0, 10 1, 10 2)');
 
 
 SELECT pgr_createtopology_layers('{
-"1": [
-"layerA"
-],"2":["layerA2"]
-}','{"groupA":["layerA","layerA2"]}'
-, '{
-"layerA": {
-"sql": "select id , geom as the_geom from \"".\"test_table_l1\"",
-"pconn": 1,
-"zconn": 2
-},
-"layerA2": {
-"sql": "select id , geom as the_geom from \"".\"test_table_l2\"",
-"pconn": 1,
-"zconn": 2
-},
-"groupA":{
-"sql":"select id , geom as the_geom from \"".\"test_table_p1\"",
-"pconn":1,
-"zconn":2
-}
-}', 'graph_lines_222', ', 0.00000001);
+  "1": [
+    "manoL"
+  ],"2":["manoL2"]
+}','{"manoP":["manoL","manoL2"]}'
+         , '{
+  "manoL": {
+    "sql": "select id , geom as the_geom from \"test_table_l1\"",
+    "pconn": 1,
+    "zconn": 2
+  },
+  "manoL2": {
+    "sql": "select id , geom as the_geom from \"test_table_l2\"",
+    "pconn": 1,
+    "zconn": 2
+  },
+  "manoP":{
+    "sql":"select id , geom as the_geom from \"test_table_p1\"",
+    "pconn":1,
+    "zconn":2
+  }
+}', 'graph_lines_222', 'mod_graphmanager', 0.00000001);
 
 ------------------------LinearLayer3d and puntual layer 2d-------------------------------
 drop TABLE IF EXISTS test_table_l1;
 create table test_table_l1(
-                            geom geometry('linestringz',4326),
-                            id SERIAL
+                                             geom geometry('linestringz',4326),
+                                             id SERIAL
 );
 insert into test_table_l1 VALUES ('SRID=4326;linestring(5 0 0,10 10 0, 15 10 0)');
 
 drop TABLE IF EXISTS test_table_l2;
 create table test_table_l2(
-                            geom geometry('linestringz',4326),
-                            id SERIAL
+                                             geom geometry('linestringz',4326),
+                                             id SERIAL
 );
 insert into test_table_l2 VALUES ('SRID=4326;linestring(5 0 1,10 10 1, 15 10 1)');
 
 drop table if EXISTS test_table_p1;
 create TABLE test_table_p1(
-                            geom geometry('point',4326),
-                            id serial
+                                             geom geometry('point',4326),
+                                             id serial
 );
 insert into test_table_p1 values('SRID=4326;point(10 10)');
 
 SELECT pgr_createtopology_layers('{
   "1": [
-    "layerA"
-  ],"2":["layerA2"]
-}','{"groupA":["layerA","layerA2"]}'
+    "manoL"
+  ],"2":["manoL2"]
+}','{"manoP":["manoL","manoL2"]}'
          , '{
-  "layerA": {
-    "sql": "select id , geom as the_geom from \"".\"test_table_l1\"",
+  "manoL": {
+    "sql": "select id , geom as the_geom from \"test_table_l1\"",
     "pconn": 0,
     "zconn": 0
   },
-  "layerA2": {
-    "sql": "select id , geom as the_geom from \"".\"test_table_l2\"",
+  "manoL2": {
+    "sql": "select id , geom as the_geom from \"test_table_l2\"",
     "pconn": 0,
     "zconn": 0
   },
-  "groupA":{
-    "sql":"select id , geom as the_geom from \"".\"test_table_p1\"",
+  "manoP":{
+    "sql":"select id , geom as the_geom from \"test_table_p1\"",
     "pconn":1,
     "zconn":2
   }
-}', 'graph_lines_222', ', 0.00000001);
+}', 'graph_lines_222', 'mod_graphmanager', 0.00000001);
 
 ----------------------Testing multilines------------------------------------------------
 drop TABLE IF EXISTS test_table_l1;
 create table test_table_l1(
-  geom geometry('multilinestring',4326),
-  id SERIAL
+                                             geom geometry('multilinestring',4326),
+                                             id SERIAL
 );
 insert into test_table_l1 VALUES ('SRID=4326;multilinestring((5 0,10 0),(10 0, 15 0))'),
                                                   ('SRID=4326;multilinestring((5 2,10 0),(10 0, 15 2))');
 
 drop table if EXISTS test_table_p1;
 create TABLE test_table_p1(
-  geom geometry('point',4326),
-  id serial
+                                             geom geometry('point',4326),
+                                             id serial
 );
 insert into test_table_p1 values('SRID=4326;point(10 0)');
 
 SELECT pgr_createtopology_layers('{
-"1": [
-"layerA"
-]
-}','{"groupA":["layerA"]}'
-, '{
-"layerA": {
-"sql": "select id , geom as the_geom from \"".\"test_table_l1\"",
-"pconn": 1,
-"zconn": 2
-},
-"groupA":{
-"sql":"select id , geom as the_geom from \"".\"test_table_p1\"",
-"pconn":1,
-"zconn":2
-}
-}', 'graph_lines_222', ', 0.00000001);
+  "1": [
+    "manoL"
+  ]
+}','{"manoP":["manoL"]}'
+         , '{
+  "manoL": {
+    "sql": "select id , geom as the_geom from \"test_table_l1\"",
+    "pconn": 1,
+    "zconn": 2
+  },
+  "manoP":{
+    "sql":"select id , geom as the_geom from \"test_table_p1\"",
+    "pconn":1,
+    "zconn":2
+  }
+}', 'graph_lines_222', 'mod_graphmanager', 0.00000001);
 
 
 -----------------testin some error---------------------------------
 drop TABLE IF EXISTS test_table_l1;
 create table test_table_l1(
-                            geom geometry('linestring',4326),
-                            id SERIAL
+                                             geom geometry('linestring',4326),
+                                             id SERIAL
 );
 insert into test_table_l1 VALUES ('SRID=4326;linestring(1 1, 2 2, 1 3)'),
-                                 ('SRID=4326;linestring(2 1, 2 2, 3 4)'),
-                                 ('SRID=4326;linestring(3 1, 2 2)'),
-                                 ('SRID=4326;linestring(4 1, 2 2)');
+                                                  ('SRID=4326;linestring(2 1, 2 2, 3 4)'),
+                                                  ('SRID=4326;linestring(3 1, 2 2)'),
+                                                  ('SRID=4326;linestring(4 1, 2 2)');
 SELECT pgr_createtopology_layers('{
   "1": [
-    "layerA"
+    "manoL"
   ]
 }',null
          , '{
-  "layerA":{
-    "sql":"select id , geom as the_geom from \"".\"test_table_l1\"",
+  "manoL":{
+    "sql":"select id , geom as the_geom from \"test_table_l1\"",
     "pconn":1,
     "zconn":2
   }
-}', 'graph_lines_222', ', 0.000001)
+}', 'graph_lines_222', 'mod_graphmanager', 0.000001)
