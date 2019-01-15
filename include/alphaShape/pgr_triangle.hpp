bdde01efa82b94f43819fc35b35b76633b66ab53
cvvergara <vicky@georepublic.de>
2019-01-15 11:53:43 -0600

    [alphaShape] adding a pgr_triangle class

---
+++ include/alphaShape/pgr_triangle.hpp
@@ -0,0 +1,58 @@
+/*PGR-GNU*****************************************************************
+file: pgr_triangle.hpp
+
+Copyright (c) 2018 pgRouting developers
+Mail: project@pgrouting.org
+
+Copyright (c) 2018 Celia Virginia Vergara Castillo
+vicky_vergara@hotmail.com
+
+------
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+********************************************************************PGR-GNU*/
+
+#ifndef INCLUDE_ALPHASHAPE_PGR_TRIANGLE_HPP_
+#define INCLUDE_ALPHASHAPE_PGR_TRIANGLE_HPP_
+#pragma once
+
+#include "cpp_common/bpoint.hpp"
+
+namespace pgrouting {
+namespace alphashape {
+
+
+class Pgr_triangle {
+ public:
+     Pgr_triangle() = default;
+     Pgr_triangle(
+             Bpoint p1, Bpoint p2, Bpoint p3
+        );
+
+     bool has_point(const Bpoint &p) const;
+     bool has_edge(const Bpoint &p1, const Bpoint &p2) const;
+
+ private:
+     Bpoint m_p1;
+     Bpoint m_p2;
+     Bpoint m_p3;
+};
+
+
+}  // namespace alphashape
+}  // namespace pgrouting
+
+#endif  // INCLUDE_ALPHASHAPE_PGR_TRIANGLE_HPP_
---
+++ src/alpha_shape/pgr_triangle.cpp
@@ -0,0 +1,52 @@
+/*PGR-GNU*****************************************************************
+file: pgr_triangle.cpp
+
+Copyright (c) 2018 pgRouting developers
+Mail: project@pgrouting.org
+
+Copyright (c) 2018 Celia Virginia Vergara Castillo
+vicky_vergara@hotmail.com
+
+------
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+********************************************************************PGR-GNU*/
+
+#include "alphaShape/pgr_triangle.hpp"
+
+namespace pgrouting {
+namespace alphashape {
+
+Pgr_triangle::Pgr_triangle(
+             Bpoint p1, Bpoint p2, Bpoint p3) :
+    m_p1(p1),
+    m_p2(p2),
+    m_p3(p3)
+{
+}
+
+bool
+Pgr_triangle::has_point(const Bpoint &p) {
+    return true;
+}
+
+bool
+Pgr_triangle::has_edge(const Bpoint &p1, const Bpoint &p2) {
+    return true;
+}
+
+}  // namespace alphashape
+}  // namespace pgrouting
