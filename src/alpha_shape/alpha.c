e1152022b97a4ef322a7e9a5f5c969a9a6c0d8d7
cvvergara <vicky@georepublic.de>
2019-01-15 10:41:27 -0600

    [alphaShape] fixing an inculde directive

--- src/alpha_shape/alpha.c
+++ src/alpha_shape/alpha.c
@@ -23,7 +23,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 ********************************************************************PGR-GNU*/
 
 #include <stdbool.h>
-# include <stdint.h>
+#include <stdint.h>
 #include "c_common/postgres_connection.h"
 
 #include "c_common/debug_macro.h"
