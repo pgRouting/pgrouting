cdd6ebb6393c57b07988d5236cdba13ed0bb132e
cvvergara <vicky@georepublic.de>
2019-02-14 09:30:14 -0600

    [pgTap] function to test alphashape old code with new code

---
+++ tools/testers/alphaShapeTester.sql
@@ -0,0 +1,56 @@
+
+CREATE OR REPLACE FUNCTION test_alpha(_tbl REGCLASS, _geom TEXT, alpha FLOAT)
+RETURNS SETOF TEXT AS
+$BODY$
+BEGIN
+    CREATE TABLE original AS
+    SELECT pgr_pointsAsPolygon(format($$
+        WITH
+        Points AS (SELECT (st_dumppoints(%2$I)).geom FROM %1$I)
+        SELECT row_number() over()::INTEGER AS id, ST_X(geom) AS x, ST_Y(geom) AS y FROM Points$$,
+        _tbl, _geom), alpha)
+    AS geom;
+
+    EXECUTE format($$
+    CREATE TABLE newquery AS
+    SELECT pgr_alphaShape1((SELECT array_agg(%2$I) FROM %1$I), sqrt(%3$s))
+    AS geom$$, _tbl, _geom, alpha);
+
+    RETURN QUERY
+    SELECT results_eq(
+        $$SELECT ST_IsValid(geom) FROM original$$,
+        $$SELECT true$$);
+
+    RETURN QUERY
+    SELECT results_eq(
+        $$SELECT ST_IsValid(geom) FROM newquery$$,
+        $$SELECT true$$);
+
+
+    RETURN QUERY
+    SELECT results_eq(
+        $$SELECT ST_Area(geom)::TEXT FROM original$$,
+        $$SELECT ST_Area(geom)::TEXT FROM newquery$$);
+
+    RETURN QUERY
+    SELECT results_eq(
+        $$SELECT count(*) FROM (SELECT ST_DumpPoints(geom) FROM original) AS a$$,
+        $$SELECT count(*) FROM (SELECT ST_DumpPoints(geom) FROM newquery) AS a$$);
+
+    RETURN QUERY
+    SELECT results_eq(
+        $$SELECT ST_NPoints(geom) FROM original$$,
+        $$SELECT ST_NPoints(geom) FROM newquery$$);
+
+    RETURN QUERY
+    SELECT set_eq(
+        $$SELECT (ST_DumpPoints(geom)).geom FROM original$$,
+        $$SELECT (ST_DumpPoints(geom)).geom FROM newquery$$);
+
+    DROP TABLE original;
+    DROP TABLE newquery;
+
+END
+$BODY$
+LANGUAGE plpgsql VOLATILE;
+
