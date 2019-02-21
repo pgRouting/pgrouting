2df85ff71a91afd77d2aa5277741b7765b321f0f
cvvergara <vicky@georepublic.de>
2019-02-21 07:16:44 -0600

    [alphaShape] adjusting test

--- test/alpha_shape/doc-pgr_pointsAsPolygon.test.sql
+++ test/alpha_shape/doc-pgr_pointsAsPolygon.test.sql
@@ -3,6 +3,6 @@
 -------------------------------------------------------------------------------
 
 \echo --q1
-SELECT ST_AsText(pgr_pointsAsPolygon('SELECT id::integer, ST_X(the_geom)::float AS x, ST_Y(the_geom)::float AS y
+SELECT ST_Area(pgr_pointsAsPolygon('SELECT id::integer, ST_X(the_geom)::float AS x, ST_Y(the_geom)::float AS y
         FROM edge_table_vertices_pgr'));
 \echo --q2
