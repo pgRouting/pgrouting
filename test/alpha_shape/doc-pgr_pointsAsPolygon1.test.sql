8a0491075168cc409262322cf3a27da6f83ead32
cvvergara <vicky@georepublic.de>
2019-02-27 09:04:35 -0600

    [alphaShape] removing points as polygon documentation queries

--- test/alpha_shape/doc-pgr_pointsAsPolygon1.test.sql
+++
@@ -1,9 +0,0 @@
--------------------------------------------------------------------------------
---              PGR_pointsAsPolygon
--------------------------------------------------------------------------------
-
-\echo --q1
-SELECT ST_Area(pgr_pointsAsPolygon('SELECT id::integer, ST_X(the_geom)::float AS x, ST_Y(the_geom)::float AS y
-        FROM edge_table_vertices_pgr'));
-SELECT ST_Area(pgr_alphaShape1((SELECT array_agg(the_geom) FROM edge_table_vertices_pgr)));
-\echo --q2
