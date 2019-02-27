d4c0033f73109123432b3ead21c4b28c02cc43a6
cvvergara <vicky@georepublic.de>
2019-02-27 12:48:55 -0600

    [alphaShape] adjustment of documentation queries

--- test/alpha_shape/doc-pgr_alphashape.test.sql
+++ test/alpha_shape/doc-pgr_alphashape.test.sql
@@ -1,5 +1,5 @@
 \echo -- q1
 SELECT ST_Npoints(pgr_alphaShape((SELECT array_agg(the_geom) FROM edge_table_vertices_pgr)));
 \echo -- q2
-SELECT ST_Area(pgr_alphaShape((SELECT array_agg(the_geom) FROM edge_table_vertices_pgr), 1.5));
+SELECT ST_Area(geom) FROM pgr_alphaShape((SELECT ST_Collect(the_geom) FROM edge_table_vertices_pgr), 1.5);
 \echo -- q3
