34e799562d7e1ee5f6dcefad38e8b450b8d42a88
cvvergara <vicky@georepublic.de>
2019-03-01 15:33:07 -0600

    [alphaShape] removing test with removed signature

--- tools/testers/alphaShapeTester.sql
+++ tools/testers/alphaShapeTester.sql
@@ -11,39 +11,6 @@ $BODY$
 BEGIN
     EXECUTE format($$
     CREATE TABLE newquery AS
-    SELECT pgr_alphaShape((SELECT array_agg(%2$I) FROM %1$I), %3$s)
-    AS geom$$, _tbl, _geom, alpha);
-
-    RETURN QUERY
-    SELECT results_eq(
-        $$SELECT ST_IsValid(geom) FROM newquery$$,
-        $$SELECT true$$,
-        'SHOULD BE: valid with spoon radius = ' || alpha);
-
-    RETURN QUERY
-    SELECT results_eq(
-        $$SELECT ST_Area(geom)::TEXT FROM newquery$$,
-        $$SELECT $$ || area || '::TEXT',
-        'SHOULD BE: ' || area || ' with spoon radius = ' || alpha);
-
-
-    RETURN QUERY
-    SELECT results_eq(
-        $$SELECT ST_IsEmpty(geom) FROM newquery$$,
-        $$SELECT $$ || isEmpty,
-        'SHOULD BE: ' || isEmpty || ' with spoon radius = ' || alpha);
-
-
-    RETURN QUERY
-    SELECT results_eq(
-        $$SELECT ST_Npoints(geom) FROM newquery$$,
-        $$SELECT $$ || Npoints,
-        'SHOULD BE: ' || Npoints || ' points with spoon radius = ' || alpha);
-
-    DROP TABLE newquery;
-
-    EXECUTE format($$
-    CREATE TABLE newquery AS
     SELECT pgr_alphaShape((SELECT ST_collect(%2$I) FROM %1$I), %3$s)
     AS geom$$, _tbl, _geom, alpha);
 
