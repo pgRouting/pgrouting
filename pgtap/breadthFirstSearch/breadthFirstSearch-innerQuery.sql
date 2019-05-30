31345072bb543da051af5c37f506e8f1375d2464
Gvs Akhil <gvs.akhil1997@gmail.com>
2019-05-31 00:25:59 +0530

    [breadthFirstSearch] Modified pgTap tests and signatures to match updated function signature

--- pgtap/breadthFirstSearch/breadthFirstSearch-innerQuery.sql
+++ pgtap/breadthFirstSearch/breadthFirstSearch-innerQuery.sql
@@ -1,22 +1,8 @@
 \i setup.sql
 
-SELECT plan(315);
-SET client_min_messages TO ERROR;
+SELECT plan(45);
 
-
-SELECT style_dijkstra('pgr_breadthFirstSearch', ', 2, 3)');
-SELECT style_dijkstra('pgr_breadthFirstSearch', ', 2, 3, true)');
-SELECT style_dijkstra('pgr_breadthFirstSearch', ', 2, 3, false)');
-
--- ONE TO ONE
-SELECT style_dijkstra('pgr_breadthFirstSearch', ', 2, 3, true)');
--- ONE TO MANY
-SELECT style_dijkstra('pgr_breadthFirstSearch', ', 2, ARRAY[3], true)');
--- MANY TO ONE
-SELECT style_dijkstra('pgr_breadthFirstSearch', ', ARRAY[2], 3, true)');
--- MANY TO MANY
-SELECT style_dijkstra('pgr_breadthFirstSearch', ', ARRAY[2], ARRAY[3], true)');
-
+SELECT style_dijkstra('pgr_primBFS', ', 5)');
 
 SELECT finish();
 ROLLBACK;
