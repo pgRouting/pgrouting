52026e63414d704c3420a9bc601dbffc640379a4
nike0good <wuhang@wuhangdeMacBook-Pro.local>
2019-06-08 16:12:05 +0800

    [toposort] Add pgtap files

---
+++ pgtap/topologicalSort/topologicalSort-innerQuery.sql
@@ -0,0 +1,14 @@
+\i setup.sql
+
+SELECT plan(1);
+
+SELECT has_function('pgr_topologicalSort',
+    ARRAY['text']);
+
+SELECT function_returns('pgr_topologicalSort',
+    ARRAY['text'],
+    'setof record');
+
+
+SELECT * FROM finish();
+ROLLBACK;
