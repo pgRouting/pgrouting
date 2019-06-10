8a67af4899f915c697241e559a3aba8c5ed08ce4
nike0good <wuhang@wuhangdeMacBook-Pro.local>
2019-06-11 02:39:05 +0800

    [toposort] fix error of topologicalSort-innerQuery.sql

--- pgtap/topologicalSort/topologicalSort-innerQuery.sql
+++ pgtap/topologicalSort/topologicalSort-innerQuery.sql
@@ -2,15 +2,12 @@
 
 SELECT plan(47);
 
-SELECT has_function('pgr_topologicalSort',
-    ARRAY['text']);
 
-SELECT function_returns('pgr_topologicalSort',
-    ARRAY['text'],
-    'setof record');
+SELECT has_function('pgr_topologicalSort');
+
+SELECT function_returns('pgr_topologicalSort', ARRAY['text'], 'setof record');
 
 SELECT style_dijkstra('pgr_topologicalSort', ')');
 
-
-SELECT * FROM finish();
+SELECT finish();
 ROLLBACK;
