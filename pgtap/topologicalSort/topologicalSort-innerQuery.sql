4c420cadf772238a7f06620c177e3e01c6363eb2
nike0good <wuhang@wuhangdeMacBook-Pro.local>
2019-06-11 16:12:52 +0800

    [toposort] hide has_function.

--- pgtap/topologicalSort/topologicalSort-innerQuery.sql
+++ pgtap/topologicalSort/topologicalSort-innerQuery.sql
@@ -1,11 +1,12 @@
 \i setup.sql
 
-SELECT plan(47);
+SELECT plan(45);
 
-
+```
 SELECT has_function('pgr_topologicalSort');
 
 SELECT function_returns('pgr_topologicalSort', ARRAY['text'], 'setof record');
+```
 
 SELECT style_dijkstra('pgr_topologicalSort', ')');
 
