762b09426d27c5a6f35e5fed8e9c30240e6b38d8
nike0good <wuhang@wuhangdeMacBook-Pro.local>
2019-06-11 17:59:48 +0800

    [toposort] hide innderQuery.sql

--- pgtap/topologicalSort/topologicalSort-innerQuery.sql
+++ pgtap/topologicalSort/topologicalSort-innerQuery.sql
@@ -1,14 +1,6 @@
 \i setup.sql
-
 SELECT plan(0);
 
-```
-SELECT has_function('pgr_topologicalSort');
-
-SELECT function_returns('pgr_topologicalSort', ARRAY['text'], 'setof record');
-```
-
-SELECT style_dijkstra('pgr_topologicalSort', ')');
 
 SELECT finish();
 ROLLBACK;
