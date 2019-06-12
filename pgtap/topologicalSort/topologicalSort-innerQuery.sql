10339f51400260dfd66851a307cd6a80d682a66d
nike0good <wuhang@wuhangdeMacBook-Pro.local>
2019-06-12 14:44:05 +0800

    [toposort] remove style_dijkstra of topologicalSort-innerQuery.sql

--- pgtap/topologicalSort/topologicalSort-innerQuery.sql
+++ pgtap/topologicalSort/topologicalSort-innerQuery.sql
@@ -1,7 +1,9 @@
 \i setup.sql
-SELECT plan(45);
+SELECT plan(2);
 
-SELECT style_dijkstra('pgr_topologicalSort', ')');
+SELECT has_function('pgr_topologicalSort');
+
+SELECT function_returns('pgr_topologicalSort', ARRAY['text'], 'setof record');
 
 
 SELECT finish();
