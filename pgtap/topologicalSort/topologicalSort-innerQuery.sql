6eb1c1ba5a825e344f7ff976e04b41622e60b68f
nike0good <wuhang@wuhangdeMacBook-Pro.local>
2019-06-13 03:29:52 +0800

    [toposort] fix modify query file

--- pgtap/topologicalSort/topologicalSort-innerQuery.sql
+++ pgtap/topologicalSort/topologicalSort-innerQuery.sql
@@ -3,12 +3,10 @@
 SELECT plan(2);
 SET client_min_messages TO ERROR;
 
-SELECT has_function('pgr_topologicalsort',
-    ARRAY['text'] );
+SELECT has_function('pgr_topologicalsort');
 
-SELECT function_returns('pgr_topologicalsort',
-    ARRAY['text'],
-    'setof record');
+
+SELECT function_returns('pgr_topologicalsort',ARRAY['text'],'setof record');
 
 SELECT finish();
 ROLLBACK;
