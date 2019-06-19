23aa589a367ec9d500443eac7cfbbc7e81fcedfe
nike0good <wuhang@wuhangdeMacBook-Pro.local>
2019-06-20 04:08:01 +0800

    [toposort] modify test files. 2

--- test/topologicalSort/doc-topologicalSort.test.sql
+++ test/topologicalSort/doc-topologicalSort.test.sql
@@ -1,15 +1,26 @@
+CREATE TABLE edge_table1 (
+    id serial,
+    source integer,
+    target integer,
+    cost double precision,
+    reverse_cost double precision
+);
+INSERT INTO edge_table1 (source,target,cost,reverse_cost) VALUES ( 1, 2,0,0);
+INSERT INTO edge_table1 (source,target,cost,reverse_cost) VALUES (2,3,0,0);
+
+
 SELECT * FROM pgr_topologicalsort(
     'SELECT id,
             source,
             target,
             cost,
             reverse_cost
-    FROM edge_table'
+    FROM edge_table1'
     
 );
 
 SELECT * FROM pgr_topologicalsort(
-  'SELECT id, source, target, cost, reverse_cost FROM edge_table WHERE id = 2'
+  'SELECT id, source, target, cost, reverse_cost FROM edge_table1'
 ); 
 
 \echo -- q1
