780c02a5bf180a4e93c4eec3e3fb09bb91f03fc5
Gvs Akhil <gvs.akhil1997@gmail.com>
2019-06-22 00:06:08 +0530

    [breadthFirstSearch] Added additional inner-query tests

--- pgtap/breadthFirstSearch/breadthFirstSearch-innerQuery.sql
+++ pgtap/breadthFirstSearch/breadthFirstSearch-innerQuery.sql
@@ -1,8 +1,15 @@
 \i setup.sql
 
-SELECT plan(45);
+SELECT plan(360);
 
 SELECT style_dijkstra('pgr_breadthFirstSearch', ', 5)');
+SELECT style_dijkstra('pgr_breadthFirstSearch', ', 5, 2)');
+SELECT style_dijkstra('pgr_breadthFirstSearch', ', ARRAY[3,5])');
+SELECT style_dijkstra('pgr_breadthFirstSearch', ', ARRAY[3,5], 2)');
+SELECT style_dijkstra('pgr_breadthFirstSearch', ', 5, directed := true)');
+SELECT style_dijkstra('pgr_breadthFirstSearch', ', 5, 2, directed := true)');
+SELECT style_dijkstra('pgr_breadthFirstSearch', ', ARRAY[3,5], directed := true)');
+SELECT style_dijkstra('pgr_breadthFirstSearch', ', ARRAY[3,5],2, directed := true)');
 
 SELECT finish();
 ROLLBACK;
