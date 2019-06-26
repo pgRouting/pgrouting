708e7c8d3161082810f84c8cd46712d1eaaa9f67
cvvergara <vicky@georepublic.de>
2019-06-26 13:51:24 -0500

    [extractVertices][documentation] fixing documentation

--- test/topology/doc-pgr_extractVertices.test.sql
+++ test/topology/doc-pgr_extractVertices.test.sql
@@ -1,15 +1,54 @@
 \echo --q1
-SELECT  * FROM pgr_extractVertices('SELECT source, target, the_geom FROM edge_table');
+SELECT  * FROM pgr_extractVertices(
+    'SELECT id, the_geom AS geom
+    FROM edge_table');
 \echo --q1.1
 
 \echo --q2
-SELECT  * FROM pgr_extractVertices('SELECT source, target FROM edge_table WHERE id < 5');
+SELECT  * FROM pgr_extractVertices(
+    'SELECT id, the_geom AS geom FROM edge_table',
+    dryrun := true);
 \echo --q2.1
 
 \echo --q3
-DROP TABLE IF EXISTS edge_table_vertices_pgr;
-SELECT  * INTO edge_table_vertices_pgr
-FROM pgr_extractVertices('SELECT source, target, the_geom FROM edge_table');
-SELECT * FROM edge_table_vertices_pgr;
+DROP TABLE IF EXISTS vertices_table;
 \echo --q3.1
+
+UPDATE edge_table
+SET source = NULL, target = NULL,
+   x1 = NULL, y1 = NULL,
+   x2 = NULL, y2 = NULL;
+\echo --q3.2
+
+SELECT  * INTO vertices_table
+FROM pgr_extractVertices('SELECT id, the_geom AS geom FROM edge_table');
+\echo --q3.3
+
+SELECT *
+FROM vertices_table;
+\echo --q3.4
+
+WITH
+    out_going AS (
+        SELECT id AS vid, unnest(out_edges) AS eid, x, y
+        FROM vertices_table
+    )
+UPDATE edge_table
+SET source = vid, x1 = x, y1 = y
+FROM out_going WHERE id = eid;
+\echo --q3.5
+
+WITH
+    in_coming AS (
+        SELECT id AS vid, unnest(in_edges) AS eid, x, y
+        FROM vertices_table
+    )
+UPDATE edge_table
+SET target = vid, x2 = x, y2 = y
+FROM in_coming WHERE id = eid;
+
+\echo --q3.6
+SELECT id, source, target, x1, y1, x2, y2
+FROM edge_table;
+\echo --q3.7
 
