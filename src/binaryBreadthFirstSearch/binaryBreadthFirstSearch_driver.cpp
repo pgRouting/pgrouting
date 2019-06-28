1412dd4873293654d8f0742bc1619e166f685311
Gvs Akhil <gvs.akhil1997@gmail.com>
2019-06-28 21:02:45 +0530

    [binaryBreadthFirstSearch] Added src/ directory files as well as function signatures to pgrouting--3.0.0.sig

---
+++ src/binaryBreadthFirstSearch/binaryBreadthFirstSearch_driver.cpp
@@ -0,0 +1,153 @@
+/*PGR-GNU*****************************************************************
+File: binaryBreadthFirstSearch_driver.cpp
+
+Generated with Template by:
+Copyright (c) 2019 pgRouting developers
+Mail: project@pgrouting.org
+
+Function's developer:
+Copyright (c) 2019 Gudesa Venkata Sai Akhil
+Mail: gvs.akhil1997@gmail.com
+
+
+------
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+********************************************************************PGR-GNU*/
+
+#include "drivers/binaryBreadthFirstSearch/binaryBreadthFirstSearch_driver.h"
+
+#include <sstream>
+#include <deque>
+#include <vector>
+#include <algorithm>
+#include <limits>
+
+//TODO : Correct the headers
+// #include "binaryBreadthFirstSearch/pgr_binaryBreadthFirstSearch.hpp"
+
+//TODO : Remove below
+
+#include <deque>
+#include <set>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <functional>
+#include <limits>
+#include <numeric>
+
+#include "cpp_common/basePath_SSEC.hpp"
+#include "cpp_common/pgr_base_graph.hpp"
+// TODO : Remove above
+
+#include "cpp_common/pgr_alloc.hpp"
+#include "cpp_common/pgr_assert.h"
+
+
+void
+do_pgr_binaryBreadthFirstSearch(
+        pgr_edge_t  *data_edges,
+        size_t total_edges,
+        int64_t  *start_vidsArr,
+        size_t size_start_vidsArr,
+        int64_t  *end_vidsArr,
+        size_t size_end_vidsArr,
+        bool directed,
+
+        General_path_element_t **return_tuples,
+        size_t *return_count,
+        char ** log_msg,
+        char ** notice_msg,
+        char ** err_msg) {
+    std::ostringstream log;
+    std::ostringstream err;
+    std::ostringstream notice;
+
+    try {
+        pgassert(total_edges != 0);
+        pgassert(!(*log_msg));
+        pgassert(!(*notice_msg));
+        pgassert(!(*err_msg));
+        pgassert(!(*return_tuples));
+        pgassert(*return_count == 0);
+
+        graphType gType = directed? DIRECTED: UNDIRECTED;
+
+        log << "Inserting vertices into a c++ vector structure";
+        std::vector<int64_t>
+            start_vertices(start_vidsArr, start_vidsArr + size_start_vidsArr);
+        std::vector< int64_t >
+            end_vertices(end_vidsArr, end_vidsArr + size_end_vidsArr);
+
+
+        std::deque< Path >paths;
+        if (directed) {
+            log << "\nWorking with directed Graph";
+            pgrouting::DirectedGraph digraph(gType);
+            digraph.insert_edges(data_edges, total_edges);
+            // paths = pgr_binaryBreadthFirstSearch(
+
+        } else {
+            log << "\nWorking with Undirected Graph";
+            pgrouting::UndirectedGraph undigraph(gType);
+            undigraph.insert_edges(data_edges, total_edges);
+            // paths = pgr_binaryBreadthFirstSearch(
+
+        }
+
+        size_t count(0);
+        count = count_tuples(paths);
+
+        if (count == 0) {
+            (*return_tuples) = NULL;
+            (*return_count) = 0;
+            notice <<
+                "No paths found";
+            *log_msg = pgr_msg(notice.str().c_str());
+            return;
+        }
+
+        (*return_tuples) = pgr_alloc(count, (*return_tuples));
+        log << "\nConverting a set of paths into the tuples";
+        (*return_count) = (collapse_paths(return_tuples, paths));
+
+        *log_msg = log.str().empty()?
+            *log_msg :
+            pgr_msg(log.str().c_str());
+        *notice_msg = notice.str().empty()?
+            *notice_msg :
+            pgr_msg(notice.str().c_str());
+    } catch (AssertFailedException &except) {
+        (*return_tuples) = pgr_free(*return_tuples);
+        (*return_count) = 0;
+        err << except.what();
+        *err_msg = pgr_msg(err.str().c_str());
+        *log_msg = pgr_msg(log.str().c_str());
+    } catch (std::exception &except) {
+        (*return_tuples) = pgr_free(*return_tuples);
+        (*return_count) = 0;
+        err << except.what();
+        *err_msg = pgr_msg(err.str().c_str());
+        *log_msg = pgr_msg(log.str().c_str());
+    } catch(...) {
+        (*return_tuples) = pgr_free(*return_tuples);
+        (*return_count) = 0;
+        err << "Caught unknown exception!";
+        *err_msg = pgr_msg(err.str().c_str());
+        *log_msg = pgr_msg(log.str().c_str());
+    }
+}
