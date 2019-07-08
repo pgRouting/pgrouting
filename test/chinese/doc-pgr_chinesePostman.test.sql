6c8b665e25bda5a8046b0a9d701ca5637f6bcc5b
cvvergara <vicky@georepublic.de>
2019-07-08 08:23:46 -0500

    [chinese] documentation queries work

--- test/chinese/doc-pgr_chinesePostman.test.sql
+++ test/chinese/doc-pgr_chinesePostman.test.sql
@@ -1,13 +1,13 @@
 
 \echo -- q1
-SELECT * FROM pgr_directedChPP_Cost(
+SELECT * FROM pgr_chinesePostman(
     'SELECT id,
      source, target,
      cost, reverse_cost FROM edge_table where id < 17'
 );
 
 \echo -- q2
-SELECT * FROM pgr_directedChPP_Cost(
+SELECT * FROM pgr_chinesePostman(
     'SELECT id,
      source, target,
      cost, reverse_cost FROM edge_table where id = 9 or id = 11 or id = 13 or id = 15'
