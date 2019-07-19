4be84eb92e42bfdb2e47ee7b379d003204b2b44f
cvvergara <vicky@georepublic.de>
2019-07-19 12:57:22 -0500

    [licenses] Standarizing some more licenses on sql directory

--- sql/common/pgrouting_version.sql
+++ sql/common/pgrouting_version.sql
@@ -1,20 +1,35 @@
-/*
--- -------------------------------------------------------------------
--- pgrouting_version.sql
--- AuthorL Stephen Woodbridge <woodbri@imaptools.com>
--- Copyright 2013 Stephen Woodbridge
--- This file is release unde an MIT-X license.
--- -------------------------------------------------------------------
-*/
+/*PGR-MIT*****************************************************************
 
-/*
-.. function:: pgr_version()
+FILE: pgrouting_version.sql
 
-   Author: Stephen Woodbridge <woodbri@imaptools.com>
+Copyright (c) 2013 pgRouting developers
+Mail: project@pgrouting.org
+Author: Stephen Woodbridge <woodbri@imaptools.com>
 
-   Returns the version of pgrouting,Git build,Git hash, Git branch and boost
-*/
+------
+MIT/X license
 
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+********************************************************************PGR-MIT*/
 
 CREATE OR REPLACE FUNCTION pgr_version()
 RETURNS TABLE(
