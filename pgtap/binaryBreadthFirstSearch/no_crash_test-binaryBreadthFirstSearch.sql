26da4c7cbb5a86f101afc6478ae18671fc56568f
Gvs Akhil <gvs.akhil1997@gmail.com>
2019-07-21 16:51:34 +0530

    [binaryBreadthFirstSearch] Updated no-crash pgTap tests

--- pgtap/binaryBreadthFirstSearch/no_crash_test-binaryBreadthFirstSearch.sql
+++ pgtap/binaryBreadthFirstSearch/no_crash_test-binaryBreadthFirstSearch.sql
@@ -1,6 +1,6 @@
 \i setup.sql
 
-SELECT plan(67);
+SELECT plan(131);
 
 PREPARE edges AS
 SELECT id, source, target, cost, reverse_cost  FROM edge_table;
@@ -22,6 +22,8 @@ $BODY$
 DECLARE
 params TEXT[];
 subs TEXT[];
+params_roadworks TEXT[];
+subs_roadworks TEXT[];
 BEGIN
     -- one to one
     params = ARRAY[
@@ -94,6 +96,79 @@ BEGIN
     'NULL::BIGINT[]'
     ]::TEXT[];
     RETURN query SELECT * FROM no_crash_test('pgr_binaryBreadthFirstSearch', params, subs);
+
+    -- using roadworks
+    -- one to one
+    params_roadworks = ARRAY[
+    '$$SELECT id, source, target, road_work as cost, reverse_road_work as reverse_cost  FROM roadworks$$'
+    ,'1::BIGINT',
+    '2::BIGINT'
+    ]::TEXT[];
+    subs_roadworks = ARRAY[
+    'NULL',
+    '(SELECT id FROM edge_table_vertices_pgr  WHERE id IN (-1))',
+    '(SELECT id FROM edge_table_vertices_pgr  WHERE id IN (-1))'
+    ]::TEXT[];
+
+    RETURN query SELECT * FROM no_crash_test('pgr_binaryBreadthFirstSearch', params_roadworks, subs_roadworks);
+
+    subs_roadworks = ARRAY[
+    'NULL',
+    'NULL::BIGINT',
+    'NULL::BIGINT'
+    ]::TEXT[];
+    RETURN query SELECT * FROM no_crash_test('pgr_binaryBreadthFirstSearch', params_roadworks, subs_roadworks);
+
+    -- one to many
+    params_roadworks = ARRAY['$$edges$$','1', 'ARRAY[2,5]::BIGINT[]']::TEXT[];
+    subs_roadworks = ARRAY[
+    'NULL',
+    '(SELECT id FROM edge_table_vertices_pgr  WHERE id IN (-1))',
+    '(SELECT array_agg(id) FROM edge_table_vertices_pgr  WHERE id IN (-1))'
+    ]::TEXT[];
+
+    RETURN query SELECT * FROM no_crash_test('pgr_binaryBreadthFirstSearch', params_roadworks, subs_roadworks);
+
+    subs_roadworks = ARRAY[
+    'NULL',
+    'NULL::BIGINT',
+    'NULL::BIGINT[]'
+    ]::TEXT[];
+    RETURN query SELECT * FROM no_crash_test('pgr_binaryBreadthFirstSearch', params_roadworks, subs_roadworks);
+
+    -- many to one
+    params_roadworks = ARRAY['$$edges$$', 'ARRAY[2,5]::BIGINT[]', '1']::TEXT[];
+    subs_roadworks = ARRAY[
+    'NULL',
+    '(SELECT array_agg(id) FROM edge_table_vertices_pgr  WHERE id IN (-1))',
+    '(SELECT id FROM edge_table_vertices_pgr  WHERE id IN (-1))'
+    ]::TEXT[];
+
+    RETURN query SELECT * FROM no_crash_test('pgr_binaryBreadthFirstSearch', params_roadworks, subs_roadworks);
+
+    subs_roadworks = ARRAY[
+    'NULL',
+    'NULL::BIGINT[]',
+    'NULL::BIGINT'
+    ]::TEXT[];
+    RETURN query SELECT * FROM no_crash_test('pgr_binaryBreadthFirstSearch', params_roadworks, subs_roadworks);
+
+    -- many to many
+    params_roadworks = ARRAY['$$edges$$','ARRAY[1]::BIGINT[]', 'ARRAY[2,5]::BIGINT[]']::TEXT[];
+    subs_roadworks = ARRAY[
+    'NULL',
+    '(SELECT array_agg(id) FROM edge_table_vertices_pgr  WHERE id IN (-1))',
+    '(SELECT array_agg(id) FROM edge_table_vertices_pgr  WHERE id IN (-1))'
+    ]::TEXT[];
+
+    RETURN query SELECT * FROM no_crash_test('pgr_binaryBreadthFirstSearch', params_roadworks, subs_roadworks);
+
+    subs_roadworks = ARRAY[
+    'NULL',
+    'NULL::BIGINT[]',
+    'NULL::BIGINT[]'
+    ]::TEXT[];
+    RETURN query SELECT * FROM no_crash_test('pgr_binaryBreadthFirstSearch', params_roadworks, subs_roadworks);
 
 END
 $BODY$
