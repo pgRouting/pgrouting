fb017d9a8dfd6849748ef90994df54d488b148d4
cvvergara <vicky@georepublic.de>
2019-07-22 13:10:10 -0500

    [pgtap][extractVertices] adding innerquery to extract vertices

--- pgtap/topology/extractVertices/innerQuery.sql
+++ pgtap/topology/extractVertices/innerQuery.sql
@@ -1,24 +1,81 @@
 \i setup.sql
 
-SELECT plan(10);
+SELECT plan(84);
 
-CREATE OR REPLACE FUNCTION style_id_s_t(fn TEXT, rest_sql TEXT)
+CREATE OR REPLACE FUNCTION style_extract_normal(fn TEXT, rest_sql TEXT)
 RETURNS SETOF TEXT AS
 $BODY$
 BEGIN
 
---with the_geom
+--with id
 RETURN QUERY SELECT test_anyInteger(fn, rest_sql,
-    ARRAY['id', 'source', 'target', 'the_geom'],
+    ARRAY['id', 'source', 'target'],
+    'id');
+RETURN QUERY SELECT test_anyInteger(fn, rest_sql,
+    ARRAY['id', 'the_geom AS geom'],
+    'id');
+
+RETURN QUERY SELECT test_anyInteger(fn, rest_sql,
+    ARRAY['id', 'ST_StartPoint(the_geom) AS startpoint, ST_ENDPoint(the_geom) AS endpoint'],
+    'id');
+
+RETURN QUERY SELECT test_anyInteger(fn, rest_sql,
+    ARRAY['id', 'source', 'target'],
     'source');
 RETURN QUERY SELECT test_anyInteger(fn, rest_sql,
-    ARRAY['id', 'source', 'target', 'the_geom'],
+    ARRAY['id', 'source', 'target'],
+    'target');
+
+--with out id
+RETURN QUERY SELECT test_anyInteger(fn, rest_sql,
+    ARRAY['source', 'target'],
+    'source');
+RETURN QUERY SELECT test_anyInteger(fn, rest_sql,
+    ARRAY['source', 'target'],
     'target');
 END;
 $BODY$
 LANGUAGE plpgsql;
 
-SELECT style_id_s_t('pgr_extractVertices', ')');
+
+CREATE OR REPLACE FUNCTION style_extract_dryrun(fn TEXT, rest_sql TEXT)
+RETURNS SETOF TEXT AS
+$BODY$
+BEGIN
+
+--with id
+RETURN QUERY SELECT test_anyNumerical(fn, rest_sql,
+    ARRAY['id', 'source', 'target'],
+    'id');
+RETURN QUERY SELECT test_anyNumerical(fn, rest_sql,
+    ARRAY['id', 'the_geom AS geom'],
+    'id');
+
+RETURN QUERY SELECT test_anyNumerical(fn, rest_sql,
+    ARRAY['id', 'ST_StartPoint(the_geom) AS startpoint, ST_ENDPoint(the_geom) AS endpoint'],
+    'id');
+
+RETURN QUERY SELECT test_anyNumerical(fn, rest_sql,
+    ARRAY['id', 'source', 'target'],
+    'source');
+RETURN QUERY SELECT test_anyNumerical(fn, rest_sql,
+    ARRAY['id', 'source', 'target'],
+    'target');
+
+--with out id
+RETURN QUERY SELECT test_anyNumerical(fn, rest_sql,
+    ARRAY['source', 'target'],
+    'source');
+RETURN QUERY SELECT test_anyNumerical(fn, rest_sql,
+    ARRAY['source', 'target'],
+    'target');
+END;
+$BODY$
+LANGUAGE plpgsql;
+
+
+SELECT style_extract_normal('pgr_extractVertices', ')');
+SELECT style_extract_dryrun('pgr_extractVertices', ', true)');
 
 SELECT finish();
 ROLLBACK;
