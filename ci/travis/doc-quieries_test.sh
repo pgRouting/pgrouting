ec66aef5ae91c3e401d7a96f3129e45f1d7a5722
cvvergara <vicky@georepublic.de>
2019-07-24 21:03:14 -0500

    [version] making travis to print out full-version

--- ci/travis/doc-quieries_test.sh
+++ ci/travis/doc-quieries_test.sh
@@ -70,6 +70,7 @@ then
     ERROR=1
 fi
 
+./tools/testers/doc_queries_generator.pl -pgver "${POSTGRESQL_VERSION}" -pguser "${PGUSER} -documentation -alg version"
 ./tools/testers/doc_queries_generator.pl -pgver "${POSTGRESQL_VERSION}" -pguser "${PGUSER}"
 
 if [ "$?" -ne 0 ]
