880fb6ab9ee939a0a4edc36216a54a87623d4535
cvvergara <vicky@georepublic.de>
2019-07-30 09:26:32 -0500

    [build] insted of definitions, adding a configured header file

--- src/version/_version.c
+++ src/version/_version.c
@@ -25,6 +25,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  ********************************************************************PGR-GNU*/
 
 #include "c_common/postgres_connection.h"
+#include "./_version.h"
 
 #define UNUSED(x) (void)(x)
 
