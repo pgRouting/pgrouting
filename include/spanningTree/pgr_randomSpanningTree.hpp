eee8b6b0bf5bb4e7ddbcd87173a9f2df34270f20
cvvergara <vicky@georepublic.de>
2019-07-31 19:15:07 -0500

    [lint] reverting some changes that did not work

--- include/spanningTree/pgr_randomSpanningTree.hpp
+++ include/spanningTree/pgr_randomSpanningTree.hpp
@@ -89,7 +89,6 @@ class Pgr_randomSpanningTree {
 
          std::vector< pgr_randomSpanningTree_t > resul;
          return resul;
-#if 0
          std::vector< pgr_randomSpanningTree_t > results;
          double totalcost = 0;
          pgr_randomSpanningTree_t tmp;
@@ -126,7 +125,6 @@ class Pgr_randomSpanningTree {
          }
          return results;
     }
-#endif
 };
 
 template < class G >
