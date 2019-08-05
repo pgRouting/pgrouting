77d861e0399aa9185fc00f0e6658e9386955dcac
nike0good <wuhang@wuhangdeMacBook-Pro.local>
2019-08-06 06:42:30 +0800

    [transitiveClosure] Update hpp file.

--- src/transitiveClosure/transitiveClosure_driver.cpp
+++ src/transitiveClosure/transitiveClosure_driver.cpp
@@ -32,32 +32,42 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 #include "cpp_common/identifiers.hpp"
 #include "cpp_common/pgr_alloc.hpp"
+#include "cpp_common/basePath_SSEC.hpp"
+#include "cpp_common/pgr_base_graph.hpp"
 
-namespace {
+
+
+// namespace {
 
 /*! @brief vertices with at least one contracted vertex
   @result The vids Identifiers with at least one contracted vertex
 */
-template <typename G>
-Identifiers<int64_t> get_vertices(const G& graph) {
-    Identifiers<int64_t> vids;
-    for (auto v : boost::make_iterator_range(boost::vertices(graph.graph))) {
-            vids += graph[v].id;
-    }
-    return vids;
+// template <typename G>
+// Identifiers<int64_t> get_vertices(const G& graph) {
+//     Identifiers<int64_t> vids;
+//     for (auto v : boost::make_iterator_range(boost::vertices(graph.graph))) {
+//             vids += graph[v].id;
+//     }
+//     return vids;
+// }
+
+// template <typename G>
+// static void process_transitiveClosure(
+//         G &graph,
+//         const std::vector< pgr_edge_t > &edges) {
+//     graph.insert_edges(edges);
+//     pgrouting::transitiveClosure::Pgr_transitiveClosure<G>.get(graph);
+// }
+
+template < class G >
+static
+boost::adjacency_list <>
+pgr_transitiveClosure(
+        G &graph) {
+    Pgr_transitiveClosure< G > fn_transitiveClosure;
+    return fn_transitiveClosure.transitiveClosure(graph);
 }
 
-template <typename G>
-static void process_transitiveClosure(
-        G &graph,
-        const std::vector< pgr_edge_t > &edges) {
-    graph.insert_edges(edges);
-
-    /*
-     * Function call to get the contracted graph.
-     */
-    pgrouting::transitiveClosure::Pgr_transitiveClosure<G> result(graph);
-}
 
 template <typename G>
 static
@@ -65,32 +75,62 @@ void get_postgres_result(
         G &graph,
         transitiveClosure_rt **return_tuples,
         size_t *count) {
-    auto vertices(get_vertices(graph));
-    (*count) = vertices.size();
+         
+
+
+    boost::adjacency_list <> TC;
+    TC = pgr_transitiveClosure(graph);
+
+    (*count) = boost::num_vertices(TC);
     (*return_tuples) = pgr_alloc((*count), (*return_tuples));
     size_t sequence = 0;
+    boost::graph_traits < boost::adjacency_list <> >::vertex_iterator i, end;
+    boost::graph_traits < boost::adjacency_list <> >::adjacency_iterator ai, a_end;
+    int ii=1;
+    for (boost::tie(i, end) = boost::vertices(TC); i != end; ++i) {
+            auto u = *i;
+            int64_t* target_array = NULL;
+            int64_t uid = graph.get_V(u);
+            boost::tie(ai, a_end) = adjacent_vertices(*i, TC);
+            
+            auto adj_siz = a_end - ai;
+            target_array = pgr_alloc(adj_siz , target_array);
+            int count = 0;
+            for (; ai != a_end; ++ai) {
+                auto v= *ai;
+                target_array[count++] = graph.get_V(v);
+            }
 
-    for (const auto id : vertices) {
-        auto v = graph.get_V(id);
-        int64_t* target_array = NULL;
-        auto vids = graph[v].target_array();
-
-        target_array = pgr_alloc(vids.size(), target_array);
-
-        int count = 0;
-        for (const auto id : vids) {
-            target_array[count++] = id;
-        }
         (*return_tuples)[sequence] = {
-            id,
+            ii++,
+            uid,
             target_array,
             count};
 
-        ++sequence;
-    }
+         ++sequence;
+    }    
+
+    // for (const auto id : vertices) {
+    //     auto v = graph.get_V(id);
+    //     int64_t* target_array = NULL;
+    //     auto vids = graph[v].target_array();
+
+    //     target_array = pgr_alloc(vids.size(), target_array);
+
+    //     int count = 0;
+    //     for (const auto id : vids) {
+    //         target_array[count++] = id;
+    //     }
+    //     (*return_tuples)[sequence] = {
+    //         id,
+    //         target_array,
+    //         count};
+
+    //     ++sequence;
+    // }
 }
 
-}  // namespace
+//}  // namespace
 
 
 
@@ -98,10 +138,10 @@ void get_postgres_result(
   edges_sql TEXT
  ***********************************************************/
 void
-do_pgr_contractGraph(
+do_pgr_transitiveClosure(
         pgr_edge_t  *data_edges,
         size_t total_edges,
-        contracted_rt **return_tuples,
+        transitiveClosure_rt **return_tuples,
         size_t *return_count,
         char **log_msg,
         char **notice_msg,
@@ -124,10 +164,11 @@ do_pgr_contractGraph(
 
 
         graphType gType = DIRECTED;
-        log << "Working with directed Graph\n";
-        pgrouting::CHDirectedGraph digraph(gType);
+        using DirectedGraph = pgrouting::DirectedGraph;
+        pgrouting::DirectedGraph digraph(gType);
 
-        process_transitiveClosure(digraph, edges);
+        // process_transitiveClosure(digraph, edges);
+        
 
         get_postgres_result(
                 digraph,
