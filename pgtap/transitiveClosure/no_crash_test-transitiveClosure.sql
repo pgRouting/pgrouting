ece8687a6ffff96821a1206b79bf4e9b38581a91
nike0good <wuhang@wuhangdeMacBook-Pro.local>
2019-08-07 00:59:44 +0800

    [transitiveClosure] Update no_crash_test-transitiveClosure.sql.

--- pgtap/transitiveClosure/no_crash_test-transitiveClosure.sql
+++ pgtap/transitiveClosure/no_crash_test-transitiveClosure.sql
@@ -1,11 +1,22 @@
 \i setup.sql
 
-SELECT plan(5);
+SELECT plan(3);
+-- flags
+-- error
 
-PREPARE edges AS
-SELECT id, source, target, cost, reverse_cost  FROM edge_table;
+SELECT throws_ok(
+    'SELECT * FROM pgr_topologicalsort(
+        ''SELECT id, source, target, cost, reverse_cost FROM edge_table id < 2'',
+        3
+    )','42883','function pgr_topologicalsort(unknown, integer) does not exist',
+    '6: Documentation says it does not work with 1 flags');
 
-SELECT isnt_empty('edges', 'Should be not empty to tests be meaningful');
+
+SELECT lives_ok(
+    'SELECT * FROM pgr_topologicalsort(
+        ''SELECT id, source, target, cost, reverse_cost FROM edge_table WHERE id = 2 ''
+    )',
+    '4: Documentation says works with no flags');
 
 
 CREATE OR REPLACE FUNCTION test_function()
@@ -16,11 +27,12 @@ params TEXT[];
 subs TEXT[];
 BEGIN
     params = ARRAY[
-    '$$SELECT id, source, target, cost, reverse_cost  FROM edge_table WHERE id = 2 $$'
+    '$$SELECT id, source, target, cost, reverse_cost  FROM edge_table WHERE id = 1 $$'
     ]::TEXT[];
     subs = ARRAY[
     'NULL',
     'NULL::BIGINT[]'
+
     ]::TEXT[];
 
     RETURN query SELECT * FROM no_crash_test('pgr_transitiveclosure', params, subs);
