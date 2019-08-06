44808b5803c31a54a38075081cf998b8741f0ce6
nike0good <wuhang@wuhangdeMacBook-Pro.local>
2019-08-06 22:21:25 +0800

    [transitiveClosure] Update no_crash_test-transitiveClosure.sql.

--- pgtap/transitiveClosure/no_crash_test-transitiveClosure.sql
+++ pgtap/transitiveClosure/no_crash_test-transitiveClosure.sql
@@ -1,56 +1,29 @@
 \i setup.sql
 
-SELECT plan(5);
+SELECT plan(7);
 
-CREATE TABLE edge_table1 (
-    id serial,
-    source integer,
-    target integer,
-    cost double precision,
-    reverse_cost double precision
-);
-INSERT INTO edge_table1 (source,target,cost,reverse_cost) VALUES ( 1, 2,0,0);
-INSERT INTO edge_table1 (source,target,cost,reverse_cost) VALUES (2,3,0,0);
+PREPARE edges AS
+SELECT id, source, target, cost, reverse_cost  FROM edge_table;
 
-SELECT has_function('pgr_transitiveClosure');
-
-SELECT function_returns('pgr_transitiveClosure', ARRAY['text'], 'setof record');
-
--- flags
--- error
-SELECT throws_ok(
-    'SELECT * FROM pgr_transitiveClosure(
-        ''SELECT id, source, target, cost, reverse_cost FROM edge_table id < 2'',
-        3
-    )','42883','function pgr_transitiveClosure(unknown, integer) does not exist',
-    '6: Documentation says it does not work with 1 flags');
+SELECT isnt_empty('edges', 'Should be not empty to tests be meaningful');
 
 
-SELECT lives_ok(
-    'SELECT * FROM pgr_transitiveClosure(
-        ''SELECT id, source, target, cost, reverse_cost FROM edge_table WHERE id = 2 ''
-    )',
-    '4: Documentation says works with no flags');
+CREATE OR REPLACE FUNCTION test_function()
+RETURNS SETOF TEXT AS
+$BODY$
+DECLARE
+params TEXT[];
+BEGIN
+    params = ARRAY[
+    '$$SELECT id, source, target, cost, reverse_cost  FROM edge_table WHERE id = 2 $$',
+    ]::TEXT[];
+    
+    RETURN query SELECT * FROM no_crash_test('pgr_transitiveClosure', params);
 
+END
+$BODY$
+LANGUAGE plpgsql VOLATILE;
 
--- prepare for testing return types
+SELECT * FROM test_function();
 
-PREPARE all_return AS
-SELECT
-    'integer'::text AS t1,
-    'bigint'::text AS t2;
-
-PREPARE q1 AS
-SELECT pg_typeof(seq)::text AS t1,
-       pg_typeof(sorted_v)::text AS t2
-    FROM (
-        SELECT * FROM pgr_transitiveClosure(
-            'SELECT id, source, target, cost, reverse_cost FROM edge_table WHERE id = 2'
-        ) ) AS a
-    limit 1;
-
-
-SELECT set_eq('q1', 'all_return', 'Expected returning, columns names & types');
-
-SELECT * FROM finish();
 ROLLBACK;
