ca659d7ee5ccadb5ea46ffd4dc1634dc74c726ad
nike0good <wuhang@wuhangdeMacBook-Pro.local>
2019-08-08 03:53:54 +0800

    [transitiveClosure] Fix pgtap bugs.

--- pgtap/transitiveClosure/no_crash_test-transitiveClosure.sql
+++ pgtap/transitiveClosure/no_crash_test-transitiveClosure.sql
@@ -5,15 +5,15 @@ SELECT plan(2);
 -- error
 
 SELECT throws_ok(
-    'SELECT * FROM pgr_topologicalsort(
+    'SELECT * FROM pgr_transitiveclosure(
         ''SELECT id, source, target, cost, reverse_cost FROM edge_table id < 2'',
         3
-    )','42883','function pgr_topologicalsort(unknown, integer) does not exist',
+    )','42883','function pgr_transitiveclosure(unknown, integer) does not exist',
     '6: Documentation says it does not work with 1 flags');
 
 
 SELECT lives_ok(
-    'SELECT * FROM pgr_topologicalsort(
+    'SELECT * FROM pgr_transitiveclosure(
         ''SELECT id, source, target, cost, reverse_cost FROM edge_table WHERE id = 2 ''
     )',
     '4: Documentation says works with no flags');
