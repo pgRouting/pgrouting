7a4ee04214dd912f8e989195f07d2febd8bd5079
cvvergara <vicky@georepublic.de>
2019-11-26 11:06:04 -0600

    [alphaShape] detecting unused files

--- include/boost/bgeometry/algorithms/detail/distance/point_to_geometry.hpp
+++ include/boost/bgeometry/algorithms/detail/distance/point_to_geometry.hpp
@@ -27,36 +27,27 @@
 #include <boost/type_traits/is_same.hpp>
 
 #if BOOST_Geometry_VERSION_OK
-#include <boost/geometry/core/closure.hpp>
+// #include <boost/geometry/core/closure.hpp>
 #include <boost/geometry/core/point_type.hpp>
-#include <boost/geometry/core/exterior_ring.hpp>
-#include <boost/geometry/core/interior_rings.hpp>
-#include <boost/geometry/core/tag.hpp>
-#include <boost/geometry/core/tags.hpp>
+// #include <boost/geometry/core/exterior_ring.hpp>
+// #include <boost/geometry/core/interior_rings.hpp>
+// #include <boost/geometry/core/tag.hpp>
+// #include <boost/geometry/core/tags.hpp>
 #include <boost/geometry/util/math.hpp>
 #include <boost/geometry/strategies/distance.hpp>
-#include <boost/geometry/strategies/tags.hpp>
+// #include <boost/geometry/strategies/tags.hpp>
 #include <boost/geometry/algorithms/assign.hpp>
 #include <boost/geometry/algorithms/covered_by.hpp>
-#include <boost/geometry/algorithms/within.hpp>
-#include <boost/geometry/algorithms/detail/distance/is_comparable.hpp>
-#include <boost/geometry/algorithms/detail/distance/iterator_selector.hpp>
+// #include <boost/geometry/algorithms/within.hpp>
+// #include <boost/geometry/algorithms/detail/distance/is_comparable.hpp>
+// #include <boost/geometry/algorithms/detail/distance/iterator_selector.hpp>
 #include <boost/geometry/algorithms/dispatch/distance.hpp>
 #else
-#include <boost/bgeometry/core/closure.hpp>
 #include <boost/bgeometry/core/point_type.hpp>
-#include <boost/bgeometry/core/exterior_ring.hpp>
-#include <boost/bgeometry/core/interior_rings.hpp>
-#include <boost/bgeometry/core/tag.hpp>
-#include <boost/bgeometry/core/tags.hpp>
 #include <boost/bgeometry/util/math.hpp>
 #include <boost/bgeometry/strategies/distance.hpp>
-#include <boost/bgeometry/strategies/tags.hpp>
 #include <boost/bgeometry/algorithms/assign.hpp>
 #include <boost/bgeometry/algorithms/covered_by.hpp>
-#include <boost/bgeometry/algorithms/within.hpp>
-#include <boost/bgeometry/algorithms/detail/distance/is_comparable.hpp>
-#include <boost/bgeometry/algorithms/detail/distance/iterator_selector.hpp>
 #include <boost/bgeometry/algorithms/dispatch/distance.hpp>
 #endif
 
