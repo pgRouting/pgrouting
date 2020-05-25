588639c27a20da00bece4afa6a6856107f054c8d
Himanshu Raj <raj.himanshu2@gmail.com>
2020-05-25 19:54:18 +0530

    Added pgTap test sql files

--- pgtap/kargersContraction/kargersContraction-innerQuery.sql
+++ pgtap/kargersContraction/kargersContraction-innerQuery.sql
@@ -1,15 +1,8 @@
 \i setup.sql
 
-SELECT plan(432);
+SELECT plan(54);
 
-SELECT style_dijkstra('pgr_kargersContraction', ', 5)');
-SELECT style_dijkstra('pgr_kargersContraction', ', 5, 2)');
-SELECT style_dijkstra('pgr_kargersContraction', ', ARRAY[3,5])');
-SELECT style_dijkstra('pgr_kargersContraction', ', ARRAY[3,5], 2)');
-SELECT style_dijkstra('pgr_kargersContraction', ', 5, directed := true)');
-SELECT style_dijkstra('pgr_kargersContraction', ', 5, 2, directed := true)');
-SELECT style_dijkstra('pgr_kargersContraction', ', ARRAY[3,5], directed := true)');
-SELECT style_dijkstra('pgr_kargersContraction', ', ARRAY[3,5],2, directed := true)');
+SELECT style_dijkstra('pgr_primDFS', ', 5)');
 
 SELECT finish();
 ROLLBACK;
