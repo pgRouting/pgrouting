bf6cc02e03d4991269779c6716a6fc55e1333588
Himanshu Raj <raj.himanshu2@gmail.com>
2020-05-25 23:42:37 +0530

    Trying to fix travis error

--- src/kargersContraction/kargersContraction_driver.cpp
+++ src/kargersContraction/kargersContraction_driver.cpp
@@ -1,14 +1,13 @@
 /*PGR-GNU*****************************************************************
-File: kargersContraction_driver.cpp
+File: prim_driver.cpp
 
 Generated with Template by:
 Copyright (c) 2015 pgRouting developers
 Mail: project@pgrouting.org
 
 Function's developer:
-Copyright (c) 2020 Himanshu Raj
-Mail:
-
+Copyright (c) 2018 Aditya Pratap Singh
+Mail: adityapratap.singh28@gmail.com
 ------
 
 This program is free software; you can redistribute it and/or modify
@@ -29,25 +28,112 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 #include "drivers/kargersContraction/kargersContraction_driver.h"
 
-#include <string.h>
 #include <sstream>
 #include <deque>
 #include <vector>
-#include <algorithm>
-//Add header files here for kargersContraction
+#include <string>
 
-// #include "kargersContraction/ch_graphs.hpp"
-// #include "kargersContraction/pgr_kargersContraction.hpp"
-
-
-
-#include "cpp_common/identifiers.hpp"
 #include "cpp_common/pgr_alloc.hpp"
+#include "cpp_common/pgr_assert.h"
 
-namespace {
+#include "spanningTree/pgr_prim.hpp"
+#include "spanningTree/details.hpp"
 
-/*! @brief vertices with at least one contracted vertex
 
-  @result The vids Identifiers with at least one contracted vertex
-*/
+void
+do_pgr_kargersContraction(
+        pgr_edge_t  *data_edges,
+        size_t total_edges,
+
+        int64_t *rootsArr,
+        size_t size_rootsArr,
+
+        char* fn_suffix,
+
+        int64_t max_depth,
+        double distance,
+
+        pgr_mst_rt **return_tuples,
+        size_t *return_count,
+
+        char ** log_msg,
+        char ** notice_msg,
+        char ** err_msg) {
+    std::ostringstream log;
+    std::ostringstream err;
+    std::ostringstream notice;
+    try {
+        pgassert(!(*log_msg));
+        pgassert(!(*notice_msg));
+        pgassert(!(*err_msg));
+        pgassert(!(*return_tuples));
+        pgassert(*return_count == 0);
+
+        std::vector<int64_t> roots(rootsArr, rootsArr + size_rootsArr);
+        std::string suffix(fn_suffix);
+
+        std::vector<pgr_mst_rt> results;
+
+        if (total_edges == 0) {
+            results = pgrouting::details::get_no_edge_graph_result(roots);
+        } else {
+            pgrouting::UndirectedGraph undigraph(UNDIRECTED);
+            undigraph.insert_min_edges_no_parallel(data_edges, total_edges);
+            pgrouting::functions::Pgr_prim<pgrouting::UndirectedGraph> prim;
+            if (suffix == "") {
+                results = prim.prim(undigraph);
+            } else if (suffix == "BFS") {
+                results = prim.primBFS(undigraph, roots, max_depth);
+            } else if (suffix == "DFS") {
+                results = prim.primDFS(undigraph, roots, max_depth);
+            } else if (suffix == "DD") {
+                results = prim.primDD(undigraph, roots, distance);
+            } else {
+                err << "Unknown Prim function";
+                *err_msg = pgr_msg(err.str().c_str());
+                return;
+            }
+        }
+
+        auto count = results.size();
+
+        if (count == 0) {
+            (*return_tuples) = NULL;
+            (*return_count) = 0;
+            notice << "No spanning tree found";
+            return;
+        }
+
+        (*return_tuples) = pgr_alloc(count, (*return_tuples));
+        for (size_t i = 0; i < count; i++) {
+            *((*return_tuples) + i) = results[i];
+        }
+        (*return_count) = count;
+
+        pgassert(*err_msg == NULL);
+        *log_msg = log.str().empty()?
+            *log_msg :
+            pgr_msg(log.str().c_str());
+        *notice_msg = notice.str().empty()?
+            *notice_msg :
+            pgr_msg(notice.str().c_str());
+    } catch (AssertFailedException &except) {
+        (*return_tuples) = pgr_free(*return_tuples);
+        (*return_count) = 0;
+        err << except.what();
+        *err_msg = pgr_msg(err.str().c_str());
+        *log_msg = pgr_msg(log.str().c_str());
+    } catch (std::exception &except) {
+        (*return_tuples) = pgr_free(*return_tuples);
+        (*return_count) = 0;
+        err << except.what();
+        *err_msg = pgr_msg(err.str().c_str());
+        *log_msg = pgr_msg(log.str().c_str());
+    } catch(...) {
+        (*return_tuples) = pgr_free(*return_tuples);
+        (*return_count) = 0;
+        err << "Caught unknown exception!";
+        *err_msg = pgr_msg(err.str().c_str());
+        *log_msg = pgr_msg(log.str().c_str());
+    }
 }
