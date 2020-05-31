b40a41e253337faf54146e6a52766c1e79f6d9f4
mahmsakr <mahmoud.sakr@ulb.ac.be>
2020-06-01 00:16:30 +0200

    adding pgtap tests for pgr_dijkstra combinations sql

---
+++ pgtap/dijkstra/dijkstra_empty_combinations_empty_result.test.sql
@@ -0,0 +1,26 @@
+
+\i setup.sql
+
+SELECT plan(1);
+
+create or REPLACE FUNCTION foo()
+RETURNS SETOF TEXT AS
+$BODY$
+BEGIN
+
+    RETURN query SELECT is_empty(
+      'SELECT path_seq,  start_vid,  end_vid, node, edge, cost, agg_cost FROM pgr_dijkstra(
+        ''SELECT id, source, target, cost, reverse_cost FROM edge_table'',
+        ''SELECT * FROM combinations_table WHERE source=-1'' ) '
+	);
+    RETURN;
+END
+$BODY$
+language plpgsql;
+
+select * from foo();
+
+-- Finish the tests and clean up.
+SELECT * FROM finish();
+ROLLBACK;
+
