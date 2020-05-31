78b9c184c9f3aadabc20cea9b47527afcd907549
prakashupes <85prakash2017@gmail.com>
2020-05-31 13:37:42 +0530

    [LTDTree]c/c++ basic templates

---
+++ src/LTDTree/LTDTree_driver.cpp
@@ -0,0 +1,46 @@
+/*PGR-GNU*****************************************************************
+File: LTDTree_driver.pp
+
+Generated with Template by:
+Copyright (c) 2015 pgRouting developers
+Mail: project@pgrouting.org
+
+Function's developer:
+Copyright (c) 2020 Prakash Tiwari
+Mail: 85prakash2017@gmail.com
+
+------
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ ********************************************************************PGR-GNU*/
+********************************************************************PGR-GNU*/
+
+#include "drivers/LTDTree/LTDTree_driver.h"
+#include "pgr_LTDTree_driver.hpp"
+#include <sstream>
+#include <deque>
+#include <vector>
+
+#include "dijkstra/pgr_dijkstra.hpp"
+
+#include "cpp_common/pgr_alloc.hpp"
+#include "cpp_common/pgr_assert.h"
+
+/**TODO**
+Look from template
+*/
+
+
