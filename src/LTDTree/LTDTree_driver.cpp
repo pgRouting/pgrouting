66d9668817a50650e7345c555e18364b1894ef13
prakashupes <85prakash2017@gmail.com>
2020-06-02 05:56:16 +0530

    [LTDTRee] result tuple struct

--- src/LTDTree/LTDTree_driver.cpp
+++ src/LTDTree/LTDTree_driver.cpp
@@ -34,4 +34,92 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 Look from template
 */
 
+#include "drivers/LTDTree/LTDTree_driver.h"
+#include "LTDTree/pgr_LTDTree_driver.hpp"
+#include "c_types/pgr_ltdtree_rt .h"
+
+
+#include <string.h>
+#include <sstream>
+#include <deque>
+#include <vector>
+#include <algorithm>
+
+
+
+#include "cpp_common/identifiers.hpp"
+#include "cpp_common/pgr_alloc.hpp"
+#include "cpp_common/basePath_SSEC.hpp"
+#include "cpp_common/pgr_base_graph.hpp"
+
+
+
+
+
+
+
+/************************************************************
+  edges_sql TEXT
+ ***********************************************************/
+void
+do_pgr_LTDTree(
+        pgr_edge_t  *data_edges,
+        size_t total_edges,
+        pgr_ltdtree_rt **return_tuples,
+        size_t *return_count,
+        char **log_msg,
+        char **notice_msg,
+        char **err_msg) {
+    std::ostringstream log;
+    std::ostringstream notice;
+    std::ostringstream err;
+    std::cout<<"Chedcking.................... "<<data_edges->cost<<" \n";
+    /*
+    try {
+        pgassert(total_edges != 0);
+        pgassert(!(*log_msg));
+        pgassert(!(*notice_msg));
+        pgassert(!(*err_msg));
+        pgassert(!(*return_tuples));
+        pgassert(*return_count == 0);
+
+
+        std::vector<pgr_edge_t> edges(data_edges, data_edges + total_edges);
+
+
+        graphType gType = DIRECTED;
+        pgrouting::DirectedGraph digraph(gType);
+        digraph.insert_edges(data_edges, total_edges);
+
+
+
+        *log_msg = log.str().empty()?
+                   *log_msg :
+                   pgr_msg(log.str().c_str());
+        *notice_msg = notice.str().empty()?
+                      *notice_msg :
+                      pgr_msg(notice.str().c_str());
+    } catch (AssertFailedException &except) {
+        (*return_tuples) = pgr_free(*return_tuples);
+        (*return_count) = 0;
+        err << except.what();
+        *err_msg = pgr_msg(err.str().c_str());
+        *log_msg = pgr_msg(log.str().c_str());
+    } catch (std::exception &except) {
+        (*return_tuples) = pgr_free(*return_tuples);
+        (*return_count) = 0;
+        err << except.what();
+        *err_msg = pgr_msg(err.str().c_str());
+        *log_msg = pgr_msg(log.str().c_str());
+    } catch(...) {
+        (*return_tuples) = pgr_free(*return_tuples);
+        (*return_count) = 0;
+        err << "Caught unknown exception!";
+        *err_msg = pgr_msg(err.str().c_str());
+        *log_msg = pgr_msg(log.str().c_str());
+    }
+    */
+
+}
+
 
