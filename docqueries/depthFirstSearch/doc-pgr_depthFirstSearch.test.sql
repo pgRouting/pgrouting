4bc81e2912c49ec1c1c94808b8f82dbebaa3fd8e
Ashish Kumar <ashishkr23438@gmail.com>
2020-06-03 17:49:36 +0530

    [depthFirstSearch] [doc] Edited query number

--- docqueries/depthFirstSearch/doc-pgr_depthFirstSearch.test.sql
+++ docqueries/depthFirstSearch/doc-pgr_depthFirstSearch.test.sql
@@ -13,29 +13,25 @@ SELECT * FROM pgr_depthFirstSearch(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table ORDER BY id',
     6
 );
-\echo --q4
 SELECT * FROM pgr_depthFirstSearch(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table ORDER BY id',
     ARRAY[8,15]
 );
-\echo --q5
 SELECT * FROM pgr_depthFirstSearch(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table ORDER BY id',
     ARRAY[8,15], max_depth := 2
 );
-\echo --q6
+\echo --q4
 SELECT * FROM pgr_depthFirstSearch(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table ORDER BY id',
     6, directed := false
 );
-\echo --q7
 SELECT * FROM pgr_depthFirstSearch(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table ORDER BY id',
     ARRAY[8,15], directed := false
 );
-\echo --q8
 SELECT * FROM pgr_depthFirstSearch(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table ORDER BY id',
     ARRAY[8,15], max_depth := 2, directed := false
 );
-\echo --q9
+\echo --q5
