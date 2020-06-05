49219408caea2c5066464c7151d18e900c860e25
Ashish Kumar <ashishkr23438@gmail.com>
2020-06-05 14:08:47 +0530

    [depthFirstSearch] Added all the queries for sample_table

--- docqueries/depthFirstSearch/depthFirstSearch-issue1348-usage.test.sql
+++ docqueries/depthFirstSearch/depthFirstSearch-issue1348-usage.test.sql
@@ -14,3 +14,64 @@ INSERT INTO sample_table (source, target, cost, reverse_cost) VALUES
     (3, 8, 10, -10),
     (6, 8, -1, 12);
 \echo -- SAMPLE TABLE ADD DATA end
+
+-- SELECT query on SAMPLE TABLE
+\echo -- q0
+SELECT * FROM sample_table ORDER BY id;
+
+-- Directed Graph with start_vid 3
+\echo -- q1
+SELECT * FROM pgr_depthFirstSearch (
+    'SELECT id, source, target, cost, reverse_cost FROM sample_table ORDER BY id',
+    3
+);
+
+-- Directed Graph with start_vid 6
+\echo -- q2
+SELECT * FROM pgr_depthFirstSearch (
+    'SELECT id, source, target, cost, reverse_cost FROM sample_table ORDER BY id',
+    6
+);
+
+-- Directed Graph with start_vid 6 and max_depth 1
+\echo -- q3
+SELECT * FROM pgr_depthFirstSearch (
+    'SELECT id, source, target, cost, reverse_cost FROM sample_table ORDER BY id',
+    6, max_depth := 1
+);
+
+-- start_vid does not exist in the Graph
+\echo -- q4
+SELECT * FROM pgr_depthFirstSearch (
+    'SELECT id, source, target, cost, reverse_cost FROM sample_table ORDER BY id',
+    2
+);
+
+-- Undirected Graph with start_vid 3
+\echo -- q5
+SELECT * FROM pgr_depthFirstSearch (
+    'SELECT id, source, target, cost, reverse_cost FROM sample_table ORDER BY id',
+    3, directed := false
+);
+
+-- Undirected Graph with start_vid 6
+\echo -- q6
+SELECT * FROM pgr_depthFirstSearch (
+    'SELECT id, source, target, cost, reverse_cost FROM sample_table ORDER BY id',
+    6, directed := false
+);
+
+-- Undirected Graph with start_vid 6 and max_depth 1
+\echo -- q7
+SELECT * FROM pgr_depthFirstSearch (
+    'SELECT id, source, target, cost, reverse_cost FROM sample_table ORDER BY id',
+    6, max_depth := 1, directed := false
+);
+
+-- Directed Graph with multiple start_vids
+\echo -- q8
+SELECT * FROM pgr_depthFirstSearch (
+    'SELECT id, source, target, cost, reverse_cost FROM sample_table ORDER BY id',
+    ARRAY[6, 3, 6]
+);
+\echo -- q9
