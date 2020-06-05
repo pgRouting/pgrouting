f9760a90eb8a9d4dc9265b4e5c33423d1ef450b9
prakashupes <85prakash2017@gmail.com>
2020-06-05 21:05:06 +0530

    [LTDTree] tests (only files)

--- pgtap/LTDTree/LTDTree-innerQuery.sql
+++ pgtap/LTDTree/LTDTree-innerQuery.sql
@@ -1,3 +1,12 @@
 \i setup.sql
 
---Need to write basic tests
+SELECT plan(1);
+
+SELECT todo_start('Complete the inner query tests');
+
+SELECT pass('Sample Test');
+
+SELECT todo_end();
+
+SELECT finish();
+ROLLBACK;
