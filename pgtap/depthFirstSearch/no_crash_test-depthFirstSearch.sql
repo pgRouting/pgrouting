4727cae3c3e2f830769d796d6e1c9c5f90e29db5
Ashish Kumar <ashishkr23438@gmail.com>
2020-06-08 17:45:56 +0530

    [depthFirstSearch] Added pgTAP test for multiple vertices with depth parameter

--- pgtap/depthFirstSearch/no_crash_test-depthFirstSearch.sql
+++ pgtap/depthFirstSearch/no_crash_test-depthFirstSearch.sql
@@ -76,6 +76,26 @@ BEGIN
 
     subs[2] := 'NULL::BIGINT[]';
     RETURN query SELECT * FROM no_crash_test('pgr_depthFirstSearch', params, subs);
+
+    -- depthFirstSearch Multiple vertices with depth
+    params = ARRAY[
+    '$$SELECT id, source, target, cost, reverse_cost  FROM edge_table$$',
+    'ARRAY[5,3]',
+    '3'
+    ]::TEXT[];
+    subs = ARRAY[
+    'NULL',
+    '(SELECT array_agg(id) FROM edge_table_vertices_pgr  WHERE id IN (-1))',
+    'NULL::BIGINT'
+    ]::TEXT[];
+
+    RETURN query SELECT * FROM no_crash_test('pgr_depthFirstSearch', params, subs);
+
+    params[1] := '$$edges$$';
+    RETURN query SELECT * FROM no_crash_test('pgr_depthFirstSearch', params, subs);
+
+    subs[2] := 'NULL::BIGINT[]';
+    RETURN query SELECT * FROM no_crash_test('pgr_depthFirstSearch', params, subs);
     PERFORM todo_end();
 END
 $BODY$
