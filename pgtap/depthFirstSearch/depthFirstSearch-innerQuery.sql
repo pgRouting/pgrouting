c5d3c381165066e607bd4a0f16fd95543a8c3f21
Ashish Kumar <ashishkr23438@gmail.com>
2020-06-10 13:02:58 +0530

    [depthFirstSearch] Added inner query pgTAP tests

--- pgtap/depthFirstSearch/depthFirstSearch-innerQuery.sql
+++ pgtap/depthFirstSearch/depthFirstSearch-innerQuery.sql
@@ -1,10 +1,26 @@
 \i setup.sql
 
-SELECT plan(1);
+SELECT plan(432);
 
 SELECT todo_start('Complete the inner query tests');
 
-SELECT pass('Sample Test');
+-- SINGLE VERTEX
+SELECT style_dijkstra('pgr_depthFirstSearch', ', 5)');
+-- Single vertex with depth
+SELECT style_dijkstra('pgr_depthFirstSearch', ', 5, 2)');
+-- Single vertex with directed parameter
+SELECT style_dijkstra('pgr_depthFirstSearch', ', 5, directed := true)');
+-- Single vertex with depth and directed parameter
+SELECT style_dijkstra('pgr_depthFirstSearch', ', 5, 2, directed := true)');
+
+-- MULTIPLE VERTICES
+SELECT style_dijkstra('pgr_depthFirstSearch', ', ARRAY[3,5])');
+-- Multiple vertices with depth
+SELECT style_dijkstra('pgr_depthFirstSearch', ', ARRAY[3,5], 2)');
+-- Multiple vertices with directed parameter
+SELECT style_dijkstra('pgr_depthFirstSearch', ', ARRAY[3,5], directed := true)');
+-- Multiple vertices with depth and directed parameter
+SELECT style_dijkstra('pgr_depthFirstSearch', ', ARRAY[3,5], 2, directed := true)');
 
 SELECT todo_end();
 
