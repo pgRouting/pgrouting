4e696a20d294a30bbd2dc31ccbb021c23d878cbf
Himanshu Raj <raj.himanshu2@gmail.com>
2020-06-10 12:21:58 +0530

    [planarGraph]SQL query working now

--- src/planarGraph/boyerMyrvold_driver.cpp
+++ src/planarGraph/boyerMyrvold_driver.cpp
@@ -27,7 +27,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 ********************************************************************PGR-GNU*/
 
-// #include "drivers/breadthFirstSearch/breadthFirstSearch_driver.h"
 #include "drivers/planarGraph/boyerMyrvold_driver.h"
 
 #include <vector>
