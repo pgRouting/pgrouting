c625cbaa1cb8b06dc47527bc43d40a25a014256c
Ashish Kumar <ashishkr23438@gmail.com>
2020-06-11 17:08:38 +0530

    [depthFirstSearch] Added pgTAP unit tests - vertex not present in directed graph

--- pgtap/depthFirstSearch/depthFirstSearch-edge-cases.sql
+++ pgtap/depthFirstSearch/depthFirstSearch-edge-cases.sql
@@ -1,6 +1,6 @@
 \i setup.sql
 
-SELECT plan(12);
+SELECT plan(18);
 
 SELECT todo_start('Must add all edge cases');
 
@@ -129,6 +129,63 @@ SELECT is_empty('depthFirstSearch9', '9: Graph with 0 edge and 0 vertex -> Empty
 SELECT is_empty('depthFirstSearch10', '10: Graph with 0 edge and 0 vertex -> Empty row is returned');
 SELECT is_empty('depthFirstSearch11', '11: Graph with 0 edge and 0 vertex -> Empty row is returned');
 SELECT is_empty('depthFirstSearch12', '12: Graph with 0 edge and 0 vertex -> Empty row is returned');
+
+-- vertex not present in graph (directed)
+
+PREPARE depthFirstSearch13 AS
+SELECT *
+FROM pgr_depthFirstSearch(
+    'SELECT id, source, target, cost, reverse_cost
+    FROM edge_table',
+    -10
+);
+
+PREPARE depthFirstSearch14 AS
+SELECT *
+FROM pgr_depthFirstSearch(
+    'SELECT id, source, target, cost, reverse_cost
+    FROM edge_table',
+    ARRAY[-10]
+);
+
+PREPARE depthFirstSearch15 AS
+SELECT *
+FROM pgr_depthFirstSearch(
+    'SELECT id, source, target, cost, reverse_cost
+    FROM edge_table',
+    ARRAY[20, -10]
+);
+
+PREPARE depthFirstSearch16 AS
+SELECT *
+FROM pgr_depthFirstSearch(
+    'SELECT id, source, target, cost, reverse_cost
+    FROM edge_table',
+    -10, max_depth => 2
+);
+
+PREPARE depthFirstSearch17 AS
+SELECT *
+FROM pgr_depthFirstSearch(
+    'SELECT id, source, target, cost, reverse_cost
+    FROM edge_table',
+    ARRAY[-10], max_depth => 2
+);
+
+PREPARE depthFirstSearch18 AS
+SELECT *
+FROM pgr_depthFirstSearch(
+    'SELECT id, source, target, cost, reverse_cost
+    FROM edge_table',
+    ARRAY[20, -10], max_depth => 2
+);
+
+SELECT is_empty('depthFirstSearch13', '13: Vertex not present in graph -> Empty row is returned');
+SELECT is_empty('depthFirstSearch14', '14: Vertex not present in graph -> Empty row is returned');
+SELECT is_empty('depthFirstSearch15', '15: Vertex not present in graph -> Empty row is returned');
+SELECT is_empty('depthFirstSearch16', '16: Vertex not present in graph -> Empty row is returned');
+SELECT is_empty('depthFirstSearch17', '17: Vertex not present in graph -> Empty row is returned');
+SELECT is_empty('depthFirstSearch18', '18: Vertex not present in graph -> Empty row is returned');
 
 
 SELECT todo_end();
