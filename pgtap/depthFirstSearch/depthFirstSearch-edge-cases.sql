8437c49204e08a83ba237dcdf371cadf3eda63cf
Ashish Kumar <ashishkr23438@gmail.com>
2020-06-11 17:02:14 +0530

    [depthFirstSearch] Added pgTAP unit tests for 0 edge, 0 vertex undirected graph

--- pgtap/depthFirstSearch/depthFirstSearch-edge-cases.sql
+++ pgtap/depthFirstSearch/depthFirstSearch-edge-cases.sql
@@ -1,6 +1,6 @@
 \i setup.sql
 
-SELECT plan(6);
+SELECT plan(12);
 
 SELECT todo_start('Must add all edge cases');
 
@@ -66,6 +66,70 @@ SELECT is_empty('depthFirstSearch3', '3: Graph with 0 edge and 0 vertex -> Empty
 SELECT is_empty('depthFirstSearch4', '4: Graph with 0 edge and 0 vertex -> Empty row is returned');
 SELECT is_empty('depthFirstSearch5', '5: Graph with 0 edge and 0 vertex -> Empty row is returned');
 SELECT is_empty('depthFirstSearch6', '6: Graph with 0 edge and 0 vertex -> Empty row is returned');
+
+-- 0 edge, 0 vertex (undirected)
+
+PREPARE depthFirstSearch7 AS
+SELECT *
+FROM pgr_depthFirstSearch(
+    'SELECT id, source, target, cost, reverse_cost
+    FROM edge_table
+    WHERE id > 18',
+    5, directed => false
+);
+
+PREPARE depthFirstSearch8 AS
+SELECT *
+FROM pgr_depthFirstSearch(
+    'SELECT id, source, target, cost, reverse_cost
+    FROM edge_table
+    WHERE id > 18',
+    ARRAY[5], directed => false
+);
+
+PREPARE depthFirstSearch9 AS
+SELECT *
+FROM pgr_depthFirstSearch(
+    'SELECT id, source, target, cost, reverse_cost
+    FROM edge_table
+    WHERE id > 18',
+    ARRAY[2, 5], directed => false
+);
+
+PREPARE depthFirstSearch10 AS
+SELECT *
+FROM pgr_depthFirstSearch(
+    'SELECT id, source, target, cost, reverse_cost
+    FROM edge_table
+    WHERE id > 18',
+    5, directed => false, max_depth => 2
+);
+
+PREPARE depthFirstSearch11 AS
+SELECT *
+FROM pgr_depthFirstSearch(
+    'SELECT id, source, target, cost, reverse_cost
+    FROM edge_table
+    WHERE id > 18',
+    ARRAY[5], directed => false, max_depth => 2
+);
+
+PREPARE depthFirstSearch12 AS
+SELECT *
+FROM pgr_depthFirstSearch(
+    'SELECT id, source, target, cost, reverse_cost
+    FROM edge_table
+    WHERE id > 18',
+    ARRAY[2, 5], directed => false, max_depth => 2
+);
+
+SELECT is_empty('depthFirstSearch7', '7: Graph with 0 edge and 0 vertex -> Empty row is returned');
+SELECT is_empty('depthFirstSearch8', '8: Graph with 0 edge and 0 vertex -> Empty row is returned');
+SELECT is_empty('depthFirstSearch9', '9: Graph with 0 edge and 0 vertex -> Empty row is returned');
+SELECT is_empty('depthFirstSearch10', '10: Graph with 0 edge and 0 vertex -> Empty row is returned');
+SELECT is_empty('depthFirstSearch11', '11: Graph with 0 edge and 0 vertex -> Empty row is returned');
+SELECT is_empty('depthFirstSearch12', '12: Graph with 0 edge and 0 vertex -> Empty row is returned');
+
 
 SELECT todo_end();
 
