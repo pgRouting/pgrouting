f14eb112bb5450a0d16b5ad8acb5161a431df009
Himanshu Raj <raj.himanshu2@gmail.com>
2020-06-18 00:12:26 +0530

    [planarGraph] plan updated in pgTap tests

--- pgtap/planarGraph/boyerMyrvold-innerQuery.sql
+++ pgtap/planarGraph/boyerMyrvold-innerQuery.sql
@@ -1,14 +1,13 @@
 \i setup.sql
 
-SELECT plan(2);
+SELECT plan(56);
 
 SET client_min_messages TO ERROR;
 
 SELECT has_function('pgr_boyerMyrvold');
-SELECT style_dijkstra('pgr_boyerMyrvold', ')');
 
 SELECT function_returns('pgr_boyerMyrvold',ARRAY['text'],'setof record');
-
+SELECT style_dijkstra('pgr_boyerMyrvold', ')');
 
 SELECT finish();
 ROLLBACK;
