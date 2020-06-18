037d5470c7d9482426bf49e5c33415594fee5990
Ashish Kumar <ashishkr23438@gmail.com>
2020-06-18 09:18:18 +0530

    [sequentialVertexColoring] Added sstream header

--- src/graphColoring/sequentialVertexColoring_driver.cpp
+++ src/graphColoring/sequentialVertexColoring_driver.cpp
@@ -28,6 +28,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 #include "drivers/graphColoring/sequentialVertexColoring_driver.h"
 
+#include <sstream>
 #include <vector>
 #include <algorithm>
 #include <string>
