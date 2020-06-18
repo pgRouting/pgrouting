76072656f1562a5e6efff6994f65baeac3c5204d
Himanshu Raj <raj.himanshu2@gmail.com>
2020-06-18 09:54:22 +0530

    [planarGraph] Fixed a bug

--- pgtap/planarGraph/boyerMyrvold-innerQuery.sql
+++ pgtap/planarGraph/boyerMyrvold-innerQuery.sql
@@ -4,10 +4,10 @@ SELECT plan(56);
 
 SET client_min_messages TO ERROR;
 
-SELECT has_function('pgr_boyerMyrvold');
+SELECT has_function('pgr_boyermyrvold');
 
-SELECT function_returns('pgr_boyerMyrvold',ARRAY['text'],'setof record');
-SELECT style_dijkstra('pgr_boyerMyrvold', ')');
+SELECT function_returns('pgr_boyermyrvold',ARRAY['text'],'setof record');
+SELECT style_dijkstra('pgr_boyermyrvold', ')');
 
 SELECT finish();
 ROLLBACK;
