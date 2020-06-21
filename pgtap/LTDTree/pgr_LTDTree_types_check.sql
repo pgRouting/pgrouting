32387f3d71288a102c870fcf71d4dfa84953c642
prakashupes <85prakash2017@gmail.com>
2020-06-22 00:07:34 +0530

    [LTDTree][pgTap] LTDTree types check

--- pgtap/LTDTree/pgr_LTDTree_types_check.sql
+++ pgtap/LTDTree/pgr_LTDTree_types_check.sql
@@ -1,12 +1,27 @@
 \i setup.sql
+SELECT plan(7);
 
-SELECT plan(1);
 
-SELECT todo_start('Complete this tests');
+SELECT has_function('pgr_dltdtree');
 
-SELECT pass('Sample Test');
+SELECT has_function('pgr_dltdtree', ARRAY['text','bigint']);
+SELECT function_returns('pgr_dltdtree', ARRAY['text','bigint','bigint'],  'setof record');
 
-SELECT todo_end();
+-- pgr_dltdtree
+-- parameter names
+SELECT bag_has(
+    $$SELECT  proargnames from pg_proc where proname = 'pgr_dltdtree'$$,
+    $$SELECT  '{"","","max_depth","directed","seq","depth","start_vid","node","edge","cost","agg_cost"}'::TEXT[] $$
+);
 
-SELECT finish();
+-- parameter types
+SELECT set_eq(
+    $$SELECT  proallargtypes from pg_proc where proname = 'pgr_dltdtree'$$,
+    $$VALUES
+        ('{25,20,20,16,20,20,20,20,20,701,701}'::OID[]),
+        ('{25,2277,20,16,20,20,20,20,20,701,701}'::OID[])
+    $$
+);
+
+SELECT * FROM finish();
 ROLLBACK;
