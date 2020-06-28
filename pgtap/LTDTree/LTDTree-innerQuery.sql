4f6a98c900e6bacbfb8031654ced378fdf547158
prakashupes <85prakash2017@gmail.com>
2020-06-28 13:46:18 +0530

    [LTDTree] negative root exception

--- pgtap/LTDTree/LTDTree-innerQuery.sql
+++ pgtap/LTDTree/LTDTree-innerQuery.sql
@@ -3,13 +3,15 @@
 SELECT plan(56);
 
 
-SELECT has_function('pgr_LTDTree',
+SELECT has_function('pgr_ltdtree',
     ARRAY['text', 'bigint']);
 
-SELECT function_returns('pgr_LTDTree',
-    ARRAY['bigint', 'bigint'],
+SELECT function_returns('pgr_ltdtree',
+    ARRAY['text', 'bigint'],
     'setof record');
 
+-- ONE TO ONE
+SELECT style_dijkstra('pgr_ltdtree', ',1)');
 
 SELECT finish();
 ROLLBACK;
