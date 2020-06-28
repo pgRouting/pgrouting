028e8128a7f3348e333f768bfb673bb9197800b7
prakashupes <85prakash2017@gmail.com>
2020-06-28 19:59:58 +0530

    [LTDTree] edge case tests

--- pgtap/LTDTree/ltdtree-edge-cases.sql
+++ pgtap/LTDTree/ltdtree-edge-cases.sql
@@ -1,6 +1,7 @@
 \i setup.sql
-SELECT plan(2);
+SELECT plan(8);
 
+-- 0 edge 0 vertex tests
 PREPARE q1 AS
 SELECT id, source, target, cost, reverse_cost
 FROM edge_table
@@ -8,6 +9,65 @@ WHERE id > 18;
 
 SELECT is_empty('q1', 'q1: Graph with 0 edge and 0 vertex');
 
+
+PREPARE ltdtree_test1 AS
+SELECT *
+FROM pgr_ltdtree(
+'SELECT id, source, target, cost, reverse_cost
+FROM edge_table
+WHERE id < 0',1
+);
+
+PREPARE ltdtree_test2 AS
+SELECT *
+FROM pgr_ltdtree(
+'SELECT id, source, target, cost, reverse_cost
+FROM edge_table
+WHERE id = 0',1
+);
+
+PREPARE ltdtree_test3 AS
+SELECT *
+FROM pgr_ltdtree(
+'SELECT id, source, target, cost, reverse_cost
+FROM edge_table
+WHERE id > 18',1
+);
+
+SELECT is_empty('ltdtree_test1', 'ltdtree_test1: Graph with 0 edge and 0 vertex');
+SELECT is_empty('ltdtree_test2', 'ltdtree_test2: Graph with 0 edge and 0 vertex');
+SELECT is_empty('ltdtree_test3', 'ltdtree_test3: Graph with 0 edge and 0 vertex');
+
+
+
+--root not present tests
+PREPARE ltdtree_test4 AS
+SELECT *
+FROM pgr_ltdtree(
+'SELECT id, source, target, cost, reverse_cost
+FROM edge_table
+WHERE id > 2',1
+);
+
+
+PREPARE ltdtree_test5 AS
+SELECT *
+FROM pgr_ltdtree(
+'SELECT id, source, target, cost, reverse_cost
+FROM edge_table
+WHERE id > 2',1
+);
+
+SELECT is_empty('ltdtree_test4', 'ltdtree_test4: Root not present in the Graph');
+SELECT is_empty('ltdtree_test5', 'ltdtree_test5: Root not present in the Graph');
+
+--vertex not present in the graph tests
+
+
+--verify results test
+
+
+--id constrained tests
 PREPARE q2 AS
 SELECT *
 FROM pgr_ltdtree(
@@ -16,4 +76,14 @@ FROM edge_table
 WHERE id > 0',1
 );
 SELECT isnt_empty('q2', 'q1: Graph with 0 edge and 0 vertex');
+
+-- Negative root tests
+PREPARE q3 AS
+SELECT *
+FROM pgr_ltdtree(
+'SELECT id, source, target, cost, reverse_cost
+FROM edge_table',-1
+);
+SELECT throws_ok('q3', 'P0001', 'Negative value found on ''root_vertex''', '3: Negative root throws');
+
 ROLLBACK;
