1576e22cd0936cb8813c39d34a204ccf92753386
Himanshu Raj <raj.himanshu2@gmail.com>
2020-07-02 17:33:54 +0530

    [makeConnected] Removed unnecessary headers and comments

--- src/makeConnected/makeConnected_driver.cpp
+++ src/makeConnected/makeConnected_driver.cpp
@@ -35,9 +35,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #include <algorithm>
 #include <limits>
 
-// #include "topologicalSort/pgr_topologicalSort.hpp"
-
-
 #include "cpp_common/pgr_alloc.hpp"
 #include "cpp_common/pgr_assert.h"
 #include "cpp_common/pgr_base_graph.hpp"
