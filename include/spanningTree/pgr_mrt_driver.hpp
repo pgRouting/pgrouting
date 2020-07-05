02ad1b968ff710f6ff5e2c2f7c419e0617061adf
prakashupes <85prakash2017@gmail.com>
2020-07-06 01:07:06 +0530

    [mrt] cpp header driver

---
+++ include/spanningTree/pgr_mrt_driver.hpp
@@ -0,0 +1,47 @@
+/*PGR-GNU*****************************************************************
+File: pgr_LTDTree.hpp
+
+Generated with Template by:
+Copyright (c) 2015 pgRouting developers
+Mail: project@pgrouting.org
+
+Function's developer:
+Copyright (c) 2020 Prakash Tiwari
+Mail: 85prakash2017@gmail.com
+
+------
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ ********************************************************************PGR-GNU*/
+
+
+#ifndef INCLUDE_PGR_MRT_DRIVER_HPP
+#define INCLUDE_PGR_MRT_DRIVER_HPP
+#pragma once
+#include <vector>
+
+#include "cpp_common/pgr_base_graph.hpp"
+#include "cpp_common/pgr_messages.h"
+#include <iostream>
+#include <algorithm>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/dominator_tree.hpp>
+#include <boost/property_map/property_map.hpp>
+#include <boost/property_map/vector_property_map.hpp>
+#include <boost/type_traits.hpp>
+
+
+#endif  // INCLUDE_PGR_MRT_DRIVER_HPP
