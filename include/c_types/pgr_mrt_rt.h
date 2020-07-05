a3cf7eff56087a57e01001807b3548116020a2df
prakashupes <85prakash2017@gmail.com>
2020-07-06 00:51:22 +0530

    [mrt] structure of result tuple

---
+++ include/c_types/pgr_mrt_rt.h
@@ -0,0 +1,44 @@
+/*PGR-GNU*****************************************************************
+File: pgr_ltdtree_rt.h
+
+Copyright (c) 2020 Prakash Tiwari
+Mail: 85prakash2017@gmail.com
+------
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ ********************************************************************PGR-GNU*/
+/*! @file */
+
+#ifndef INCLUDE_C_TYPES_PGR_MRT_RT_H_
+#define INCLUDE_C_TYPES_PGR_MRT_RT_H_
+#pragma once
+
+/* for int64_t */
+#ifdef __cplusplus
+#   include <cstdint>
+#else
+#   include <stdint.h>
+#endif
+
+typedef struct {
+    //int64_t seq;
+    int64_t size;
+    int64_t *tree_edges;
+    //int64_t sdom;
+
+} pgr_mrt_rt;
+
+#endif  // INCLUDE_C_TYPES_PGR_MST_RT_H_
