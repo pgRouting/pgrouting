bc31c7ca5ba28ef92e596cf064f2cc8aa6850d7c
Himanshu Raj <raj.himanshu2@gmail.com>
2020-07-06 10:29:28 +0530

    [makeConnected] Added the required headers for implementation

--- include/makeConnected/pgr_makeConnected.hpp
+++ include/makeConnected/pgr_makeConnected.hpp
@@ -34,6 +34,8 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 #include "cpp_common/pgr_base_graph.hpp"
 #include "c_types/pgr_boyer_t.h"
+#include <boost/graph/connected_components.hpp>
+#include <boost/graph/make_connected.hpp>
 //******************************************
 using namespace boost;
 namespace pgrouting {
