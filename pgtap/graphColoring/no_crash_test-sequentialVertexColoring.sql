f65266e186944eb5540ab17ef98fedb703f29d04
Ashish Kumar <ashishkr23438@gmail.com>
2020-07-07 10:29:18 +0530

    [sequentialVertexColoring] [pgtap] Adding no crash test

--- pgtap/graphColoring/no_crash_test-sequentialVertexColoring.sql
+++ pgtap/graphColoring/no_crash_test-sequentialVertexColoring.sql
@@ -1,10 +1,21 @@
 \i setup.sql
 
-SELECT plan(1);
+SELECT plan(3);
 
 SELECT todo_start('Complete the no crash tests');
 
-SELECT pass( 'A sample test' );
+PREPARE edges AS
+SELECT id, source, target, cost, reverse_cost  FROM edge_table;
+
+PREPARE null_ret AS
+SELECT id FROM edge_table_vertices_pgr  WHERE id IN (-1);
+
+PREPARE null_ret_arr AS
+SELECT array_agg(id) FROM edge_table_vertices_pgr  WHERE id IN (-1);
+
+SELECT isnt_empty('edges', 'Should be not empty to tests be meaningful');
+SELECT is_empty('null_ret', 'Should be empty to tests be meaningful');
+SELECT set_eq('null_ret_arr', 'SELECT NULL::BIGINT[]', 'Should be empty to tests be meaningful');
 
 SELECT todo_end();
 
