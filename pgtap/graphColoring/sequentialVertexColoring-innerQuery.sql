2c1297b28490722dd2d57e289541e97d627b9f8a
Ashish Kumar <ashishkr23438@gmail.com>
2020-07-07 11:20:18 +0530

    [sequentialVertexColoring] [pgtap] Removed todo

--- pgtap/graphColoring/sequentialVertexColoring-innerQuery.sql
+++ pgtap/graphColoring/sequentialVertexColoring-innerQuery.sql
@@ -2,11 +2,7 @@
 
 SELECT plan(54);
 
-SELECT todo_start('Complete the inner query tests');
-
 SELECT style_dijkstra('pgr_sequentialVertexColoring', ')');
-
-SELECT todo_end();
 
 SELECT * FROM finish();
 ROLLBACK;
