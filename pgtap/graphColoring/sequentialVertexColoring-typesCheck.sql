f54374cff026f19fb4395a3deb352922e7445ce4
Ashish Kumar <ashishkr23438@gmail.com>
2020-07-07 11:18:33 +0530

    [sequentialVertexColoring] [pgtap] Adding parameter names check

--- pgtap/graphColoring/sequentialVertexColoring-typesCheck.sql
+++ pgtap/graphColoring/sequentialVertexColoring-typesCheck.sql
@@ -1,6 +1,6 @@
 \i setup.sql
 
-SELECT plan(3);
+SELECT plan(4);
 
 SELECT todo_start('Complete the types check');
 
@@ -8,6 +8,14 @@ SELECT has_function('pgr_sequentialvertexcoloring');
 
 SELECT has_function('pgr_sequentialvertexcoloring', ARRAY['text']);
 SELECT function_returns('pgr_sequentialvertexcoloring', ARRAY['text'],  'setof record');
+
+-- pgr_sequentialvertexcoloring
+-- parameter names
+SELECT bag_has(
+    $$SELECT  proargnames from pg_proc where proname = 'pgr_sequentialvertexcoloring'$$,
+    $$SELECT  '{"","node","color"}'::TEXT[] $$
+);
+
 
 SELECT todo_end();
 
