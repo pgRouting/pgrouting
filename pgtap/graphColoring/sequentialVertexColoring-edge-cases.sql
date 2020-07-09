7a4c2befddfd3f1849b6183acd5f6937aa52248d
Ashish Kumar <ashishkr23438@gmail.com>
2020-07-09 21:16:57 +0530

    [sequentialVertexColoring] [pgtap] Added 0 edge, 0 vertex test

--- pgtap/graphColoring/sequentialVertexColoring-edge-cases.sql
+++ pgtap/graphColoring/sequentialVertexColoring-edge-cases.sql
@@ -1,12 +1,27 @@
 \i setup.sql
 
-SELECT plan(1);
+SELECT plan(2);
 
-SELECT todo_start('Complete the edge cases (unit tests)');
+-- 0 edge, 0 vertex tests
 
-SELECT pass( 'A sample test' );
+PREPARE q1 AS
+SELECT id, source, target, cost, reverse_cost
+FROM edge_table
+WHERE id > 18;
 
-SELECT todo_end();
+-- Graph is empty - it has 0 edge and 0 vertex
+SELECT is_empty('q1', 'q1: Graph with 0 edge and 0 vertex');
+
+-- 0 edge, 0 vertex tests (directed)
+
+PREPARE sequentialVertexColoring2 AS
+SELECT *
+FROM pgr_sequentialVertexColoring(
+    'q1'
+);
+
+SELECT is_empty('sequentialVertexColoring2', '2: Graph with 0 edge and 0 vertex -> Empty row is returned');
+
 
 SELECT * FROM finish();
 ROLLBACK;
