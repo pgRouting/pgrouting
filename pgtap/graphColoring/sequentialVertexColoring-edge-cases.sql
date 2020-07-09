d464d8b57dc427a10ea5a57af7664004d013f432
Ashish Kumar <ashishkr23438@gmail.com>
2020-07-09 21:20:34 +0530

    [sequentialVertexColoring] [pgtap] Added 1 vertex test

--- pgtap/graphColoring/sequentialVertexColoring-edge-cases.sql
+++ pgtap/graphColoring/sequentialVertexColoring-edge-cases.sql
@@ -1,6 +1,6 @@
 \i setup.sql
 
-SELECT plan(2);
+SELECT plan(4);
 
 -- 0 edge, 0 vertex tests
 
@@ -9,10 +9,7 @@ SELECT id, source, target, cost, reverse_cost
 FROM edge_table
 WHERE id > 18;
 
--- Graph is empty - it has 0 edge and 0 vertex
 SELECT is_empty('q1', 'q1: Graph with 0 edge and 0 vertex');
-
--- 0 edge, 0 vertex tests (directed)
 
 PREPARE sequentialVertexColoring2 AS
 SELECT *
@@ -21,6 +18,24 @@ FROM pgr_sequentialVertexColoring(
 );
 
 SELECT is_empty('sequentialVertexColoring2', '2: Graph with 0 edge and 0 vertex -> Empty row is returned');
+
+
+-- 1 vertex tests
+
+PREPARE q3 AS
+SELECT id, source, 2 AS target, cost, reverse_cost
+FROM edge_table
+WHERE id = 2;
+
+SELECT set_eq('q3', $$VALUES (2, 2, 2, -1, 1)$$, 'q3: Graph with only vertex 2');
+
+PREPARE sequentialVertexColoring4 AS
+SELECT *
+FROM pgr_sequentialVertexColoring(
+    'q3'
+);
+
+SELECT set_eq('sequentialVertexColoring4', $$VALUES (2, 0)$$, '4: Node 2 with color 0 is returned');
 
 
 SELECT * FROM finish();
