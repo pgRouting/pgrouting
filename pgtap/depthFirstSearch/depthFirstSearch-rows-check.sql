7f02f7f8dd66ae0239bcb056c6d9cf1d05f657cc
Ashish Kumar <ashishkr23438@gmail.com>
2020-07-10 19:02:00 +0530

    [depthFirstSearch] [pgtap] Test to check whether same set of rows are returned

--- pgtap/depthFirstSearch/depthFirstSearch-rows-check.sql
+++ pgtap/depthFirstSearch/depthFirstSearch-rows-check.sql
@@ -1,6 +1,6 @@
 \i setup.sql
 
-SELECT plan(5);
+SELECT plan(1);
 
 
 -- Check whether the same set of rows are returned always
@@ -15,33 +15,22 @@ SELECT id, source, target, cost, reverse_cost
 FROM edge_table
 ORDER BY id DESC;
 
-SELECT SETSEED(1);
 
-PREPARE q3 AS
-SELECT id, source, target, cost, reverse_cost
-FROM edge_table
-ORDER BY RANDOM();
+PREPARE depthFirstSearch1 AS
+SELECT *
+FROM pgr_depthFirstSearch(
+    'q1',
+    1
+);
 
-PREPARE q4 AS
-SELECT id, source, target, cost, reverse_cost
-FROM edge_table
-ORDER BY RANDOM();
+PREPARE depthFirstSearch2 AS
+SELECT *
+FROM pgr_depthFirstSearch(
+    'q2',
+    1
+);
 
-PREPARE q5 AS
-SELECT id, source, target, cost, reverse_cost
-FROM edge_table
-ORDER BY RANDOM();
-
-PREPARE q6 AS
-SELECT id, source, target, cost, reverse_cost
-FROM edge_table
-ORDER BY RANDOM();
-
-SELECT set_eq('q1', 'q2', '1: Should return same set of rows');
-SELECT set_eq('q1', 'q3', '2: Should return same set of rows');
-SELECT set_eq('q1', 'q4', '3: Should return same set of rows');
-SELECT set_eq('q1', 'q5', '4: Should return same set of rows');
-SELECT set_eq('q1', 'q6', '5: Should return same set of rows');
+SELECT set_eq('depthFirstSearch1', 'depthFirstSearch2', '1: Should return same set of rows');
 
 
 SELECT * FROM finish();
