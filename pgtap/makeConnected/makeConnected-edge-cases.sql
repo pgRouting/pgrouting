3c06379e012399117c4c4fc202b75598c76a8232
Himanshu Raj <raj.himanshu2@gmail.com>
2020-07-14 23:11:40 +0530

    [makeConnected] Added pgTap tests makeConnected

--- pgtap/makeConnected/makeConnected-edge-cases.sql
+++ pgtap/makeConnected/makeConnected-edge-cases.sql
@@ -24,5 +24,28 @@ FROM pgr_makeConnected(
 
 SELECT is_empty('makeConnected2', '2: Graph with 0 edge and 0 vertex -> Empty row is returned');
 
+-- vertex not present in graph tests
+
+PREPARE q3 AS
+SELECT id, source, target, cost, reverse_cost
+FROM edge_table
+WHERE id = -10;
+
+SELECT is_empty('q3', 'q3: Vertex Not present in the graph');
+
+PREPARE makeConnected4 AS
+SELECT *
+FROM pgr_makeConnected(
+    'q4'
+);
+
+SELECT is_empty('makeConnected4', '4: Vertex Not present in the graph -> Empty row is returned');
+
+
+
+
+
+
+
 SELECT * FROM finish();
 ROLLBACK;
