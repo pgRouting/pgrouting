030be287967a506cadd1fda10cd01f903467046d
Himanshu Raj <raj.himanshu2@gmail.com>
2020-07-17 23:14:28 +0530

    [planarGraph][docqueries] Added Non-planar Graphs Doc

--- docqueries/planarGraph/doc-pgr_boyerMyrvold.test.sql
+++ docqueries/planarGraph/doc-pgr_boyerMyrvold.test.sql
@@ -1,18 +1,21 @@
 \echo -- q1
 SELECT * FROM pgr_boyerMyrvold(
     'SELECT id, source, target, cost, reverse_cost
-        FROM edge_table
-     WHERE id > 18'
+        FROM edge_table'
 );
 
 \echo -- q2
-SELECT * FROM pgr_boyerMyrvold(
-    'SELECT id, source, target, cost, reverse_cost
-        FROM edge_table
-     WHERE id > 18'
-);
+INSERT INTO edge_table (source, target, cost, reverse_cost) VALUES
+(1,3,1,-1),(1,4,1,-1),(1,5,1,-1),(2,4,1,-1),(2,5,1,-1),(3,5,1,-1),(4,5,1,-1);
 
 \echo -- q3
+
+SELECT * FROM pgr_boyerMyrvold(
+    'SELECT id, source, target, cost, reverse_cost
+        FROM edge_table'
+);
+
+\echo -- q4
 SELECT * FROM pgr_boyerMyrvold(
 $$
  SELECT id, source, target, cost, reverse_cost FROM edge_table
@@ -27,5 +30,3 @@ $$
                        )
 $$
  );
-
-\echo -- q4
