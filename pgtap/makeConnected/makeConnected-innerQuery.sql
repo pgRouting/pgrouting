25563c2c2bb7ea590aba412f624fcfe99f04951c
Himanshu Raj <raj.himanshu2@gmail.com>
2020-07-18 12:19:49 +0530

    [makeConnected][pgTap] Added No Crash Tests

--- pgtap/makeConnected/makeConnected-innerQuery.sql
+++ pgtap/makeConnected/makeConnected-innerQuery.sql
@@ -4,10 +4,10 @@ SELECT plan(56);
 
 SET client_min_messages TO ERROR;
 
-SELECT has_function('pgr_makeConnected');
+SELECT has_function('pgr_makeconnected');
 
-SELECT function_returns('pgr_makeConnected',ARRAY['text'],'setof record');
-SELECT style_dijkstra('pgr_makeConnected', ')');
+SELECT function_returns('pgr_makeconnected',ARRAY['text'],'setof record');
+SELECT style_dijkstra('pgr_makeconnected', ')');
 
 SELECT finish();
 ROLLBACK;
