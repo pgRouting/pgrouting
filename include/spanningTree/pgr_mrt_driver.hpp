2cdb536b62c513a7e977ab3c82880d69e4b139ab
prakashupes <85prakash2017@gmail.com>
2020-07-19 23:03:15 +0530

    [MRT] fixing boost error

--- include/spanningTree/pgr_mrt_driver.hpp
+++ include/spanningTree/pgr_mrt_driver.hpp
@@ -1,5 +1,5 @@
 /*PGR-GNU*****************************************************************
-File: pgr_LTDTree.hpp
+File: pgr_mrt_driver.hpp
 
 Generated with Template by:
 Copyright (c) 2015 pgRouting developers
@@ -41,11 +41,14 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #include "c_types/pgr_mrt_rt.h"
 #include <boost/graph/two_graphs_common_spanning_trees.hpp>
 
+#include <boost/graph/depth_first_search.hpp>
+
 using namespace boost;
 using namespace  std;
 
 namespace pgrouting {
     namespace functions {
+        
 
         template<class Graph>
         class Pgr_mrt : public pgrouting::Pgr_messages {
@@ -53,6 +56,7 @@ namespace pgrouting {
             std::vector<pgr_mrt_rt> results;
             typedef typename Graph::E E;
             typedef typename Graph::E_i E_i;
+            typedef typename Graph::V V;
 
             std::vector<pgr_mrt_rt> pgr_mrt(
                 Graph &graph_1,
@@ -69,12 +73,14 @@ namespace pgrouting {
                     log<<"In directed graph total edges G1 "<<graph_1.num_edges()<<endl;
                     log<<"In directed graph total edges G2 "<<graph_2.num_edges()<<endl;
                     log<<"Need coding"<<endl;
+                 
                 }
                 else
-                {
+                {/*
+                
                     log<<"Working with undirected graph"<<endl;
 
-                    vector<bool> inL(graph_1.num_edges(), false);
+                    std::vector<bool> inL;
 
                     std::vector< std::vector<bool> > coll;
                     boost::tree_collector<
@@ -83,11 +89,13 @@ namespace pgrouting {
                     > tree_collector(coll);
                     boost::two_graphs_common_spanning_trees
                             (
-                                    graph_1,
-                                    graph_2,
+                                    graph_1.graph,
+                                    graph_2.graph,
                                     tree_collector,
                                     inL
                             );
+                   */         
+                           
                 }
                 return results;
 
