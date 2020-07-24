a00a89d1f484a53b56df4fe22df79379f3ba40b5
prakashupes <85prakash2017@gmail.com>
2020-07-24 23:30:12 +0530

    [bipartite]signature error solved

--- sql/bipartite/bipartite.sql
+++ sql/bipartite/bipartite.sql
@@ -31,7 +31,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 ---------------
 
 CREATE OR REPLACE FUNCTION pgr_bipartite(
-    TEXT,    -- edges_sql (required)
+    TEXT, -- edges_sql (required)
 
     OUT node BIGINT,
     OUT color BIGINT)
