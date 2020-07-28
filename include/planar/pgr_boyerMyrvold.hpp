7fffaadc288ca11bfd6a5ce5812ddcf6c76fefc8
Himanshu Raj <raj.himanshu2@gmail.com>
2020-07-28 22:36:47 +0530

    Returning the value of function

--- include/planar/pgr_boyerMyrvold.hpp
+++ include/planar/pgr_boyerMyrvold.hpp
@@ -87,22 +87,7 @@ class Pgr_boyerMyrvold : public pgrouting::Pgr_messages {
      bool
      generateisPlanar(
        const G &graph ) {
-      return false;
-      std::vector< pgr_boyer_t > results;
-      auto check = boyer_myrvold_planarity_test(graph.graph);
-      if(check){
-        E_i ei, ei_end;
-        int i;
-      for (boost::tie(ei, ei_end) = edges(graph.graph),i = 0; ei != ei_end; ++ei,++i){
-          int64_t src = graph[graph.source(*ei)].id;
-          int64_t tgt = graph[graph.target(*ei)].id;
-          double cost = graph[*ei].cost;
-          pgr_boyer_t tmp;
-          tmp.source = src;
-          tmp.target = tgt;
-          tmp.cost = cost;
-          results.push_back(tmp);
-     }
+      return (boyer_myrvold_planarity_test(graph.graph));
 
     }
       return results;
