530cc831fe26e22cb56d4f92675c26af0099d823
Ashish Kumar <ashishkr23438@gmail.com>
2020-07-28 19:52:04 +0530

    [depthFirstSearch] Added depth at the end of all functions

--- include/visitors/dfs_visitor.hpp
+++ include/visitors/dfs_visitor.hpp
@@ -58,17 +58,20 @@ class Dfs_visitor : public boost::default_dfs_visitor {
      template <typename B_G>
          void start_vertex(V v, const B_G&) {
              std::cout << "start vertex " << v << "\t\t" << "color " << m_colors[v] << "\n";
+             std::cout << "depth " << depth << "\n";
          }
      template <typename B_G>
          void discover_vertex(V v, const B_G&) {
              std::cout << "\n";
              std::cout << "discover vertex " << v << "\t" << "color " << m_colors[v] << "\n";
+             std::cout << "depth " << depth << "\n";
          }
      template <typename B_G>
          void examine_edge(E e, const B_G&) {
              int64_t source = boost::source(e, m_graph), target = boost::target(e, m_graph);
              std::cout << "examine edge " << e << "\t" << "vertex " << source << " color " << m_colors[source]
                        << " vertex " << target << " color " << m_colors[target] << "\n";
+             std::cout << "depth " << depth << "\n";
          }
      template <typename B_G>
          void tree_edge(E e, const B_G&) {
@@ -85,12 +88,14 @@ class Dfs_visitor : public boost::default_dfs_visitor {
              int64_t source = boost::source(e, m_graph), target = boost::target(e, m_graph);
              std::cout << "back edge " << e << "\t" << "vertex " << source << " color " << m_colors[source]
                        << " vertex " << target << " color " << m_colors[target] << "\n";
+             std::cout << "depth " << depth << "\n";
          }
      template <typename B_G>
          void forward_or_cross_edge(E e, const B_G&) {
              int64_t source = boost::source(e, m_graph), target = boost::target(e, m_graph);
              std::cout << "forward or cross edge " << e << "\t" << "vertex " << source << " color " << m_colors[source]
                        << " vertex " << target << " color " << m_colors[target] << "\n";
+             std::cout << "depth " << depth << "\n";
          }
 #endif
      template <typename B_G>
@@ -100,12 +105,13 @@ class Dfs_visitor : public boost::default_dfs_visitor {
                        << " vertex " << target << " color " << m_colors[target] << "\n";
              if (edge_set.find(e) != edge_set.end()) {
                  depth--;
-                 std::cout << "depth " << depth << "\n";
              }
+             std::cout << "depth " << depth << "\n";
          }
      template <typename B_G>
          void finish_vertex(V v, const B_G&) {
              std::cout << "finish vertex " << v << "\t\t" << "color " << m_colors[v] << "\n";
+             std::cout << "depth " << depth << "\n";
          }
 
  private:
