ad1eae52a85a8d74d750d516e9ca50d39873434e
prakashupes <85prakash2017@gmail.com>
2020-07-28 21:37:57 +0530

    [ltdtree]tests with new signature

--- pgtap/LTDTree/ltdtree-edge-cases.sql
+++ pgtap/LTDTree/ltdtree-edge-cases.sql
@@ -10,57 +10,57 @@ WHERE id > 18;
 SELECT is_empty('q1', 'q1: Graph with 0 edge and 0 vertex');
 
 
-PREPARE lengauer_tarjan_dominator_tree_test1 AS
+PREPARE LengauerTarjanDominatorTree_test1 AS
 SELECT *
-FROM pgr_lengauer_tarjan_dominator_tree(
+FROM pgr_lengauerTarjanDominatorTree(
 'SELECT id, source, target, cost, reverse_cost
 FROM edge_table
 WHERE id < 0',1
 );
 
-PREPARE lengauer_tarjan_dominator_tree_test2 AS
+PREPARE LengauerTarjanDominatorTree_test2 AS
 SELECT *
-FROM pgr_lengauer_tarjan_dominator_tree(
+FROM pgr_lengauerTarjanDominatorTree(
 'SELECT id, source, target, cost, reverse_cost
 FROM edge_table
 WHERE id = 0',1
 );
 
-PREPARE lengauer_tarjan_dominator_tree_test3 AS
+PREPARE LengauerTarjanDominatorTree_test3 AS
 SELECT *
-FROM pgr_lengauer_tarjan_dominator_tree(
+FROM pgr_lengauerTarjanDominatorTree(
 'SELECT id, source, target, cost, reverse_cost
 FROM edge_table
 WHERE id > 18',1
 );
 
-SELECT is_empty('lengauer_tarjan_dominator_tree_test1', 'lengauer_tarjan_dominator_tree_test1: Graph with 0 edge and 0 vertex');
-SELECT is_empty('lengauer_tarjan_dominator_tree_test2', 'lengauer_tarjan_dominator_tree_test2: Graph with 0 edge and 0 vertex');
-SELECT is_empty('lengauer_tarjan_dominator_tree_test3', 'lengauer_tarjan_dominator_tree_test3: Graph with 0 edge and 0 vertex');
+SELECT is_empty('LengauerTarjanDominatorTree_test1', 'LengauerTarjanDominatorTree_test1: Graph with 0 edge and 0 vertex');
+SELECT is_empty('LengauerTarjanDominatorTree_test2', 'LengauerTarjanDominatorTree_test2: Graph with 0 edge and 0 vertex');
+SELECT is_empty('LengauerTarjanDominatorTree_test3', 'LengauerTarjanDominatorTree_test3: Graph with 0 edge and 0 vertex');
 
 
 
 --root not present tests
 /*
-PREPARE lengauer_tarjan_dominator_tree_test4 AS
+PREPARE LengauerTarjanDominatorTree_test4 AS
 SELECT *
-FROM pgr_lengauer_tarjan_dominator_tree(
+FROM pgr_LengauerTarjanDominatorTree_tree(
 'SELECT id, source, target, cost, reverse_cost
 FROM edge_table
 WHERE id > 2',1
 );
 
 
-PREPARE lengauer_tarjan_dominator_tree_test5 AS
+PREPARE LengauerTarjanDominatorTree_test5 AS
 SELECT *
-FROM pgr_lengauer_tarjan_dominator_tree(
+FROM pgr_LengauerTarjanDominatorTree(
 'SELECT id, source, target, cost, reverse_cost
 FROM edge_table
 WHERE id > 2',1
 );
 
-SELECT is_empty('lengauer_tarjan_dominator_tree_test4', 'lengauer_tarjan_dominator_tree_test4: Root not present in the Graph');
-SELECT is_empty('lengauer_tarjan_dominator_tree_test5', 'lengauer_tarjan_dominator_tree_test5: Root not present in the Graph');
+SELECT is_empty('LengauerTarjanDominatorTree_test4', 'LengauerTarjanDominatorTree_test4: Root not present in the Graph');
+SELECT is_empty('LengauerTarjanDominatorTree_test5', 'LengauerTarjanDominatorTree_test5: Root not present in the Graph');
 */
 --vertex not present in the graph tests
 
@@ -71,7 +71,7 @@ SELECT is_empty('lengauer_tarjan_dominator_tree_test5', 'lengauer_tarjan_dominat
 --id constrained tests
 PREPARE q2 AS
 SELECT *
-FROM pgr_lengauer_tarjan_dominator_tree(
+FROM pgr_lengauerTarjanDominatorTree(
 'SELECT id, source, target, cost, reverse_cost
 FROM edge_table
 WHERE id > 0',1
@@ -81,7 +81,7 @@ SELECT isnt_empty('q2', 'q2: Graph with 0 edge and 0 vertex');
 -- Negative root tests
 PREPARE q3 AS
 SELECT *
-FROM pgr_lengauer_tarjan_dominator_tree(
+FROM pgr_lengauerTarjanDominatorTree(
 'SELECT id, source, target, cost, reverse_cost
 FROM edge_table',-1
 );
