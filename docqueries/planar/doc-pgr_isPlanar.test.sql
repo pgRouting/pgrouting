ec2efc6a0a66bcfd7aebc33d7ab09a5c751929da
Himanshu Raj <raj.himanshu2@gmail.com>
2020-07-29 18:13:39 +0530

    [isPlanar][docqueries] Modified docqueries for non-planar graphs

--- docqueries/planar/doc-pgr_isPlanar.test.sql
+++ docqueries/planar/doc-pgr_isPlanar.test.sql
@@ -6,4 +6,10 @@ SELECT * FROM pgr_isPlanar(
 \echo -- q2
 
 INSERT INTO edge_table (source, target, cost, reverse_cost) VALUES
-(1,3,1,-1),(1,4,1,-1),(1,5,1,-1),(2,4,1,-1),(2,5,1,-1),(3,5,1,-1),(4,5,1,-1);
+(1, 3, 1, -1), (1, 4, 1, -1), (1, 5, 1, -1), (2, 4, 1, -1), (2, 5, 1, -1), (3, 5, 1, -1), (4, 5, 1, -1);
+
+\echo -- q3
+SELECT * FROM pgr_isPlanar(
+    'SELECT id, source, target, cost, reverse_cost
+        FROM edge_table'
+);
