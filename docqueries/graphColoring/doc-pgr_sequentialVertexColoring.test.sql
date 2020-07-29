1c40a7579c7588bc7fe1ff46f34425bdc965a7dd
Ashish Kumar <ashishkr23438@gmail.com>
2020-07-29 16:32:33 +0530

    [sequentialVertexColoring] Removed additional examples from docqueries

--- docqueries/graphColoring/doc-pgr_sequentialVertexColoring.test.sql
+++ docqueries/graphColoring/doc-pgr_sequentialVertexColoring.test.sql
@@ -4,34 +4,3 @@ SELECT * FROM pgr_sequentialVertexColoring(
     ORDER BY id'
 );
 \echo -- q2
-
--- Examples for :ref:`fig2-undirect-Cost-Reverse`
--------------------------------------------------------------------------------
-
-\echo -- q3
-SELECT * FROM pgr_sequentialVertexColoring(
-    'SELECT id, source, target, cost, reverse_cost FROM edge_table
-    WHERE id = 1
-    ORDER BY id'
-);
-SELECT * FROM pgr_sequentialVertexColoring(
-    'SELECT id, source, target, cost, reverse_cost FROM edge_table
-    WHERE id < 5
-    ORDER BY id'
-);
-SELECT * FROM pgr_sequentialVertexColoring(
-    'SELECT id, source, target, cost, reverse_cost FROM edge_table
-    WHERE id >= 4 AND id <= 6
-    ORDER BY id'
-);
-SELECT * FROM pgr_sequentialVertexColoring(
-    'SELECT id, source, target, cost, reverse_cost FROM edge_table
-    WHERE id IN (8, 10, 11, 12)
-    ORDER BY id'
-);
-SELECT * FROM pgr_sequentialVertexColoring(
-    'SELECT id, source, target, cost, reverse_cost FROM edge_table
-    WHERE id IN (5, 8, 9, 11)
-    ORDER BY id'
-);
-\echo -- q4
