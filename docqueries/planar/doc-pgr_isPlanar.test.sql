2c63a347a08bb7adac36d681dc280dc103c14fc9
Himanshu Raj <raj.himanshu2@gmail.com>
2020-07-29 17:58:08 +0530

    [isPlanar][doc] Additional One Example

--- docqueries/planar/doc-pgr_isPlanar.test.sql
+++ docqueries/planar/doc-pgr_isPlanar.test.sql
@@ -3,4 +3,7 @@ SELECT * FROM pgr_isPlanar(
     'SELECT id, source, target, cost, reverse_cost
         FROM edge_table'
 );
- \echo -- q5
+\echo -- q2
+
+INSERT INTO edge_table (source, target, cost, reverse_cost) VALUES
+(1,3,1,-1),(1,4,1,-1),(1,5,1,-1),(2,4,1,-1),(2,5,1,-1),(3,5,1,-1),(4,5,1,-1);
