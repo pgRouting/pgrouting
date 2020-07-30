718965846ae16c0672ea5a4d4e8867b0c1cc5c5b
Himanshu Raj <raj.himanshu2@gmail.com>
2020-07-30 15:50:24 +0530

    [makeConnected][doc] Modified docqueries

--- docqueries/makeConnected/doc-pgr_makeConnected.test.sql
+++ docqueries/makeConnected/doc-pgr_makeConnected.test.sql
@@ -1,24 +1,18 @@
 \echo -- q1
-SELECT * FROM pgr_makeConnected(
+SELECT * FROM pgr_connectedComponents(
     'SELECT id, source, target, cost, reverse_cost
         FROM edge_table'
 );
 
 \echo -- q2
-SELECT * FROM pgr_connectedComponents(
+
+SELECT * FROM pgr_makeConnected(
     'SELECT id, source, target, cost, reverse_cost
         FROM edge_table'
 );
 
 \echo -- q3
 
-SELECT * FROM pgr_makeConnected(
-    'SELECT id, source, target, cost, reverse_cost
-        FROM edge_table'
-);
+INSERT INTO edge_table (source,target,cost,reverse_cost) VALUES (13, 14, 1, -1), (15, 16, 1, -1);
 
 \echo -- q4
-
-INSERT INTO edge_table (source,target,cost,reverse_cost) VALUES (13,14,1,-1), (15,16,1,-1);
-
-\echo -- q5
