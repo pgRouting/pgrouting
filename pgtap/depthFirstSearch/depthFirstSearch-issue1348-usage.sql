d05e12d77fdcf5bf0798258435d867eac9d1f21d
Ashish Kumar <ashishkr23438@gmail.com>
2020-07-31 18:26:53 +0530

    [depthFirstSearch] Modified issue1348 pgtap test file

--- pgtap/depthFirstSearch/depthFirstSearch-issue1348-usage.sql
+++ pgtap/depthFirstSearch/depthFirstSearch-issue1348-usage.sql
@@ -1,4 +1,8 @@
-\echo -- SAMPLE TABLE CREATE start
+\i setup.sql
+
+SELECT plan(8);
+
+
 CREATE TABLE sample_table (
     id BIGSERIAL,
     source BIGINT,
@@ -6,82 +10,141 @@ CREATE TABLE sample_table (
     cost FLOAT,
     reverse_cost FLOAT
 );
-\echo -- SAMPLE TABLE CREATE end
-\echo
 
-\echo -- SAMPLE TABLE ADD DATA start
 INSERT INTO sample_table (source, target, cost, reverse_cost) VALUES
     (3, 6, 20, 15),
     (3, 8, 10, -10),
     (6, 8, -1, 12);
-\echo -- SAMPLE TABLE ADD DATA end
-\echo
 
--- SELECT query on SAMPLE TABLE
-\echo -- q0
-SELECT * FROM sample_table ORDER BY id;
+PREPARE q0 AS
+SELECT id, source, target, cost, reverse_cost FROM sample_table
+ORDER BY id;
 
--- Directed Graph with start_vid 3
-\echo -- q1
+-- Directed Graph with Root vid 3
+PREPARE q1 AS
 SELECT * FROM pgr_depthFirstSearch (
-    'SELECT id, source, target, cost, reverse_cost FROM sample_table
-    ORDER BY id',
+    'q0',
     3
 );
 
--- Directed Graph with start_vid 6
-\echo -- q2
+SELECT set_eq('q1',
+    $$VALUES
+        (1, 0, 3, 3, -1, 0, 0),
+        (2, 1, 3, 6, 1, 20, 20),
+        (3, 1, 3, 8, 2, 10, 10)
+    $$,
+    'q1: Directed Graph with Root vid 3'
+);
+
+-- Directed Graph with Root vid 6
+PREPARE q2 AS
 SELECT * FROM pgr_depthFirstSearch (
-    'SELECT id, source, target, cost, reverse_cost FROM sample_table
-    ORDER BY id',
+    'q0',
     6
 );
 
--- Directed Graph with start_vid 6 and max_depth 1
-\echo -- q3
+SELECT set_eq('q2',
+    $$VALUES
+        (1, 0, 6, 6, -1, 0, 0),
+        (2, 1, 6, 3, 1, 15, 15),
+        (3, 2, 6, 8, 2, 10, 25)
+    $$,
+    'q2: Directed Graph with Root vid 6'
+);
+
+-- Directed Graph with Root vid 6 and max_depth 1
+PREPARE q3 AS
 SELECT * FROM pgr_depthFirstSearch (
-    'SELECT id, source, target, cost, reverse_cost FROM sample_table
-    ORDER BY id',
+    'q0',
     6, max_depth => 1
 );
 
--- start_vid does not exist in the Graph
-\echo -- q4
+SELECT set_eq('q3',
+    $$VALUES
+        (1, 0, 6, 6, -1, 0, 0),
+        (2, 1, 6, 3, 1, 15, 15)
+    $$,
+    'q3: Directed Graph with Root vid 6 and max_depth 1'
+);
+
+-- Root vid does not exist in the Graph
+PREPARE q4 AS
 SELECT * FROM pgr_depthFirstSearch (
-    'SELECT id, source, target, cost, reverse_cost FROM sample_table
-    ORDER BY id',
+    'q0',
     2
 );
 
--- Undirected Graph with start_vid 3
-\echo -- q5
+SELECT is_empty('q4',
+    'q4: Vertex not present in graph -> Empty row is returned'
+);
+
+-- Undirected Graph with Root vid 3
+PREPARE q5 AS
 SELECT * FROM pgr_depthFirstSearch (
-    'SELECT id, source, target, cost, reverse_cost FROM sample_table
-    ORDER BY id',
+    'q0',
     3, directed => false
 );
 
--- Undirected Graph with start_vid 6
-\echo -- q6
+SELECT set_eq('q5',
+    $$VALUES
+        (1, 0, 3, 3, -1, 0, 0),
+        (2, 1, 3, 6, 1, 20, 20),
+        (3, 2, 3, 8, 3, 12, 32)
+    $$,
+    'q5: Undirected Graph with Root vid 3'
+);
+
+-- Undirected Graph with Root vid 6
+PREPARE q6 AS
 SELECT * FROM pgr_depthFirstSearch (
-    'SELECT id, source, target, cost, reverse_cost FROM sample_table
-    ORDER BY id',
+    'q0',
     6, directed => false
 );
 
--- Undirected Graph with start_vid 6 and max_depth 1
-\echo -- q7
+SELECT set_eq('q6',
+    $$VALUES
+        (1, 0, 6, 6, -1, 0, 0),
+        (2, 1, 6, 3, 1, 20, 20),
+        (3, 2, 6, 8, 2, 10, 30)
+    $$,
+    'q6: Undirected Graph with Root vid 6'
+);
+
+-- Undirected Graph with Root vid 6 and max_depth 1
+PREPARE q7 AS
 SELECT * FROM pgr_depthFirstSearch (
-    'SELECT id, source, target, cost, reverse_cost FROM sample_table
-    ORDER BY id',
+    'q0',
     6, directed => false, max_depth => 1
 );
 
--- Directed Graph with multiple start_vids
-\echo -- q8
+SELECT set_eq('q7',
+    $$VALUES
+        (1, 0, 6, 6, -1, 0, 0),
+        (2, 1, 6, 3, 1, 20, 20),
+        (3, 1, 6, 8, 3, 12, 12)
+    $$,
+    'q7: Undirected Graph with Root vid 6 and max_depth 1'
+);
+
+-- Directed Graph with multiple Root vids
+PREPARE q8 AS
 SELECT * FROM pgr_depthFirstSearch (
-    'SELECT id, source, target, cost, reverse_cost FROM sample_table
-    ORDER BY id',
+    'q0',
     ARRAY[6, 3, 6]
 );
-\echo -- q9
+
+SELECT set_eq('q8',
+    $$VALUES
+        (1, 0, 3, 3, -1, 0, 0),
+        (2, 1, 3, 6, 1, 20, 20),
+        (3, 1, 3, 8, 2, 10, 10),
+        (4, 0, 6, 6, -1, 0, 0),
+        (5, 1, 6, 3, 1, 15, 15),
+        (6, 2, 6, 8, 2, 10, 25)
+    $$,
+    'q8: Directed Graph with multiple Root vids'
+);
+
+
+SELECT * FROM finish();
+ROLLBACK;
