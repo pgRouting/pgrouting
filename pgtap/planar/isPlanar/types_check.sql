12b7d943f90143846e5ead5d9ee74ec323e377b4
Himanshu Raj <raj.himanshu2@gmail.com>
2020-08-01 23:35:27 +0530

    [isPlanar][pgTAP] Modified pgtap tests

--- pgtap/planar/isPlanar/types_check.sql
+++ pgtap/planar/isPlanar/types_check.sql
@@ -1,50 +1,27 @@
 \i setup.sql
 
-SELECT plan(5);
+SELECT plan(2);
 
-SELECT has_function('pgr_boyermyrvold');
+SELECT has_function('pgr_isplanar');
 
-SELECT function_returns('pgr_boyermyrvold', ARRAY['text'], 'setof record');
-
-
--- flags
--- error
-
-SELECT lives_ok(
-    'SELECT * FROM pgr_boyerMyrvold(
-        ''SELECT id, source, target, cost, reverse_cost FROM edge_table''
-    )',
-    '3: Documentation says works with no flags');
-
-SELECT throws_ok(
-    'SELECT * FROM pgr_boyerMyrvold(
-        ''SELECT id, source, target, cost, reverse_cost FROM edge_table id < 17'',
-        3
-    )','42883','function pgr_boyermyrvold(unknown, integer) does not exist',
-    '4: Documentation says it does not work with 1 flags');
+SELECT function_returns('pgr_isplanar', ARRAY['text'], 'boolean');
 
 
--- prepare for testing return types
+-- pgr_isplanar
+-- parameter names
 
-PREPARE all_return AS
-SELECT
-    'bigint'::text AS t1,
-    'bigint'::text AS t2,
-    'bigint'::text AS t3,
-    'double precision'::text AS t4;
-
-PREPARE q5 AS
-SELECT pg_typeof(seq)::text AS t1,
-       pg_typeof(source)::text AS t2,
-       pg_typeof(target)::text AS t3,
-       pg_typeof(cost)::text AS t4
-    FROM (
-        SELECT * FROM pgr_boyerMyrvold(
-            'SELECT id, source, target, cost, reverse_cost FROM edge_table WHERE id < 17'
-        ) ) AS a LIMIT 1;
-
-
-SELECT set_eq('q5', 'all_return', 'Expected returning, columns names & types');
+-- SELECT bag_has(
+--     $$SELECT  proargnames from pg_proc where proname = 'pgr_isplanar'$$,
+--     $$SELECT  ''::TEXT[] $$
+-- );
+--
+-- -- parameter types
+-- SELECT set_eq(
+--     $$SELECT  proallargtypes from pg_proc where proname = 'pgr_isplanar'$$,
+--     $$VALUES
+--         (''::OID[])
+--     $$
+-- );
 
 SELECT * FROM finish();
 ROLLBACK;
