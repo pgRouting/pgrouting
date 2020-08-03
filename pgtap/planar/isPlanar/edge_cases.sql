f43cf0dfa1bd57ac317ee501399ab833ae611ae5
Himanshu Raj <raj.himanshu2@gmail.com>
2020-08-03 17:14:49 +0530

    [isPlanar][pgtap] Added vertex not present

--- pgtap/planar/isPlanar/edge_cases.sql
+++ pgtap/planar/isPlanar/edge_cases.sql
@@ -1,6 +1,6 @@
 \i setup.sql
 
-SELECT plan(2);
+SELECT plan(3);
 
 
 
@@ -18,11 +18,20 @@ SELECT is_empty('q1', 'q1: Graph with 0 edge and 0 vertex');
 
 PREPARE isPlanar1 AS
 SELECT *
-FROM pgr_isPlanar(
-    'q1'
-);
+FROM pgr_isPlanar('q1');
 
 SELECT set_eq('isPlanar1',$$VALUES('f'::bool) $$, '2: Graph with 0 edge and 0 vertex -> Empty row is returned');
+
+
+-- vertex not present in graph test
+
+PREPARE q3 AS
+SELECT id, source, target, cost, reverse_cost
+FROM edge_table WHERE source = 50;
+
+SELECT is_empty('q3','3: Vertex 50 does not exist in sample data');
+
+
 
 
 SELECT * FROM finish();
