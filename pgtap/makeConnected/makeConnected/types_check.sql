71f4e0067b6dc61e0a74bb8ad072f1f1021fbbb9
Himanshu Raj <raj.himanshu2@gmail.com>
2020-08-04 18:31:23 +0530

    [makeConnected][pgtap] New types_check done for makeConnected

--- pgtap/makeConnected/makeConnected/types_check.sql
+++ pgtap/makeConnected/makeConnected/types_check.sql
@@ -1,48 +1,26 @@
 \i setup.sql
 
-SELECT plan(5);
+SELECT plan(4);
 
 SELECT has_function('pgr_makeconnected');
 
 SELECT function_returns('pgr_makeconnected', ARRAY['text'], 'setof record');
 
 
--- flags
--- error
+-- pgr_makeconnected
+-- parameter names
+SELECT bag_has(
+    $$SELECT  proargnames from pg_proc where proname = 'pgr_makeconnected'$$,
+    $$SELECT  '{"",seq,node_from,node_to}'::TEXT[] $$
+);
 
-SELECT lives_ok(
-    'SELECT * FROM pgr_makeConnected(
-        ''SELECT id, source, target, cost, reverse_cost FROM edge_table''
-    )',
-    '3: Documentation says works with no flags');
-
-SELECT throws_ok(
-    'SELECT * FROM pgr_makeConnected(
-        ''SELECT id, source, target, cost, reverse_cost FROM edge_table id < 17'',
-        3
-    )','42883','function pgr_makeconnected(unknown, integer) does not exist',
-    '4: Documentation says it does not work with 1 flags');
-
-
--- prepare for testing return types
-
-PREPARE all_return AS
-SELECT
-    'bigint'::text AS t1,
-    'bigint'::text AS t2,
-    'bigint'::text AS t3;
-
-PREPARE q5 AS
-SELECT pg_typeof(seq)::text AS t1,
-       pg_typeof(node_from)::text AS t2,
-       pg_typeof(node_to)::text AS t3
-    FROM (
-        SELECT * FROM pgr_makeConnected(
-            'SELECT id, source, target, cost, reverse_cost FROM edge_table WHERE id <= 18'
-        ) ) AS a LIMIT 1;
-
-
-SELECT set_eq('q5', 'all_return', 'Expected returning, columns names & types');
+-- parameter types
+SELECT set_eq(
+    $$SELECT  proallargtypes from pg_proc where proname = 'pgr_makeconnected'$$,
+    $$VALUES
+        ('{25,20,20,20}'::OID[])
+    $$
+);
 
 SELECT * FROM finish();
 ROLLBACK;
