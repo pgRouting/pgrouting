b7e1bff069f1b2af58c57345d9d8a1bc3daa7125
Himanshu Raj <raj.himanshu2@gmail.com>
2020-08-06 21:33:04 +0530

    [isPlanar][pgtap] todo added

--- pgtap/planar/isPlanar/types_check.sql
+++ pgtap/planar/isPlanar/types_check.sql
@@ -1,6 +1,6 @@
 \i setup.sql
 
-SELECT plan(2);
+SELECT plan(4);
 
 SELECT has_function('pgr_isplanar');
 
@@ -9,19 +9,23 @@ SELECT function_returns('pgr_isplanar', ARRAY['text'], 'boolean');
 
 -- pgr_isplanar
 -- parameter names
+SELECT todo_start('Fix types_check');
 
--- SELECT bag_has(
---     $$SELECT  proargnames from pg_proc where proname = 'pgr_isplanar'$$,
---     $$SELECT  ''::TEXT[] $$
--- );
---
--- -- parameter types
--- SELECT set_eq(
---     $$SELECT  proallargtypes from pg_proc where proname = 'pgr_isplanar'$$,
---     $$VALUES
---         (''::OID[])
---     $$
--- );
+SELECT bag_has(
+    $$SELECT  proargnames from pg_proc where proname = 'pgr_isplanar'$$,
+    $$SELECT  '{}'::TEXT[] $$
+);
+-- parameter types
+SELECT set_eq(
+    $$SELECT  proallargtypes from pg_proc where proname = 'pgr_isplanar'$$,
+    $$VALUES
+        ('{}'::OID[])
+    $$
+);
+
+SELECT todo_end();
+
+
 
 SELECT * FROM finish();
 ROLLBACK;
