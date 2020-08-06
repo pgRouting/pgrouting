696e9955090d585b8c5169982bc724f28374cf5b
prakashupes <85prakash2017@gmail.com>
2020-08-06 20:21:05 +0530

    [ltstree] added one more example

--- docqueries/dominator/doc-lengauerTarjanDominatorTree.test.sql
+++ docqueries/dominator/doc-lengauerTarjanDominatorTree.test.sql
@@ -1,3 +1,11 @@
-\echo -- q1
-   select * from pgr_lengauerTarjanDominatorTree($$select * from edge_table$$,1);
-\echo -- q2
+\echo --q1
+   SELECT * FROM pgr_lengauertarjandominatortree(
+    $$SELECT id,source,target,cost,reverse_cost FROM edge_table$$,
+    1
+);
+\echo --q2
+   SELECT * FROM pgr_lengauertarjandominatortree(
+    $$SELECT id,source,target,cost,reverse_cost FROM edge_table$$,
+    16
+);
+\echo --q3
