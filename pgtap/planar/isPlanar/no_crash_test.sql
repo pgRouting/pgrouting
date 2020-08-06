b7cf8887082b56cd335db91be6f8dd9e955c560f
Himanshu Raj <raj.himanshu2@gmail.com>
2020-08-06 21:33:04 +0530

    [isPlanar][pgtap] todo added

--- pgtap/planar/isPlanar/no_crash_test.sql
+++ pgtap/planar/isPlanar/no_crash_test.sql
@@ -1,30 +1,30 @@
 \i setup.sql
 
-SELECT plan(1);
+SELECT plan(5);
 
 PREPARE edges AS
 SELECT id, source, target, cost, reverse_cost  FROM edge_table;
 
 SELECT isnt_empty('edges', 'Should not be empty true to tests be meaningful');
+SELECT todo_start('Fix these checks');
+CREATE OR REPLACE FUNCTION test_function()
+RETURNS SETOF TEXT AS
+$BODY$
+DECLARE
+params TEXT[];
+subs TEXT[];
+BEGIN
+    params = ARRAY['$$SELECT id, source, target, cost, reverse_cost  FROM edge_table$$']::TEXT[];
+    subs = ARRAY[
+    'NULL'
+    ]::TEXT[];
 
--- CREATE OR REPLACE FUNCTION test_function()
--- RETURNS SETOF TEXT AS
--- $BODY$
--- DECLARE
--- params TEXT[];
--- subs TEXT[];
--- BEGIN
---     params = ARRAY['$$SELECT id, source, target, cost, reverse_cost  FROM edge_table$$']::TEXT[];
---     subs = ARRAY[
---     'NULL'
---     ]::TEXT[];
---
---     RETURN query SELECT * FROM no_crash_test('pgr_isplanar', params, subs);
--- END
--- $BODY$
--- LANGUAGE plpgsql VOLATILE;
---
---
--- SELECT * FROM test_function();
+    RETURN query SELECT * FROM no_crash_test('pgr_isplanar', params, subs);
+END
+$BODY$
+LANGUAGE plpgsql VOLATILE;
 
+
+SELECT * FROM test_function();
+SELECT todo_end();
 ROLLBACK;
