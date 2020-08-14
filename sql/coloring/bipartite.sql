ef00f578ffaf4a269bdc0ae325ba025f4ac14711
prakashupes <85prakash2017@gmail.com>
2020-08-14 11:27:49 +0530

    [bipartite] renamed output column to vertex_id and color_id

--- sql/coloring/bipartite.sql
+++ sql/coloring/bipartite.sql
@@ -33,8 +33,8 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 CREATE OR REPLACE FUNCTION pgr_bipartite(
     TEXT, -- edges_sql (required)
 
-    OUT node BIGINT,
-    OUT color BIGINT)
+    OUT vertex_id BIGINT,
+    OUT color_id BIGINT)
 RETURNS SETOF RECORD AS
 $BODY$
 BEGIN
