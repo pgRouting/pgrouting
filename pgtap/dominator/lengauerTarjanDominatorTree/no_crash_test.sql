e4396a15843542e8261945fbaec04be30dec2eab
prakashupes <85prakash2017@gmail.com>
2020-08-21 00:10:56 +0530

    Added 5 new tests

--- pgtap/dominator/lengauerTarjanDominatorTree/no_crash_test.sql
+++ pgtap/dominator/lengauerTarjanDominatorTree/no_crash_test.sql
@@ -6,13 +6,6 @@ PREPARE edges AS
 SELECT id, source, target, cost, reverse_cost  FROM edge_table;
 
 SELECT isnt_empty('edges', 'Should be not empty to tests be meaningful');
-/*
-SELECT is_empty('null_ret', 'Should be empty to tests be meaningful');
-
-SELECT set_eq('null_ret_arr', 'SELECT NULL::BIGINT[]', 'Should be empty to tests be meaningful');
-
-
- */
 
 CREATE OR REPLACE FUNCTION test_function()
 RETURNS SETOF TEXT AS
