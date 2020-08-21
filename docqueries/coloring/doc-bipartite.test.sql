642f5d9145f7ca333fe0af4252eaaa27802370e5
prakashupes <85prakash2017@gmail.com>
2020-08-21 19:51:03 +0530

    Modified docqueries with new exmple

--- docqueries/coloring/doc-bipartite.test.sql
+++ docqueries/coloring/doc-bipartite.test.sql
@@ -1,21 +1,12 @@
-CREATE TABLE five_vertices_table (                                                                                    
-    id BIGSERIAL,
-    source BIGINT,
-    target BIGINT,
-    cost FLOAT,
-    reverse_cost FLOAT
-);
-INSERT INTO five_vertices_table (source, target, cost, reverse_cost) VALUES (1,2,1,1),(2,3,1,1),(3,4,1,-1),(4,5,1,1),(5,1,1,-1);
 \echo --q1
 SELECT * FROM pgr_bipartite(
     $$SELECT id,source,target,cost,reverse_cost FROM edge_table$$
 );
 \echo --q2
-SELECT * FROM pgr_bipartite(
-    $$SELECT id,source,target,cost,reverse_cost FROM edge_table WHERE id IN (8, 10, 11, 12)$$
-);
+INSERT INTO edge_table (source, target, cost, reverse_cost) VALUES
+(1, 7, 1, 1);
 \echo --q3
 SELECT * FROM pgr_bipartite(
-    $$SELECT id,source,target,cost,reverse_cost FROM five_vertices_table$$
+    $$SELECT id,source,target,cost,reverse_cost FROM edge_table$$
 );
 \echo --q4
