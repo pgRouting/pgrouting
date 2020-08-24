6a36728b8d6e68ccbf75c060dc7ee6ee964b27ab
Ashish Kumar <ashishkr23438@gmail.com>
2020-08-24 21:59:49 +0530

    Updated users documentation for 3.2 Added documentation for pgr_lengauerTarjanDominatorTree Added documentation for pgr_bipartite

--- 3.2/en/_static/documentation_options.js
+++ 3.2/en/_static/documentation_options.js
@@ -3,7 +3,9 @@ var DOCUMENTATION_OPTIONS = {
     VERSION: 'v3.2.0-dev',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
+    BUILDER: 'html',
     FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
     HAS_SOURCE: false,
     SOURCELINK_SUFFIX: '.txt',
     NAVIGATION_WITH_KEYS: false
