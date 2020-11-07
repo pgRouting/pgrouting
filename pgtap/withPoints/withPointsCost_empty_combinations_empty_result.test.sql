4a7b6b986b2527b752ce74f0349bc42703bde48e
Ashish Kumar <ashishkr23438@gmail.com>
2020-11-07 20:11:51 +0530

    [withPointsCost][pgtap] Added the tests

---
+++ pgtap/withPoints/withPointsCost_empty_combinations_empty_result.test.sql
@@ -0,0 +1,26 @@
+
+\i setup.sql
+
+SELECT plan(1);
+
+create or REPLACE FUNCTION foo()
+RETURNS SETOF TEXT AS
+$BODY$
+BEGIN
+
+    RETURN query SELECT is_empty(
+      'SELECT start_pid,  end_pid, agg_cost FROM pgr_withPointsCost(
+        ''SELECT id, source, target, cost, reverse_cost FROM edge_table'',
+        ''SELECT pid, edge_id, fraction, side from pointsOfInterest'',
+        ''SELECT * FROM combinations_table WHERE source IN (-1)'' ) '
+    );
+    RETURN;
+END
+$BODY$
+language plpgsql;
+
+select * from foo();
+
+-- Finish the tests and clean up.
+SELECT * FROM finish();
+ROLLBACK;
