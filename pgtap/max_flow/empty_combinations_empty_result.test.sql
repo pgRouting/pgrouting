5a3cf96bfaf767326b7a1c9179e348341a6c0d84
Ashish Kumar <ashishkr23438@gmail.com>
2020-12-07 22:01:46 +0530

    [maxFlow][pgtap] Added tests for combinations signature

---
+++ pgtap/max_flow/empty_combinations_empty_result.test.sql
@@ -0,0 +1,39 @@
+
+\i setup.sql
+
+SELECT plan(1);
+
+create or REPLACE FUNCTION foo( TestFunction TEXT, sql_EdgesQuery TEXT )
+RETURNS SETOF TEXT AS
+$BODY$
+BEGIN
+
+    IF TestFunction = 'pgr_maxFlow' THEN
+        RETURN query SELECT set_eq( 'SELECT * FROM ' || TestFunction || '( $$' || sql_EdgesQuery || '$$,
+            $$SELECT * FROM combinations_table WHERE source IN (-1)$$ ) ',
+            'SELECT NULL::BIGINT'
+        );
+    ELSIF TestFunction = 'pgr_maxFlowMinCost_Cost' THEN
+        RETURN query SELECT set_eq( 'SELECT * FROM ' || TestFunction || '( $$' || sql_EdgesQuery || '$$,
+            $$SELECT * FROM combinations_table WHERE source IN (-1)$$ ) ',
+            'SELECT NULL::FLOAT'
+        );
+    ELSE
+        RETURN query SELECT is_empty( 'SELECT * FROM ' || TestFunction || '( $$' || sql_EdgesQuery || '$$,
+            $$SELECT * FROM combinations_table WHERE source IN (-1)$$ ) '
+        );
+    END IF;
+    RETURN;
+END
+$BODY$
+language plpgsql;
+
+-- test pgr_maxFlow
+select * from foo(
+    'pgr_maxFlow',
+    'SELECT id, source, target, capacity, reverse_capacity FROM edge_table'
+);
+
+-- Finish the tests and clean up.
+SELECT * FROM finish();
+ROLLBACK;
