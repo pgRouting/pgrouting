194e8a805dccbb6fca9b9f6f09e6157185c36c88
Ashish Kumar <ashishkr23438@gmail.com>
2020-12-13 22:55:42 +0530

    [bellmanFord][pgtap] Added missing types check

--- pgtap/bellman_ford/bellman_ford/bellman_ford-typesCheck.sql
+++ pgtap/bellman_ford/bellman_ford/bellman_ford-typesCheck.sql
@@ -1,6 +1,6 @@
 
 UPDATE edge_table SET cost = sign(cost), reverse_cost = sign(reverse_cost);
-SELECT plan(9);
+SELECT plan(14);
 
 SELECT has_function('pgr_bellmanford');
 
@@ -8,6 +8,10 @@ SELECT has_function('pgr_bellmanford', ARRAY['text','bigint','bigint','boolean']
 SELECT has_function('pgr_bellmanford', ARRAY['text','bigint','anyarray','boolean']);
 SELECT has_function('pgr_bellmanford', ARRAY['text','anyarray','bigint','boolean']);
 SELECT has_function('pgr_bellmanford', ARRAY['text','anyarray','anyarray','boolean']);
+SELECT function_returns('pgr_bellmanford', ARRAY['text','bigint','bigint','boolean'], 'setof record');
+SELECT function_returns('pgr_bellmanford', ARRAY['text','bigint','anyarray','boolean'], 'setof record');
+SELECT function_returns('pgr_bellmanford', ARRAY['text','anyarray','bigint','boolean'], 'setof record');
+SELECT function_returns('pgr_bellmanford', ARRAY['text','anyarray','anyarray','boolean'], 'setof record');
 
 -- testing column names
 SELECT bag_has(
@@ -30,4 +34,13 @@ SELECT bag_has(
     $$SELECT  '{"","","","directed","seq","path_seq","start_vid","end_vid","node","edge","cost","agg_cost"}'::TEXT[] $$
 );
 
-
+-- parameter types
+SELECT set_eq(
+    $$SELECT  proallargtypes from pg_proc where proname = 'pgr_bellmanford'$$,
+    $$VALUES
+        ('{25,20,20,16,23,23,20,20,701,701}'::OID[]),
+        ('{25,20,2277,16,23,23,20,20,20,701,701}'::OID[]),
+        ('{25,2277,20,16,23,23,20,20,20,701,701}'::OID[]),
+        ('{25,2277,2277,16,23,23,20,20,20,20,701,701}'::OID[])
+    $$
+);
