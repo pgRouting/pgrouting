b50a7a461d8ce8a022ff41f98e61b11845ed1eaa
Ashish Kumar <ashishkr23438@gmail.com>
2020-12-14 12:08:34 +0530

    [dagShortestPath][docqueries] Added for combinations signature

--- docqueries/dagShortestPath/doc-pgr_dagShortestPath.test.sql
+++ docqueries/dagShortestPath/doc-pgr_dagShortestPath.test.sql
@@ -22,4 +22,8 @@ SELECT * FROM pgr_dagShortestPath(
     ARRAY[1, 4],ARRAY[12,6]
 );
 \echo -- q5
-
+SELECT * FROM pgr_dagShortestPath(
+    'SELECT id, source, target, cost FROM edge_table',
+    'SELECT * FROM ( VALUES (1, 6), (4, 12) ) AS t(source, target)'
+);
+\echo -- q6
