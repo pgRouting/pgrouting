05551b0d45414686e8f3f6bf0c379515f6802d8c
Ashish Kumar <ashishkr23438@gmail.com>
2020-12-14 12:01:45 +0530

    [dagShortestPath][pgtap] Added tests for combinations signature

--- pgtap/dagShortestPath/no_crash_test-dagShortestPath.test.sql
+++ pgtap/dagShortestPath/no_crash_test-dagShortestPath.test.sql
@@ -1,10 +1,13 @@
 \i setup.sql
 
 UPDATE edge_table SET cost = sign(cost), reverse_cost = sign(reverse_cost);
-SELECT plan(67);
+SELECT plan(81);
 
 PREPARE edges AS
 SELECT id, source, target, cost  FROM edge_table;
+
+PREPARE combinations AS
+SELECT source, target  FROM combinations_table;
 
 PREPARE null_ret AS
 SELECT id FROM edge_table_vertices_pgr  WHERE id IN (-1);
@@ -12,8 +15,13 @@ SELECT id FROM edge_table_vertices_pgr  WHERE id IN (-1);
 PREPARE null_ret_arr AS
 SELECT array_agg(id) FROM edge_table_vertices_pgr  WHERE id IN (-1);
 
+PREPARE null_combinations AS
+SELECT source, target FROM combinations_table WHERE source IN (-1);
+
 SELECT isnt_empty('edges', 'Should be not empty to tests be meaningful');
+SELECT isnt_empty('combinations', 'Should be not empty to tests be meaningful');
 SELECT is_empty('null_ret', 'Should be empty to tests be meaningful');
+SELECT is_empty('null_combinations', 'Should be empty to tests be meaningful');
 SELECT set_eq('null_ret_arr', 'SELECT NULL::BIGINT[]', 'Should be empty to tests be meaningful');
 
 
@@ -93,6 +101,21 @@ BEGIN
     'NULL',
     'NULL::BIGINT[]',
     'NULL::BIGINT[]'
+    ]::TEXT[];
+    RETURN query SELECT * FROM no_crash_test('pgr_dagShortestPath', params, subs);
+
+    -- Combinations SQL
+    params = ARRAY['$$edges$$', '$$combinations$$']::TEXT[];
+    subs = ARRAY[
+    'NULL',
+    '$$(SELECT source, target FROM combinations_table  WHERE source IN (-1))$$'
+    ]::TEXT[];
+
+    RETURN query SELECT * FROM no_crash_test('pgr_dagShortestPath', params, subs);
+
+    subs = ARRAY[
+    'NULL',
+    'NULL::TEXT'
     ]::TEXT[];
     RETURN query SELECT * FROM no_crash_test('pgr_dagShortestPath', params, subs);
 
