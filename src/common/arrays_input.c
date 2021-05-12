559731cd55edb61fd4d57bf3d89612ccfe47d971
Shobhit <000shobhitchaurasia@gmail.com>
2021-05-13 01:33:45 +0530

    Updated changes

--- src/common/arrays_input.c
+++ src/common/arrays_input.c
@@ -52,32 +52,33 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 static
 int64_t*
 pgr_get_bigIntArr(ArrayType *v, size_t *arrlen, bool allow_empty) {
-    clock_t start_t = clock(); 
-    int64_t *c_array = NULL;  
+    clock_t start_t = clock();
+    int64_t *c_array = NULL;
 
-    Oid     element_type = ARR_ELEMTYPE(v); 
-    int    *dim = ARR_DIMS(v); 
+    Oid     element_type = ARR_ELEMTYPE(v);
+    int    *dim = ARR_DIMS(v);
     int     ndim = ARR_NDIM(v);
     int     nitems = ArrayGetNItems(ndim, dim);
     Datum  *elements;
-    bool   *nulls; 
-    int16   typlen; 
-    bool    typbyval; 
-    char    typalign; 
+    bool   *nulls;
+    int16   typlen;
+    bool    typbyval;
+    char    typalign;
 
     assert((*arrlen) == 0);
 
-    
+
     if (allow_empty && (ndim == 0 || nitems <= 0)) {
         PGR_DBG("ndim %i nitems % i", ndim, nitems);
         return (int64_t*)NULL;
     }
-    /* array is not empty */
+    /* the array is not empty */
+
     if (ndim != 1) {
         elog(ERROR, "One dimension expected");
         return (int64_t*)NULL;
     }
-    
+
     if (nitems <= 0) {
         elog(ERROR, "No elements found");
         return (int64_t*)NULL;
@@ -101,7 +102,7 @@ pgr_get_bigIntArr(ArrayType *v, size_t *arrlen, bool allow_empty) {
     deconstruct_array(v, element_type, typlen, typbyval,
             typalign, &elements, &nulls,
             &nitems);
-    
+
     c_array = (int64_t *) palloc(sizeof(int64_t) * (size_t)nitems);
     if (!c_array) {
         elog(ERROR, "Out of memory!");
@@ -109,7 +110,7 @@ pgr_get_bigIntArr(ArrayType *v, size_t *arrlen, bool allow_empty) {
 
 
     int i;
-    for (i = 0; i < nitems; i++) {  
+    for (i = 0; i < nitems; i++) {
         if (nulls[i]) {
             pfree(c_array);
             elog(ERROR, "NULL value found in Array!");
@@ -140,6 +141,7 @@ pgr_get_bigIntArr(ArrayType *v, size_t *arrlen, bool allow_empty) {
 int64_t* pgr_get_bigIntArray(size_t *arrlen, ArrayType *input) {
     return pgr_get_bigIntArr(input, arrlen, false);
 }
+
 
 
 int64_t* pgr_get_bigIntArray_allowEmpty(size_t *arrlen, ArrayType *input) {
