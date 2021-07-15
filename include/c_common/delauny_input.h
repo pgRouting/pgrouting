06e47347cf86e995fef7ef9105749c808e8e0f8a
nishkakotian <nishka308@gmail.com>
2021-07-16 00:22:51 +0530

    [C/C++] adjustments because of change on delauny_t.h

--- include/c_common/delauny_input.h
+++ include/c_common/delauny_input.h
@@ -28,7 +28,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 /* for size-t */
 #include <stddef.h>
 
-#include "c_types/delauny_t.h"
+typedef struct Delauny_t Delauny_t;
 
 /*!
   bigint tid,
