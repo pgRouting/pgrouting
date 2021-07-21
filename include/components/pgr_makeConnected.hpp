f6311c3a4beed58826d8e8380308f15496f8df05
nishkakotian <nishka308@gmail.com>
2021-07-21 17:39:43 +0530

    [C/C++] adjustments because of change on pgr_makeConnected_t.h

--- include/components/pgr_makeConnected.hpp
+++ include/components/pgr_makeConnected.hpp
@@ -37,6 +37,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #include <set>
 #include <map>
 
+#include "c_types/pgr_makeConnected_t.h"
 #include "cpp_common/pgr_messages.h"
 #include "cpp_common/pgr_base_graph.hpp"
 #include "cpp_common/interruption.h"
