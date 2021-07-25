61460b40f0ee96f76593756ee406e70b78d8885e
Ri7Sh <rigvitasharma@gmail.com>
2021-07-25 11:01:54 +0000

    [C/C++] adjustments because of change on routes_t.h

--- src/dijkstra/dijkstraVia_driver.cpp
+++ src/dijkstra/dijkstraVia_driver.cpp
@@ -32,7 +32,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #include <vector>
 
 #include "dijkstra/pgr_dijkstraVia.hpp"
-
+#include "c_types/routes_t.h"
 #include "cpp_common/pgr_alloc.hpp"
 #include "cpp_common/pgr_assert.h"
 
