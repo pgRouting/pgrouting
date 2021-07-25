23ccd2ddda1f0078fd0e66c4a9c8cf5081805917
Jayita Pramanik <jayitapramanik2018@gmail.com>
2021-07-25 12:48:35 +0530

    Adjustment because of changes on c_types/pgr_mst_rt.h

--- src/spanningTree/mst_common.cpp
+++ src/spanningTree/mst_common.cpp
@@ -27,6 +27,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #include <string>
 #include <sstream>
 
+#include "c_types/pgr_mst_rt.h"
 #include "cpp_common/pgr_assert.h"
 #include "cpp_common/pgr_alloc.hpp"
 
