0166e8fbc9a0c5c649377a0418122ddba5da667e
ManasviGoyal <mg.manasvi@gmail.com>
2021-07-27 16:21:48 +0530

    adjustments for change in pgr_randomSpanningTree

--- include/drivers/spanningTree/randomSpanningTree_driver.h
+++ include/drivers/spanningTree/randomSpanningTree_driver.h
@@ -39,7 +39,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #endif
 
 #include "c_types/pgr_edge_t.h"
-#include "c_types/pgr_randomSpanningTree_t.h"
+typedef pgr_randomSpanningTree_t pgr_randomSpanningTree_t;
 
 #ifdef __cplusplus
 extern "C" {
