f4210dfc7cd35faa6046f771f853bdbd6cfd158c
Ri7Sh <rigvitasharma@gmail.com>
2021-07-28 18:22:26 +0000

    [C/C++] adjustments because of change on point_on_edge_t.h

--- include/withPoints/pgr_withPoints.hpp
+++ include/withPoints/pgr_withPoints.hpp
@@ -31,7 +31,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #pragma once
 #include <vector>
 
-#include "c_types/point_on_edge_t.h"
+typedef struct Point_on_edge_t Point_on_edge_t;
 #include "cpp_common/pgr_messages.h"
 #include "cpp_common/basePath_SSEC.hpp"
 
