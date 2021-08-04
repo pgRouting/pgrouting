2578ec7377f7d0f5c64d1fabd38483fb3e01370e
Abhineet Choudhary <abhichou4@gmail.com>
2021-08-04 22:55:07 +0530

    [c_types] revert change, test size_t fix

--- include/c_types/trsp/trsp.h
+++ include/c_types/trsp/trsp.h
@@ -32,12 +32,13 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 /* for int64_t */
 #ifdef __cplusplus
 #   include <cstdint>
-#else
-#   include <stdint.h>
-#endif
-
 typedef int64_t int64;
 typedef double float8;
+#else
+#   include <stdint.h>
+#   include <stddef.h>
+#endif
+
 
 struct edge_t {
     int64 id;
