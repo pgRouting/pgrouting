c397410c1747ce73e7736f20cc1c2be591681298
cvvergara <vicky@georepublic.de>
2021-08-05 08:29:59 -0500

    [trsp][c_types] trsp/trsp.h

--- include/trsp/trsp_core.h
+++ include/trsp/trsp_core.h
@@ -1,8 +1,7 @@
 /*PGR-GNU*****************************************************************
 
-File: trsp_core.cpp
+FILE: trsp.h
 
-Generated with Template by:
 Copyright (c) 2015 pgRouting developers
 Mail: project@pgrouting.org
 
@@ -24,17 +23,24 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
  ********************************************************************PGR-GNU*/
 
-#include "trsp/trsp_core.h"
+#ifndef INCLUDE_TRSP_TRSP_CORE_H_
+#define INCLUDE_TRSP_TRSP_CORE_H_
 
-#ifdef __MINGW32__
-#include <winsock2.h>
-#include <windows.h>
+#ifdef __cplusplus
+#   include <cstddef>
+#   include <cstdint>
+#else
+#   include <stddef.h>
+#   include <stdint.h>
 #endif
 
-#include <vector>
-#include <utility>
-#include "trsp/GraphDefinition.h"
+typedef struct restrict_t restrict_t;
+typedef struct edge_t edge_t;
+typedef struct path_element_tt path_element_tt;
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 int trsp_edge_wrapper(
     edge_t *edges,
@@ -50,39 +56,10 @@ int trsp_edge_wrapper(
     path_element_tt **path,
     size_t *path_count,
     char **err_msg
-    ) {
-    try {
-        std::vector<PDVI> ruleTable;
+);
 
-        size_t i, j;
-        ruleTable.clear();
-        for (i = 0; i < restrict_count; i++) {
-            std::vector<int64> seq;
-            seq.clear();
-            seq.push_back(restricts[i].target_id);
-            for (j = 0; j < MAX_RULE_LENGTH && restricts[i].via[j] >- 1; j++) {
-                seq.push_back(restricts[i].via[j]);
-            }
-            ruleTable.push_back(make_pair(restricts[i].to_cost, seq));
-        }
-
-        GraphDefinition gdef;
-        auto res = gdef.my_dijkstra1(edges, edge_count, start_edge, start_pos,
-            end_edge, end_pos, directed, has_reverse_cost, path, path_count,
-            err_msg, ruleTable);
-
-
-        if (res < 0)
-            return res;
-        else
-            return EXIT_SUCCESS;
-    }
-    catch(std::exception& e) {
-        *err_msg = const_cast<char *>(e.what());
-        return -1;
-    }
-    catch(...) {
-        *err_msg = const_cast<char *>("Caught unknown exception!");
-        return -1;
-    }
+#ifdef __cplusplus
 }
+#endif
+
+#endif  // INCLUDE_TRSP_TRSP_CORE_H_
