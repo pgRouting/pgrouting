32fd228dc2f42610d53c2f0e514b8d8aeed87ca7
cvvergara <vicky@georepublic.de>
2021-08-25 07:36:56 -0500

    [CI][Windows] github Action to build in windows

--- src/transitiveClosure/transitiveClosure_driver.cpp
+++ src/transitiveClosure/transitiveClosure_driver.cpp
@@ -22,7 +22,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 #include "drivers/transitiveClosure/transitiveClosure_driver.h"
 
-#include <string.h>
+#include <string>
 #include <sstream>
 #include <deque>
 #include <vector>
