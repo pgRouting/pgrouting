bfc1f98e6ccedb0cfd143eb19d25fb3d6cc6f2f3
cvvergara <vicky@georepublic.de>
2021-08-27 21:18:16 -0500

    [c_types] pgr_boyer_t -> IID_t_rt

--- src/planar/boyerMyrvold.c
+++ src/planar/boyerMyrvold.c
@@ -47,7 +47,7 @@ static void
 process(
     char *edges_sql,
 
-    pgr_boyer_t **result_tuples,
+    IID_t_rt **result_tuples,
     size_t *result_count) {
     pgr_SPI_connect();
 
@@ -112,7 +112,7 @@ PGDLLEXPORT Datum _pgr_boyermyrvold(PG_FUNCTION_ARGS) {
     TupleDesc tuple_desc;
 
     /**************************************************************************/
-    pgr_boyer_t *result_tuples = NULL;
+    IID_t_rt *result_tuples = NULL;
     size_t result_count = 0;
     /**************************************************************************/
 
@@ -152,7 +152,7 @@ PGDLLEXPORT Datum _pgr_boyermyrvold(PG_FUNCTION_ARGS) {
 
     funcctx = SRF_PERCALL_SETUP();
     tuple_desc = funcctx->tuple_desc;
-    result_tuples = (pgr_boyer_t *)funcctx->user_fctx;
+    result_tuples = (IID_t_rt *)funcctx->user_fctx;
 
     if (funcctx->call_cntr < funcctx->max_calls) {
         HeapTuple tuple;
