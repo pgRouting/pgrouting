79c4642e7fab0aff4eade5fec387c38e09518a60
Ashish Kumar <ashishkr23438@gmail.com>
2021-09-27 22:35:28 +0530

    [CI] Add script to remove obsolete #~ entries from po files

---
+++ tools/transifex/remove_obsolete_entries.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# ------------------------------------------------------------------------------
+# pgRouting Scripts
+# Copyright(c) pgRouting Contributors
+#
+# Remove all the obsolete entries, i.e. lines starting with #~ from .po files
+# ------------------------------------------------------------------------------
+
+# For all the chapter files
+for file in locale/en/LC_MESSAGES/*.po; do
+    if grep -q '#~' $file; then
+        perl -pi -0777 -e 's/#~.*//s' $file
+        git add $file
+    fi
+done
+
