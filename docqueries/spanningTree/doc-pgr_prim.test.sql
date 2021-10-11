e6ec8631552f0ae7a34631a4c81a575e3275cef8
cvvergara <vicky@georepublic.de>
2021-10-11 10:00:00 -0500

    [spanningTree] fixing PLSQL

--- docqueries/spanningTree/doc-pgr_prim.test.sql
+++ docqueries/spanningTree/doc-pgr_prim.test.sql
@@ -1,7 +1,7 @@
 
-\echo -- q1
+/* -- q1 */
 SELECT edge, cost FROM pgr_prim(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table WHERE id < 14'
 ) ORDER BY edge;
 
-\echo -- q2
+/* -- q2 */
