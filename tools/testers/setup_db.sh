16019a518ef34c75d89f5d60b85542fbe3f85cda
cvvergara <vicky@georepublic.de>
2021-10-13 11:43:12 -0500

    [tools] adjusting testing scripts

--- tools/testers/setup_db.sh
+++ tools/testers/setup_db.sh
@@ -1,36 +1,31 @@
-\set QUIET 1
+#!/bin/bash
+# ------------------------------------------------------------------------------
+# Travis CI scripts
+# Copyright(c) pgRouting Contributors
+#
+# Test pgRouting
+# ------------------------------------------------------------------------------
 
--- Tests for pgroutng.
+set -e
 
--- Format the output for nice TAP.
-\pset format unaligned
-\pset tuples_only true
-\pset pager
+psql "$1" -U "$3"  -d "$2" -X -q --set client_min_messages=WARNING --set ON_ERROR_STOP=1 --pset pager=off \
+    -c "CREATE EXTENSION IF NOT EXISTS pgtap; CREATE EXTENSION IF NOT EXISTS pgrouting WITH VERSION '${4}' CASCADE;"
 
--- Revert all changes on failure.
-\set ON_ERROR_ROLLBACK 1
-\set ON_ERROR_STOP true
-
-SET client_min_messages = WARNING;
-
-CREATE EXTENSION IF NOT EXISTS postgis;
-CREATE EXTENSION IF NOT EXISTS pgtap;
-CREATE EXTENSION IF NOT EXISTS pgrouting;
-
---    i vrppdtw_data.sql
---    i pickDeliver_pgtap_data.sql
-BEGIN;
-
-    \i sampledata.sql
-    \i vrppdtw_data.sql
-    \i solomon_100_rc101.data.sql
-    \i innerQuery.sql
-    \i innerQuery_old.sql
-    \i inner_styles.sql
-    \i old_inner_styles.sql
-    \i no_crash_test.sql
-    \i alphaShapeTester.sql
-    \i binaryBreadthFirstSearch_pgtap_data.sql
-    \i general_pgtap_tests.sql
-
-END;
+psql "$1" -U "$3"  -d "$2" -X -q --set client_min_messages=WARNING --set ON_ERROR_STOP=1 --pset pager=off \
+    -f sampledata.sql \
+    -f vrppdtw_data.sql \
+    -f solomon_100_rc101.data.sql \
+    -f innerQuery.sql \
+    -f innerQuery_old.sql \
+    -f inner_styles.sql \
+    -f old_inner_styles.sql \
+    -f no_crash_test.sql \
+    -f alphaShapeTester.sql \
+    -f binaryBreadthFirstSearch_pgtap_data.sql \
+    -f general_pgtap_tests.sql \
+    -f no_crash_general.sql \
+    -f dijkstra_pgtap_tests.sql \
+    -f tmp_net.sql \
+    -f flow_pgtap_tests.sql \
+    -f trsp_tests.sql \
+    -f tsp_pgtap_tests.sql
