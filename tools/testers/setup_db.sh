19d8f90556a854828994b78ea2bc6a7c0768128d
cvvergara <vicky@georepublic.de>
2021-11-08 10:41:13 -0600

    [ci] adding to postgres 14

--- tools/testers/setup_db.sh
+++ tools/testers/setup_db.sh
@@ -8,10 +8,10 @@
 
 set -e
 
-psql "$1" -U "$3"  -d "$2" -X -q --set client_min_messages=WARNING --set ON_ERROR_STOP=1 --pset pager=off \
+psql -p "$1" -U "$3"  -d "$2" -X -q --set client_min_messages=WARNING --set ON_ERROR_STOP=1 --pset pager=off \
     -c "CREATE EXTENSION IF NOT EXISTS pgtap; CREATE EXTENSION IF NOT EXISTS pgrouting WITH VERSION '${4}' CASCADE;"
 
-psql "$1" -U "$3"  -d "$2" -X -q --set client_min_messages=WARNING --set ON_ERROR_STOP=1 --pset pager=off \
+psql -p "$1" -U "$3"  -d "$2" -X -q --set client_min_messages=WARNING --set ON_ERROR_STOP=1 --pset pager=off \
     -f sampledata.sql \
     -f vrppdtw_data.sql \
     -f solomon_100_rc101.data.sql \
