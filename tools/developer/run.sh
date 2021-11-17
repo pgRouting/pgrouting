61f66f87c74a22297d6b8d61f27f1ac66699bdc5
cvvergara <vicky@georepublic.de>
2021-11-17 13:44:45 -0600

    [tests] adjusting files to changes on tests extensions

--- tools/developer/run.sh
+++ tools/developer/run.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+DIR=$(git rev-parse --show-toplevel)
+pushd "${DIR}" > /dev/null || exit 1
+
 
 # copy this file into the root of your repository
 # adjust to your needs
@@ -10,178 +13,116 @@ set -e
 VERSION=$(grep -Po '(?<=project\(PGROUTING VERSION )[^;]+' CMakeLists.txt)
 echo "pgRouting VERSION ${VERSION}"
 
-# set up your postgres version and port
-PGVERSION="12"
+# set up your postgres version, port and compiler (if more than one)
+PGVERSION="13"
 PGPORT="5432"
 PGBIN="/usr/lib/postgresql/${PGVERSION}/bin"
-
-# Compiler setup
-
 # When more than one compiler is installed
-GCC="8"
+GCC=""
 
-ALLDIRS="
-allpairs
-alpha_shape
-astar
-bdAstar
-bdDijkstra
-bellman_ford
-breadthFirstSearch
-chinese
-common
-components
-contraction
-dagShortestPath
-dijkstra
-driving_distance
-internalQueryTests
-ksp
-lineGraph
-max_flow
-mincut
-pickDeliver
-spanningTree
-topologicalSort
-topology
-transitiveClosure
-trsp
-tsp
-version
-vrp_basic
-withPoints"
+QUERIES_DIRS=$(ls docqueries -1)
+TAP_DIRS=$(ls pgtap -1)
 
-TESTDIRS=${ALLDIRS}
-TESTDIRS="dijkstra"
+QUERIES_DIRS="dijkstra"
+TAP_DIRS=""
+
+function set_cmake {
+    # Using all defaults
+    #cmake ..
+
+    # Options Release RelWithDebInfo MinSizeRel Debug
+    #cmake  -DCMAKE_BUILD_TYPE=Debug ..
+
+    # Additional debug information
+    #cmake -DPgRouting_DEBUG=ON -DCMAKE_BUILD_TYPE=Debug ..
+
+    # with documentation (like the one the website)
+    #cmake  -DDOC_USE_BOOTSTRAP=ON -DWITH_DOC=ON ..
+
+    # with developers documentation
+    #cmake  -DWITH_DOC=ON -DBUILD_DOXY=ON ..
+
+    cmake  -DPOSTGRESQL_BIN=${PGBIN} -DDOC_USE_BOOTSTRAP=ON -DWITH_DOC=ON -DBUILD_DOXY=ON  -DBUILD_LATEX=ON  -DCMAKE_BUILD_TYPE=Debug ..
+}
+
+function tap_test {
+    echo --------------------------------------------
+    echo pgTap test all
+    echo --------------------------------------------
+
+    dropdb --if-exists -p $PGPORT ___pgr___test___
+    createdb  -p $PGPORT ___pgr___test___
+    echo $PGPORT
+    tools/testers/pg_prove_tests.sh vicky $PGPORT
+    dropdb  -p $PGPORT ___pgr___test___
+}
+
+function action_tests {
+    echo --------------------------------------------
+    echo  Update signatures
+    echo --------------------------------------------
+
+    tools/release-scripts/get_signatures.sh -p ${PGPORT}
+    tools/release-scripts/notes2news.pl
+    bash tools/scripts/test_signatures.sh
+    bash tools/scripts/test_shell.sh
+    bash tools/scripts/test_license.sh
+    bash tools/scripts/code_checker.sh
+    tools/testers/doc_queries_generator.pl  -documentation  -pgport $PGPORT
+}
+
+function set_compiler {
+    echo ------------------------------------
+    echo ------------------------------------
+    echo "Compiling with G++-$1"
+    echo ------------------------------------
+
+    if [ -n "$1" ]; then
+        update-alternatives --set gcc "/usr/bin/gcc-$1"
+    fi
+}
+
+function build_doc {
+    pushd build > /dev/null || exit 1
+    #rm -rf doc/*
+    make doc
+    #rm -rf doxygen/*
+    make doxy
+    popd > /dev/null || exit 1
+}
 
 
 function test_compile {
 
-echo ------------------------------------
-echo ------------------------------------
-echo "Compiling with G++-$1"
-echo ------------------------------------
-
-if [ -n "$1" ]; then
-    update-alternatives --set gcc "/usr/bin/gcc-$1"
-fi
+    set_compiler ${GCC}
 
 
-cd build/
-
-# Using all defaults
-#cmake ..
-
-# Options Release RelWithDebInfo MinSizeRel Debug
-#cmake  -DCMAKE_BUILD_TYPE=Debug ..
-
-# Additional debug information
-#cmake -DPgRouting_DEBUG=ON -DCMAKE_BUILD_TYPE=Debug ..
-
-# with documentation (like the one the website)
-#cmake  -DDOC_USE_BOOTSTRAP=ON -DWITH_DOC=ON ..
-
-# with developers documentation
-#cmake  -DWITH_DOC=ON -DBUILD_DOXY=ON ..
-
-cmake  -DPOSTGRESQL_BIN=${PGBIN} -DDOC_USE_BOOTSTRAP=ON -DWITH_DOC=ON -DBUILD_DOXY=ON  -DBUILD_LATEX=ON  -DCMAKE_BUILD_TYPE=Debug ..
-
-make -j 4
-sudo make install
-cd ..
+    pushd build > /dev/null || exit 1
+    set_cmake
+    make -j 4
+    sudo make install
+    popd > /dev/null || exit 1
 
 
-echo
-echo --------------------------------------------
-echo  Execute documentation queries for a particular directories
-echo --------------------------------------------
+    echo --------------------------------------------
+    echo  Execute tap_directories
+    echo --------------------------------------------
+    for d in ${TAP_DIRS}
+    do
+        bash taptest.sh  "${d}" "-p ${PGPORT}"
+    done
 
+    echo --------------------------------------------
+    echo  Execute documentation queries
+    echo --------------------------------------------
+    for d in ${QUERIES_DIRS}
+    do
+        #tools/testers/doc_queries_generator.pl  -alg "${d}" -documentation  -pgport "${PGPORT}"
+        tools/testers/doc_queries_generator.pl  -alg "${d}" -pgport "${PGPORT}"
+    done
 
-# choose what is going to be tested while developing
-for d in ${TESTDIRS}
-do
-    #tools/testers/doc_queries_generator.pl  -alg ${d} -documentation  -pgport ${PGPORT}
-    tools/testers/doc_queries_generator.pl  -alg ${d} -pgport ${PGPORT}
-    taptest.sh  ${d} -p ${PGPORT}
-done
-
-
-
-
-
-
-echo
-echo --------------------------------------------
-echo  Update signatures
-echo --------------------------------------------
-
-tools/release-scripts/get_signatures.sh -p ${PGPORT}
-
-
-if git status | grep 'pgrouting--' ; then
-    echo "**************************************************"
-    echo "           WARNING"
-    echo "the signatures changed, copyed the generated files"
-    echo "Plese verify the changes are minimal"
-    echo "**************************************************"
-    git diff sql/sigs
-fi
-
-
-################################
-################################
-## checks all the repository
-#
-#  the rest of the script use PGPORT variable
-################################
-################################
-echo
-echo --------------------------------------------
-echo  Update / Verify NEWS
-echo --------------------------------------------
-tools/release-scripts/notes2news.pl
-if git status | grep 'NEWS'; then
-    echo "**************************************************"
-    echo "           WARNING"
-    echo "the signatures changed, copying generated files"
-    echo "Plese verify the changes are minimal"
-    echo "**************************************************"
-    git diff NEWS
-fi
-
-########################################################
-#  Execute documentation queries for the whole project
-########################################################
-tools/testers/doc_queries_generator.pl  -documentation  -pgport $PGPORT
-tools/testers/doc_queries_generator.pl -pgport $PGPORT
-
-cd build
-#rm -rf doc/*
-make doc
-#rm -rf doxygen/*
-make doxy
-cd ..
-
-########################################################
-# pgTap test all
-########################################################
-
-dropdb --if-exists -p $PGPORT ___pgr___test___
-createdb  -p $PGPORT ___pgr___test___
-echo $PGPORT
-tools/testers/pg_prove_tests.sh vicky $PGPORT
-dropdb  -p $PGPORT ___pgr___test___
-
-#tools/testers/update-tester.sh
-
+    tap_test
+    action_tests
 }
 
-# Uncomment what you need
-for compiler in ${GCC}
-do
-    if [ -n "$1" ]; then
-        echo "Fresh build"
-        rm -rf build/*
-    fi
-    test_compile ${compiler}
-done
+test_compile
--- tools/developer/taptest.sh
+++ tools/developer/taptest.sh
@@ -15,12 +15,14 @@ DIR="$1"
 shift
 PGFLAGS=$*
 
-#QUIET="-v"
+echo "dir ${DIR}"
+echo "pgflags ${PGFLAGS}"
+QUIET="-v"
 QUIET="-q"
 
 PGDATABASE="___pgr___test___"
-#PGRVERSION="3.2.1 3.2.0 3.1.3 3.1.2 3.1.1 3.1.0 3.0.5 3.0.4 3.0.3 3.0.2 3.0.1 3.0.0"
-PGRVERSION="3.2.1"
+#PGRVERSION="3.3.0 3.2.1 3.2.0 3.1.3 3.1.2 3.1.1 3.1.0 3.0.5 3.0.4 3.0.3 3.0.2 3.0.1 3.0.0"
+PGRVERSION="3.3.0"
 
 cd tools/testers/
 
@@ -32,11 +34,29 @@ do
 
     dropdb --if-exists "${PGFLAGS}" "${PGDATABASE}"
     createdb "${PGFLAGS}" "${PGDATABASE}"
-    psql -c "CREATE EXTENSION pgRouting WITH VERSION '${v}' CASCADE" -d "${PGDATABASE}"
 
-    echo "../../pgtap/${DIR}"
+    psql "$PGFLAGS" -d "$PGDATABASE" -X -q --set client_min_messages=WARNING --set ON_ERROR_STOP=1 --pset pager=off \
+        -c "CREATE EXTENSION IF NOT EXISTS pgtap; CREATE EXTENSION IF NOT EXISTS pgrouting WITH VERSION '${v}' CASCADE;"
 
-    psql "$PGFLAGS"  -f setup_db.sql -d "${PGDATABASE}"
-    pg_prove "$QUIET" --normalize --directive --recurse --ext .sql "${PGFLAGS}"  -d "${PGDATABASE}" "../../pgtap/${DIR}"
-    dropdb --if-exists "${PGFLAGS}" "${PGDATABASE}"
+    psql "${PGFLAGS}" -d "$PGDATABASE" -X -q --set client_min_messages=WARNING --set ON_ERROR_STOP=1 --pset pager=off \
+        -f sampledata.sql \
+        -f vrppdtw_data.sql \
+        -f solomon_100_rc101.data.sql \
+        -f innerQuery.sql \
+        -f innerQuery_old.sql \
+        -f inner_styles.sql \
+        -f old_inner_styles.sql \
+        -f no_crash_test.sql \
+        -f alphaShapeTester.sql \
+        -f binaryBreadthFirstSearch_pgtap_data.sql \
+        -f general_pgtap_tests.sql \
+        -f no_crash_general.sql \
+        -f dijkstra_pgtap_tests.sql \
+        -f tmp_net.sql \
+        -f flow_pgtap_tests.sql \
+        -f trsp_tests.sql \
+        -f tsp_pgtap_tests.sql
+
+    pg_prove "$QUIET" --normalize --directive --recurse "${PGFLAGS}"  -d "${PGDATABASE}" "../../pgtap/${DIR}"
+    #dropdb --if-exists "${PGFLAGS}" "${PGDATABASE}"
 done
