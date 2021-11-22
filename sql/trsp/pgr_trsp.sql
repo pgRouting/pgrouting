307679727c0e7a5e4551c25161385eb1b6796a38
cvvergara <vicky@georepublic.de>
2021-11-22 13:57:12 -0600

    [trsp][sql] removing has_reverse flag

--- sql/trsp/pgr_trsp.sql
+++ sql/trsp/pgr_trsp.sql
@@ -20,20 +20,10 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
  ********************************************************************PGR-GNU*/
-/*  pgr_trsp    VERTEX
-
- - if size of restrictions_sql  is Zero or no restrictions_sql are given
-     then call to pgr_dijkstra is made
-
- - because it reads the data wrong, when there is a reverse_cost column:
-   - put all data costs in one cost column and
-   - a call is made to trsp without only the positive values
-*/
 
 
 /* TODO
 * The restrictions sql should be the second parameter
-* remove has_rcost
 */
 --v3.0
 CREATE FUNCTION pgr_trsp(
@@ -41,8 +31,6 @@ CREATE FUNCTION pgr_trsp(
     INTEGER, -- from_vid (required)
     INTEGER, -- to_vid (required)
     BOOLEAN, -- directed (required)
-    BOOLEAN, -- has_rcost (required)
-
     restrictions_sql TEXT DEFAULT NULL,
 
     OUT seq INTEGER,
@@ -57,31 +45,12 @@ DECLARE
     start_vid INTEGER := $2;
     end_vid INTEGER   := $3;
     directed BOOLEAN  := $4;
-    has_rcost BOOLEAN := $5;
 
 has_reverse BOOLEAN;
 new_sql TEXT;
 restrictions_query TEXT;
 trsp_sql TEXT;
 BEGIN
-  /* TODO if the user add the column is because he wants to use it */
-  /*
-    has_reverse =_pgr_parameter_check('dijkstra', edges_sql, false);
-
-    new_sql := edges_sql;
-    IF (has_reverse != has_rcost) THEN  -- user contradiction
-        IF (has_reverse) THEN  -- it has reverse_cost but user don't want it.
-            -- to be on the safe side because it reads the data wrong, sending only postitive values
-            new_sql :=
-            'WITH old_sql AS (' || edges_sql || ')' ||
-            '   SELECT id, source, target, cost FROM old_sql';
-        ELSE -- it does not have reverse_cost but user wants it
-            RAISE EXCEPTION 'Error, reverse_cost is used, but query did''t return ''reverse_cost'' column'
-            USING ERRCODE := 'XX000';
-        END IF;
-    END IF;
-    */
-
     /*
     TODO This should be handled by C code
     */
@@ -99,7 +68,7 @@ BEGIN
     */
     restrictions_query = $$
         WITH old_restrictions AS ( $$ ||
-            $6 || $$
+            $5 || $$
         )
         SELECT ROW_NUMBER() OVER() AS id,
             _pgr_array_reverse(array_prepend(target_id, string_to_array(via_path::text, ',')::INTEGER[])) AS path,
@@ -125,7 +94,7 @@ ROWS 1000;
 
 -- COMMENTS
 
-COMMENT ON FUNCTION pgr_trsp(TEXT, INTEGER, INTEGER, BOOLEAN, BOOLEAN, TEXT)
+COMMENT ON FUNCTION pgr_trsp(TEXT, INTEGER, INTEGER, BOOLEAN, TEXT)
 IS 'pgr_trsp
 - Parameters
     - edges SQL with columns: id, source, target, cost [,reverse_cost]
