6624a0b880d2872d07db5a6b16409f8a9433e808
cvvergara <vicky@georepublic.de>
2021-12-07 12:01:09 -0600

    [trsp][pgtap][docqueries] make a dijkstra

--- tools/testers/trsp_tests.sql
+++ tools/testers/trsp_tests.sql
@@ -1,8 +1,9 @@
 
-CREATE OR REPLACE FUNCTION compare_trsp_dijkstra(cant INTEGER default 18, flag boolean default true, k INTEGER DEFAULT 1)
+CREATE OR REPLACE FUNCTION compare_trsp_dijkstra(cant INTEGER default 18, flag boolean default true, t INTEGER DEFAULT 1)
 RETURNS SETOF TEXT AS
 $BODY$
 DECLARE
+k INTEGER := 0;
 dijkstra_sql TEXT;
 trsp_sql TEXT;
 directed TEXT;
@@ -14,30 +15,32 @@ BEGIN
   PREPARE with_reverse_cost AS SELECT id, source, target, cost, reverse_cost from edge_table ORDER BY id;
   PREPARE no_reverse_cost AS SELECT id, source, target, cost from edge_table ORDER BY id;
 
-  IF k = 1 THEN
+  IF t = 1 THEN
     PREPARE restrictions_sql AS SELECT * FROM new_restrictions WHERE id > 7;
-  ELSE
+  ELSIF t = 2 THEN
     PREPARE restrictions_sql AS SELECT 1 AS id, 100::float AS cost, ARRAY[33, 32, 25] AS path;
+  ELSIF t = 3 THEN
+    PREPARE restrictions_sql AS SELECT * FROM new_restrictions;
   END IF;
 
   FOR i IN 1.. cant BY 2 LOOP
     FOR j IN 1..cant LOOP
-      -- this special case is tested on the other test
-      -- TODO this should return empty set
-      CONTINUE WHEN i = j;
+      -- For related restrictions only when source an target are equal it is garanteed that the results are the same as
+      -- dijkstra
+      IF (t = 3) THEN CONTINUE WHEN i != j; END IF;
 
       dijkstra_sql := 'SELECT seq, node, edge::integer, cost::text FROM pgr_dijkstra($$with_reverse_cost$$, '
         || i || ', ' || j || ', ' || flag || ')';
       trsp_sql := 'SELECT seq, node, edge, cost::text from pgr_trsp($$with_reverse_cost$$, $$restrictions_sql$$, '
         || i || ', ' || j || ', ' || flag || ')';
-      msg := k || '-1 ' || directed || ', with reverse_cost from '  || i || ' to ' || j;
+      msg := t || '-' || k || '-1 ' || directed || ', with reverse_cost from '  || i || ' to ' || j;
       RETURN QUERY SELECT set_eq(trsp_sql, dijkstra_sql, msg);
 
       dijkstra_sql := 'SELECT seq, node, edge::integer, cost::text FROM pgr_dijkstra($$no_reverse_cost$$, '
         || i || ', ' || j || ', ' || flag || ')';
       trsp_sql := 'SELECT seq, node, edge, cost::text from pgr_trsp($$no_reverse_cost$$, $$restrictions_sql$$, '
         || i || ', ' || j || ', ' || flag || ')';
-      msg := k || '-4 ' || directed || ', NO reverse_cost from '  || i || ' to ' || j;
+      msg := t || '-' || k || '-2 ' || directed || ', NO reverse_cost from '  || i || ' to ' || j;
       RETURN QUERY SELECT set_eq(trsp_sql, dijkstra_sql, msg);
 
       k := k + 1;
