eacc4aace9a63dcb186349567719c951270f4ddf
cvvergara <vicky@georepublic.de>
2021-12-08 10:28:30 -0600

    [trsp][docqueries] backend function: _trsp -> _pgr_trsp

--- docqueries/trsp/trsp-renumber.test.sql
+++ docqueries/trsp/trsp-renumber.test.sql
@@ -7,13 +7,13 @@ SELECT * FROM pgr_trsp(
   17, 22, false
 );
 
-SELECT * FROM _pgr_trsp(
+SELECT * FROM pgr_trsp(
   $$SELECT id, source, target, cost FROM edge_table$$,
   $$ SELECT 1 AS id, ARRAY[4,7] AS path, 100 AS cost $$,
   2, 7, false
 );
 
-SELECT * FROM _pgr_trsp(
+SELECT * FROM pgr_trsp(
   $$SELECT id, source, target, cost FROM edge_table$$,
   $$SELECT * FROM new_restrictions$$,
   2, 7, false
