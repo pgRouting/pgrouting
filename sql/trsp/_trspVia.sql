a89295d25985fbe8380e8824ed4e85251bc56833
cvvergara <vicky@georepublic.de>
2021-12-09 20:54:11 -0600

    [trspVia][sql] meaninfull names same as dijkstratrsp

--- sql/trsp/_trspVia.sql
+++ sql/trsp/_trspVia.sql
@@ -28,91 +28,35 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 --v4.0
 CREATE FUNCTION _pgr_trspVia (
-    sql text,
-    turn_restrict_sql text,
-    vids ANYARRAY,
-    directed boolean,
+  TEXT,
+  TEXT,
+  ANYARRAY,
+  BOOLEAN,
 
-    OUT seq INTEGER,
-    OUT id1 INTEGER,
-    OUT id2 INTEGER,
-    OUT id3 INTEGER,
-    OUT cost FLOAT
+  OUT path_id INTEGER,
+  OUT path_seq INTEGER,
+  OUT start_vid BIGINT,
+  OUT end_vid BIGINT,
+  OUT node BIGINT,
+  OUT edge BIGINT,
+  OUT cost FLOAT,
+  OUT agg_cost FLOAT
 )
 RETURNS SETOF RECORD AS
-$body$
-/*
- *  pgr_trsp(sql text, vids integer[], directed boolean, has_reverse_cost boolean, turn_restrict_sql text DEFAULT NULL::text)
- *
- *  Compute TRSP with via points. We compute the path between vids[i] and vids[i+1] and chain the results together.
- *
- *  NOTE: this is a prototype function, we can gain a lot of efficiencies by implementing this in C/C++
- *
-*/
-declare
+$BODY$
+DECLARE
     i integer;
-    rr RECORD;
-    lrr RECORD;
-    lrra boolean := false;
-    seq1 integer := 0;
-    seq2 integer := 0;
-    restrictions_query TEXT;
-
 begin
 
-  /*
-    restrictions_query = $$
-    WITH old_restrictions AS ( $$ ||
-        $2 || $$
-    )
-    SELECT ROW_NUMBER() OVER() AS id,
-    _pgr_array_reverse(array_prepend(target_id, string_to_array(via_path, ',')::INTEGER[])) AS path,
-    to_cost AS cost
-    FROM old_restrictions;
-    $$;
-*/
+    FOR i IN 1 .. array_length(vids, 1) - 1
+    LOOP
+        RETURN QUERY
+        SELECT i, a.seq, a.start_vid, a.end_vid, a.node, a.edge, a.cost, a.agg_cost
+        FROM pgr_trsp($1, $2, vids[i], vids[i+1], $4) AS a;
+    END LOOP;
 
-
-    -- loop through each pair of vids and compute the path
-    for i in 1 .. array_length(vids, 1)-1 loop
-        seq2 := seq2 + 1;
-        for rr in select a.seq, seq2 as id1, a.node::INTEGER as id2, a.edge::INTEGER as id3, a.cost
-                    from pgr_trsp(sql, $2, vids[i], vids[i+1], directed) as a loop
-            -- filter out the individual path ends except the last one
-            -- we might not want to do this so we can know where the via points are in the path result
-            -- but this needs more thought
-            --raise notice 'rr: %', rr;
-            if rr.id3 = -1 then
-                lrr := rr;
-                lrra := true;
-            else
-                seq1 := seq1 + 1;
-                rr.seq := seq1;
-
-                seq := rr.seq;
-                id1 := rr.id1;
-                id2 := rr.id2;
-                id3 := rr.id3;
-                cost := rr.cost;
-                return next;
-            end if;
-        end loop;
-    end loop;
-
-    if lrra then
-        seq1 := seq1 + 1;
-        lrr.seq := seq1;
-
-        seq := lrr.seq;
-        id1 := lrr.id1;
-        id2 := lrr.id2;
-        id3 := lrr.id3;
-        cost := lrr.cost;
-        return next;
-    end if;
-    return;
 end;
-$body$
+$BODY$
 LANGUAGE plpgsql VOLATILE STRICT
 cost 100
 rows 1000;
--- sql/trsp/trspVia.sql
+++ sql/trsp/trspVia.sql
@@ -33,39 +33,65 @@ TODO
 - restrictions sql should be the second parameter (DONE)
 - Do not accept NULL restrictions (DONE)
 - has_rcost should be removed (DONE)
-- Results when empty restrictions or unrelated restrictions should be same a dijskstra
-- restrictions sql should be the new restrictions
-- Directed flag should be optional (DOING)
-- Results columns with meaningful names
-- Results columns more like pgr_dijkstraVia
+- Results when empty restrictions or unrelated restrictions should be same a dijskstra (DOING)
+- restrictions sql should be the new restrictions (DONE)
+- Directed flag should be optional (DONE)
+- Results columns with meaningful names (DOING)
+- Results columns more like pgr_dijkstraVia (DOING)
 - Perform the via on the C/C++ code instead of on the SQL code
 - function should be STRICT (DONE)
 - must accept prepared statements (DONE)
 */
 --v3.0
 CREATE FUNCTION pgr_trspVia(
-    TEXT, -- edges SQL (required)
-    TEXT, -- restrictions SQL
-    ANYARRAY,  -- via vids (required)
-    directed BOOLEAN DEFAULT true,
+  TEXT, -- edges SQL (required)
+  TEXT, -- restrictions SQL
+  ANYARRAY,  -- via vids (required)
+  directed BOOLEAN DEFAULT true,
 
-    OUT seq INTEGER,
-    OUT id1 INTEGER,
-    OUT id2 INTEGER,
-    OUT id3 INTEGER,
-    OUT cost FLOAT
+  OUT seq INTEGER,
+  OUT path_id INTEGER,
+  OUT path_seq INTEGER,
+  OUT start_vid BIGINT,
+  OUT end_vid BIGINT,
+  OUT node BIGINT,
+  OUT edge BIGINT,
+  OUT cost FLOAT,
+  OUT agg_cost FLOAT,
+  OUT route_agg_cost FLOAT
 )
 RETURNS SETOF RECORD AS
 
 $BODY$
+BEGIN
 
-  SELECT * FROM _pgr_trspVia(
-    _pgr_get_statement($1),
-    _pgr_get_statement($2),
-    $3, $4);
+  RETURN QUERY
+  WITH
+  the_results AS (
+    SELECT ROW_NUMBER() OVER()::INTEGER AS seq, *, lag(a.cost) OVER()
+    FROM _pgr_trspVia(
+        _pgr_get_statement($1),
+        _pgr_get_statement($2),
+        $3, $4) AS a
+  ),
+  the_results1 AS (
+    SELECT *, sum(a.lag) OVER(ORDER BY a.path_id, a.path_seq) AS route_agg_cost
+    FROM the_results AS a
+  ),
+  path_id_is_complete AS (
+    SELECT count(distinct the_results.path_id) = array_length($3, 1) - 1 AS is_ok,
+      max(the_results.seq) AS last_row
+    FROM the_results
+  )
+  SELECT r.seq, r.path_id, r.path_seq, r.start_vid, r.end_vid, r.node,
+    CASE WHEN r.seq = last_row THEN -2 ELSE r.edge END,
+    r.cost, r.agg_cost, coalesce(r.route_agg_cost,0)
+  FROM path_id_is_complete, the_results1 AS r WHERE is_ok;
 
+
+END;
 $BODY$
-LANGUAGE SQL VOLATILE STRICT
+LANGUAGE plpgsql VOLATILE STRICT
 COST 100
 ROWS 1000;
 
