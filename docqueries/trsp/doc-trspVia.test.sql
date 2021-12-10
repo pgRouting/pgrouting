d8cea8b02a5c4c3e922678f3548f968fa0c69d54
cvvergara <vicky@georepublic.de>
2021-12-09 20:54:40 -0600

    [trspVia][docqueries] meaninfull names same as dijkstratrsp

--- docqueries/trsp/doc-trspVia.test.sql
+++ docqueries/trsp/doc-trspVia.test.sql
@@ -3,5 +3,5 @@ SELECT * FROM pgr_trspVia(
   'SELECT id::INTEGER, source::INTEGER, target::INTEGER, cost FROM edge_table',
   $$SELECT * FROM new_restrictions$$,
   ARRAY[2,7,11]::INTEGER[],
-  false)
+  false);
 /* --q2 */
