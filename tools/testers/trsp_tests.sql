94fa98091fa4a44ccb71f685f958027a41e77bb4
cvvergara <vicky@georepublic.de>
2021-12-10 11:39:42 -0600

    [tspVia][pgtap] adding strict and u_turn_on_edge

--- tools/testers/trsp_tests.sql
+++ tools/testers/trsp_tests.sql
@@ -65,6 +65,8 @@ k INTEGER;
 directed TEXT;
 msg TEXT;
 msg_end TEXT;
+all_found BOOLEAN;
+allow_u BOOLEAN;
 BEGIN
   directed = 'Undirected';
   IF flag THEN directed = 'Directed'; END IF;
@@ -76,34 +78,42 @@ BEGIN
   --msg_end = E'\n' || trsp_sql || E'\n' || dijkstraVia_sql;
   msg_end = ' ';
 
-  FOR i IN 1.. cant LOOP
-    FOR j IN 1.. cant LOOP
+  FOR i IN 1 .. cant LOOP
+    FOR j IN 1 .. cant LOOP
+      FOR l IN 0 .. 1 LOOP
+        FOR m IN 0 .. 1 LOOP
+          all_found := (l=1);
+          allow_u := (m=1);
 
-      dijkstraVia_sql := 'SELECT * FROM pgr_dijkstraVia( '
-        || with_reverse_cost
-        || ', ARRAY[1, ' || i || ', ' || j || '], ' || flag || ', TRUE)';
+          dijkstraVia_sql := 'SELECT * FROM pgr_dijkstraVia( '
+            || with_reverse_cost
+            || ', ARRAY[1, ' || i || ', ' || j || '], ' || flag || ',' || all_found || ',' || allow_u || ')';
 
-      trsp_sql := 'SELECT * FROM pgr_trspVia('
-        || with_reverse_cost  || ','
-        || empty_restrictions || ', ARRAY[1, ' || i || ', ' || j || '], ' || flag || ')';
+          trsp_sql := 'SELECT * FROM pgr_trspVia('
+            || with_reverse_cost  || ','
+            || empty_restrictions
+            || ', ARRAY[1, ' || i || ', ' || j || '], ' || flag || ',' || all_found || ',' || allow_u || ')';
 
-      msg := '-1- ' || directed || ', with reverse_cost: from 1 to '  || i || ' to ' || j || msg_end;
-      RETURN query SELECT set_eq(trsp_sql, dijkstraVia_sql, msg);
+          msg := '-1- ' || directed || ', with reverse_cost: from 1 to '  || i || ' to ' || j || msg_end;
+          RETURN query SELECT set_eq(trsp_sql, dijkstraVia_sql, msg);
 
-      dijkstraVia_sql := 'SELECT * FROM pgr_dijkstraVia( '
-        || no_reverse_cost
-        || ', ARRAY[1, ' || i || ', ' || j || '], ' || flag || ', TRUE)';
+          dijkstraVia_sql := 'SELECT * FROM pgr_dijkstraVia( '
+            || no_reverse_cost
+            || ', ARRAY[1, ' || i || ', ' || j || '], ' || flag || ',' || all_found || ',' || allow_u || ')';
 
-      trsp_sql := 'SELECT * FROM pgr_trspVia('
-        || no_reverse_cost  || ','
-        || empty_restrictions || ', ARRAY[1, ' || i || ', ' || j || '], ' || flag || ')';
+          trsp_sql := 'SELECT * FROM pgr_trspVia('
+            || no_reverse_cost  || ','
+            || empty_restrictions
+            || ', ARRAY[1, ' || i || ', ' || j || '], ' || flag || ',' || all_found || ',' || allow_u || ')';
 
-      msg := '-1- ' || directed || ', no reverse_cost: from 1 to '  || i || ' to ' || j || msg_end;
-      RETURN query SELECT set_eq(trsp_sql, dijkstraVia_sql, msg);
+          msg := '-1- ' || directed || ', no reverse_cost: from 1 to '  || i || ' to ' || j || msg_end;
+          RETURN query SELECT set_eq(trsp_sql, dijkstraVia_sql, msg);
 
 
-      k := k + 1;
+          k := k + 1;
 
+        END LOOP;
+      END LOOP;
     END LOOP;
   END LOOP;
 END
