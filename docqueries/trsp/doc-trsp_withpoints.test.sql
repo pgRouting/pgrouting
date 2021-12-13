b3b72f9839fac257db6dbdb003d4da7dac66119c
cvvergara <vicky@georepublic.de>
2021-12-13 14:10:13 -0600

    [trsp_withPoints][docqueries] adding additional tests that should return equivalent results

--- docqueries/trsp/doc-trsp_withpoints.test.sql
+++ docqueries/trsp/doc-trsp_withpoints.test.sql
@@ -6,11 +6,31 @@ SELECT * FROM pgr_trsp_withPoints(
   $$SELECT * FROM new_restrictions$$,
   $$SELECT pid, edge_id, fraction, side from pointsOfInterest$$,
   7, 12);
+SELECT * FROM pgr_trsp_withPoints(
+  $$SELECT id, source, target, cost, reverse_cost FROM edge_table ORDER BY id$$,
+  $$SELECT * FROM new_restrictions WHERE id > 7$$,
+  $$SELECT pid, edge_id, fraction, side from pointsOfInterest$$,
+  7, 12);
 SELECT * FROM pgr_withPoints(
   $$SELECT id, source, target, cost, reverse_cost FROM edge_table ORDER BY id$$,
   $$SELECT pid, edge_id, fraction, side from pointsOfInterest$$,
   7, 12);
-  -- -1, -3);
+
+SELECT * FROM pgr_trsp_withPoints(
+  $$SELECT id, source, target, cost, reverse_cost FROM edge_table ORDER BY id$$,
+  $$SELECT * FROM new_restrictions$$,
+  $$SELECT pid, edge_id, fraction, side from pointsOfInterest$$,
+  -1, -3);
+SELECT * FROM pgr_trsp_withPoints(
+  $$SELECT id, source, target, cost, reverse_cost FROM edge_table ORDER BY id$$,
+  $$SELECT * FROM new_restrictions WHERE id > 7$$,
+  $$SELECT pid, edge_id, fraction, side from pointsOfInterest$$,
+  -1, -3);
+SELECT * FROM pgr_withPoints(
+  $$SELECT id, source, target, cost, reverse_cost FROM edge_table ORDER BY id$$,
+  $$SELECT pid, edge_id, fraction, side from pointsOfInterest$$,
+  -1, -3);
+
 /* --e2 */
 SELECT * FROM pgr_trsp_withPoints(
   $$SELECT id, source, target, cost, reverse_cost FROM edge_table ORDER BY id$$,
