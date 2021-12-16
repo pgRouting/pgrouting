cc7e00bedb489116b93e2ec2390e9af862f536d2
cvvergara <vicky@georepublic.de>
2021-12-15 21:36:29 -0600

    [trspWithPoints][docqueries] adjusting name on results

--- docqueries/trsp/trsp-any-02.test.sql
+++ docqueries/trsp/trsp-any-02.test.sql
@@ -1,26 +1,22 @@
 SET extra_float_digits=-3;
 
 CREATE TABLE restrictions2 (
-    rid integer NOT NULL,
-    to_cost double precision,
-    teid integer,
-    feid integer,
-    via text
+    id SERIAL PRIMARY KEY,
+    path BIGINT[],
+    cost float
 );
 
-INSERT INTO  restrictions2 (rid, to_cost, teid, feid, via) VALUES
-(1,100,7,4,NULL),
-(2,4,8,3,'5'),
-(3,100,9,16,NULL);
+INSERT INTO restrictions2 (path, cost) VALUES
+(ARRAY[4, 7], 100),
+(ARRAY[5, 3], 4),
+(ARRAY[16, 9], 100);
 
 UPDATE edge_table SET cost = cost + 0.001 * id * id, reverse_cost = reverse_cost + 0.001 * id * id;
 
-select * from pgr_withpointstrsp(
-    'select id::integer, source::integer, target::integer,cost, reverse_cost from edge_table ORDER BY ID',
-    1,    -- edge_id for start
-    0.5,  -- midpoint of edge
-    6,    -- edge_id of route end
-    0.5,  -- midpoint of edge
-    true, -- directed graph?
-    -- include the turn restrictions
-    'select to_cost, teid as target_id, feid||coalesce('',''||via,'''') as via_path from restrictions2');
+SELECT * FROM pgr_trsp_withPoints(
+  $$SELECT * FROM edge_table$$,
+  $$SELECT * FROM restrictions2$$,
+  $$(SELECT 1 AS pid, 1 AS edge_id, 0.5::float  AS fraction)
+  UNION
+  (SELECT 2, 6, 0.5)$$,
+  -1, -2, true);
