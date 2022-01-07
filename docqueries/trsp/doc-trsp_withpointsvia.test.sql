3e2595db2e4b292195a5afe8adc191ccbe5d157b
cvvergara <vicky@georepublic.de>
2022-01-07 13:38:36 -0600

    [trsp_withPointsvia][docqueries] updating results

--- docqueries/trsp/doc-trsp_withpointsvia.test.sql
+++ docqueries/trsp/doc-trsp_withpointsvia.test.sql
@@ -29,9 +29,9 @@ SELECT * FROM pgr_trsp_withPointsVia(
   $$SELECT id::INTEGER, source::INTEGER, target::INTEGER, cost FROM edge_table$$,
   $$SELECT * FROM new_restrictions$$,
   $$SELECT pid, edge_id, fraction, side from pointsOfInterest$$,
-  ARRAY[2,7,11]::INTEGER[], directed => false, driving_side => 'l', details => true, U_turn_on_edge => false);
+  ARRAY[2,7,11]::INTEGER[], directed => false, driving_side => 'l', details => true, allow_u_turn => false);
 SELECT * FROM pgr_trsp_withPointsVia(
   $$SELECT id::INTEGER, source::INTEGER, target::INTEGER, cost FROM edge_table$$,
   $$SELECT * FROM new_restrictions$$,
   $$SELECT pid, edge_id, fraction, side from pointsOfInterest$$,
-  ARRAY[2,7,11]::INTEGER[], directed => false, driving_side => 'l', details => true, U_turn_on_edge => false, strict => true);
+  ARRAY[2,7,11]::INTEGER[], directed => false, driving_side => 'l', details => true, allow_u_turn => false, strict => true);
