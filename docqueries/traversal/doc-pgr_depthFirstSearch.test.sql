ea6cd3ab0735290f590e5ea54c0658b9e895dbfc
cvvergara <vicky@georepublic.de>
2022-04-22 15:20:13 -0500

    depthFirstSearch removing unused query

--- docqueries/traversal/doc-pgr_depthFirstSearch.test.sql
+++ docqueries/traversal/doc-pgr_depthFirstSearch.test.sql
@@ -1,25 +1,16 @@
-/* -- q1 */
-SELECT * FROM pgr_depthFirstSearch(
-    'SELECT id, source, target, cost, reverse_cost FROM edge_table
-    ORDER BY id',
-    2
-);
 /* -- q2 */
 SELECT * FROM pgr_depthFirstSearch(
-    'SELECT id, source, target, cost, reverse_cost FROM edge_table
-    ORDER BY id',
-    2, directed => false, max_depth => 2
-);
+  'SELECT id, source, target, cost, reverse_cost FROM edge_table
+  ORDER BY id',
+  2);
 /* -- q3 */
 SELECT * FROM pgr_depthFirstSearch(
-    'SELECT id, source, target, cost, reverse_cost FROM edge_table
-    ORDER BY id',
-    ARRAY[11, 2], directed => false, max_depth => 2
-);
+  'SELECT id, source, target, cost, reverse_cost FROM edge_table
+  ORDER BY id',
+  ARRAY[11, 2], directed => false, max_depth => 2);
 /* -- q4 */
 SELECT * FROM pgr_depthFirstSearch(
-    'SELECT id, source, target, cost, reverse_cost FROM edge_table
-    ORDER BY id DESC',
-    2
-);
+  'SELECT id, source, target, cost, reverse_cost FROM edge_table
+  ORDER BY id DESC',
+  2);
 /* -- q5 */
