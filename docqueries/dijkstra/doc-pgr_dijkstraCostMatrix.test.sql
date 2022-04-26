9e7526be6e77de8a554fef20dafafb8ae38a9f5f
cvvergara <vicky@georepublic.de>
2022-04-26 10:00:22 -0500

    [bdDijkstra][dijkstra][CostMatrix] standarizing documentation examples

--- docqueries/dijkstra/doc-pgr_dijkstraCostMatrix.test.sql
+++ docqueries/dijkstra/doc-pgr_dijkstraCostMatrix.test.sql
@@ -2,16 +2,13 @@
 SELECT * FROM pgr_dijkstraCostMatrix(
   'SELECT id, source, target, cost, reverse_cost FROM edge_table',
   (SELECT array_agg(id) FROM edge_table_vertices_pgr WHERE id < 5),
-  false
-);
+  false);
 /* -- q3 */
 SELECT * FROM pgr_TSP(
   $$
   SELECT * FROM pgr_dijkstraCostMatrix(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table',
     (SELECT array_agg(id) FROM edge_table_vertices_pgr WHERE id < 5),
-    false
-  )
-  $$
-);
+    false)
+  $$);
 /* -- q4 */
