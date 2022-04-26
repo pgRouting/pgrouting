c3005d4194e82fe76b46310eed84f3c2b38165ed
cvvergara <vicky@georepublic.de>
2022-04-26 11:52:09 -0500

    [bdDijkstra][dijkstra] standarizing documentation examples

--- docqueries/bdDijkstra/doc-pgr_bdDijkstra.test.sql
+++ docqueries/bdDijkstra/doc-pgr_bdDijkstra.test.sql
@@ -1,30 +1,37 @@
-
-/* -- q1 */
-SELECT * FROM pgr_bdDijkstra(
-    'SELECT id, source, target, cost, reverse_cost FROM edge_table',
-    2, 3
-);
-
 /* -- q2 */
 SELECT * FROM pgr_bdDijkstra(
-    'SELECT id, source, target, cost, reverse_cost FROM edge_table',
-    2, 3,
-    false
-);
+  'select id, source, target, cost, reverse_cost from edge_table',
+  2, 3, true);
 /* -- q3 */
 SELECT * FROM pgr_bdDijkstra(
-    'SELECT id, source, target, cost, reverse_cost FROM edge_table',
-    2, ARRAY[3, 11]);
+  'select id, source, target, cost, reverse_cost from edge_table',
+  2, ARRAY[3, 12]);
 /* -- q4 */
 SELECT * FROM pgr_bdDijkstra(
-    'SELECT id, source, target, cost, reverse_cost FROM edge_table',
-    ARRAY[2, 7], 3);
+  'select id, source, target, cost, reverse_cost from edge_table',
+  ARRAY[2, 7], 12);
 /* -- q5 */
 SELECT * FROM pgr_bdDijkstra(
-    'SELECT id, source, target, cost, reverse_cost FROM edge_table',
-    ARRAY[2, 7], ARRAY[3, 11]);
+  'select id, source, target, cost, reverse_cost from edge_table',
+  ARRAY[2, 7], ARRAY[3, 12],
+  directed => false);
+/* -- q51 */
+SELECT source, target FROM combinations_table;
+/* -- q52 */
+SELECT * FROM pgr_bdDijkstra(
+  'SELECT id, source, target, cost, reverse_cost FROM edge_table',
+  'SELECT source, target FROM combinations_table',
+  false);
 /* -- q6 */
 SELECT * FROM pgr_bdDijkstra(
-    'SELECT id, source, target, cost, reverse_cost FROM edge_table',
-    'SELECT * FROM ( VALUES (2, 3), (7, 11) ) AS t(source, target)');
+  'select id, source, target, cost, reverse_cost from edge_table',
+  ARRAY[5, 3, 4, 3, 3, 4], ARRAY[3, 5, 3, 4]);
 /* -- q7 */
+SELECT * FROM pgr_bdDijkstra(
+  'select id, source, target, cost, reverse_cost from edge_table',
+  ARRAY[5, 3, 4], ARRAY[5, 3, 4]);
+/* -- q8 */
+SELECT * FROM pgr_bdDijkstra(
+  'SELECT id, source, target, cost, reverse_cost FROM edge_table',
+  'SELECT * FROM (VALUES (2, 3), (2, 5), (11, 3)) AS combinations (source, target)');
+/* -- q9 */
