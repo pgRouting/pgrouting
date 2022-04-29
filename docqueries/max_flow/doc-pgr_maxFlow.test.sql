d07c47facaa1a058716fa20edbe0994620bfba5c
cvvergara <vicky@georepublic.de>
2022-04-29 13:14:06 -0500

    [flow][docqueries] standarizing examples of maxFlow

--- docqueries/max_flow/doc-pgr_maxFlow.test.sql
+++ docqueries/max_flow/doc-pgr_maxFlow.test.sql
@@ -1,61 +1,35 @@
---These tests used the sample data provided here: http://docs.pgrouting.org/2.2/en/doc/src/developer/sampledata.html#sampledata
---The edge in the edge table are augmented with road-like categories for capacity values.
-
-
-
 /* -- q1 */
 SELECT * FROM pgr_maxFlow(
-    'SELECT id,
-            source,
-            target,
-            capacity,
-            reverse_capacity
-    FROM edge_table'
-    , 6, 11
-);
-
+  'SELECT id, source, target, capacity, reverse_capacity
+  FROM edge_table',
+  6, 11);
 /* -- q2 */
 SELECT * FROM pgr_maxFlow(
-    'SELECT id,
-            source,
-            target,
-            capacity,
-            reverse_capacity
-    FROM edge_table'
-    , 6, ARRAY[11, 1, 13]
-);
-
+  'SELECT id, source, target, capacity, reverse_capacity
+  FROM edge_table'
+  , 6, ARRAY[1, 3, 11]);
 /* -- q3 */
 SELECT * FROM pgr_maxFlow(
-    'SELECT id,
-            source,
-            target,
-            capacity,
-            reverse_capacity
-    FROM edge_table'
-    , ARRAY[6, 8, 12], 11
-);
-
+  'SELECT id, source, target, capacity, reverse_capacity
+  FROM edge_table'
+  , ARRAY[6, 8, 12], 11);
 /* -- q4 */
 SELECT * FROM pgr_maxFlow(
-    'SELECT id,
-            source,
-            target,
-            capacity,
-            reverse_capacity
-    FROM edge_table'
-    , ARRAY[6, 8, 12], ARRAY[1, 3, 11]
-);
-
+  'SELECT id, source, target, capacity, reverse_capacity
+  FROM edge_table'
+  , ARRAY[6, 8, 12], ARRAY[1, 3, 11]);
 /* -- q5 */
+SELECT source, target FROM combinations_table
+WHERE target NOT IN (1, 2);
+/* -- q51 */
 SELECT * FROM pgr_maxFlow(
-    'SELECT id,
-            source,
-            target,
-            capacity,
-            reverse_capacity
-    FROM edge_table',
-    'SELECT * FROM ( VALUES (6, 1), (8, 3), (12, 11), (8, 1) ) AS t(source, target)'
-);
-
+  'SELECT id, source, target, capacity, reverse_capacity
+  FROM edge_table',
+  'SELECT * FROM combinations_table
+   WHERE target NOT IN (1, 2)');
 /* -- q6 */
+SELECT * FROM pgr_maxFlow(
+  'SELECT id, source, target, capacity, reverse_capacity
+  FROM edge_table',
+  'SELECT * FROM (VALUES (1, 3), (2, 4), (2, 17)) AS t(source, target)');
+/* -- q7 */
