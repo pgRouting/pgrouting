bd7caa1240deddfe972a633a6da3cac8d335a113
nitishchauhan0022 <nitishchauhan0022@gmail.com>
2022-05-17 22:08:20 +0530

    pgr_bddijkstra to pgr_dijkstra

--- docqueries/dijkstra/doc-pgr_dijkstra.test.sql
+++ docqueries/dijkstra/doc-pgr_dijkstra.test.sql
@@ -1,37 +1,37 @@
 /* -- q2 */
-SELECT * FROM pgr_bdDijkstra(
+SELECT * FROM pgr_Dijkstra(
   'select id, source, target, cost, reverse_cost from edge_table',
   2, 3, true);
 /* -- q3 */
-SELECT * FROM pgr_bdDijkstra(
+SELECT * FROM pgr_Dijkstra(
   'select id, source, target, cost, reverse_cost from edge_table',
   2, ARRAY[3, 12]);
 /* -- q4 */
-SELECT * FROM pgr_bdDijkstra(
+SELECT * FROM pgr_Dijkstra(
   'select id, source, target, cost, reverse_cost from edge_table',
   ARRAY[2, 7], 12);
 /* -- q5 */
-SELECT * FROM pgr_bdDijkstra(
+SELECT * FROM pgr_Dijkstra(
   'select id, source, target, cost, reverse_cost from edge_table',
   ARRAY[2, 7], ARRAY[3, 12],
   directed => false);
 /* -- q51 */
 SELECT source, target FROM combinations_table;
 /* -- q52 */
-SELECT * FROM pgr_bdDijkstra(
+SELECT * FROM pgr_Dijkstra(
   'SELECT id, source, target, cost, reverse_cost FROM edge_table',
   'SELECT source, target FROM combinations_table',
   false);
 /* -- q6 */
-SELECT * FROM pgr_bdDijkstra(
+SELECT * FROM pgr_Dijkstra(
   'select id, source, target, cost, reverse_cost from edge_table',
   ARRAY[5, 3, 4, 3, 3, 4], ARRAY[3, 5, 3, 4]);
 /* -- q7 */
-SELECT * FROM pgr_bdDijkstra(
+SELECT * FROM pgr_Dijkstra(
   'select id, source, target, cost, reverse_cost from edge_table',
   ARRAY[5, 3, 4], ARRAY[5, 3, 4]);
 /* -- q8 */
-SELECT * FROM pgr_bdDijkstra(
+SELECT * FROM pgr_Dijkstra(
   'SELECT id, source, target, cost, reverse_cost FROM edge_table',
   'SELECT * FROM (VALUES (2, 3), (2, 5), (11, 3)) AS combinations (source, target)');
 /* -- q9 */
