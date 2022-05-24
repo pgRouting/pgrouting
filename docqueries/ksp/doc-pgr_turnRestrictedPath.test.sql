348b176c977146ce521601aff599ec1947084800
cvvergara <vicky@georepublic.de>
2022-05-24 07:42:10 -0500

    [turnRestrictedPath] adjusting to sampledata changes

--- docqueries/ksp/doc-pgr_turnRestrictedPath.test.sql
+++ docqueries/ksp/doc-pgr_turnRestrictedPath.test.sql
@@ -2,29 +2,25 @@
 SELECT * FROM pgr_turnRestrictedPath(
   $$SELECT id, source, target, cost, reverse_cost FROM edge_table$$,
   $$SELECT path, cost FROM restrictions$$,
-  8, 10, 3
-);
+  3, 8, 3);
 /* -- q2 */
 SELECT * FROM pgr_turnRestrictedPath(
   $$SELECT id, source, target, cost, reverse_cost FROM edge_table$$,
   $$SELECT path, cost FROM restrictions$$,
-  8, 10, 3,
-  strict => true
-);
+  3, 8, 3,
+  strict => true);
 /* -- q3 */
 SELECT * FROM pgr_turnRestrictedPath(
   $$SELECT id, source, target, cost, reverse_cost FROM edge_table$$,
   $$SELECT path, cost FROM restrictions$$,
-  8, 10, 3,
-  directed => false
-);
+  3, 8, 3,
+  directed => false);
 /* -- q4 */
 SELECT * FROM pgr_turnRestrictedPath(
   $$SELECT id, source, target, cost, reverse_cost FROM edge_table$$,
   $$SELECT path, cost FROM restrictions$$,
-  8, 10, 3,
+  3, 8, 3,
   directed => false,
   heap_paths => true,
-  stop_on_first => false
-);
+  stop_on_first => false);
 /* -- q5 */
