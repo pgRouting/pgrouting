5a62f761ceeb76f01a7879e8331601a895f0b4e3
cvvergara <vicky@georepublic.de>
2022-05-25 10:58:00 -0500

    [mincut] adjusted to sample data changes

--- docqueries/mincut/doc-stoerWagner.test.sql
+++ docqueries/mincut/doc-stoerWagner.test.sql
@@ -1,31 +1,19 @@
 /* -- q1 */
 SELECT * FROM pgr_stoerWagner(
-    'SELECT id, source, target, cost, reverse_cost 
-        FROM edge_table 
-     WHERE id < 17'
-);
-
+  'SELECT id, source, target, cost, reverse_cost
+  FROM edge_table WHERE id < 17');
 /* -- q2 */
 SELECT * FROM pgr_stoerWagner(
-    'SELECT id, source, target, cost, reverse_cost 
-        FROM edge_table 
-     WHERE id = 18'
-);
-
+  'SELECT id, source, target, cost, reverse_cost
+  FROM edge_table WHERE id = 18');
 /* -- q3 */
 SELECT * FROM pgr_stoerWagner(
-$$
- SELECT id, source, target, cost, reverse_cost FROM edge_table 
-    where source = any (ARRAY(SELECT node FROM pgr_connectedComponents(
-                            'SELECT id, source, target, cost, reverse_cost FROM edge_table ')
-                        WHERE component = 14)
-                       ) 
-                   OR     
-          target = any (ARRAY(SELECT node FROM pgr_connectedComponents(
-                            'SELECT id, source, target, cost, reverse_cost FROM edge_table ')
-                        WHERE component = 14)
-                       )
-$$
- );
-
+  $$
+  SELECT id, source, target, cost, reverse_cost FROM edge_table
+  WHERE source IN (
+      SELECT node FROM pgr_connectedComponents(
+      'SELECT id, source, target, cost, reverse_cost FROM edge_table ')
+      WHERE component = 2)
+  $$
+);
 /* -- q4 */
