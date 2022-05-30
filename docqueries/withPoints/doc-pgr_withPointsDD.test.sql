601728e6b0f125a70d142fbbfb2366228288d1e6
cvvergara <vicky@georepublic.de>
2022-05-30 18:22:50 -0500

    [withPointsDD] adjusted to sample data changes

--- docqueries/withPoints/doc-pgr_withPointsDD.test.sql
+++ docqueries/withPoints/doc-pgr_withPointsDD.test.sql
@@ -1,29 +1,23 @@
 SET extra_float_digits=-3;
-
-/* --q1 */
+/* -- q2 */
 SELECT * FROM pgr_withPointsDD(
-    'SELECT id, source, target, cost, reverse_cost FROM edge_table ORDER BY id',
-    'SELECT pid, edge_id, fraction, side from pointsOfInterest',
-    -1, 3.8);
-/* --q2 */
+  'SELECT id, source, target, cost, reverse_cost FROM edge_table ORDER BY id',
+  'SELECT pid, edge_id, fraction, side from pointsOfInterest',
+  -1, 3.3,
+  driving_side => 'r',
+  details => true);
+/* -- q3 */
 SELECT * FROM pgr_withPointsDD(
-    'SELECT id, source, target, cost, reverse_cost FROM edge_table ORDER BY id',
-    'SELECT pid, edge_id, fraction, side from pointsOfInterest',
-    -1, 3.8,
-    driving_side := 'r',
-    details := true);
-/* --q3 */
+  'SELECT id, source, target, cost, reverse_cost FROM edge_table ORDER BY id',
+  'SELECT pid, edge_id, fraction, side from pointsOfInterest',
+  ARRAY[-1, 16], 3.3,
+  driving_side => 'l',
+  equicost => true);
+/* -- q4 */
 SELECT * FROM pgr_withPointsDD(
-    'SELECT id, source, target, cost, reverse_cost FROM edge_table ORDER BY id',
-    'SELECT pid, edge_id, fraction, side from pointsOfInterest',
-    -1, 3.8,
-    driving_side := 'l',
-    details := true);
-/* --q4 */
-SELECT * FROM pgr_withPointsDD(
-    'SELECT id, source, target, cost, reverse_cost FROM edge_table ORDER BY id',
-    'SELECT pid, edge_id, fraction, side from pointsOfInterest',
-    -1, 3.8,
-    driving_side := 'b',
-    details := true);
-/* --q5 */
+  'SELECT id, source, target, cost, reverse_cost FROM edge_table ORDER BY id',
+  'SELECT pid, edge_id, fraction, side from pointsOfInterest',
+  -1, 3.3,
+  driving_side => 'b',
+  details => true);
+/* -- q5 */
