642679b04334e6a10045fc3b82b6a67e324bcf39
cvvergara <vicky@georepublic.de>
2022-06-01 11:11:34 -0500

    [src][toposort] add missing include

--- include/topologicalSort/pgr_topologicalSort.hpp
+++ include/topologicalSort/pgr_topologicalSort.hpp
@@ -34,6 +34,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 
 #include <iostream>
+#include <string>
 #include <vector>
 #include <algorithm>
 #include <sstream>
--- src/topologicalSort/topologicalSort_driver.cpp
+++ src/topologicalSort/topologicalSort_driver.cpp
@@ -29,6 +29,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #include "drivers/topologicalSort/topologicalSort_driver.h"
 
 #include <sstream>
+#include <string>
 #include <deque>
 #include <vector>
 #include <algorithm>
