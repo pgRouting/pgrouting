80e8f7676ae438e036e3fe53da84b89ef27d6dd9
cvvergara <vicky@georepublic.de>
2022-06-30 11:36:44 -0500

    [maxCarinalityMatch][docqueries] Using new signature * On function example * On how to migrate

--- docqueries/max_flow/doc-pgr_maxCardinalityMatch.test.sql
+++ docqueries/max_flow/doc-pgr_maxCardinalityMatch.test.sql
@@ -1,5 +1,4 @@
 /* -- q2 */
 SELECT * FROM pgr_maxCardinalityMatch(
-  'SELECT id, source, target, cost AS going, reverse_cost AS coming
-  FROM edges');
+  'SELECT id, source, target, cost, reverse_cost FROM edges');
 /* -- q3 */
