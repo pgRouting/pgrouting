d9fae61fff2fb664f4c89ea5930392980f1a55c0
cvvergara <vicky@georepublic.de>
2022-07-05 10:41:07 -0500

    [breadthFirstSearch][docqueries]  porting and adjusting  queries

--- docqueries/breadthFirstSearch/doc-pgr_binaryBreadthFirstSearch.test.sql
+++ docqueries/breadthFirstSearch/doc-pgr_binaryBreadthFirstSearch.test.sql
@@ -1,71 +1,29 @@
-/* --data-start */
-
-DROP TABLE IF EXISTS roadworks CASCADE;
-CREATE table roadworks (
-    id BIGINT not null primary key,
-    source BIGINT,
-    target BIGINT,
-    road_work FLOAT,
-    reverse_road_work FLOAT
-
-);
-
-
-INSERT INTO roadworks(
-  id, source, target, road_work, reverse_road_work) VALUES
-  (1,  1,  2,  0,  0),
-  (2,  2,  3,  -1,  1),
-  (3,  3,  4,  -1,  0),
-  (4,  2,  5,  0,  0),
-  (5,  3,  6,  1,  -1),
-  (6,  7,  8,  1,  1),
-  (7,  8,  5,  0,  0),
-  (8,  5,  6,  1,  1),
-  (9,  6,  9,  1,  1),
-  (10,  5,  10,  1,  1),
-  (11,  6,  11,  1,  -1),
-  (12,  10,  11,  0,  -1),
-  (13,  11,  12,  1,  -1),
-  (14,  10,  13,  1,  1),
-  (15,  9,  12,  0,  0),
-  (16,  4,  9,  0,  0),
-  (17,  14,  15,  0,  0),
-  (18,  16,  17,  0,  0);
-
-/* --data-end */
-
-/*  -- q1 */
+/* -- q1 */
 SELECT * FROM pgr_binaryBreadthFirstSearch(
-    'SELECT id, source, target, road_work as cost, reverse_road_work as reverse_cost FROM roadworks',
-    2, 3
-);
+  'SELECT id, source, target, cost, reverse_cost from edges',
+  6, 10, true);
 /* -- q2 */
 SELECT * FROM pgr_binaryBreadthFirstSearch(
-    'SELECT id, source, target, road_work as cost, reverse_road_work as reverse_cost FROM roadworks',
-    2, 3,
-    FALSE
-);
+  'SELECT id, source, target, cost, reverse_cost from edges',
+  6, ARRAY[10, 17]);
 /* -- q3 */
 SELECT * FROM pgr_binaryBreadthFirstSearch(
-    'SELECT id, source, target, road_work as cost FROM roadworks',
-    2, ARRAY[3,5],
-    FALSE
-);
+  'SELECT id, source, target, cost, reverse_cost from edges',
+  ARRAY[6, 1], 17);
 /* -- q4 */
 SELECT * FROM pgr_binaryBreadthFirstSearch(
-    'SELECT id, source, target, road_work as cost, reverse_road_work as reverse_cost FROM roadworks',
-    ARRAY[2,11], 5
-);
+  'SELECT id, source, target, cost, reverse_cost from edges',
+  ARRAY[6, 1], ARRAY[10, 17],
+  directed => false);
 /* -- q5 */
+SELECT source, target FROM combinations;
+/* -- q51 */
 SELECT * FROM pgr_binaryBreadthFirstSearch(
-    'SELECT id, source, target, road_work as cost, reverse_road_work as reverse_cost FROM roadworks',
-    ARRAY[2,11], ARRAY[3,5],
-    FALSE
-);
+  'SELECT id, source, target, cost, reverse_cost FROM edges',
+  'SELECT source, target FROM combinations',
+  false);
 /* -- q6 */
 SELECT * FROM pgr_binaryBreadthFirstSearch(
-    'SELECT id, source, target, road_work as cost, reverse_road_work as reverse_cost FROM roadworks',
-    'SELECT * FROM ( VALUES (2, 3), (11, 5) ) AS t(source, target)',
-    FALSE
-);
+  'SELECT id, source, target, cost, reverse_cost FROM edges',
+  'SELECT * FROM (VALUES (6, 10), (6, 7), (12, 10)) AS combinations (source, target)');
 /* -- q7 */
--- docqueries/breadthFirstSearch/doc-pgr_breadthFirstSearch.test.sql
+++ docqueries/breadthFirstSearch/doc-pgr_breadthFirstSearch.test.sql
@@ -1,21 +1,16 @@
-/* --q1 */
+/* -- q1 */
 SELECT * FROM pgr_breadthFirstSearch(
-    'SELECT id, source, target, cost, reverse_cost FROM edge_table ORDER BY id',
-    2
-);
-/* --q2 */
+  'SELECT id, source, target, cost, reverse_cost
+  FROM edges ORDER BY id',
+  6);
+/* -- q2 */
 SELECT * FROM pgr_breadthFirstSearch(
-    'SELECT id, source, target, cost, reverse_cost FROM edge_table ORDER BY id',
-    ARRAY[11,12], max_depth := 2
-);
-/* --q3 */
+  'SELECT id, source, target, cost, reverse_cost
+  FROM edges ORDER BY id',
+  ARRAY[12, 6], directed => false, max_depth => 2);
+/* -- q3 */
 SELECT * FROM pgr_breadthFirstSearch(
-    'SELECT id, source, target, cost, reverse_cost FROM edge_table ORDER BY id',
-    ARRAY[11,12], max_depth := 2, directed := false
-);
-/* --q4 */
-SELECT * FROM pgr_breadthFirstSearch(
-    'SELECT id, source, target, cost, reverse_cost FROM edge_table ORDER BY id',
-    -10
-);
-/* --q5 */
+  'SELECT id, source, target, cost, reverse_cost
+  FROM edges ORDER BY id DESC',
+  6);
+/* -- q4 */
