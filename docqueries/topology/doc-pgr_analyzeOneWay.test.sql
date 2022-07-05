525503148a64c9292c25320a77346d6d9ad8712c
cvvergara <vicky@georepublic.de>
2022-07-05 10:44:08 -0500

    [topology][docqueries]  porting and adjusting  queries

--- docqueries/topology/doc-pgr_analyzeOneWay.test.sql
+++ docqueries/topology/doc-pgr_analyzeOneWay.test.sql
@@ -1,5 +1,12 @@
 /* --q1 */
-	SELECT pgr_analyzeOneWay('edge_table',
+ALTER TABLE edges ADD COLUMN dir TEXT;
+SELECT  pgr_createTopology('edges', 0.001, 'geom');
+UPDATE edges SET
+dir = CASE WHEN (cost>0 AND reverse_cost>0) THEN 'B'   -- both ways
+           WHEN (cost>0 AND reverse_cost<0) THEN 'FT'  -- direction of the LINESSTRING
+           WHEN (cost<0 AND reverse_cost>0) THEN 'TF'  -- reverse direction of the LINESTRING
+           ELSE '' END;                                -- unknown
+	SELECT pgr_analyzeOneWay('edges',
         ARRAY['', 'B', 'TF'],
         ARRAY['', 'B', 'FT'],
         ARRAY['', 'B', 'FT'],
--- docqueries/topology/doc-pgr_extractVertices.test.sql
+++ docqueries/topology/doc-pgr_extractVertices.test.sql
@@ -1,54 +1,44 @@
 /* --q1 */
 SELECT  * FROM pgr_extractVertices(
-    'SELECT id, the_geom AS geom
-    FROM edge_table');
+  'SELECT id, geom FROM edges');
 /* --q1.1 */
-
 /* --q2 */
 SELECT  * FROM pgr_extractVertices(
-    'SELECT id, the_geom AS geom FROM edge_table',
-    dryrun := true);
+  'SELECT id, geom FROM edges',
+  dryrun => true);
 /* --q2.1 */
-
 /* --q3 */
 DROP TABLE IF EXISTS vertices_table;
 /* --q3.1 */
-
-UPDATE edge_table
+UPDATE edges
 SET source = NULL, target = NULL,
-   x1 = NULL, y1 = NULL,
-   x2 = NULL, y2 = NULL;
+x1 = NULL, y1 = NULL,
+x2 = NULL, y2 = NULL;
 /* --q3.2 */
-
 SELECT  * INTO vertices_table
-FROM pgr_extractVertices('SELECT id, the_geom AS geom FROM edge_table');
+FROM pgr_extractVertices('SELECT id, geom FROM edges ORDER BY id');
 /* --q3.3 */
-
 SELECT *
 FROM vertices_table;
 /* --q3.4 */
-
 WITH
-    out_going AS (
-        SELECT id AS vid, unnest(out_edges) AS eid, x, y
-        FROM vertices_table
-    )
-UPDATE edge_table
+out_going AS (
+  SELECT id AS vid, unnest(out_edges) AS eid, x, y
+  FROM vertices_table
+)
+UPDATE edges
 SET source = vid, x1 = x, y1 = y
 FROM out_going WHERE id = eid;
 /* --q3.5 */
-
 WITH
-    in_coming AS (
-        SELECT id AS vid, unnest(in_edges) AS eid, x, y
-        FROM vertices_table
-    )
-UPDATE edge_table
+in_coming AS (
+  SELECT id AS vid, unnest(in_edges) AS eid, x, y
+  FROM vertices_table
+)
+UPDATE edges
 SET target = vid, x2 = x, y2 = y
 FROM in_coming WHERE id = eid;
-
 /* --q3.6 */
 SELECT id, source, target, x1, y1, x2, y2
-FROM edge_table;
+FROM edges ORDER BY id;
 /* --q3.7 */
-
