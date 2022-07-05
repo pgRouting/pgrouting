1f4892add4bb7e1aa474bad24064307309e8c5b8
cvvergara <vicky@georepublic.de>
2022-07-05 10:40:27 -0500

    [astar][docqueries]  porting and adjusting  queries

--- docqueries/astar/doc-aStarCost.test.sql
+++ docqueries/astar/doc-aStarCost.test.sql
@@ -1,27 +1,48 @@
-/* -- q1 */
-SELECT * FROM pgr_aStarCost(
-    'SELECT id, source, target, cost, reverse_cost, x1, y1, x2, y2 FROM edge_table',
-    2, 12);
 /* -- q2 */
 SELECT * FROM pgr_aStarCost(
-    'SELECT id, source, target, cost, reverse_cost, x1, y1, x2, y2 FROM edge_table',
-    2, 12,
-    directed := false, heuristic := 2);
+  'SELECT id, source, target, cost, reverse_cost, x1, y1, x2, y2
+  FROM edges',
+  6, 12,
+  directed => true, heuristic => 2);
 /* -- q3 */
 SELECT * FROM pgr_aStarCost(
-    'SELECT id, source, target, cost, reverse_cost, x1, y1, x2, y2 FROM edge_table',
-    2, ARRAY[3, 12], heuristic := 2);
+  'SELECT id, source, target, cost, reverse_cost, x1, y1, x2, y2
+  FROM edges',
+  6, ARRAY[10, 12],
+  heuristic => 3, factor => 3.5);
 /* -- q4 */
 SELECT * FROM pgr_aStarCost(
-    'SELECT id, source, target, cost, reverse_cost, x1, y1, x2, y2 FROM edge_table',
-    ARRAY[7, 2], 12, heuristic := 0);
+  'SELECT id, source, target, cost, reverse_cost, x1, y1, x2, y2
+  FROM edges',
+  ARRAY[6, 8], 10,
+  false, heuristic => 4);
 /* -- q5 */
 SELECT * FROM pgr_aStarCost(
-    'SELECT id, source, target, cost, reverse_cost, x1, y1, x2, y2 FROM edge_table',
-    ARRAY[7, 2], ARRAY[3, 12], heuristic := 2);
-/* -- q6 */
+  'SELECT id, source, target, cost, reverse_cost, x1, y1, x2, y2
+  FROM edges',
+  ARRAY[6, 8], ARRAY[10, 12],
+  factor => 0.5);
+/* -- q51 */
+SELECT * FROM combinations;
+/* -- q52 */
 SELECT * FROM pgr_aStarCost(
-    'SELECT id, source, target, cost, reverse_cost, x1, y1, x2, y2 FROM edge_table',
-    'SELECT * FROM ( VALUES (7, 3), (2, 12) ) AS t(source, target)',
-    heuristic := 2);
+  'SELECT id, source, target, cost, reverse_cost, x1, y1, x2, y2
+  FROM edges',
+  'SELECT * FROM combinations',
+  factor => 0.5);
+/* -- q6 */
+ SELECT * FROM pgr_aStarCost(
+  'SELECT id, source, target, cost, reverse_cost, x1, y1, x2, y2
+  FROM edges',
+  ARRAY[7, 10, 15, 10, 10, 15], ARRAY[10, 7, 10, 15]);
 /* -- q7 */
+SELECT * FROM pgr_aStarCost(
+  'SELECT id, source, target, cost, reverse_cost, x1, y1, x2, y2
+  FROM edges',
+  ARRAY[7, 10, 15], ARRAY[7, 10, 15]);
+/* -- q8 */
+SELECT * FROM pgr_aStarCost(
+  'SELECT id, source, target, cost, reverse_cost, x1, y1, x2, y2
+  FROM edges',
+  'SELECT * FROM (VALUES (6, 10), (6, 7), (12, 10)) AS combinations (source, target)');
+-/* -- q9 */
