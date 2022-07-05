6090e5cd81bb4cf935ff42fb4bf294ebbc31bb6d
cvvergara <vicky@georepublic.de>
2022-07-05 10:37:39 -0500

    [allpairs][docqueries]  porting and adjusting  queries

--- docqueries/allpairs/doc-johnson.test.sql
+++ docqueries/allpairs/doc-johnson.test.sql
@@ -1,12 +1,6 @@
 /* -- q1 */
 SELECT * FROM pgr_johnson(
-    'SELECT source, target, cost FROM edge_table WHERE id < 5
-         ORDER BY id'
-);
+  'SELECT source, target, cost FROM edges
+  WHERE id < 5'
+) ORDER BY start_vid, end_vid;
 /* -- q2 */
-SELECT * FROM pgr_johnson(
-    'SELECT source, target, cost FROM edge_table WHERE id < 5
-         ORDER BY id',
-    false
-);
-/* -- q3 */
