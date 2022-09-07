55ad1bbc6f13c56523cb55bd4507ae3e22d36ae7
ntishchauhan0022 <nitishchauhan0022@gmail.com>
2022-09-07 15:13:51 +0530

    [C/C++][hawickCircuits] adding code for pgr_hawickCircuits

---
+++ include/circuits/hawickcircuits.hpp
@@ -0,0 +1,155 @@
+/*PGR-GNU*****************************************************************
+
+Copyright (c) 2022 pgRouting developers
+Mail: project@pgrouting.org
+
+Copyright (c) 2022 Nitish Chauhan
+Mail: nitishchauhan0022 at gmail.com
+
+------
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ ********************************************************************PGR-GNU*/
+
+#ifndef INCLUDE_CIRCUITS_HAWICKCIRCUITS_HPP_
+#define INCLUDE_CIRCUITS_HAWICKCIRCUITS_HPP_
+#pragma once
+
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/graph_traits.hpp>
+#include <boost/graph/hawick_circuits.hpp>
+
+#include <iostream>
+#include <iterator>
+#include <deque>
+
+#include "cpp_common/basePath_SSEC.hpp"
+#include "cpp_common/pgr_base_graph.hpp"
+#include "cpp_common/interruption.h"
+#include "cpp_common/pgr_assert.h"
+#include "c_types/circuits_rt.h"
+
+/** @file hawickcirucits.hpp
+ * @brief The main file which calls the respective boost function.
+ *
+ * Contains actual implementation of the function and the calling
+ * of the respective boost function.
+ */
+
+namespace pgrouting {
+namespace functions {
+
+template <typename G, typename E>
+class circuit_detector{
+ public:
+    /** @brief circuit detector to get the results
+      *
+      *
+      * @param graph     the graph containing the edges
+      * @param result    container for storing the result
+      *
+      */
+    circuit_detector(
+        G &graph,
+        std::deque<circuits_rt> &data) :
+    m_graph(graph),
+    m_data(data) {}
+    template <typename P, typename Gr>
+    void cycle(P const &p, Gr const& Grap) {
+        if (p.empty()) {
+        return;
+        }
+        int seq = 0;
+        typename P::const_iterator i, before_end = boost::prior(p.end());
+        auto start_vid = m_graph[*p.begin()].id;
+        auto end_vid = start_vid;
+        double agg_cost = 0;
+        E edge;
+        for (i = p.begin(); i != before_end; ++i, ++seq) {
+            auto node = m_graph[*i].id;
+            edge = boost::edge(*i, *(i+1), Grap).first;
+            auto cost = m_graph[edge].cost;
+            auto id = m_graph[edge].id;
+            m_data.push_back({circuit_No, seq, start_vid, end_vid, node, id, cost, agg_cost});
+            agg_cost = agg_cost + cost;
+            }
+            auto node = m_graph[*i].id;
+            edge = boost::edge(*(boost::prior(p.end())), *(p.begin()), Grap).first;
+            auto cost = m_graph[edge].cost;
+            auto id = m_graph[edge].id;
+            m_data.push_back({circuit_No, seq, start_vid, end_vid, node, id, cost, agg_cost});
+            agg_cost = agg_cost + cost;
+            ++seq;
+            m_data.push_back({circuit_No, seq, start_vid, end_vid, start_vid, -1, 0, agg_cost});
+            circuit_No++;
+        }
+
+ private:
+    G &m_graph;
+    std::deque<circuits_rt> &m_data;
+    int circuit_No = 1;
+};
+
+template <class G>
+class pgr_hawickCircuits{
+ public:
+      typedef typename G::E E;
+
+    /** @name hawickcircuit
+      * @{
+      *
+      */
+
+     /** @brief hawickcircuit function
+      *
+      * It does all the processing and returns the results.
+      *
+      * @param graph  the graph containing the edges
+      *
+      * @returns results, when results are found
+      *
+      * @see [boost::hawickcircuit]
+      * (https://www.boost.org/libs/graph/doc/hawick_circuits.html)
+      */
+
+      std::deque<circuits_rt> hawickCircuits(G & graph) {
+      // results storing the output
+      std::deque<circuits_rt> results;
+
+      // a circuit detector to provide the mechanism to store the circuit
+      circuit_detector <G, E> detector(graph, results);
+
+      /* abort in case of an interruption occurs (e.g. the query is being cancelled) */
+        CHECK_FOR_INTERRUPTS();
+         try {
+             boost::hawick_unique_circuits(graph.graph, detector);
+         } catch (boost::exception const& ex) {
+             (void)ex;
+             throw;
+         } catch (std::exception &e) {
+             (void)e;
+             throw;
+         } catch (...) {
+             throw;
+         }
+    return results;
+    //@}
+}
+};
+}  // namespace functions
+}  // namespace pgrouting
+
+#endif  // INCLUDE_CIRCUITS_HAWICKCIRCUITS_HPP_
---
+++ src/circuits/hawickCircuits_driver.cpp
@@ -0,0 +1,167 @@
+/*PGR-GNU*****************************************************************
+File: hawickCircuits_driver.cpp
+
+Generated with Template by:
+Copyright (c) 2022 pgRouting developers
+Mail: project@pgrouting.org
+
+Function's developer:
+Copyright (c) 2022 Nitish Chauhan
+Mail: nitishchauhan0022 at gmail.com
+
+------
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ ********************************************************************PGR-GNU*/
+
+
+
+#include <sstream>
+#include <deque>
+#include <algorithm>
+#include <string>
+
+#include "cpp_common/pgr_alloc.hpp"
+#include "cpp_common/pgr_assert.h"
+
+#include "drivers/circuits/hawickcircuits_driver.h"
+#include "circuits/hawickcircuits.hpp"
+
+/** @file hawickCircuits_driver.cpp
+ * @brief Handles actual calling of function in the `hawickcircuits.hpp` file.
+ *
+ */
+
+/***********************************************************************
+ *
+ *   pgr_hawickcircuits(edges_sql TEXT);
+ *
+ ***********************************************************************/
+
+/** @brief Calls the main function defined in the C++ Header file.
+ *
+ * @param graph      the graph containing the edges
+ * @param log        stores the log message
+ *
+ * @returns results, when results are found
+ */
+
+template < class G >
+std::deque <circuits_rt>
+pgr_hawickCircuits(G &graph) {
+    pgrouting::functions::pgr_hawickCircuits < G > fn_hawickCircuits;
+    auto results = fn_hawickCircuits.hawickCircuits(graph);
+    return results;
+}
+
+/** @brief Performs exception handling and converts the results to postgres.
+ *
+ * @pre log_msg is empty
+ * @pre notice_msg is empty
+ * @pre err_msg is empty
+ * @pre return_tuples is empty
+ * @pre return_count is 0
+ *
+ * It builds the undirected graph using the `data_edges` variable.
+ * Then, it passes the required variables to the template function
+ * `cuthillMckeeOrdering` which calls the main function
+ * defined in the C++ Header file. It also does exception handling.
+ *
+ * @param data_edges     the set of edges from the SQL query
+ * @param total_edges    the total number of edges in the SQL query
+ * @param return_tuples  the rows in the result
+ * @param return_count   the count of rows in the result
+ * @param log_msg        stores the log message
+ * @param notice_msg     stores the notice message
+ * @param err_msg        stores the error message
+ *
+ * @returns void
+ */
+
+void
+do_hawickCircuits(
+        Edge_t  *data_edges,
+        size_t total_edges,
+
+        circuits_rt **return_tuples,
+        size_t *return_count,
+
+        char ** log_msg,
+        char ** notice_msg,
+        char ** err_msg) {
+    std::ostringstream log;
+    std::ostringstream err;
+    std::ostringstream notice;
+    try {
+        pgassert(!(*log_msg));
+        pgassert(total_edges != 0);
+        pgassert(!(*notice_msg));
+        pgassert(!(*err_msg));
+        pgassert(!(*return_tuples));
+        pgassert(*return_count == 0);
+
+        std::deque < circuits_rt > results;
+
+        graphType gType = DIRECTED;
+        pgrouting::DirectedGraph digraph(gType);
+
+        digraph.insert_edges(data_edges, total_edges);
+
+        results = pgr_hawickCircuits(digraph);
+
+        auto count = results.size();
+
+        if (count == 0) {
+            (*return_tuples) = NULL;
+            (*return_count) = 0;
+            notice << "No Circuit found";
+            *log_msg = pgr_msg(notice.str().c_str());
+            return;
+        }
+
+        (*return_tuples) = pgr_alloc(count, (*return_tuples));
+        for (size_t i = 0; i < count; i++) {
+            *((*return_tuples) + i) = results[i];
+        }
+        (*return_count) = count;
+
+        pgassert(*err_msg == NULL);
+        *log_msg = log.str().empty()?
+            *log_msg :
+            pgr_msg(log.str().c_str());
+        *notice_msg = notice.str().empty()?
+            *notice_msg :
+            pgr_msg(notice.str().c_str());
+    } catch (AssertFailedException &except) {
+        (*return_tuples) = pgr_free(*return_tuples);
+        (*return_count) = 0;
+        err << except.what();
+        *err_msg = pgr_msg(err.str().c_str());
+        *log_msg = pgr_msg(log.str().c_str());
+    } catch (std::exception &except) {
+        (*return_tuples) = pgr_free(*return_tuples);
+        (*return_count) = 0;
+        err << except.what();
+        *err_msg = pgr_msg(err.str().c_str());
+        *log_msg = pgr_msg(log.str().c_str());
+    } catch(...) {
+        (*return_tuples) = pgr_free(*return_tuples);
+        (*return_count) = 0;
+        err << "Caught unknown exception!";
+        *err_msg = pgr_msg(err.str().c_str());
+        *log_msg = pgr_msg(log.str().c_str());
+    }
+}
