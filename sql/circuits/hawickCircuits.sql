a3edcfa69828753fce86a730f1a1903331488cc4
ntishchauhan0022 <nitishchauhan0022@gmail.com>
2022-09-07 15:17:56 +0530

    [sql][hawickCircuits] adding SQL files for pgr_hawickcircuits

---
+++ sql/circuits/hawickCircuits.sql
@@ -0,0 +1,63 @@
+/*PGR-GNU*****************************************************************
+File: hawickCircuits.sql
+
+Generated with Template by:
+Copyright (c) 2022 pgRouting developers
+Mail: project@pgrouting.org
+
+Function's developer:
+Copyright (c) 2022 Nitish Chauhan
+Mail: nitishchauhan0022 at gmail.com
+
+------
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ ********************************************************************PGR-GNU*/
+---------------
+-- pgr_hawickCircuits
+---------------
+
+CREATE FUNCTION pgr_hawickCircuits(
+    TEXT, -- edges_sql (required)
+
+    OUT seq INTEGER,
+    OUT path_id INTEGER,
+    OUT path_seq INTEGER,
+    OUT start_vid BIGINT,
+    OUT end_vid BIGINT,
+    OUT node BIGINT,
+    OUT edge BIGINT,
+    OUT cost FLOAT,
+    OUT agg_cost FLOAT)
+
+RETURNS SETOF RECORD AS
+$BODY$
+BEGIN
+    RETURN QUERY
+    SELECT *
+    FROM _pgr_hawickCircuits(_pgr_get_statement($1));
+END;
+$BODY$
+LANGUAGE plpgsql VOLATILE STRICT;
+
+COMMENT ON FUNCTION pgr_hawickCircuits(TEXT)
+IS 'pgr_hawickCircuits
+- EXPERIMENTAL
+- Parameters:
+    - Edges SQL with columns: id, source, target, cost [,reverse_cost]
+- Documentation:
+    - ${PROJECT_DOC_LINK}/hawickCircuits.html
+';
