1e6ae61a23f4640de5052700dc62a4a9b6b5789a
vicky <vicky@georepublic.de>
2022-11-15 11:42:49 +0900

    [docqueries/lineGraph] [license][Incubation] Adding license

--- docqueries/lineGraph/doc-pgr_lineGraph.test.sql
+++ docqueries/lineGraph/doc-pgr_lineGraph.test.sql
@@ -1,3 +1,4 @@
+-- Creative Commons Attribution-Share Alike 3.0 License : https://creativecommons.org/licenses/by-sa/3.0/
 /* -- q1 */
 SELECT * FROM pgr_lineGraph(
   'SELECT id, source, target, cost, reverse_cost FROM edges'
