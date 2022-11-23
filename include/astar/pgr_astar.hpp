c2b63b6d97285f78c03d93b40f068e037fa47372
cvvergara <vicky@georepublic.de>
2022-11-23 13:37:28 -0600

    [astar] fixing lint errors from new linter

--- include/astar/pgr_astar.hpp
+++ include/astar/pgr_astar.hpp
@@ -28,11 +28,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #define INCLUDE_ASTAR_PGR_ASTAR_HPP_
 #pragma once
 
-#include <boost/config.hpp>
-#include <boost/graph/graph_traits.hpp>
-#include <boost/graph/adjacency_list.hpp>
-#include <boost/graph/astar_search.hpp>
-
 #include <cmath>
 #include <deque>
 #include <limits>
@@ -40,6 +35,11 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #include <vector>
 #include <set>
 #include <map>
+
+#include <boost/config.hpp>
+#include <boost/graph/graph_traits.hpp>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/astar_search.hpp>
 
 #include "cpp_common/basePath_SSEC.hpp"
 #include "cpp_common/pgr_base_graph.hpp"
