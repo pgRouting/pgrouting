31770544ff6b5dccd95119f41bb814457807a65d
cvvergara <vicky@georepublic.de>
2022-11-23 13:37:28 -0600

    [bellman_ford] fixing lint errors from new linter

--- include/bellman_ford/pgr_bellman_ford.hpp
+++ include/bellman_ford/pgr_bellman_ford.hpp
@@ -28,9 +28,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #define INCLUDE_BELLMAN_FORD_PGR_BELLMAN_FORD_HPP_
 #pragma once
 
-#include <boost/config.hpp>
-#include <boost/graph/adjacency_list.hpp>
-#include <boost/graph/bellman_ford_shortest_paths.hpp>
 #include <deque>
 #include <set>
 #include <vector>
@@ -40,6 +37,9 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #include <functional>
 #include <limits>
 #include <map>
+#include <boost/config.hpp>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/bellman_ford_shortest_paths.hpp>
 #include "cpp_common/pgr_messages.h"
 #include "cpp_common/basePath_SSEC.hpp"
 #include "cpp_common/pgr_base_graph.hpp"
