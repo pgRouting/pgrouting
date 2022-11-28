de1f9059ef3fd1620c25e9aadd3434eb3f5d9d9e
cvvergara <vicky@georepublic.de>
2022-11-28 11:03:36 -0600

    [docqueries][tsp] do not use deprecated tsp on examples of cost matrix

--- docqueries/astar/doc-aStarCostMatrix.test.sql
+++ docqueries/astar/doc-aStarCostMatrix.test.sql
@@ -12,7 +12,5 @@ SELECT * FROM pgr_TSP(
     'SELECT id, source, target, cost, reverse_cost, x1, y1, x2, y2 FROM edges',
     (SELECT array_agg(id) FROM vertices WHERE id IN (5, 6, 10, 15)),
     directed=> false, heuristic => 2)
-  $$,
-  randomize => false
-);
+  $$);
 /* -- q4 */
