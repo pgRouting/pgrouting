33162a8a6aa788471f1e3f3ba30658a4c2f05e1b
Regina Obe <lr@pcorp.us>
2023-04-01 18:20:27 -0400

    Degree refinements 1. Show example using a prepped edges table 2. More discussion of why we can skip degree in some cases

--- docqueries/topology/degree.test.sql
+++ docqueries/topology/degree.test.sql
@@ -1,10 +1,13 @@
 -- CopyRight(c) pgRouting developers
 -- Creative Commons Attribution-Share Alike 3.0 License : https://creativecommons.org/licenses/by-sa/3.0/
 /* -- q1 */
+CREATE TEMP TABLE tmp_edges_vertices_pgr IF NOT EXISTS AS
+SELECT id, in_edges, out_edges
+    FROM pgr_extractVertices('SELECT id, geom FROM edges');
 SELECT * FROM pgr_degree(
   $$SELECT id FROM edges$$,
   $$SELECT id, in_edges, out_edges
-    FROM pgr_extractVertices('SELECT id, geom FROM edges')$$);
+    FROM tmp_edges_vertices_pgr$$);
 /* -- q2 */
 SELECT * FROM pgr_degree(
   $$SELECT id FROM edges WHERE id < 17$$,
