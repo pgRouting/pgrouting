2107e30f94468e2d20ca09dfd7a43054b3b59268
Regina Obe <lr@pcorp.us>
2023-04-19 13:12:48 -0400

    Missed ;

--- docqueries/topology/degree.test.sql
+++ docqueries/topology/degree.test.sql
@@ -1,7 +1,7 @@
 -- CopyRight(c) pgRouting developers
 -- Creative Commons Attribution-Share Alike 3.0 License : https://creativecommons.org/licenses/by-sa/3.0/
 /* -- q1 */
-DROP TABLE IF EXISTS tmp_edges_vertices_pgr
+DROP TABLE IF EXISTS tmp_edges_vertices_pgr;
 CREATE TEMP TABLE tmp_edges_vertices_pgr AS
 SELECT id, in_edges, out_edges
     FROM pgr_extractVertices('SELECT id, geom FROM edges');
