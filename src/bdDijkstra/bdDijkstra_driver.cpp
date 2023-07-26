5ddb725f5edfed08133623d91719dcec32a02285
cvvergara <vicky@erosion.dev>
2023-07-26 09:02:33 -0600

    [bdDijstra] cleanup

--- src/bdDijkstra/bdDijkstra_driver.cpp
+++ src/bdDijkstra/bdDijkstra_driver.cpp
@@ -34,14 +34,13 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #include <vector>
 #include <algorithm>
 
-#include "bdDijkstra/pgr_bdDijkstra.hpp"
-
 
 #include "cpp_common/combinations.h"
 #include "cpp_common/pggetdata.hpp"
 #include "cpp_common/pgr_alloc.hpp"
 #include "cpp_common/pgr_assert.h"
 #include "cpp_common/pgr_base_graph.hpp"
+#include "bdDijkstra/pgr_bdDijkstra.hpp"
 
 #include "c_types/ii_t_rt.h"
 
