efb4c17a11420c80fe83c60d92ba25ee0366f952
cvvergara <vicky@erosion.dev>
2023-07-27 13:30:45 -0600

    TSP cleanup

--- src/tsp/TSP.c
+++ src/tsp/TSP.c
@@ -58,23 +58,6 @@ process(
     char* notice_msg = NULL;
     char* err_msg = NULL;
 
-#if 0
-    IID_t_rt *distances = NULL;
-    size_t total_distances = 0;
-    pgr_get_matrixRows(matrix_sql, &distances, &total_distances, &err_msg);
-    throw_error(err_msg, matrix_sql);
-    if (total_distances == 0) {
-        ereport(WARNING,
-                (errmsg("Insufficient data found on inner query."),
-                 errhint("%s", matrix_sql)));
-        (*result_count) = 0;
-        (*result_tuples) = NULL;
-        pgr_SPI_finish();
-        return;
-    }
-#endif
-
-
     clock_t start_t = clock();
     pgr_do_tsp(
             matrix_sql,
@@ -100,9 +83,6 @@ process(
     if (log_msg) pfree(log_msg);
     if (notice_msg) pfree(notice_msg);
     if (err_msg) pfree(err_msg);
-#if 0
-    if (distances) pfree(distances);
-#endif
 
     pgr_SPI_finish();
 }
