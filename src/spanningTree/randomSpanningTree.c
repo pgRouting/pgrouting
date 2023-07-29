6ed1461810b48592c8247682810ceebfeeec2ef1
cvvergara <vicky@erosion.dev>
2023-07-29 11:22:15 -0600

    [spanningTree] pgdata_getters.h only needed for reading arrays
    Removing from files that do not have arrays
    Keeping in old code of trsp because the read of edges query is on C

--- src/spanningTree/randomSpanningTree.c
+++ src/spanningTree/randomSpanningTree.c
@@ -33,7 +33,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #include "c_common/e_report.h"
 #include "c_common/time_msg.h"
 
-#include "c_common/pgdata_getters.h"
 
 #include "drivers/spanningTree/randomSpanningTree_driver.h"
 
