785c79871cca60b7ea84bd9d8f51c108f25e7a9b
cvvergara <vicky@erosion.dev>
2023-07-29 11:22:15 -0600

    [allpairs] pgdata_getters.h only needed for reading arrays
    Removing from files that do not have arrays
    Keeping in old code of trsp because the read of edges query is on C

---
+++ include/allpairs/process_allpairs.h
@@ -0,0 +1,49 @@
+/*PGR-GNU*****************************************************************
+File: process_allpairs.h
+
+Function's developer:
+Copyright (c) 2023 Celia Virginia Vergara Castillo
+Mail: vicky_vergara@hotmail.com
+
+------
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ ********************************************************************PGR-GNU*/
+
+#ifndef INCLUDE_ALLPAIRS_PROCESS_ALLPAIRS_H_
+#define INCLUDE_ALLPAIRS_PROCESS_ALLPAIRS_H_
+#pragma once
+
+#ifdef __cplusplus
+#include <cstddef>
+using IID_t_rt = struct IID_t_rt;
+#else
+#include <stddef.h>
+#include <stdbool.h>
+typedef struct IID_t_rt IID_t_rt;
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void process_allpairs(char*, bool, int, IID_t_rt **, size_t *);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // INCLUDE_ALLPAIRS_PROCESS_ALLPAIRS_H_
