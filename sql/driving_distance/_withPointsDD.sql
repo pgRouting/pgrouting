e630b66f5f0d04df83681414fb92e93c81bdce39
Squarege <1263174378@qq.com>
2023-08-17 14:56:24 +0800

    [withPointsDD][sql] Changing signature and standardizing results
    * Changing driving side from optional to compulsory param
    * Including start_vid and depth in the output of single vertex overload
    * Including depth in the output of multiple vertices overload
    * Updating v3.6 signature file
    * Passing update test

--- sql/driving_distance/_withPointsDD.sql
+++ sql/driving_distance/_withPointsDD.sql
@@ -1,8 +1,16 @@
 /*PGR-GNU*****************************************************************
 File: _withPointsDD.sql
 
+Generated with Template by:
+Copyright (c) 2015 pgRouting developers
+Mail: project at pgrouting.org
+
+Function's developer:
 Copyright (c) 2015 Celia Virginia Vergara Castillo
-Mail: vicky_vergara@hotmail.com
+Mail: vicky at erosion.dev
+
+Copyright (c) 2023 Yige Huang
+Mail: square1ge at gmail.com
 
 ------
 
@@ -26,6 +34,35 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 -- pgr_withPointsDD
 -------------------
 
+--v3.6
+CREATE FUNCTION _pgr_v4withPointsDD(
+    edges_sql TEXT,
+    points_sql TEXT,
+    start_pid ANYARRAY,
+    distance FLOAT,
+    driving_side CHAR,
+
+    directed BOOLEAN DEFAULT true,
+    details BOOLEAN DEFAULT false,
+    equicost BOOLEAN DEFAULT false,
+
+    OUT seq BIGINT,
+    OUT depth BIGINT,
+    OUT start_vid BIGINT,
+    OUT node BIGINT,
+    OUT edge BIGINT,
+    OUT cost FLOAT,
+    OUT agg_cost FLOAT)
+RETURNS SETOF RECORD AS
+'MODULE_PATHNAME'
+LANGUAGE C VOLATILE STRICT;
+
+-- COMMENTS
+
+COMMENT ON FUNCTION _pgr_v4withPointsDD(TEXT, TEXT, ANYARRAY, FLOAT, CHAR, BOOLEAN, BOOLEAN, BOOLEAN)
+IS 'pgRouting internal function';
+
+/* TODO remove on v4 */
 --v3.0
 CREATE FUNCTION _pgr_withPointsDD(
     edges_sql TEXT,
@@ -51,4 +88,4 @@ LANGUAGE C VOLATILE STRICT;
 -- COMMENTS
 
 COMMENT ON FUNCTION _pgr_withPointsDD(TEXT, TEXT, ANYARRAY, FLOAT, BOOLEAN, CHAR, BOOLEAN, BOOLEAN)
-IS 'pgRouting internal function';
+IS 'pgRouting deprecated function';
--- sql/scripts/build-extension-update-files.pl
+++ sql/scripts/build-extension-update-files.pl
@@ -239,6 +239,8 @@ sub generate_upgrade_script {
             push @commands, drop_special_case_function("pgr_bdastar(text,bigint,bigint,boolean,integer,numeric,numeric)");
             push @commands, drop_special_case_function("pgr_bdastar(text,bigint,anyarray,boolean,integer,numeric,numeric)");
             push @commands, drop_special_case_function("pgr_bdastar(text,anyarray,bigint,boolean,integer,numeric,numeric)");
+            push @commands, drop_special_case_function("pgr_withpointsdd(text,text,anyarray,double precision,boolean,character,boolean,boolean)");
+            push @commands, drop_special_case_function("pgr_withpointsdd(text,text,bigint,double precision,boolean,character,boolean)");
 
         }
 
