89cecda061e992a107e449a43d8149f3bbb5fdcd
Squarege <1263174378@qq.com>
2023-08-17 14:56:24 +0800

    [withPointsDD][sql] Changing signature and standardizing results
    * Changing driving side from optional to compulsory param
    * Including start_vid and depth in the output of single vertex overload
    * Including depth in the output of multiple vertices overload
    * Updating v3.6 signature file
    * Passing update test

--- sql/driving_distance/withPointsDD.sql
+++ sql/driving_distance/withPointsDD.sql
@@ -1,8 +1,13 @@
 /*PGR-GNU*****************************************************************
 File: withPointsDD.sql
 
+Generated with Template by:
+Copyright (c) 2015 pgRouting developers
+Mail: project at pgrouting.org
+
+Function's developer:
 Copyright (c) 2015 Celia Virginia Vergara Castillo
-Mail: vicky_vergara@hotmail.com
+Mail: vicky at erosion.dev
 
 ------
 
@@ -23,6 +28,101 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  ********************************************************************PGR-GNU*/
 
 -- SINGLE
+--v3.6
+CREATE FUNCTION pgr_withPointsDD(
+    TEXT,   --edges_sql (required)
+    TEXT,   -- points_sql (required)
+    BIGINT, -- from_vid (required)
+    FLOAT,  -- distance (required)
+    CHAR,   -- driving_side (required)
+
+    directed BOOLEAN DEFAULT true,
+    details BOOLEAN DEFAULT false,
+
+    OUT seq BIGINT,
+    OUT depth BIGINT,
+    OUT start_vid BIGINT,
+    OUT node BIGINT,
+    OUT edge BIGINT,
+    OUT cost FLOAT,
+    OUT agg_cost FLOAT)
+RETURNS SETOF RECORD AS
+$BODY$
+    SELECT *
+    FROM _pgr_v4withPointsDD(_pgr_get_statement($1), _pgr_get_statement($2), ARRAY[$3]::BIGINT[], $4, $5, $6, $7, false);
+$BODY$
+LANGUAGE SQL VOLATILE STRICT
+COST 100
+ROWS 1000;
+
+-- MULTIPLE
+--v3.6
+CREATE FUNCTION pgr_withPointsDD(
+    TEXT,     --edges_sql (required)
+    TEXT,     -- points_sql (required)
+    ANYARRAY, -- from_vid (required)
+    FLOAT,    -- distance (required)
+    CHAR,     -- driving_side (required)
+
+    directed BOOLEAN DEFAULT true,
+    details BOOLEAN DEFAULT false,
+    equicost BOOLEAN DEFAULT false,
+
+    OUT seq BIGINT,
+    OUT depth BIGINT,
+    OUT start_vid BIGINT,
+    OUT node BIGINT,
+    OUT edge BIGINT,
+    OUT cost FLOAT,
+    OUT agg_cost FLOAT)
+RETURNS SETOF RECORD AS
+$BODY$
+    SELECT *
+    FROM _pgr_v4withPointsDD(_pgr_get_statement($1), _pgr_get_statement($2), $3, $4, $5, $6, $7, $8);
+$BODY$
+LANGUAGE SQL VOLATILE STRICT
+COST 100
+ROWS 1000;
+
+
+-- COMMENTS
+
+COMMENT ON FUNCTION pgr_withPointsDD(TEXT, TEXT, BIGINT, FLOAT, CHAR, BOOLEAN, BOOLEAN)
+IS 'pgr_withPointsDD(Single Vertex)
+- PROPOSED
+- Parameters:
+    - Edges SQL with columns: id, source, target, cost [,reverse_cost]
+    - Points SQL with columns: [pid], edge_id, fraction[,side]
+    - From vertex identifier
+    - Distance
+    - Driving_side
+- Optional Parameters
+    - directed := true
+    - details := false
+- Documentation:
+    - ${PROJECT_DOC_LINK}/pgr_withPointsDD.html
+';
+
+
+COMMENT ON FUNCTION pgr_withPointsDD(TEXT, TEXT, ANYARRAY, FLOAT, CHAR, BOOLEAN, BOOLEAN, BOOLEAN)
+IS 'pgr_withPointsDD(Multiple Vertices)
+- PROPOSED
+- Parameters:
+    - Edges SQL with columns: id, source, target, cost [,reverse_cost]
+    - Points SQL with columns: [pid], edge_id, fraction[,side]
+    - From ARRAY[vertices identifiers]
+    - Distance
+    - Driving_side
+- Optional Parameters
+    - directed := true
+    - details := false
+    - equicost := false
+- Documentation:
+    - ${PROJECT_DOC_LINK}/pgr_withPointsDD.html
+';
+
+/* TODO remove on V4 */
+-- SINGLE
 --v2.6
 CREATE FUNCTION pgr_withPointsDD(
     TEXT,   --edges_sql (required)
@@ -41,10 +141,14 @@ CREATE FUNCTION pgr_withPointsDD(
     OUT agg_cost FLOAT)
 RETURNS SETOF RECORD AS
 $BODY$
-    SELECT seq, node, edge, cost, agg_cost
-    FROM _pgr_withPointsDD(_pgr_get_statement($1), _pgr_get_statement($2), ARRAY[$3]::BIGINT[], $4, $5, $6, $7, false);
+BEGIN
+    RAISE WARNING 'pgr_withpointsdd(text,text,bigint,double precision,boolean,character,boolean) is been deprecated';
+    RETURN QUERY
+    SELECT a.seq, a.node, a.edge, a.cost, a.agg_cost
+    FROM _pgr_withPointsDD(_pgr_get_statement($1), _pgr_get_statement($2), ARRAY[$3]::BIGINT[], $4, $5, $6, $7, false) AS a;
+END;
 $BODY$
-LANGUAGE SQL VOLATILE STRICT
+LANGUAGE plpgsql VOLATILE STRICT
 COST 100
 ROWS 1000;
 
@@ -69,10 +173,14 @@ CREATE FUNCTION pgr_withPointsDD(
     OUT agg_cost FLOAT)
 RETURNS SETOF RECORD AS
 $BODY$
+BEGIN
+  RAISE WARNING 'pgr_withpointsdd(text,text,anyarray,double precision,boolean,character,boolean,boolean) is been deprecated';
+  RETURN QUERY
     SELECT *
     FROM _pgr_withPointsDD(_pgr_get_statement($1), _pgr_get_statement($2), $3, $4, $5, $6, $7, $8);
+END;
 $BODY$
-LANGUAGE SQL VOLATILE STRICT
+LANGUAGE plpgsql VOLATILE STRICT
 COST 100
 ROWS 1000;
 
@@ -80,35 +188,8 @@ ROWS 1000;
 -- COMMENTS
 
 COMMENT ON FUNCTION pgr_withPointsDD(TEXT, TEXT, BIGINT, FLOAT, BOOLEAN, CHAR, BOOLEAN)
-IS 'pgr_withPointsDD(Single Vertex)
-- PROPOSED
-- Parameters:
-    - Edges SQL with columns: id, source, target, cost [,reverse_cost]
-    - Points SQL with columns: [pid], edge_id, fraction[,side]
-    - From vertex identifier
-    - Distance
-- Optional Parameters
-    - directed := true
-    - driving_side := b
-    - details := false
-- Documentation:
-    - ${PROJECT_DOC_LINK}/pgr_withPointsDD.html
-';
+IS 'pgRouting deprecated function';
 
 
 COMMENT ON FUNCTION pgr_withPointsDD(TEXT, TEXT, ANYARRAY, FLOAT, BOOLEAN, CHAR, BOOLEAN, BOOLEAN)
-IS 'pgr_withPointsDD(Multiple Vertices)
-- PROPOSED
-- Parameters:
-    - Edges SQL with columns: id, source, target, cost [,reverse_cost]
-    - Points SQL with columns: [pid], edge_id, fraction[,side]
-    - From ARRAY[vertices identifiers]
-    - Distance
-- Optional Parameters
-    - directed := true
-    - driving_side := b
-    - details := false
-    - equicost := false
-- Documentation:
-    - ${PROJECT_DOC_LINK}/pgr_withPointsDD.html
-';
+IS 'pgRouting deprecated function';
