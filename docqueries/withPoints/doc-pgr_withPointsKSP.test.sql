0a51a372226d461acc36a8b98bdd28ca376753dd
Abhinav Jain <this.abhinav@gmail.com>
2023-08-21 03:10:07 +0530

    [withPointsKSP][docqueries] Updating for withPointsKSP and migration docs

--- docqueries/withPoints/doc-pgr_withPointsKSP.test.sql
+++ docqueries/withPoints/doc-pgr_withPointsKSP.test.sql
@@ -5,20 +5,30 @@ SET extra_float_digits=-3;
 SELECT * FROM pgr_withPointsKSP(
     'SELECT id, source, target, cost, reverse_cost FROM edges ORDER BY id',
     'SELECT pid, edge_id, fraction, side from pointsOfInterest',
-    -1, -2, 2);
+    -1, -2, 2, 'l');
 /* --q2 */
 SELECT * FROM pgr_withPointsKSP(
-    'SELECT id, source, target, cost, reverse_cost FROM edges ORDER BY id',
-    'SELECT pid, edge_id, fraction, side from pointsOfInterest',
-    -1, -2, 2,
-    driving_side := 'l', details := true);
+  'SELECT id, source, target, cost, reverse_cost FROM edges ORDER BY id',
+  'SELECT pid, edge_id, fraction, side from pointsOfInterest',
+  -1, ARRAY[-3, 7], 2, 'B',
+  directed => false);
 /* --q3 */
 SELECT * FROM pgr_withPointsKSP(
-    'SELECT id, source, target, cost, reverse_cost FROM edges ORDER BY id',
-    'SELECT pid, edge_id, fraction, side from pointsOfInterest',
-    -1, -2, 2,
-    heap_paths := true, driving_side := 'r', details := true);
+  'SELECT id, source, target, cost, reverse_cost FROM edges ORDER BY id',
+  'SELECT pid, edge_id, fraction, side from pointsOfInterest',
+  ARRAY[-1, 6], -3, 1,'r', details:= true);
 /* --q4 */
+SELECT * FROM pgr_withPointsKSP(
+  'SELECT id, source, target, cost, reverse_cost FROM edges ORDER BY id',
+  'SELECT pid, edge_id, fraction, side from pointsOfInterest',
+  ARRAY[-1, 6], ARRAY[-3, 1], 1, 'l', heap_paths:= true);
+/* --q5 */
+SELECT * FROM pgr_withPointsKSP(
+  'SELECT id, source, target, cost, reverse_cost FROM edges ORDER BY id',
+  'SELECT pid, edge_id, fraction, side from pointsOfInterest',
+  'SELECT * FROM (VALUES (-1, 10), (6, -3)) AS combinations(source, target)',
+  2, 'r', details => true);
+/* --q6 */
 SELECT * FROM pgr_withPointsKSP(
   $e$ SELECT * FROM edges $e$,
   $p$ SELECT edge_id, round(fraction::numeric, 2) AS fraction, side
@@ -27,6 +37,16 @@ SELECT * FROM pgr_withPointsKSP(
         (SELECT ST_POINT(2.9, 1.8)),
         0.5, cap => 2)
   $p$,
-  1, -1, 2,
-  driving_side := 'r');
-/* -- q5 */
+  1, -1, 2,'r');
+/* --q7 */
+SELECT * FROM pgr_withPointsKSP(
+    'SELECT id, source, target, cost, reverse_cost FROM edges ORDER BY id',
+    'SELECT pid, edge_id, fraction, side from pointsOfInterest',
+    -1, -3, 2, 'l', details := true);
+/* --q8 */
+SELECT * FROM pgr_withPointsKSP(
+    'SELECT id, source, target, cost, reverse_cost FROM edges ORDER BY id',
+    'SELECT pid, edge_id, fraction, side from pointsOfInterest',
+    -1, -2, 2, 'r',
+    heap_paths := true, details := true);
+/* --q9 */
