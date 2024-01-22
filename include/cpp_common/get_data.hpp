2fc1d869b0bb5b41688b9fe153419dff051f9536
cvvergara <vicky@erosion.dev>
2024-01-22 12:36:49 -0600

    get_data: New template function to read data from C++

--- include/cpp_common/get_data.hpp
+++ include/cpp_common/get_data.hpp
@@ -39,7 +39,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 namespace pgrouting {
 
-/** @brief Retrives the tuples
+/** @brief Retrives the tuples for TRSP
  * @tparam Data_type Scructure of data
  * @tparam Func fetcher function
  * @param[in] sql  Query to be processed
@@ -48,6 +48,8 @@ namespace pgrouting {
  * @param[in] flag useful flag depending on data
  * @param[in] info information about the data
  * @param[in] func fetcher function to be used
+ *
+ * @todo: V4 remove this template function
  */
 template <typename Data_type, typename Func>
 void get_data(
@@ -101,6 +103,62 @@ void get_data(
     (*total_pgtuples) = total_tuples;
 }
 
+namespace pgget {
+/** @brief Retrives the tuples
+ * @tparam Data_type Scructure of data
+ * @tparam Func fetcher function
+ * @param[in] sql  Query to be processed
+ * @param[in] flag useful flag depending on data
+ * @param[in] info information about the data
+ * @param[in] func fetcher function to be used
+ */
+template <typename Data_type, typename Func>
+std::vector<Data_type> get_data(
+        const std::string& sql,
+        bool flag,
+        std::vector<Column_info_t> info,
+        Func func) {
+    const int tuple_limit = 1000000;
+
+    size_t total_tuples;
+    size_t valid_pgtuples;
+
+    auto SPIplan = pgr_SPI_prepare(sql.c_str());
+    auto SPIportal = pgr_SPI_cursor_open(SPIplan);
+
+    bool moredata = true;
+    total_tuples = valid_pgtuples = 0;
+
+    int64_t default_id = 0;
+    std::vector<Data_type> tuples;
+
+    while (moredata == true) {
+        SPI_cursor_fetch(SPIportal, true, tuple_limit);
+        auto tuptable = SPI_tuptable;
+        auto tupdesc = SPI_tuptable->tupdesc;
+        if (total_tuples == 0) fetch_column_info(tupdesc, info);
+
+        size_t ntuples = SPI_processed;
+        total_tuples += ntuples;
+
+        if (ntuples > 0) {
+            tuples.reserve(total_tuples);
+            for (size_t t = 0; t < ntuples; t++) {
+                tuples.push_back(func(tuptable->vals[t], tupdesc, info,
+                        &default_id,
+                        &valid_pgtuples, flag));
+            }
+            SPI_freetuptable(tuptable);
+        } else {
+            moredata = false;
+        }
+    }
+
+    SPI_cursor_close(SPIportal);
+    return tuples;
+}
+
+}  // namespace pgget
 }  // namespace pgrouting
 
 #endif  // INCLUDE_CPP_COMMON_GET_DATA_HPP_
