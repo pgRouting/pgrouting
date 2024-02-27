d693401ed9c7188f99d956817760ff2ed88a3412
cvvergara <vicky@erosion.dev>
2024-02-27 10:23:17 -0600

    (allpairs) Adjusting code to pgr_base_graph_changes

--- src/allpairs/floydWarshall_driver.cpp
+++ src/allpairs/floydWarshall_driver.cpp
@@ -72,12 +72,12 @@ pgr_do_floydWarshall(
 
         if (directed) {
             log << "Processing Directed graph\n";
-            pgrouting::DirectedGraph digraph(directed);
+            pgrouting::DirectedGraph digraph;
             digraph.insert_edges(edges);
             pgr_floydWarshall(digraph, *return_count, return_tuples);
         } else {
             log << "Processing Undirected graph\n";
-            pgrouting::UndirectedGraph undigraph(directed);
+            pgrouting::UndirectedGraph undigraph;
             undigraph.insert_edges(edges);
             pgr_floydWarshall(undigraph, *return_count, return_tuples);
         }
