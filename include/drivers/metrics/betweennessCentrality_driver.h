24cc62e208b93946a46de18e91892b52f550b794
Arun Thakur <bedupako12mas@gmail.com>
2024-08-17 00:55:22 +0530

    (betweennessCentrality) adding C/C++ code

---
+++ include/drivers/metrics/betweennessCentrality_driver.h
@@ -0,0 +1,64 @@
+/*PGR-GNU*****************************************************************
+File: betweennessCentrality_driver.h
+
+Generated with Template by:
+Copyright (c) 2015 pgRouting developers
+Mail: project@pgrouting.org
+
+Function's developer:
+Copyright (c) 2024 Arun Thakur
+Mail: bedupako12mas at gmail.com
+
+------
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ ********************************************************************PGR-GNU*/
+
+#ifndef INCLUDE_DRIVERS_METRICS_BETWEENNESSCENTRALITY_DRIVER_H_
+#define INCLUDE_DRIVERS_METRICS_BETWEENNESSCENTRALITY_DRIVER_H_
+#pragma once
+
+/* for size-t */
+#ifdef __cplusplus
+#   include <cstddef>
+using IID_t_rt = struct IID_t_rt;
+#else
+#   include <stddef.h>
+#include <stdbool.h>
+typedef struct IID_t_rt IID_t_rt;
+#endif
+
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void
+pgr_do_betweennessCentrality(
+    char*,
+    bool,
+
+    IID_t_rt**,
+    size_t*,
+    char**,
+    char**);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // INCLUDE_DRIVERS_METRICS_BETWEENNESSCENTRALITY_DRIVER_H_
