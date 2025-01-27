e2097967f0356f67fd2ad818aa6a884af10d4211
cvvergara <vicky@erosion.dev>
2025-01-26 19:13:04 -0600

    (C/drivingdistance) Adding a deprecation notice to _pgr_drivingdistance

--- src/driving_distance/driving_distance.c
+++ src/driving_distance/driving_distance.c
@@ -37,10 +37,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 #include "drivers/driving_distance/driving_distance_driver.h"
 
-
-PGDLLEXPORT Datum _pgr_drivingdistance(PG_FUNCTION_ARGS);
-PG_FUNCTION_INFO_V1(_pgr_drivingdistance);
-
 PGDLLEXPORT Datum _pgr_drivingdistancev4(PG_FUNCTION_ARGS);
 PG_FUNCTION_INFO_V1(_pgr_drivingdistancev4);
 
@@ -164,10 +160,15 @@ _pgr_drivingdistancev4(PG_FUNCTION_ARGS) {
 }
 
 
-/* Old code starts here
- * TODO(v4) remove old code
- * its code that is used when there is an old version of SQL 3.5 and under
+/* Deprecated code starts here
+ * This code is used on v3.5 and under
+ *
+ * TODO(v5) Move to legacy
  */
+
+PGDLLEXPORT Datum _pgr_drivingdistance(PG_FUNCTION_ARGS);
+PG_FUNCTION_INFO_V1(_pgr_drivingdistance);
+
 PGDLLEXPORT Datum
 _pgr_drivingdistance(PG_FUNCTION_ARGS) {
     FuncCallContext     *funcctx;
@@ -175,6 +176,12 @@ _pgr_drivingdistance(PG_FUNCTION_ARGS) {
 
     MST_rt  *result_tuples = 0;
     size_t result_count = 0;
+
+    ereport(NOTICE, (
+            errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+            errmsg("A stored procedure is using deprecated C internal function '%s'", __func__),
+            errdetail("Library function '%s' was deprecated in pgRouting %s", __func__, "3.6.0"),
+            errhint("Consider upgrade pgRouting")));
 
     if (SRF_IS_FIRSTCALL()) {
         MemoryContext   oldcontext;
