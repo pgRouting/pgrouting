3abd5839cd91920a889113ab3bab8afe63e514a5
cvvergara <vicky@erosion.dev>
2025-03-20 13:21:56 -0600

    (contraction/pgtap) the tests that need to pass
    (cherry picked from commit efdf56360a0530ec842f92df3da53a2d864bf702)

--- tools/testers/contraction_tapfuncs.sql
+++ tools/testers/contraction_tapfuncs.sql
@@ -16,6 +16,143 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  ********************************************************************PGR-GNU*/
+
+CREATE OR REPLACE PROCEDURE create_contracted_graph(directed BOOLEAN, fn TEXT, methods INTEGER[] DEFAULT NULL::INTEGER[]) AS
+$BODY$
+
+BEGIN
+
+  IF NOT min_version('3.8.0') THEN RETURN; END IF;
+
+  DELETE FROM edges WHERE id >= 19;
+  ALTER SEQUENCE edges_id_seq RESTART WITH 19;
+  UPDATE edges SET (is_new, contracted_vertices)=(false,NULL);
+  UPDATE vertices SET (is_contracted, contracted_vertices)=(false,NULL);
+
+  -- add extra columns to the edges and vertices table
+
+  DROP TABLE IF EXISTS contraction_info;
+  IF fn = 'pgr_contraction' THEN
+    EXECUTE format($q$
+    CREATE TABLE contraction_info AS
+    SELECT * FROM %s(
+      'SELECT id, source, target, cost, reverse_cost FROM edges ORDER BY id',
+      %s, '%s'::INTEGER[]);
+    $q$, fn, directed::TEXT, methods);
+  ELSE
+    EXECUTE format($q$
+    CREATE TABLE contraction_info AS
+    SELECT * FROM %s(
+      'SELECT id, source, target, cost, reverse_cost FROM edges ORDER BY id',
+      '%s');
+    $q$, fn, directed::TEXT);
+  END IF;
+
+  -- add the new edges
+  INSERT INTO edges(source, target, cost, reverse_cost, contracted_vertices, is_new)
+  SELECT source, target, cost, -1, contracted_vertices, true
+  FROM contraction_info
+  WHERE type = 'e';
+
+  -- Indicate vertices that were contracted
+  UPDATE vertices
+  SET is_contracted = true
+  WHERE id IN (SELECT  unnest(contracted_vertices) FROM  contraction_info);
+
+  -- add the contracted vertices on the vertices table
+  UPDATE vertices
+  SET contracted_vertices = contraction_info.contracted_vertices
+  FROM contraction_info
+  WHERE type = 'v' AND vertices.id = contraction_info.id;
+
+  DROP VIEW IF EXISTS contracted_graph;
+  CREATE VIEW contracted_graph AS
+  SELECT id,source, target, cost, reverse_cost, contracted_vertices FROM edges
+  WHERE
+    EXISTS (SELECT id FROM vertices AS v WHERE NOT is_contracted AND v.id = edges.source)
+    AND
+    EXISTS (SELECT id FROM vertices AS v WHERE NOT is_contracted AND v.id = edges.target);
+
+END
+$BODY$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION path_cost_contraction(BIGINT, BIGINT, BOOLEAN)
+RETURNS SETOF TEXT AS
+$BODY$
+DECLARE q TEXT;
+BEGIN
+
+    RETURN QUERY
+      SELECT round(agg_cost::numeric, 3)::TEXT FROM pgr_dijkstra(
+      'WITH cul_de_sac AS (
+        SELECT contracted_vertices || id as v
+        FROM vertices WHERE ' || $1 ||' = ANY(contracted_vertices) OR ' || $2 ||' = ANY(contracted_vertices)),
+      linears_to_expand AS (
+        SELECT id, contracted_vertices
+        FROM edges WHERE is_new AND (' || $1 ||' = ANY(contracted_vertices) OR '|| $2 ||'  = ANY(contracted_vertices))
+      ),
+      verts AS (SELECT * FROM cul_de_sac UNION SELECT contracted_vertices FROM linears_to_expand)
+      SELECT id, source, target, cost, reverse_cost FROM edges, verts WHERE source = ANY(v) OR target = ANY(v)
+
+      UNION
+
+      SELECT id, source, target, cost, reverse_cost FROM contracted_graph LEFT JOIN linears_to_expand c USING (id) WHERE c.id IS NULL',
+      $1, $2, $3) WHERE edge = -1;
+
+END;
+$BODY$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION compare_dijkstra_contraction(
+    BIGINT, BIGINT, BOOLEAN)
+RETURNS SETOF TEXT AS
+$BODY$
+DECLARE
+    q TEXT;
+BEGIN
+
+  RETURN QUERY
+  SELECT set_eq(
+    $$SELECT * FROM path_cost_contraction($$|| $1 || $$,$$ || $2 || $$,$$ || $3::BOOLEAN || $$)$$,
+    $$SELECT  round(agg_cost::numeric, 3)::TEXT
+    FROM pgr_dijkstra(
+      'SELECT id, source, target, cost, reverse_cost FROM edges WHERE id < 19',
+      $$|| $1 || $$,$$ || $2 || $$,$$ || $3::BOOLEAN || $$) WHERE edge = -1
+    $$,
+    'From ' || $1 || ' to ' || $2 || 'Directed = ' || $3::BOOLEAN);
+
+END
+$BODY$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION compare_dijkstra_contraction(BOOLEAN)
+RETURNS SETOF TEXT AS
+$BODY$
+DECLARE
+    i INTEGER;
+    j INTEGER;
+
+BEGIN
+
+  IF NOT min_version('3.8.0') THEN
+    RETURN QUERY
+    SELECT skip(1, 'contraction function/signature is new on 3.8.0');
+    RETURN;
+  END IF;
+
+  FOR i IN 1..18 LOOP
+    FOR j IN 1..18 LOOP
+      RETURN QUERY
+      SELECT compare_dijkstra_contraction(i, j, $1);
+    END LOOP;
+  END LOOP;
+
+END
+$BODY$
+LANGUAGE plpgsql;
+
+
 CREATE OR REPLACE FUNCTION test_ids_size(pow INTEGER, directed BOOLEAN)
 RETURNS SETOF TEXT AS
 $BODY$
