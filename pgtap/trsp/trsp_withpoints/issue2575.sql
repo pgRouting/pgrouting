212617ee082a5f11b6c932c97c796e267e11d6fe
cvvergara <vicky@erosion.dev>
2025-04-09 10:00:46 -0600

    (pgtap/createTopology) Not testing deprecated function

--- pgtap/trsp/trsp_withpoints/issue2575.sql
+++ pgtap/trsp/trsp_withpoints/issue2575.sql
@@ -32,7 +32,7 @@ CREATE TABLE railroad (
     y1 double precision,
     x2 double precision,
     y2 double precision,
-    the_geom geometry
+    geom geometry
 );
 
 -- Create restrictions table
@@ -48,21 +48,34 @@ INSERT INTO railroad (x1,y1,x2,y2) VALUES
   (1,0,2,1),(2,1,3,1),(3,1,4,0)
 ;
 
-UPDATE railroad SET the_geom = ST_makeline(ST_point(x1,y1),ST_point(x2,y2));
-UPDATE railroad SET cost = ST_length(the_geom), reverse_cost = ST_length(the_geom);
-SELECT pgr_createTopology('railroad',0.001);
+UPDATE railroad SET geom = ST_makeline(ST_point(x1,y1),ST_point(x2,y2));
+UPDATE railroad SET cost = ST_length(geom), reverse_cost = ST_length(geom);
+
+SELECT * INTO rvertices
+FROM pgr_extractVertices('SELECT id, geom FROM railroad ORDER BY id');
+
+/* -- set the source information */
+UPDATE railroad AS e
+SET source = v.id
+FROM rvertices AS v
+WHERE ST_StartPoint(e.geom) = v.geom;
+/* -- set the target information */
+UPDATE railroad AS e
+SET target = v.id
+FROM rvertices AS v
+WHERE ST_EndPoint(e.geom) = v.geom;
 
 INSERT INTO restrictions_railroad (cost, path)
 VALUES (100, ARRAY[9,2]),(100, ARRAY[2,9]),(100, ARRAY[7,2]),(100, ARRAY[2,7]);
 
 PREPARE dijkstra AS
 SELECT seq, path_seq, node, edge, cost, agg_cost
-FROM pgr_dijkstra('SELECT * FROM railroad', 4, 1);
+FROM pgr_dijkstra('SELECT * FROM railroad', 7, 1);
 
 SELECT set_eq('dijkstra', $$VALUES
-  (1, 1, 4, 3, 1, 0),
-  (2, 2, 3, 2, 3, 1),
-  (3, 3, 2, 1, 1, 4),
+  (1, 1, 7, 3, 1, 0),
+  (2, 2, 6, 2, 3, 1),
+  (3, 3, 3, 1, 1, 4),
   (4, 4, 1,-1, 0, 5)
   $$, 'expected dijkstra');
 
@@ -75,8 +88,8 @@ FROM pgr_withPoints(
 
 SELECT set_eq('between2nodes', $$VALUES
   (1, 1, -1, 2, 0.75,               0),
-  (2, 2,  3, 9, 1.4142135623730951, 0.75),
-  (3, 3,  8, 8, 0.5,                2.164213562373095),
+  (2, 2,  6, 9, 1.4142135623730951, 0.75),
+  (3, 3,  5, 8, 0.5,                2.164213562373095),
   (4, 4, -2,-1, 0,                  2.664213562373095)
   $$, 'expected withPoints');
 
@@ -89,13 +102,13 @@ FROM pgr_trsp_withPoints(
   -1, -2);
 SELECT set_eq('between2nodeswithRestriction', $$VALUES
    (1, 1, -1, -2, -1, 2, 0.75,                0),
-   (2, 2, -1, -2,  3, 3, 1,                   0.75),
-   (3, 3, -1, -2,  4, 4, 5,                   1.75),
-   (4, 4, -1, -2,  5, 5, 5,                   6.75),
-   (5, 5, -1, -2,  6, 6, 5,                  11.75),
+   (2, 2, -1, -2,  6, 3, 1,                   0.75),
+   (3, 3, -1, -2,  7, 4, 5,                   1.75),
+   (4, 4, -1, -2,  8, 5, 5,                   6.75),
+   (5, 5, -1, -2,  2, 6, 5,                  11.75),
    (6, 6, -1, -2,  1, 1, 1,                  16.75),
-   (7, 7, -1, -2,  2, 7, 1.4142135623730958, 17.75),
-   (8, 8, -1, -2,  7, 8, 0.5,                19.164213562373096),
+   (7, 7, -1, -2,  3, 7, 1.4142135623730958, 17.75),
+   (8, 8, -1, -2,  4, 8, 0.5,                19.164213562373096),
    (9, 9, -1, -2, -2,-1, 0,                  19.664213562373096)
   $$, 'expected withPoints');
 
