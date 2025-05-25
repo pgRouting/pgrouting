a6dd9571fc22738d09b26421293bcb26718de6b1
cvvergara <vicky@erosion.dev>
2025-05-24 18:41:24 -0600

    Removing unused struct and fixing some mails

--- src/max_flow/maximumcardinalitymatching.cpp
+++ src/max_flow/maximumcardinalitymatching.cpp
@@ -6,11 +6,11 @@ Mail: project@pgrouting.org
 
 Refactoring
 Copyright (c) 2022 Celia Vriginia Vergara Castillo
-Mail: vicky_vergara at hotmail.com
+Mail: vicky at erosion.dev
 
 Function's developer:
 Copyright (c) 2016 Andrea Nardelli
-Mail: nrd.nardelli@gmail.com
+Mail: nrd.nardelli at gmail.com
 
 ------
 
