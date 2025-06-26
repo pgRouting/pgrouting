c68e2e020db210b50e68cbd24aca066ce05fb10a
Saloni <chaudharysaloni2510@gmail.com>
2025-06-26 16:24:21 +0000

    Updated and corrected src/metrics/metrics_driver.cpp

--- src/metrics/metrics_driver.cpp
+++ src/metrics/metrics_driver.cpp
@@ -1,5 +1,7 @@
 /*PGR-GNU*****************************************************************
-File: metrics_driver.cpp
+File: allpairs_driver.cpp
+
+Generated with Template by:
 Copyright (c) 2025 pgRouting developers
 Mail: project@pgrouting.org
 
@@ -31,119 +33,79 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #include <vector>
 #include <string>
 
+#include "metrics/bandwidth.hpp"
 #include "cpp_common/pgdata_getters.hpp"
-#include "cpp_common/alloc.hpp"
 #include "cpp_common/assert.hpp"
 #include "cpp_common/to_postgres.hpp"
-#include "cpp_common/base_graph.hpp"
-
-#include "metrics/bandwidth.hpp"
-#include "c_types/bandwidth_rt.h"  
-
-namespace pgrouting::graph {
-
-template <class G>
-std::vector<GraphBandwidth_rt>
-run_metric(G& graph) {
-    std::vector<GraphBandwidth_rt> results;
-    int result = pgrouting::metrics::bandwidth(graph);
-
-    GraphBandwidth_rt tuple;
-    tuple.id = 1;  // constant as you are computing a graph-level metric
-    tuple.bandwidth = result;
-
-    results.push_back(tuple);
-    return results;
-}
-
-}  // namespace graph
+#include "c_types/iid_t_rt.h"  
 
 
 void
 do_metrics(
-    GraphBandwidth_rt **return_tuples,
-    size_t *return_count,
-    char **log_msg,
-    char **notice_msg,
-    char **err_msg,
     std::string edges_sql,
-    bool directed) {
 
-    using pgrouting::pgr_alloc;
+    IID_t_rt **return_tuples,
+    size_t *return_count,
+    char ** log_msg,
+    char ** err_msg) {
     using pgrouting::to_pg_msg;
     using pgrouting::pgr_free;
 
     std::ostringstream log;
     std::ostringstream err;
-    std::ostringstream notice;
-    const char *hint = nullptr;
+    std::string hint;
 
     try {
         pgassert(!(*log_msg));
-        pgassert(!(*notice_msg));
         pgassert(!(*err_msg));
         pgassert(!(*return_tuples));
         pgassert(*return_count == 0);
 
-        hint = edges_sql.c_str();
-        auto edges = pgrouting::pgget::get_edges(edges_sql, true, true);
+        using pgrouting::metrics::bandwidth;
+
+        hint = edges_sql;
+        auto edges = pgrouting::pgget::get_edges(std::string(edges_sql), true, true);
 
         if (edges.empty()) {
-            *notice_msg = to_pg_msg("No edges found");
-            *log_msg = hint ? to_pg_msg(hint) : to_pg_msg(log);
-            return;
+            throw std::string("No edges found");
         }
 
-        std::vector<GraphBandwidth_rt> results;
+        hint = "";
 
-        if (directed) {
-            log << "Processing directed graph\n";
-            pgrouting::DirectedGraph digraph;
-            digraph.insert_edges(edges);
-            results = pgrouting::graph::run_metric(digraph);
-        } else {
-            log << "Processing undirected graph\n";
-            pgrouting::UndirectedGraph undigraph;
-            undigraph.insert_edges(edges);
-            results = pgrouting::graph::run_metric(undigraph);
-        }
 
-        size_t count = results.size();
-        if (count == 0) {
-            *return_tuples = nullptr;
+        log << "Processing Undirected graph\n";
+        pgrouting::UndirectedGraph undigraph;
+        undigraph.insert_edges(edges);
+
+
+
+        if (*return_count == 0) {
+            err <<  "No result generated, report this error\n";
+            *err_msg = to_pg_msg(err);
+            *return_tuples = NULL;
             *return_count = 0;
-            *log_msg = to_pg_msg(log);
-            *notice_msg = to_pg_msg("No results found");
             return;
         }
 
-        *return_tuples = pgr_alloc(count, *return_tuples);
-        for (size_t i = 0; i < count; ++i) {
-            (*return_tuples)[i] = results[i];
-        }
-
-        *return_count = count;
         *log_msg = to_pg_msg(log);
-        *notice_msg = to_pg_msg(notice);
-
-    } catch (const AssertFailedException &except) {
-        *return_tuples = pgr_free(*return_tuples);
-        *return_count = 0;
+    } catch (AssertFailedException &except) {
+        (*return_tuples) = pgr_free(*return_tuples);
+        (*return_count) = 0;
         err << except.what();
         *err_msg = to_pg_msg(err);
         *log_msg = to_pg_msg(log);
     } catch (const std::string &ex) {
         *err_msg = to_pg_msg(ex);
-        *log_msg = hint ? to_pg_msg(hint) : to_pg_msg(log);
-    } catch (const std::exception &except) {
-        *return_tuples = pgr_free(*return_tuples);
-        *return_count = 0;
+        *log_msg = hint.empty()? to_pg_msg(hint) : to_pg_msg(log);
+    } catch (std::exception &except) {
+        (*return_tuples) = pgr_free(*return_tuples);
+        (*return_count) = 0;
         err << except.what();
         *err_msg = to_pg_msg(err);
         *log_msg = to_pg_msg(log);
-    } catch (...) {
-        *return_tuples = pgr_free(*return_tuples);
-        *return_count = 0;
+    } catch(...) {
+        (*return_tuples) = pgr_free(*return_tuples);
+        (*return_count) = 0;
         err << "Caught unknown exception!";
         *err_msg = to_pg_msg(err);
         *log_msg = to_pg_msg(log);
