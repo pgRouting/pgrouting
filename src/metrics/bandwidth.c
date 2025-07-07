cfeed27ddb0619673deb6b5b6ae4c6e4b6e27e82
Saloni <chaudharysaloni2510@gmail.com>
2025-07-07 19:53:04 +0000

    Fixed bandwidth.c

--- src/metrics/bandwidth.c
+++ src/metrics/bandwidth.c
@@ -43,62 +43,20 @@ PG_FUNCTION_INFO_V1(_pgr_bandwidth);
 
 PGDLLEXPORT Datum
 _pgr_bandwidth(PG_FUNCTION_ARGS) {
-    FuncCallContext     *funcctx;
-    TupleDesc            tuple_desc;
 
     IID_t_rt *result_tuples = NULL;
     size_t result_count = 0;
 
-    if (SRF_IS_FIRSTCALL()) {
-        MemoryContext   oldcontext;
-        funcctx = SRF_FIRSTCALL_INIT();
-        oldcontext = MemoryContextSwitchTo(funcctx->multi_call_memory_ctx);
+    pgr_process_metrics(
+	text_to_cstring(PG_GETARG_TEXT_P(0)),
+        0,  /* bandwidth */
+        &result_tuples,
+        &result_count);
 
-
-        pgr_process_metrics(
-                text_to_cstring(PG_GETARG_TEXT_P(0)),
-                0,  /* bandwidth */
-                &result_tuples,
-                &result_count);
-
-        funcctx->max_calls = result_count;
-        funcctx->user_fctx = result_tuples;
-        if (get_call_result_type(fcinfo, NULL, &tuple_desc) != TYPEFUNC_COMPOSITE) {
-            ereport(ERROR,
-                    (errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
-                     errmsg("function returning record called in context "
-                         "that cannot accept type record")));
-        }
-
-        funcctx->tuple_desc = tuple_desc;
-        MemoryContextSwitchTo(oldcontext);
-    }
-
-    funcctx = SRF_PERCALL_SETUP();
-    tuple_desc = funcctx->tuple_desc;
-    result_tuples = (IID_t_rt*) funcctx->user_fctx;
-
-    if (funcctx->call_cntr < funcctx->max_calls) {
-        HeapTuple    tuple;
-        Datum        result;
-        Datum        *values;
-        bool         *nulls;
-
-        values = palloc(3 * sizeof(Datum));
-        nulls = palloc(3 * sizeof(bool));
-
-        values[0] = Int64GetDatum(result_tuples[funcctx->call_cntr].from_vid);
-        nulls[0] = false;
-        values[1] = Int64GetDatum(result_tuples[funcctx->call_cntr].to_vid);
-        nulls[1] = false;
-        values[2] = Float8GetDatum(result_tuples[funcctx->call_cntr].cost);
-        nulls[2] = false;
-
-        tuple = heap_form_tuple(tuple_desc, values, nulls);
-        result = HeapTupleGetDatum(tuple);
-        SRF_RETURN_NEXT(funcctx, result);
-    } else {
-        SRF_RETURN_DONE(funcctx);
-    }
+     if (result_count == 0) {
+     	     PG_RETURN_UINT64(0);
+     } else {	
+	     PG_RETURN_UINT64(result_tuples[0].from_vid);
+     }
 }
 
