cb6e04230bdb09f05e43c9fc3765af17b8c9490e
cvvergara <vicky@erosion.dev>
2025-08-18 09:31:34 -0600

    (C/C++) bandwidth: Adding the C/C++ code

---
+++ src/metrics/metrics_driver.cpp
@@ -0,0 +1,114 @@
+/*PGR-GNU*****************************************************************
+File: allpairs_driver.cpp
+
+Generated with Template by:
+Copyright (c) 2025 pgRouting developers
+Mail: project@pgrouting.org
+
+Developer: 
+Copyright (c) 2025 Saloni Kumari
+Mail: chaudharysaloni2510 at gmail.com
+
+------
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+********************************************************************PGR-GNU*/
+
+#include "drivers/metrics_driver.hpp"
+
+#include <sstream>
+#include <vector>
+#include <string>
+
+#include "metrics/bandwidth.hpp"
+#include "cpp_common/pgdata_getters.hpp"
+#include "cpp_common/assert.hpp"
+#include "cpp_common/to_postgres.hpp"
+#include "c_types/iid_t_rt.h"
+
+void
+do_metrics(
+    std::string edges_sql,
+    int which,
+
+    IID_t_rt **return_tuples,
+    size_t *return_count,
+    char ** log_msg,
+    char ** err_msg) {
+    using pgrouting::to_pg_msg;
+    using pgrouting::pgr_free;
+
+    std::ostringstream log;
+    std::ostringstream err;
+    std::string hint;
+
+    try {
+        pgassert(!(*log_msg));
+        pgassert(!(*err_msg));
+        pgassert(!(*return_tuples));
+        pgassert(*return_count == 0);
+
+        hint = edges_sql;
+        auto edges = pgrouting::pgget::get_edges(std::string(edges_sql), true, true);
+
+        if (edges.empty()) {
+            throw std::string("No edges found");
+        }
+
+        hint = "";
+
+        auto vertices(pgrouting::extract_vertices(edges));
+        pgrouting::UndirectedGraph undigraph(vertices);
+
+        undigraph.insert_edges(edges);
+
+        uint64_t result = 0;
+
+        if (which == 0) {
+            log << "call the function which calculates the bandwidth";
+            result = pgrouting::metrics::bandwidth(undigraph);
+        }
+
+        log << "result = " << result;
+
+        *return_tuples = new IID_t_rt[1];
+        (*return_tuples)[0].from_vid = static_cast<int64_t>(result);
+        *return_count = 1;
+
+        *log_msg = to_pg_msg(log);
+    } catch (AssertFailedException &except) {
+        (*return_tuples) = pgr_free(*return_tuples);
+        (*return_count) = 0;
+        err << except.what();
+        *err_msg = to_pg_msg(err);
+        *log_msg = to_pg_msg(log);
+    } catch (const std::string &ex) {
+        *err_msg = to_pg_msg(ex);
+        *log_msg = hint.empty()? to_pg_msg(hint) : to_pg_msg(log);
+    } catch (std::exception &except) {
+        (*return_tuples) = pgr_free(*return_tuples);
+        (*return_count) = 0;
+        err << except.what();
+        *err_msg = to_pg_msg(err);
+        *log_msg = to_pg_msg(log);
+    } catch(...) {
+        (*return_tuples) = pgr_free(*return_tuples);
+        (*return_count) = 0;
+        err << "Caught unknown exception!";
+        *err_msg = to_pg_msg(err);
+        *log_msg = to_pg_msg(log);
+    }
+}
---
+++ src/metrics/metrics_process.cpp
@@ -0,0 +1,81 @@
+/*PGR-GNU*****************************************************************
+File: metrics_process.cpp
+
+Copyright (c) 2025 pgRouting developers
+Mail: project@pgrouting.org
+
+Function's developer:
+Copyright (c) 2025 Saloni Kumari
+Mail: chaudharysaloni2510 at gmail.com
+
+Generated with Template by:
+Copyright (c) 2025 pgRouting developers
+Mail: project@pgrouting.org
+
+------
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ ********************************************************************PGR-GNU*/
+
+#include "process/metrics_process.h"
+
+#include <string>
+
+extern "C" {
+#include "c_common/postgres_connection.h"
+#include "c_common/e_report.h"
+#include "c_common/time_msg.h"
+}
+
+#include "c_types/iid_t_rt.h"
+#include "cpp_common/assert.hpp"
+#include "drivers/metrics_driver.hpp"
+
+#if 0
+which = 0 -> bandwidth
+
+This is c++ code, linked as C code, because pgr_process_metrics is called from C code
+#endif
+void pgr_process_metrics(const char* edges_sql, int which, IID_t_rt **result_tuples, size_t *result_count) {
+    pgassert(edges_sql);
+    pgassert(!(*result_tuples));
+    pgassert(*result_count == 0);
+    pgr_SPI_connect();
+    char* log_msg = NULL;
+    char* notice_msg = NULL;
+    char* err_msg = NULL;
+
+    clock_t start_t = clock();
+    do_metrics(
+            edges_sql,
+            which,
+            result_tuples, result_count,
+            &log_msg, &err_msg);
+
+    if (which == 0) {
+        time_msg(std::string(" processing pgr_bandwidth").c_str(), start_t, clock());
+    }
+
+    if (err_msg && (*result_tuples)) {
+        pfree(*result_tuples);
+        (*result_tuples) = NULL;
+        (*result_count) = 0;
+    }
+
+    pgr_global_report(&log_msg, &notice_msg, &err_msg);
+
+    pgr_SPI_finish();
+}
