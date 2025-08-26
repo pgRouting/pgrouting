7988a3adcb759f250d53fee524f308a6c5aa7b97
wifi:) <wifiBlack@gmail.com>
2025-08-26 16:59:47 +0800

    (minDegreeOrdering/sql) Adding SQL code for pgr_minDegreeOrdering

---
+++ sql/ordering/_minDegreeOrdering.sql
@@ -0,0 +1,49 @@
+/*PGR-GNU*****************************************************************
+File: _minDegreeOrdering.sql
+
+Generated with Template by:
+Copyright (c) 2025 pgRouting developers
+Mail: project@pgrouting.org
+
+Function's developer:
+Copyright (c) 2025 Fan Wu
+Mail: wifiblack0131 at gmail.com
+
+------
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ ********************************************************************PGR-GNU*/
+
+----------------------------
+-- _pgr_minDegreeOrdering
+----------------------------
+
+--v4.0
+CREATE FUNCTION _pgr_minDegreeOrdering(
+    TEXT,
+    OUT seq BIGINT,
+    OUT node BIGINT
+    )
+
+RETURNS SETOF RECORD AS
+'MODULE_PATHNAME'
+LANGUAGE C IMMUTABLE STRICT;
+
+-- COMMENTS
+
+COMMENT ON FUNCTION _pgr_minDegreeOrdering(TEXT)
+IS 'pgRouting internal function';
+
---
+++ sql/ordering/minDegreeOrdering.sql
@@ -0,0 +1,55 @@
+/*PGR-GNU*****************************************************************
+File: minDegreeOrdering.sql
+
+Generated with Template by:
+Copyright (c) 2025 pgRouting developers
+Mail: project@pgrouting.org
+
+Function's developer:
+Copyright (c) 2025 Fan Wu
+Mail: wifiblack0131 at gmail.com
+
+------
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ ********************************************************************PGR-GNU*/
+
+----------------------------
+-- pgr_minDegreeOrdering
+----------------------------
+
+--v4.0
+CREATE FUNCTION pgr_minDegreeOrdering(
+    TEXT, -- edges_sql (required)
+    OUT seq BIGINT,
+    OUT node BIGINT)
+RETURNS SETOF RECORD AS
+$BODY$
+    SELECT seq, node
+    FROM _pgr_minDegreeOrdering(_pgr_get_statement($1));
+$BODY$
+LANGUAGE SQL VOLATILE STRICT;
+
+-- COMMENTS
+
+COMMENT ON FUNCTION pgr_minDegreeOrdering(TEXT)
+IS 'pgr_minDegreeOrdering
+- EXPERIMENTAL
+- Parameters:
+    - Edges SQL with columns: id, source, target, cost [,reverse_cost]
+- Documentation:
+    - ${PROJECT_DOC_LINK}/pgr_minDegreeOrdering.html
+';
