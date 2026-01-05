c1f716fb9665adae6b57f23d2f37236760fb16d9
cvvergara <vicky@erosion.dev>
2026-01-05 11:45:56 -0600

    (C++) No need of template: cuthillMckee
    - Works only for undirected graph
    - Breaking into header and implementation

--- include/ordering/cuthillMckeeOrdering.hpp
+++ include/ordering/cuthillMckeeOrdering.hpp
@@ -31,128 +31,16 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #define INCLUDE_ORDERING_CUTHILLMCKEEORDERING_HPP_
 #pragma once
 
-#include <algorithm>
 #include <vector>
-#include <map>
-#include <cstdint>
-
-#include <boost/property_map/property_map.hpp>
-#include <boost/graph/graph_traits.hpp>
-#include <boost/property_map/vector_property_map.hpp>
-#include <boost/type_traits.hpp>
-#include <boost/graph/adjacency_list.hpp>
-#include <boost/graph/cuthill_mckee_ordering.hpp>
-
 #include "cpp_common/base_graph.hpp"
-#include "cpp_common/interruption.hpp"
-#include "cpp_common/messages.hpp"
-
-#include "c_types/ii_t_rt.h"
-
-/** @file cuthillMckeeOrdering.hpp
- * @brief The main file which calls the respective boost function.
- *
- * Contains actual implementation of the function and the calling
- * of the respective boost function.
- */
 
 
 namespace pgrouting {
 namespace functions {
 
-template <class G>
-class CuthillMckeeOrdering : public Pgr_messages {
- public:
-    typedef typename G::V V;
-    typedef typename G::E E;
-    typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS> Graph;
-    typedef boost::graph_traits<Graph>::vertices_size_type size_type;
-    typedef boost::graph_traits<Graph>::vertex_descriptor Vertex;
+std::vector<pgrouting::UndirectedGraph::V>
+cuthillMckeeOrdering(pgrouting::UndirectedGraph &graph);
 
-    /** @name CuthillMckeeOrdering
-      * @{
-      *
-      */
-
-     /** @brief cuthillMckeeOrdering function
-      *
-      * It does all the processing and returns the results.
-      *
-      * @param graph      the graph containing the edges
-      *
-      * @returns results, when results are found
-      *
-      * @see [boost::cuthill_mckee_ordering]
-      * (https://www.boost.org/libs/graph/doc/cuthill_mckee_ordering.html)
-      */
-
-        std::vector<II_t_rt>
-        cuthillMckeeOrdering(G &graph) {
-        std::vector<II_t_rt>results;
-
-        // map which store the indices with their nodes.
-        auto i_map = boost::get(boost::vertex_index, graph.graph);
-
-        // vector which will store the order of the indices.
-        std::vector<Vertex> inv_perm(boost::num_vertices(graph.graph));
-
-        // vector which will store the color of all the vertices in the graph
-        std::vector <boost::default_color_type> colors(boost::num_vertices(graph.graph));
-
-        // An iterator property map which records the color of each vertex
-        auto color_map = boost::make_iterator_property_map(&colors[0], i_map, colors[0]);
-
-        // map which store the degree of each vertex.
-        auto out_deg = boost::make_out_degree_map(graph.graph);
-
-         /* abort in case of an interruption occurs (e.g. the query is being cancelled) */
-         CHECK_FOR_INTERRUPTS();
-
-         try {
-             boost::cuthill_mckee_ordering(graph.graph, inv_perm.rbegin(), color_map, out_deg);
-         } catch (boost::exception const& ex) {
-             (void)ex;
-             throw;
-         } catch (std::exception &e) {
-             (void)e;
-             throw;
-         } catch (...) {
-             throw;
-         }
-
-         results = get_results(inv_perm, graph);
-
-         return results;
-     }
-
-      //@}
-
- private:
-     /** @brief to get the results
-      *
-      * Uses the `inv_perm` vector to get the results i.e. the ordering.
-      *
-      * @param inv_perm    vector which contains the new ordering of indices.
-      * @param graph       the graph containing the edges
-      *
-      * @returns `results` vector
-      */
-     std::vector <II_t_rt> get_results(
-            std::vector <size_type> & inv_perm,
-            const G &graph) {
-            std::vector <II_t_rt> results;
-
-        for (std::vector<Vertex>::const_iterator i = inv_perm.begin();
-             i != inv_perm.end(); ++i) {
-            log << inv_perm[*i] << " ";
-            auto seq = graph[*i].id;
-            results.push_back({{seq}, {static_cast<int64_t>(graph.graph[*i].id)}});
-            seq++;
-            }
-
-            return results;
-        }
-};
 }  // namespace functions
 }  // namespace pgrouting
 
