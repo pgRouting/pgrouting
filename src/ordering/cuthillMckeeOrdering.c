3101e48cb388783a7ba4e1aade2a230c5061cca8
cvvergara <vicky@erosion.dev>
2026-01-05 12:02:34 -0600

    cuthillMckeeOrdering.c using ordering_driver

--- src/ordering/cuthillMckeeOrdering.c
+++ src/ordering/cuthillMckeeOrdering.c
@@ -32,57 +32,18 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #include "c_common/e_report.h"
 #include "c_common/time_msg.h"
 
-#include "c_types/ii_t_rt.h"
-
-
-#include "drivers/ordering/cuthillMckeeOrdering_driver.h"
+#include "process/ordering_process.h"
 
 PGDLLEXPORT Datum _pgr_cuthillmckeeordering(PG_FUNCTION_ARGS);
 PG_FUNCTION_INFO_V1(_pgr_cuthillmckeeordering);
 
-static
-void
-process(
-        char* edges_sql,
-
-        II_t_rt **result_tuples,
-        size_t *result_count) {
-    pgr_SPI_connect();
-    char* log_msg = NULL;
-    char* notice_msg = NULL;
-    char* err_msg = NULL;
-
-    (*result_tuples) = NULL;
-    (*result_count) = 0;
-
-    clock_t start_t = clock();
-    pgr_do_cuthillMckeeOrdering(
-            edges_sql,
-
-            result_tuples,
-            result_count,
-            &log_msg,
-            &notice_msg,
-            &err_msg);
-    time_msg("processing cuthillmckeeordering", start_t, clock());
-
-    if (err_msg && (*result_tuples)) {
-        pfree(*result_tuples);
-        (*result_tuples) = NULL;
-        (*result_count) = 0;
-    }
-
-    pgr_global_report(&log_msg, &notice_msg, &err_msg);
-
-    pgr_SPI_finish();
-}
 
 PGDLLEXPORT Datum
 _pgr_cuthillmckeeordering(PG_FUNCTION_ARGS) {
     FuncCallContext     *funcctx;
     TupleDesc           tuple_desc;
 
-    II_t_rt *result_tuples = NULL;
+    int64_t *result_tuples = NULL;
     size_t result_count = 0;
 
     if (SRF_IS_FIRSTCALL()) {
@@ -90,8 +51,10 @@ _pgr_cuthillmckeeordering(PG_FUNCTION_ARGS) {
         funcctx = SRF_FIRSTCALL_INIT();
         oldcontext = MemoryContextSwitchTo(funcctx->multi_call_memory_ctx);
 
-        process(
+
+        pgr_process_ordering(
                 text_to_cstring(PG_GETARG_TEXT_P(0)),
+                1, /* Cuthill Mckee Ordering*/
                 &result_tuples,
                 &result_count);
 
@@ -111,7 +74,7 @@ _pgr_cuthillmckeeordering(PG_FUNCTION_ARGS) {
 
     funcctx = SRF_PERCALL_SETUP();
     tuple_desc = funcctx->tuple_desc;
-    result_tuples = (II_t_rt*) funcctx->user_fctx;
+    result_tuples = (int64_t*) funcctx->user_fctx;
 
     if (funcctx->call_cntr < funcctx->max_calls) {
         HeapTuple    tuple;
@@ -119,7 +82,7 @@ _pgr_cuthillmckeeordering(PG_FUNCTION_ARGS) {
         Datum        *values;
         bool*        nulls;
 
-        size_t num  = 3;
+        size_t num  = 2;
         values = palloc(num * sizeof(Datum));
         nulls = palloc(num * sizeof(bool));
 
@@ -130,7 +93,7 @@ _pgr_cuthillmckeeordering(PG_FUNCTION_ARGS) {
         }
 
         values[0] = Int64GetDatum((int64_t)funcctx->call_cntr + 1);
-        values[1] = Int64GetDatum(result_tuples[funcctx->call_cntr].d2.value);
+        values[1] = Int64GetDatum(result_tuples[funcctx->call_cntr]);
 
         tuple = heap_form_tuple(tuple_desc, values, nulls);
         result = HeapTupleGetDatum(tuple);
