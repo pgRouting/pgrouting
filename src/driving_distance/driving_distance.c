0776dc4719a93cfc71f7e21c9c9b8a537f99003f
cvvergara <vicky@erosion.dev>
2026-01-19 15:19:46 -0600

    (src) Standardizing GPL-2 license text (.c)

--- src/driving_distance/driving_distance.c
+++ src/driving_distance/driving_distance.c
@@ -1,7 +1,7 @@
 /*PGR-GNU*****************************************************************
 File: driving_distance.c
 
-Copyright (c) 2015 pgRouting developers
+Copyright (c) 2007-2026 pgRouting developers
 Mail: project@pgrouting.org
 
 Copyright (c) 2015 Celia Virginia Vergara Castillo
--- src/trsp/trsp.c
+++ src/trsp/trsp.c
@@ -1,7 +1,7 @@
 /*PGR-GNU*****************************************************************
 File: trsp.c
 
-Copyright (c) 2017 pgRouting developers
+Copyright (c) 2013-2026 pgRouting developers
 Mail: project@pgrouting.org
 Copyright (c) 2017 Vicky Vergara
 Mail: vicky at erosion.dev
