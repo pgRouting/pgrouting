93eff42e21449fbeb12c9e39a086b6bb460ccc4e
cvvergara <vicky@erosion.dev>
2026-01-19 15:38:49 -0600

    (scripts) Standardizing GPL-2 license text (.pl)

--- tools/testers/doc_queries_generator.pl
+++ tools/testers/doc_queries_generator.pl
@@ -1,17 +1,16 @@
-#! /usr/bin/perl -w
-
+#!/usr/bin/perl -w
 =pod
+/*PGR-GNU*****************************************************************
 File: doc_queries_generator.pl
 
-Copyright (c) 2013 pgRouting developers
+Copyright (c) 2013-2026 pgRouting developers
+Mail: project@pgrouting.org
 
 Function contributors:
   	Celia Virginia Vergara Castillo
   	Stephen Woodbridge
   	Vadim Zhukov
 	Nagase Ko
-
-Mail:
 
 ------
 
@@ -28,6 +27,8 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ ********************************************************************PGR-GNU*/
 =cut
 
 
