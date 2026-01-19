bfbc6fb61e89fc04515dc2b3085088a695bd29ce
cvvergara <vicky@erosion.dev>
2026-01-19 15:19:46 -0600

    (src) Standardizing GPL-2 license text (.c)

--- src/metrics/bandwidth.c
+++ src/metrics/bandwidth.c
@@ -2,7 +2,7 @@
 File: bandwidth.c
 
 Generated with Template by:
-Copyright (c) 2025 pgRouting developers
+Copyright (c) 2015-2026 pgRouting developers
 Mail: project@pgrouting.org
 
 Function's developer:
