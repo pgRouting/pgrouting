b0d091883ddbd590352cbafd467f44acf2e8dede
cvvergara <vicky@erosion.dev>
2026-02-10 10:05:45 -0600

    code_checker.sh: ignoring include order on astar_process

--- tools/scripts/code_checker.sh
+++ tools/scripts/code_checker.sh
@@ -26,6 +26,7 @@ INCLUDE_ORDER="
 -build/include_order:src/dijkstra/shortestPath_process.cpp,
 -build/include_order:src/metrics/metrics_process.cpp,
 -build/include_order:src/ordering/ordering_process.cpp,
+-build/include_order:src/astar/astar_process.cpp,
 -build/include_order:src/allpairs/allpairs_process.cpp"
 
 if test -z "$DIRECTORY"; then
