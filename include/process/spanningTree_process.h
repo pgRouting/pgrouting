bb46f5f406a6c4eeb9bc6ee2e97944dd54c09f6a
cvvergara <vicky@erosion.dev>
2026-02-19 12:36:46 -0600

    New process and driver for spanning tree like functions

---
+++ include/process/spanningTree_process.h
@@ -0,0 +1,66 @@
+/*PGR-GNU*****************************************************************
+File: spanningTree_process.h
+
+Copyright (c) 2026-2026 pgRouting developers
+Mail: project@pgrouting.org
+
+Design of one process & driver file by
+Copyright (c) 2026 Celia Virginia Vergara Castillo
+Mail: vicky at erosion.dev
+
+------
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ ********************************************************************PGR-GNU*/
+
+#ifndef INCLUDE_PROCESS_SPANNINGTREE_PROCESS_H_
+#define INCLUDE_PROCESS_SPANNINGTREE_PROCESS_H_
+#pragma once
+
+#ifdef __cplusplus
+#include <cstddef>
+#include <cstdint>
+using MST_rt = struct MST_rt;
+using ArrayType = struct ArrayType;
+#else
+#include <stddef.h>
+#include <stdint.h>
+typedef struct MST_rt MST_rt;
+typedef struct ArrayType ArrayType;
+#endif
+
+#include "c_common/enums.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void pgr_process_spanningTree(
+        const char*,
+        ArrayType*,
+
+        bool, int64_t, double, bool,
+
+        const char*,
+        enum Which,
+
+        MST_rt**, size_t*);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // INCLUDE_PROCESS_SPANNINGTREE_PROCESS_H_
