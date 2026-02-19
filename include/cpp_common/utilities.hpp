b5b79175129d381713a5b91a65712812d2021c2b
cvvergara <vicky@erosion.dev>
2026-02-19 10:27:29 -0600

    (utilities) Preparing for spanning tree like functions

--- include/cpp_common/utilities.hpp
+++ include/cpp_common/utilities.hpp
@@ -26,9 +26,13 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #define INCLUDE_CPP_COMMON_UTILITIES_HPP_
 #pragma once
 
+#include <set>
+#include <vector>
 #include <string>
+#include <cstdint>
 
 #include "c_common/enums.h"
+#include "c_types/mst_rt.h"
 
 namespace pgrouting {
 
@@ -37,6 +41,7 @@ std::string get_name(Which, bool, bool);
 std::string get_name(Which, bool, bool, bool);
 char estimate_drivingSide(char, Which);
 void get_new_queries(const std::string&, const std::string&, std::string&, std::string&);
+std::vector<MST_rt> only_root_result(const std::set<int64_t>&);
 
 }  //  namespace pgrouting
 
