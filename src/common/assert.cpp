f96da0b9c4841da4d59cd314734144f48832bf25
prashsti29 <prashsti_sa@ce.iitr.ac.in>
2026-03-07 22:28:58 +0530

    add missing <iterator> include in src/common/assert.cpp

--- src/common/assert.cpp
+++ src/common/assert.cpp
@@ -37,7 +37,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 #include <string>
 #include <exception>
-
+#include <iterator>
 
 std::string get_backtrace() {
 #ifdef __GLIBC__
