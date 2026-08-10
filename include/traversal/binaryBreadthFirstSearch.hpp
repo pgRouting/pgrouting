350b1e53387c035c2e484ea05f81997f0ebad3e7
cvvergara <vicky@erosion.dev>
2026-08-10 07:22:37 -0600

    binaryBreadthFirstSearch: include cmath for std:isinf

--- include/traversal/binaryBreadthFirstSearch.hpp
+++ include/traversal/binaryBreadthFirstSearch.hpp
@@ -33,6 +33,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #include <limits>
 #include <vector>
 #include <set>
+#include <cmath>
 #include <map>
 #include <utility>
 #include <string>
