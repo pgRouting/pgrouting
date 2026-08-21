3229d86ae769a4f44f028e972259979958ad3d3b
Mohit Rawat <mohit25rawat@gmail.com>
2026-08-21 12:09:36 +0530

    [makeMaximalPlanar/sql] Adding SQL code for pgr_makeMaximalPlanar

---
+++ sql/planar/_makeMaximalPlanar.sql
@@ -0,0 +1,47 @@
+/*PGR-GNU*****************************************************************
+File: _makeMaximalPlanar.sql
+
+Copyright (c) 2015-2026 pgRouting developers
+Mail: project@pgrouting.org
+
+Copyright (c) 2026 Mohit Rawat
+Mail: mohit25rawat at gmail.com
+
+------
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ ********************************************************************PGR-GNU*/
+-------------------------
+-------------------------
+-- _makeMaximalPlanar
+-------------------------
+-------------------------
+
+--v4.1
+CREATE FUNCTION _pgr_makeMaximalPlanar(
+  TEXT,   -- edges_sql (required)
+
+  OUT seq BIGINT,
+  OUT start_vid BIGINT,
+  OUT end_vid BIGINT)
+
+RETURNS SETOF RECORD AS
+'MODULE_PATHNAME'
+LANGUAGE c VOLATILE STRICT;
+
+
+COMMENT ON FUNCTION _pgr_makeMaximalPlanar(TEXT)
+IS 'pgRouting internal function';
