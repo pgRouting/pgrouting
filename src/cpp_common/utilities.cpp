fa047cd3a44d379c772c1eecd38eb6ea723345df
cvvergara <vicky@erosion.dev>
2026-09-25 20:50:15 -0600

    (maxWeightMatch) Adjusting enumeration and utilities

--- src/cpp_common/utilities.cpp
+++ src/cpp_common/utilities.cpp
@@ -108,9 +108,9 @@ get_name(Which which) {
             break;
         case BICONNECTEDPLANAR:
             return "pgr_makeBiconnectedPlanar";
-        case MAXWEIGHTEDMATCHING:
-            return "pgr_maxWeightedMatching";
-
+            break;
+        case MAXWEIGHTMATCH:
+            return "pgr_maxWeightedMatch";
         case ARTICULATIONPOINTS:
             return "pgr_articulationPoints";
             break;
@@ -133,7 +133,6 @@ get_name(Which which) {
             return "pgr_planarFaces";
         case CORENUMBERS:
             return "pgr_coreNumbers";
-            break;
         default:
             return "unknown";
             break;
